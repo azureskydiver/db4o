15a23642e5cc99c6548184716473073ea5dbe644
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2005-04-06 21:43:50 +0000

    KnownClasses checks

--- db4oj/test/src/com/db4o/test/KnownClasses.java
+++ db4oj/test/src/com/db4o/test/KnownClasses.java
@@ -1,16 +1,43 @@
 package com.db4o.test;
 
+import com.db4o.*;
+import com.db4o.ext.*;
 import com.db4o.reflect.*;
 
 public class KnownClasses {
+    
+    public static final Class[] NOT_WANTED = new Class[] {
+        Db4oDatabase.class,
+        PBootRecord.class,
+        StaticClass.class,
+        MetaClass.class,
+        MetaField.class
+    };
+    
+    public void storeOne(){
+        // make sure there is one in the database
+    }
 	
 	public void test(){
+        boolean found = false;
 		ReflectClass[] knownClasses = Test.objectContainer().knownClasses();
 		for (int i = 0; i < knownClasses.length; i++) {
-			System.out.println(knownClasses[i]);
+            if(knownClasses[i].isPrimitive()){
+                Test.error();
+            }
+            if(knownClasses[i].isSecondClass()){
+                Test.error();
+            }
+            if(knownClasses[i].getName().equals(this.getClass().getName())){
+                found = true;
+            }
+            for (int j = 0; j < NOT_WANTED.length; j++) {
+                if(knownClasses[i].getName().equals(NOT_WANTED[j].getName())){
+                    Test.error();
+                }
+            }
 		}
-		
-		
+        Test.ensure(found);
 	}
 
 }
