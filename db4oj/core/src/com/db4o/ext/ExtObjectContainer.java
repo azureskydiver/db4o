5f879ae05bd7d122effc4f33a934cf910c1e0406
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2005-04-06 00:12:53 +0000

    deleted internal.io package

--- db4oj/core/src/com/db4o/ext/ExtObjectContainer.java
+++ db4oj/core/src/com/db4o/ext/ExtObjectContainer.java
@@ -8,7 +8,6 @@ import com.db4o.Db4o;
 import com.db4o.ObjectContainer;
 import com.db4o.ObjectServer;
 import com.db4o.config.Configuration;
-import com.db4o.internal.io.*;
 import com.db4o.reflect.*;
 import com.db4o.reflect.Reflector;
 import com.db4o.replication.ReplicationConflictHandler;
