866c5268313e929575e8bac9b7bbc8ac28cf35b7
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2005-04-07 23:33:35 +0000

    EvaluationDelegateWrapper to support marshalling/unmarshalling of delegates in mono

--- db4on/in/sharp/core/com/db4o/reflect/net/NetReflector.cs
+++ db4on/in/sharp/core/com/db4o/reflect/net/NetReflector.cs
@@ -35,7 +35,7 @@ namespace com.db4o.reflect.net
 			if (a_object == null){
 				return null;
 			}
-			return _parent.forClass(j4o.lang.Class.getClassForObject(a_object));
+			return forClass(j4o.lang.Class.getClassForObject(a_object));
 		}
 
 		public virtual bool isCollection(com.db4o.reflect.ReflectClass candidate){
