001035a647e9b2518657498f6e71ba971f98161d
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2005-04-07 23:24:23 +0000

    EvaluationDelegateWrapper to support marshalling/unmarshalling of delegates in mono

--- db4oj/core/src/com/db4o/QEvaluation.java
+++ db4oj/core/src/com/db4o/QEvaluation.java
@@ -7,37 +7,37 @@ package com.db4o;
  */
 public class QEvaluation extends QCon {
 
-    private transient Object i_evaluation;
-    byte[] i_marshalledEvaluation;
-    int i_marshalledID;
-    boolean i_isDelegate;
+	private transient Object i_evaluation;
 
-    public QEvaluation() {
-        // C/S only
-    }
+	byte[] i_marshalledEvaluation;
 
-    QEvaluation(Transaction a_trans, Object a_evaluation, boolean a_isDelegate) {
-        super(a_trans);
-        i_evaluation = a_evaluation;
-        i_isDelegate = a_isDelegate;
-    }
+	int i_marshalledID;
 
-    void evaluateEvaluationsExec(QCandidates a_candidates, boolean rereadObject) {
-        if (rereadObject) {
-            a_candidates.traverse(new Visitor4() {
-                public void visit(Object a_object) {
-                    ((QCandidate) a_object).useField(null);
-                }
-            });
-        }
-        a_candidates.filter(this);
-    }
+	public QEvaluation() {
+		// C/S only
+	}
+
+	QEvaluation(Transaction a_trans, Object a_evaluation) {
+		super(a_trans);
+		i_evaluation = a_evaluation;
+	}
+
+	void evaluateEvaluationsExec(QCandidates a_candidates, boolean rereadObject) {
+		if (rereadObject) {
+			a_candidates.traverse(new Visitor4() {
+				public void visit(Object a_object) {
+					((QCandidate) a_object).useField(null);
+				}
+			});
+		}
+		a_candidates.filter(this);
+	}
 
     void marshall() {
         super.marshall();
 		int[] id = {0};
 		if(Deploy.csharp){
-		    i_marshalledEvaluation = i_trans.i_stream.marshall(i_evaluation, id);
+		    i_marshalledEvaluation = i_trans.i_stream.marshall(Platform.wrapEvaluation(i_evaluation), id);
 		}else{
 		    try{
 		        // try serialisation. If it fails, store as db4o.
@@ -48,7 +48,7 @@ public class QEvaluation extends QCon {
 		    
 		}
 		i_marshalledID = id[0];
-    }
+	}
 
     void unmarshall(Transaction a_trans) {
         if (i_trans == null) {
@@ -65,22 +65,22 @@ public class QEvaluation extends QCon {
         }
     }
 
-    public void visit(Object obj) {
-		QCandidate candidate = (QCandidate)obj;		
-        try {
-            Platform.evaluationEvaluate(i_evaluation, candidate);
-            if(! candidate.i_include){
-            	doNotInclude(candidate.getRoot());
-            }
-        } catch (Exception e) {
-            candidate.include(false);
+	public void visit(Object obj) {
+		QCandidate candidate = (QCandidate) obj;
+		try {
+			Platform.evaluationEvaluate(i_evaluation, candidate);
+			if (!candidate.i_include) {
+				doNotInclude(candidate.getRoot());
+			}
+		} catch (Exception e) {
+			candidate.include(false);
 			doNotInclude(candidate.getRoot());
-            // TODO: implement Exception callback for the user coder
-            // at least for test cases
-        }
-    }
-    
-    boolean supportsIndex(){
-        return false;
-    }
+			// TODO: implement Exception callback for the user coder
+			// at least for test cases
+		}
+	}
+
+	boolean supportsIndex() {
+		return false;
+	}
 }
