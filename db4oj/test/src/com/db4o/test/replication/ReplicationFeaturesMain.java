90bcd0a504a2cb17c12589ee5b826cd6226ef0ed
klaus_wuestefeld <klaus_wuestefeld@915b37e7-0715-0410-8bc3-b14b59444072>
2005-04-14 18:39:49 +0000

    Cleaned up.

--- db4oj/test/src/com/db4o/test/replication/ReplicationFeaturesMain.java
+++ db4oj/test/src/com/db4o/test/replication/ReplicationFeaturesMain.java
@@ -2,8 +2,6 @@
 
 package com.db4o.test.replication;
 
-import java.io.*;
-
 import com.db4o.*;
 import com.db4o.ext.*;
 import com.db4o.query.*;
@@ -12,28 +10,18 @@ import com.db4o.test.*;
 
 public class ReplicationFeaturesMain {
 
-    private static final String FILE_B = "replicationTestB.yap";
     private static ExtObjectContainer _a;
     private static ExtObjectContainer _b;
 
     
-    private static void exit() {
-        _a.close();
-        _b.close();
-        
-        System.out.println("Done.");
-        System.exit(0);
-    }
-
     public void configure(){
-        new File(FILE_B).delete();
         Db4o.configure().generateUUIDs(Integer.MAX_VALUE);
         Db4o.configure().generateVersionNumbers(Integer.MAX_VALUE);
     }
 	
 	private void setUpObjectContainers() {
 		_a = Test.objectContainer();
-		_b = Db4o.openFile(FILE_B).ext();
+		_b = Test.replica();
 	}
 	
 	public void store() {
@@ -51,50 +39,49 @@ public class ReplicationFeaturesMain {
     
     public void test() {
 		setUpObjectContainers();
-		
-		
-		
 
-        replicateInBothDirections();
-
-        System.out.println("Checking a -> b");
-        checkAllEqual(_a, _b);
-        System.out.println("Checking b -> a");
-        checkAllEqual(_b, _a);
-
-//      TODO: replication.setDirection(...);
-//      TODO: cascading
-//      TODO: replication.checkConflict(obj); //(peek)
-
-		
-		_b.close();
-    }
-
-    private static void replicateInBothDirections(){
-        final ReplicationProcess replication = _a.replicationBegin(_b, new ReplicationConflictHandler() {
+		final ReplicationProcess replication = _a.replicationBegin(_b, new ReplicationConflictHandler() {
             public Object resolveConflict(ReplicationProcess process, Object a, Object b) {
             	return null;
             }
         });
 
+        replicateInBothDirections(replication);
+
+        checkAllEqual(_a, _b);
+        checkAllEqual(_b, _a);
+		
+		Test.ensure(objectsToReplicate(replication, _a).size() == 0);
+		Test.ensure(objectsToReplicate(replication, _b).size() == 0);
+	
+//      TODO: replication.setDirection(...);
+//      TODO: replication.checkConflict(obj); //(peek)
+		
+    }
+
+    private static void replicateInBothDirections(ReplicationProcess replication){
         replicate(replication, _a);
         replicate(replication, _b);
         replication.commit();
     }
 
     private static void replicate(ReplicationProcess replication, ExtObjectContainer origin) {
-        Query q = origin.query();
-        q.constrain(Replicated.class);
-        replication.whereModified(q);
-        ObjectSet objectSet = q.execute();
+        ObjectSet objectSet = objectsToReplicate(replication, origin);
         while(objectSet.hasNext()){
             Object obj = objectSet.next();
             Db4oUUID uuid = origin.getObjectInfo(obj).getUUID();
 
-            System.out.println("Replicating:" + obj);
             replication.replicate(obj);
         }
     }
+
+	private static ObjectSet objectsToReplicate(ReplicationProcess replication, ExtObjectContainer origin) {
+		Query q = origin.query();
+		q.constrain(Replicated.class);
+		replication.whereModified(q);
+		ObjectSet objectSet = q.execute();
+		return objectSet;
+	}
     
     private static void checkAllEqual(ExtObjectContainer con1, ExtObjectContainer con2){
         DeepCompare comparator = new DeepCompare();
@@ -110,8 +97,6 @@ public class ReplicationFeaturesMain {
             Object obj2 = con2.getByUUID(uuid);
             con2.activate(obj2, Integer.MAX_VALUE);
 
-            System.out.println("Comparing: " + obj1 + "  to: " + obj2);
-            
             Test.ensure(comparator.isEqual(obj1, obj2));
         }
     }
