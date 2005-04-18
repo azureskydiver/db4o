c4a2972c2e7cd13754493decae6809d5d75b0b89
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2005-04-18 16:54:02 +0000

    renamed to TestSuite

--- db4oj/test/src/com/db4o/test/jdk5/Jdk5TestSuite.java
+++ db4oj/test/src/com/db4o/test/jdk5/Jdk5TestSuite.java
@@ -2,10 +2,11 @@ package com.db4o.test.jdk5;
 
 import com.db4o.test.*;
 
-public class Jdk5Tests extends TestSuite{   
+public class Jdk5TestSuite extends TestSuite{   
     public Class[] tests(){
         return new Class[] {
             Jdk5EnumTest.class
+            // Jdk5DeleteEnum.class
         };
     }
 }
