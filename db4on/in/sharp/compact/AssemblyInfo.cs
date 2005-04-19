3c2126769bfa4c9985ebb86b15b345885c91194c
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2005-04-19 01:20:53 +0000

    first 4.5 build attempt

--- db4on/in/sharp/compact/AssemblyInfo.cs
+++ db4on/in/sharp/compact/AssemblyInfo.cs
@@ -1,16 +1,16 @@
-/* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
+/* Copyright (C) 2005   db4objects Inc.   http://www.db4o.com */
 
 using System.Reflection;
 using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("db4o - database for objects")]
-[assembly: AssemblyDescription("db4o 4.3.001 .NET CompactFramework")]
+[assembly: AssemblyDescription("db4o 4.5.001 .NET CompactFramework")]
 [assembly: AssemblyConfiguration(".NET CompactFramework")]
 [assembly: AssemblyCompany("db4objects Inc., San Mateo, CA, USA")]
 [assembly: AssemblyProduct("db4o - database for objects")]
 [assembly: AssemblyCopyright("db4o 2005")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]	
-[assembly: AssemblyVersion("4.3.001")]
+[assembly: AssemblyVersion("4.5.001")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
