8c9808ccf716f7c20d98fda172a5baf3b3611365
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2005-04-22 22:40:11 +0000

    replication crashes db4o with linked list

--- db4oj/test/src/com/db4o/test/replication/ReplicationTestSuite.java
+++ db4oj/test/src/com/db4o/test/replication/ReplicationTestSuite.java
@@ -6,6 +6,7 @@ public class ReplicationTestSuite extends TestSuite{
 
 	public Class[] tests() {
 		return new Class[] {
+// crashes            // ReplicateDb4oList.class,
 			ReplicationFeaturesMain.class,
             R0to4Runner.class,
 			ReplicationFeatures.class
