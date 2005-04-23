7f5a4e48e53157cc9d272d7e9e228fc044e1fe11
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2005-04-23 00:55:23 +0000

    Add comparison to TSerializable

--- db4oj/test/src/com/db4o/test/ByteArray.java
+++ db4oj/test/src/com/db4o/test/ByteArray.java
@@ -1,49 +1,90 @@
 package com.db4o.test;
 
+import java.io.Serializable;
+
 import com.db4o.ObjectSet;
+import com.db4o.config.TSerializable;
 import com.db4o.query.Query;
 
-class ByteArrayHolder {
+interface IByteArrayHolder {
+	byte[] getBytes();
+}
+
+class ByteArrayHolder implements IByteArrayHolder {
 	
-	byte[] bytes;
+	byte[] _bytes;
 	
 	public ByteArrayHolder(byte[] bytes) {
-		this.bytes = bytes;
+		this._bytes = bytes;
 	}
+	
+	public byte[] getBytes() {
+		return _bytes;
+	}
+}
+
+class SerializableByteArrayHolder implements Serializable, IByteArrayHolder {
+
+	private static final long serialVersionUID = 1L;
+	
+	byte[] _bytes;
+	
+	public SerializableByteArrayHolder(byte[] bytes) {
+		this._bytes = bytes;
+	}
+	
+	public byte[] getBytes() {
+		return _bytes;
+	}	
 }
 
 public class ByteArray {
 	
-	static final int ITERATIONS = 50;
+	static final int ITERATIONS = 15;
 	
 	static final int INSTANCES = 2;
 	
-	static final int ARRAY_LENGTH = 2*1024*1024;
+	static final int ARRAY_LENGTH = 1024*512;
 	
 	public void store() {
+		Test.close();
+		
+		com.db4o.Db4o.configure().objectClass(SerializableByteArrayHolder.class).translate(new TSerializable());		
+		Test.open();
+		
 		for (int i=0; i<INSTANCES; ++i) {
 			Test.store(new ByteArrayHolder(createByteArray()));
+			Test.store(new SerializableByteArrayHolder(createByteArray()));
 		}
 	}
 	
-	public void test() {
+	public void testByteArrayHolder() {
+		timeQueryLoop("raw byte array", ByteArrayHolder.class);
+	}
+	
+	public void testSerializableByteArrayHolder() {
+		timeQueryLoop("TSerializable", SerializableByteArrayHolder.class);
+	}
+
+	private void timeQueryLoop(String label, final Class clazz) {
 		long start = System.currentTimeMillis();
 		for (int i=0; i<ITERATIONS; ++i) {
 			
-			Test.reOpen();
+			Test.close();
+			Test.open();
 			
 			Query query = Test.query();
-			query.constrain(ByteArrayHolder.class);
+			query.constrain(clazz);
 			
 			ObjectSet os = query.execute();
 			Test.ensure(INSTANCES == os.size());
 			
 			while (os.hasNext()) {
-				Test.ensure(ARRAY_LENGTH == ((ByteArrayHolder)os.next()).bytes.length);
+				Test.ensure(ARRAY_LENGTH == ((IByteArrayHolder)os.next()).getBytes().length);
 			}
 		}
 		long end = System.currentTimeMillis();
-		System.err.println("" + ITERATIONS + " iterations took " + (end-start) + "ms");
+		System.err.println(label + ": " + ITERATIONS + " iterations took " + (end-start) + "ms");
 	}
 	
 	byte[] createByteArray() {
