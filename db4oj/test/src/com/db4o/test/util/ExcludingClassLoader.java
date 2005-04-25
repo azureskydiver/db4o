1b77ce84c196d3f28834247ca988f74613ed0cd3
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2005-04-25 20:58:22 +0000

    reset global reflector when client reflector is configured, use 'class-hiding' reflector for browser tests

---
+++ db4oj/test/src/com/db4o/test/util/ExcludingClassLoader.java
@@ -0,0 +1,37 @@
+package com.db4o.test.util;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcludingClassLoader extends URLClassLoader {
+	private List excludedNames;
+	
+	public ExcludingClassLoader(ClassLoader parent,List excludedNames) {
+		super(new URL[]{},parent);
+		this.excludedNames=excludedNames;
+	}
+
+	protected synchronized Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+		if(excludedNames.contains(name)) {
+			throw new ClassNotFoundException(name);
+		}
+		return super.loadClass(name, resolve);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		ClassLoader parent=ExcludingClassLoader.class.getClassLoader();
+		String excName=ExcludingClassLoader.class.getName();
+		List excluded=new ArrayList();
+		ClassLoader incLoader=new ExcludingClassLoader(parent,excluded);
+		System.out.println(incLoader.loadClass(excName));
+		excluded.add(excName);
+		try {
+			System.out.println(incLoader.loadClass(excName));
+		}
+		catch(ClassNotFoundException exc) {
+			System.out.println("Ok, not found.");
+		}
+	}
+}
