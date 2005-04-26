f763e67b61a8316ba5ae7cb26da1741fdace5fc6
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2005-04-26 22:31:05 +0000

    RuntimeException with cause constructor

--- db4on/in/sharp/j4o/lang/RuntimeException.cs
+++ db4on/in/sharp/j4o/lang/RuntimeException.cs
@@ -6,10 +6,16 @@ namespace j4o.lang {
 
     public class RuntimeException : Exception {
 
+        private Exception _cause;
+
         public RuntimeException() {
         }
 
         public RuntimeException(String message) : base(message) {
+        }
+
+        public RuntimeException(Exception cause){
+            _cause = cause;
         }
 
         public virtual Exception fillInStackTrace() {
