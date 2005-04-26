ebcac8a50472adabe8a6e2cd31007adb03d462c3
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2005-04-26 14:02:39 +0000

    Cache j4o.lang.Class for typeof(byte[]) (getClassForType is slow).

--- db4on/in/sharp/net/TSerializable.cs
+++ db4on/in/sharp/net/TSerializable.cs
@@ -22,10 +22,11 @@ namespace com.db4o
     /// <exclude />
     public class TSerializable : ObjectConstructor {
 
+		static Class _byteArrayType = Class.getClassForType(typeof(byte[]));
+
         public Object onStore(ObjectContainer objectContainer, Object obj){
             MemoryStream memoryStream = new MemoryStream();
             new BinaryFormatter().Serialize(memoryStream, obj);
-            memoryStream.Close();
             return memoryStream.GetBuffer();
         }
 
@@ -34,14 +35,13 @@ namespace com.db4o
 
         public Object onInstantiate(ObjectContainer objectContainer, Object obj){
             MemoryStream memoryStream = new MemoryStream((byte[])obj);
-            Object ret = new BinaryFormatter().Deserialize(memoryStream);
-            memoryStream.Close();
-            return ret;
+            return new BinaryFormatter().Deserialize(memoryStream);
         }
 
         public Class storedClass(){
-            return Class.getClassForType(typeof(byte[]));
+            return _byteArrayType;
         }
 
 	}
 }
+
