1c2173bffe4bd134f8e5ae3cb2f6333b36d040bd
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2005-05-03 18:02:22 +0000

    TSerializable belongs to com.db4o.config like the java version

--- db4on/in/sharp/net/TSerializable.cs
+++ db4on/in/sharp/net/TSerializable.cs
@@ -8,7 +8,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 using j4o.lang;
 using com.db4o.config;
 
-namespace com.db4o
+namespace com.db4o.config
 {
 
     /// <summary>
@@ -19,7 +19,6 @@ namespace com.db4o
     /// - the persistent type will never be refactored<br />
     /// - querying for type members is not necessary<br />
     /// </summary>
-    /// <exclude />
     public class TSerializable : ObjectConstructor {
 
 		static Class _byteArrayType = Class.getClassForType(typeof(byte[]));
