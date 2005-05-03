48c2eafc26f42eca3ee956a19b639a165850193c
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2005-05-03 18:03:12 +0000

    handle System.Type specially (not working)

---
+++ db4on/in/sharp/core/com/db4o/config/TType.cs
@@ -0,0 +1,33 @@
+/* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
+
+using System;
+using j4o.lang;
+using com.db4o;
+
+namespace com.db4o.config {
+
+	/// <exclude />
+    public class TType : ObjectConstructor {
+		
+		static readonly Class _stringClass = Class.getClassForType(typeof(String));
+      
+        public void onActivate(ObjectContainer objectContainer, object obj, object members) {
+        }
+      
+        public Object onInstantiate(ObjectContainer objectContainer, object obj) {
+            try { 
+                return Class.forName((String)obj).getNetType();
+            }  catch (Exception exception) { 
+                return null;
+            }
+        }
+      
+        public Object onStore(ObjectContainer objectContainer, object obj) {
+            return Class.getClassForType((System.Type)obj).getName(); 
+        }
+      
+        public Class storedClass() {
+            return _stringClass;
+        }
+    }
+}
