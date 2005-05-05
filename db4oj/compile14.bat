cde18f203d5875d135ff17bacbeb34cdb1b27b37
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2005-05-05 00:11:41 +0000

    compile script for compiling with 1.4 for JProbe

--- db4oj/compile14.bat
+++ db4oj/compile14.bat
@@ -2,10 +2,12 @@
 SET SJAVAC="C:\Program Files\Java\jdk1.5.0_01\bin\javac.exe"
 SET SROOT=C:\_db4o\HEAD\db4oj\
 
+md %SROOT%bin14
+
 del c:\javacdirfile
 
-dir core/src/*.java /B /S >> c:\javacdirfile
-dir test/src/*.java /B /S >> c:\javacdirfile
+dir %SROOT%\core\src\*.java /B /S >> c:\javacdirfile
+REM dir %SROOT%\test\src\*.java /B /S >> c:\javacdirfile
 
 %SJAVAC% -d %SROOT%bin14 -target jsr14 @c:\javacdirfile 
 
