947c8d3c688239df6ff1649a37f6512b1ca2586d
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2005-05-07 15:21:10 +0000

    MigrateFromNull fix

---
+++ db4oj/test/src/com/db4o/test/replication/MigrateFromNull.java
@@ -0,0 +1,18 @@
+/* Copyright (C) 2004 - 2005  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.test.replication;
+
+import com.db4o.test.*;
+
+
+public class MigrateFromNull {
+    
+    public void test(){
+        try{
+            Test.objectContainer().migrateFrom(null);
+        }catch(Exception e){
+            Test.error();
+        }
+    }
+
+}
