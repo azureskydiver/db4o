9a978de9f454d5c8379fffe99ca922dc18347776
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2005-05-15 19:08:37 +0000

    Project files for VS.NET 2005 targeting the Mobile 5.0 SDK

---
+++ db4on/test/src/com/db4o/test/cs/MarshalByRefDatabase.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using com.db4o;
+
+namespace com.db4o.test.cs
+{
+    /// <summary>
+    /// A facade to an ObjectContainer executing in a different AppDomain.
+    /// </summary>
+    public class MarshalByRefDatabase : MarshalByRefObject, IDisposable
+    {
+        protected ObjectServer _server;
+        protected ObjectContainer _container;
+
+        public void Open(string fname, bool clientServer)
+        {
+            if (clientServer)
+            {
+                _server = Db4o.openServer(fname, 0);
+                _container = _server.openClient();
+            }
+            else
+            {
+                _container = Db4o.openFile(fname);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (null != _container)
+            {
+                _container.close();
+                _container = null;
+            }
+            if (null != _server)
+            {
+                _server.close();
+                _server = null;
+            }
+            // MAGIC: give some time for the db4o background threads to exit
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
