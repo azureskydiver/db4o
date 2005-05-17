6d7834e8f4b0894dd937a8ab679a2b989faa202d
david_orme <david_orme@915b37e7-0715-0410-8bc3-b14b59444072>
2005-05-17 22:06:07 +0000

    Cleaned up interface

--- objectmanager/src/com/db4o/browser/model/IDatabase.java
+++ objectmanager/src/com/db4o/browser/model/IDatabase.java
@@ -1,14 +1,14 @@
 package com.db4o.browser.model;
 
 import com.db4o.ObjectSet;
-import com.db4o.binding.dataeditors.IObjectEditorFactory;
+import com.db4o.binding.dataeditors.IObjectEditor;
 import com.db4o.query.Query;
 import com.db4o.reflect.ReflectClass;
 import com.db4o.reflect.Reflector;
 
 
 
-public interface IDatabase extends IObjectEditorFactory {
+public interface IDatabase {
 	
 	void open(Db4oConnectionSpec spec);
     
@@ -39,4 +39,7 @@ public interface IDatabase extends IObjectEditorFactory {
 
     public abstract Query query();
 
+    public abstract IObjectEditor construct();
+    
+    public abstract IObjectEditor construct(Object toEdit);
 }
