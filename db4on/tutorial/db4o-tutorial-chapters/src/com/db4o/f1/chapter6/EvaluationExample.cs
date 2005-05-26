d2f58e0df445bdbbbf39795a070a6337f79f37a0
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2005-05-26 13:22:54 +0000

    fixed compile bugs in .net evaluation tutorial example

--- db4on/tutorial/db4o-tutorial-chapters/src/com/db4o/f1/chapter6/EvaluationExample.cs
+++ db4on/tutorial/db4o-tutorial-chapters/src/com/db4o/f1/chapter6/EvaluationExample.cs
@@ -8,8 +8,8 @@ using com.db4o.query;
 namespace com.db4o.f1.chapter6 {
 	public class EvaluationExample : Util {
 	  public static void Main(string[] args) {
-	    File.delete(Util.YAPFILENAME);
-	    ObjectContainer db=Db4o.openFile(Util.YAPFILENAME);
+	    File.Delete(Util.YapFileName);
+	    ObjectContainer db=Db4o.openFile(Util.YapFileName);
 	    try {
 	      storeCars(db);
 	      queryWithEvaluation(db);
@@ -22,20 +22,20 @@ namespace com.db4o.f1.chapter6 {
 	  public static void storeCars(ObjectContainer db) {
 	    Pilot pilot1=new Pilot("Michael Schumacher",100);
 	    Car car1=new Car("Ferrari");
-	    car1.setPilot(pilot1);
-	    car1.snapshot();
+	    car1.Pilot=pilot1;
+	    car1.Snapshot();
 	    db.set(car1);
 	    Pilot pilot2=new Pilot("Rubens Barrichello",99);
 	    Car car2=new Car("BMW");
-	    car2.setPilot(pilot2);
-	    car2.snapshot();
-	    car2.snapshot();
+	    car2.Pilot=pilot2;
+	    car2.Snapshot();
+	    car2.Snapshot();
 	    db.set(car2);
 	  }
 		
 	  public static void queryWithEvaluation(ObjectContainer db) {
 	    Query query=db.query();
-	    query.constrain(Car.class);
+	    query.constrain(typeof(Car));
 	    query.constrain(new EvenHistoryEvaluation());
 	    ObjectSet result=query.execute();
 	    Util.listResult(result);
