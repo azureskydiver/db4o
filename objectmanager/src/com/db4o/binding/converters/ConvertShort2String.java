b95d289ca1ead440e3e7bad52b038c6e64eb2e17
david_orme <david_orme@915b37e7-0715-0410-8bc3-b14b59444072>
2005-05-31 00:43:41 +0000

    Fixed problem querying short values

---
+++ objectmanager/src/com/db4o/binding/converters/ConvertShort2String.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2005 db4objects Inc.  http://www.db4o.com
+ */
+package com.db4o.binding.converters;
+
+import com.db4o.binding.converter.IConverter;
+
+/**
+ * ConvertShort2String.
+ *
+ * @author djo
+ */
+public class ConvertShort2String implements IConverter {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.binding.converter.IConverter#convert(java.lang.Object)
+	 */
+	public Object convert(Object source) {
+        Short i = (Short) source;
+		return i.toString();
+	}
+
+}
