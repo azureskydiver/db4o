ed5cb1d56b554f04c73c6c3e91dd2b01f3ee504d
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2005-06-02 15:59:41 +0000

    format .net integral decimals with bigdecimal

--- db4oj/core/src/com/db4o/handlers/NetDecimal.java
+++ db4oj/core/src/com/db4o/handlers/NetDecimal.java
@@ -2,18 +2,54 @@
 
 package com.db4o.handlers;
 
+import java.math.*;
+
 import com.db4o.*;
 
 /**
  * @exclude
  */
 public class NetDecimal extends NetSimpleTypeHandler{
+	private final static BigInteger FACTOR=new BigInteger("100",16);
 
 	public NetDecimal(YapStream stream) {
 		super(stream, 21, 16);
 	}
 	
 	public String toString(byte[] bytes) {
-		return "no converter for System.Decimal, mscorlib";
+		//return "no converter for System.Decimal, mscorlib";
+		//return bitString(bytes);
+		return convert(bytes);
+	}
+	
+	private String bitString(byte[] bytes) {
+		StringBuffer str=new StringBuffer();
+		for(int i=0;i<bytes.length;i++) {
+			for(int j=7;j>=0;j--) {
+				int curbit=(bytes[i]>>j)&0x1;
+				str.append(String.valueOf(curbit));
+			}
+		}
+		System.err.println(str);
+		return str.toString();
+	}
+	
+	private String convert(byte[] bytes) {
+		BigInteger mantissa=BigInteger.ZERO;
+		for(int blockoffset=8;blockoffset>=0;blockoffset-=4) {
+			for(int byteidx=0;byteidx<4;byteidx++) {
+				mantissa=mantissa.multiply(FACTOR);
+				int idx=blockoffset+byteidx;
+				mantissa=mantissa.add(new BigInteger(String.valueOf(bytes[idx]&0xff),10));
+			}
+		}
+		int exponent=bytes[13]&0x1f;
+		int sign=bytes[12];
+		BigDecimal factor=BigDecimal.ONE.divide(BigDecimal.TEN.pow(exponent));
+		BigDecimal val=new BigDecimal(mantissa).multiply(factor);
+		if(sign!=0) {
+			val=val.negate();
+		}
+		return val.toString();
 	}
 }
