0bd248fed7c9fa4bd374f22ff22c427205c679a6
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2005-06-02 16:32:16 +0000

    utc formatting for datetime

--- db4oj/core/src/com/db4o/handlers/NetDateTime.java
+++ db4oj/core/src/com/db4o/handlers/NetDateTime.java
@@ -11,6 +11,22 @@ import com.db4o.*;
  */
 // TODO: Between .NET and Java there seems to be a difference of two days between era offsets?!?
 public class NetDateTime extends NetSimpleTypeHandler{
+	private final static String ZEROES="0000";
+	
+	private final static String[] MONTHS= {
+		"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"May",
+		"Jun",
+		"Jul",
+		"Aug",
+		"Sep",
+		"Oct",
+		"Nov",
+		"Dec"
+	};
 	
     // ms between 01.01.0001,00:00:00.000 and 01.01.1970,00:00:00.000
 	//private static final long ERA_DIFFERENCE_IN_MS = 62135604000000L; // Carl's diff
@@ -30,6 +46,33 @@ public class NetDateTime extends NetSimpleTypeHandler{
             ticks = (ticks << 8) + (long)(bytes[i] & 255);
         }
         long ms = ticks / TICKS_TO_MS_RATIO - ERA_DIFFERENCE_IN_MS;
-        return new Date(ms).toString();
+        Date date=new Date(ms);
+        Calendar cal=Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        cal.setTime(date);
+        StringBuffer result=new StringBuffer()
+        	.append(prependZeroes(cal.get(Calendar.YEAR),4))
+        	.append('-')
+        	.append(MONTHS[cal.get(Calendar.MONTH)])
+        	.append('-')
+        	.append(prependZeroes(cal.get(Calendar.DAY_OF_MONTH),2))
+        	.append(", ")
+        	.append(prependZeroes(cal.get(Calendar.HOUR_OF_DAY),2))
+        	.append(':')
+        	.append(prependZeroes(cal.get(Calendar.MINUTE),2))
+        	.append(':')
+        	.append(prependZeroes(cal.get(Calendar.SECOND),2))
+        	.append('.')
+        	.append(prependZeroes(cal.get(Calendar.MILLISECOND),3))
+        	.append(" UTC");
+        return result.toString();
     }
+	
+	private String prependZeroes(int val,int size) {
+		String str=String.valueOf(val);
+		int missing=size-str.length();
+		if(missing>0) {
+			str=ZEROES.substring(0,missing)+str;
+		}
+		return str;
+	}
 }
