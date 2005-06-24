2e0290b5bea2ee3f8911977a2be96980aaa2780d
david_orme <david_orme@915b37e7-0715-0410-8bc3-b14b59444072>
2005-06-24 16:17:23 +0000

    Fixed launcher scripts to reflect new SWT libs from RC3 on Linux

--- objectmanager/launcher/scripts/linux/objectmanager.sh
+++ objectmanager/launcher/scripts/linux/objectmanager.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export VMEXE=java
-$VMEXE -Djava.library.path=lib/linux -classpath lib/db4o-4.5-java1.4.jar:lib/kxml-plugin.jar:lib/osgi_core.jar:lib/xmlpull_1_1_3_1.jar:lib/jakarta-oro-2.0.7.jar:lib/runtime.jar:lib/xswt.jar:lib/jface.jar:lib/objectmanager.jar:lib/linux/swt-cairo.jar:lib/linux/swt.jar:lib/linux/swt-mozilla.jar:lib/linux/swt-pi.jar com.db4o.browser.gui.standalone.StandaloneBrowser
 
+# $VMEXE -Djava.library.path=lib/linux -classpath lib/db4o-4.5-java1.4.jar:lib/kxml-plugin.jar:lib/osgi_core.jar:lib/xmlpull_1_1_3_1.jar:lib/jakarta-oro-2.0.7.jar:lib/runtime.jar:lib/xswt.jar:lib/jface.jar:lib/objectmanager.jar:lib/linux/swt-cairo.jar:lib/linux/swt.jar:lib/linux/swt-mozilla.jar:lib/linux/swt-pi.jar com.db4o.browser.gui.standalone.StandaloneBrowser
+
+$VMEXE -Djava.library.path=lib/linux -classpath lib/db4o-4.5-java1.4.jar:lib/kxml-plugin.jar:lib/osgi_core.jar:lib/xmlpull_1_1_3_1.jar:lib/jakarta-oro-2.0.7.jar:lib/runtime.jar:lib/xswt.jar:lib/jface.jar:lib/objectmanager.jar:lib/linux/swt.jar:lib/linux/swt-gtk.jar com.db4o.browser.gui.standalone.StandaloneBrowser
