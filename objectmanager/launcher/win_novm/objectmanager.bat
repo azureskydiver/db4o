6c90d994886b1dae94f78cd4225a18c2830a7516
david_orme <david_orme@915b37e7-0715-0410-8bc3-b14b59444072>
2005-06-24 15:57:29 +0000

    Removed old cruft

--- objectmanager/launcher/win_novm/objectmanager.bat
+++
@@ -1,4 +0,0 @@
-set VMEXE=java
-%VMEXE% -Djava.library.path=lib\windows -classpath lib\osgi_core.jar;lib\runtime.jar;lib\objectmanager.jar;lib\db4o-4.5-java1.4.jar;lib\jakarta-oro-2.0.7.jar;lib\jface.jar;lib\kxml-plugin.jar;lib\windows\swt-cairo.jar;lib\windows\swt-mozilla.jar;lib\windows\swt-pi.jar;lib\windows\swt.jar;lib\xmlpull_1_1_3_1.jar;lib\xswt.jar;lib\windows\swt.jar com.db4o.browser.gui.standalone.StandaloneBrowser
-
-
