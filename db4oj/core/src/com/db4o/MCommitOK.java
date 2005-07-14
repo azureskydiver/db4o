e4d8f4072dcae236fd884d62449736c8a460c9c8
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2005-07-14 14:02:25 +0000

    COMMIT_OK fix for last remaining C/S problem after big refactoring

---
+++ db4oj/core/src/com/db4o/MCommitOK.java
@@ -0,0 +1,13 @@
+/* Copyright (C) 2004 - 2005  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o;
+
+import com.db4o.foundation.network.*;
+
+final class MCommitOK extends Msg {
+    final boolean processMessageAtServer(YapSocket in) {
+        getTransaction().commit();
+        Msg.OK.write(getStream(), in);
+        return true;
+    }
+}
