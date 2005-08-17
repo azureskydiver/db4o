a09a30550cd82d8ed4b9bec56706ecf5bba877ad
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2005-08-17 20:49:31 +0000

    native query files

---
+++ db4on/core/src/core/com/db4o/YapStream2_0.cs
@@ -0,0 +1,45 @@
+/* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
+
+namespace com.db4o
+{
+#if NET_2_0
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+
+    public partial class YapStream
+    {
+        class GenericPredicateEvaluation<T> : DelegateEnvelope, com.db4o.query.Evaluation
+        {
+            public GenericPredicateEvaluation(Predicate<T> predicate) : base(predicate)
+            {
+            }
+
+            public void evaluate(com.db4o.query.Candidate candidate)
+            {
+                // use starting _ for PascalCase conversion purposes
+                Predicate<T> _predicate = (Predicate<T>)GetContent();
+                candidate.include(_predicate((T)candidate.getObject()));
+            }
+        }
+
+        public IList<Extent> query<Extent>(Predicate<Extent> match)
+        {
+            com.db4o.query.Query q = query();
+            q.constrain(typeof(Extent));
+            q.constrain(new GenericPredicateEvaluation<Extent>(match));
+            return ToList<Extent>(q.execute());
+        }
+
+        private static IList<Extent> ToList<Extent>(ObjectSet os)
+        {
+            List<Extent> found = new List<Extent>(os.Count);
+            foreach (object element in os)
+            {
+                found.Add((Extent)element);
+            }
+            return found;
+        }
+    }
+#endif
+}
