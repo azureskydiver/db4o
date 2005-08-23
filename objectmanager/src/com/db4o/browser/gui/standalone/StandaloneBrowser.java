9598bfa5e6029511a5069dc8ddd096f3fee62864
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2005-08-23 19:11:06 +0000

    updated version numbers

--- objectmanager/src/com/db4o/browser/gui/standalone/StandaloneBrowser.java
+++ objectmanager/src/com/db4o/browser/gui/standalone/StandaloneBrowser.java
@@ -66,7 +66,7 @@ import com.swtworkbench.community.xswt.metalogger.TeeLogger;
 public class StandaloneBrowser implements IControlFactory {
     
     public static final String APPNAME = "db4o Object Manager";
-    public static final String VERSION = "1.2.100";
+    public static final String VERSION = "1.5";
     public static final String LOGFILE = ".objectmanager.log";
     private static final String LOGCONFIG = ".objectmanager.logconfig";
     
