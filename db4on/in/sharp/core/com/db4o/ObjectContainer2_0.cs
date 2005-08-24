bdf2fdef451f6e5bcee518b874edd0ed1eafbcee
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2005-08-24 15:00:50 +0000

    funkified Predicate's documentation a little

--- db4on/in/sharp/core/com/db4o/ObjectContainer2_0.cs
+++ db4on/in/sharp/core/com/db4o/ObjectContainer2_0.cs
@@ -7,7 +7,19 @@ namespace com.db4o
 
 	/// <summary>
 	/// A native query predicate.
-	/// </summary>
+	/// </summary>	
+	/// <remarks>
+	/// The type parameter specifies the extent for the query.<br />
+	/// For all instances of the extent that are to be included into the
+	/// resultset of the query, the method returns true. For all instances
+	/// that are not to be included the method returns false. <br /><br />
+	/// Here is an example of an anonymous method that follows these conventions:<br />
+	/// <pre><code>
+	/// delegate(Cat cat) {<br />
+	///     return cat.name.equals("Frizz");<br />
+	/// }<br />
+	/// </code></pre><br /><br />
+	/// </remarks>
     public delegate bool Predicate<T>(T candidate);
 
     public partial interface ObjectContainer
