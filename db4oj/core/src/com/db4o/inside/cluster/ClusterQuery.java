f5ea0944157ed626dd4a3956163cb493f0985e32
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2005-09-07 13:03:57 +0000

    Package structure from cluster.insider to inside.cluster

--- db4oj/core/src/com/db4o/inside/cluster/ClusterQuery.java
+++ db4oj/core/src/com/db4o/inside/cluster/ClusterQuery.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2004 - 2005  db4objects Inc.  http://www.db4o.com */
 
-package com.db4o.cluster.inside;
+package com.db4o.inside.cluster;
 
 import com.db4o.*;
 import com.db4o.cluster.*;
