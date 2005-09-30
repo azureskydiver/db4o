9ad0ecdf4555424461018e2b2f8db487c07eb7b0
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2005-09-30 20:27:43 +0000

    starting nq integration

--- db4ojdk5/core/src/com/db4o/query/Predicate.java
+++ db4ojdk5/core/src/com/db4o/query/Predicate.java
@@ -6,7 +6,6 @@ import java.io.*;
 import java.lang.reflect.*;
 
 import com.db4o.*;
-import com.db4o.inside.*;
 
 /**
  * Extend this class and add your #match() method to run native queries.
@@ -25,62 +24,55 @@ import com.db4o.inside.*;
  * resultset of the query, the method returns true. For all instances
  * that are not to be included the method returns false. <br><br>
  * Here is an example of a #match method that follows these conventions:<br> 
- * <code>
+ * <pre><code>
  * public boolean match(Cat cat){<br>
  *     return cat.name.equals("Frizz");<br>
  * }<br>
- * </code><br><br>
+ * </code></pre><br><br>
  * Native queries for Java JDK5 and above define a #match method in the 
  * abstract Predicate class to ensure these conventions, using generics.
  * Without generics the method is not definable in the Predicate class
  * since alternative method parameter classes would not be possible.
  */
-public abstract class Predicate <ExtentType> implements Serializable{
-    
-    public abstract boolean match (ExtentType candidate);
-    
-    private transient Method _matchMethod;
-    
-    private transient Class _extent;
-    
-    public <ExtentType> Predicate  (){
-        findMatchMethod();
-        if(_matchMethod == null){
-            Exceptions4.throwRuntimeException(64);
-        }
-    }
-    
-    /**
-     * public for implementation reasons. Do not call. 
-     */
-    public final Class getExtent(){
-        return _extent;
-    }
-    
-    private void findMatchMethod() {
-        Method[] methods=getClass().getMethods();
-        for (int methodIdx = 0; methodIdx < methods.length; methodIdx++) {
-            Method curMethod=methods[methodIdx];
-            if(curMethod.getName().equals("match") &&
-               curMethod.getReturnType().equals(Boolean.TYPE)) {
-                Class[] paramTypes = curMethod.getParameterTypes();
-                if(paramTypes != null && paramTypes.length == 1){
-                    _extent = paramTypes[0];
-                    _matchMethod = curMethod;
-                    Platform4.setAccessible(curMethod);
-                    if(_extent != YapConst.CLASS_OBJECT){
-                        return;
-                    }
-                }
-            }
-        }
-    }
-    
-    /**
-     * public for implementation reasons. Do not call. 
-     */
-    public final boolean invoke (ExtentType obj){
-        return match(obj);
-    }
-    
+public abstract class Predicate<ExtentType> implements Serializable{
+	public final static String PREDICATEMETHOD_NAME="match";
+	
+	private transient Method cachedFilterMethod=null;
+	
+	private Method getFilterMethod() {
+		if(cachedFilterMethod!=null) {
+			return cachedFilterMethod;
+		}
+		Method[] methods=getClass().getMethods();
+		for (int methodIdx = 0; methodIdx < methods.length; methodIdx++) {
+			Method method=methods[methodIdx];
+			if((method.getName().equals(PREDICATEMETHOD_NAME))&&method.getParameterTypes().length==1) {					
+				String targetName=method.getParameterTypes()[0].getName();
+				if(!"java.lang.Object".equals(targetName)) {
+					cachedFilterMethod=method;
+					return method;
+				}
+			}
+		}
+
+		return null;
+	}
+
+	public Class<ExtentType> extentType() {
+		return (Class<ExtentType>)getFilterMethod().getParameterTypes()[0];
+	}
+
+	public abstract boolean match(ExtentType candidate);
+	
+	public boolean appliesTo(ExtentType candidate) {
+		try {
+			Method filterMethod=getFilterMethod();
+			filterMethod.setAccessible(true);
+			Object ret=filterMethod.invoke(this,new Object[]{candidate});
+			return ((Boolean)ret).booleanValue();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 }
