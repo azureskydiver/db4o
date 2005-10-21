41560cc69a0d1faf7179ba8f82516a525ec2bc24
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2005-10-21 19:36:05 +0000

    encode isSymmetric information inside ComparisonOperator

--- db4onqopt/core/src/com/db4o/nativequery/expr/cmp/ComparisonOperator.java
+++ db4onqopt/core/src/com/db4o/nativequery/expr/cmp/ComparisonOperator.java
@@ -5,16 +5,18 @@ public final class ComparisonOperator {
 	public final static int SMALLER_ID=1;
 	public final static int GREATER_ID=2;
 	
-	public final static ComparisonOperator EQUALS=new ComparisonOperator(EQUALS_ID,"==");
-	public final static ComparisonOperator SMALLER=new ComparisonOperator(SMALLER_ID,"<");
-	public final static ComparisonOperator GREATER=new ComparisonOperator(GREATER_ID,">");
+	public final static ComparisonOperator EQUALS=new ComparisonOperator(EQUALS_ID,"==", true);
+	public final static ComparisonOperator SMALLER=new ComparisonOperator(SMALLER_ID,"<", false);
+	public final static ComparisonOperator GREATER=new ComparisonOperator(GREATER_ID,">", false);
 	
 	private int _id;
 	private String _op;
+	private boolean _symmetric;
 	
-	private ComparisonOperator(int id,String op) {
+	private ComparisonOperator(int id, String op, boolean symmetric) {
 		_id=id;
 		_op=op;
+		_symmetric=symmetric;
 	}
 	
 	public int id() {
@@ -23,5 +25,9 @@ public final class ComparisonOperator {
 	
 	public String toString() {
 		return _op;
+	}
+	
+	public boolean isSymmetric() {
+		return _symmetric;
 	}
 }
