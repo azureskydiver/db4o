c16c223c08beef3d3cdc2517a66698f622360ef4
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2005-10-21 19:36:21 +0000

    formatting

--- db4oj/core/src/com/db4o/inside/query/Db4oNQOptimizer.java
+++ db4oj/core/src/com/db4o/inside/query/Db4oNQOptimizer.java
@@ -3,5 +3,5 @@ package com.db4o.inside.query;
 import com.db4o.query.*;
 
 public interface Db4oNQOptimizer {
-	void optimize(Query query,Predicate filter);
+	void optimize(Query query, Predicate filter);
 }
