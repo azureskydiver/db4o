b3093eb5744d8caa25db6e9a9be8d3b0b8459989
albert_kwan <albert_kwan@915b37e7-0715-0410-8bc3-b14b59444072>
2005-10-21 13:56:52 +0000

    no message

--- db4oj/core/src/com/db4o/MigrationConnection.java
+++ db4oj/core/src/com/db4o/MigrationConnection.java
@@ -2,27 +2,37 @@
 
 package com.db4o;
 
-import com.db4o.foundation.*;
-
+import com.db4o.foundation.Hashtable4;
 
 /**
- * @exclude
+ * Refer to Klaus's posting at development newsgroup:
+ * Does it not require the hashCode to be unique for each object?
+ * (System.identityHashCode does not necessarily produce unique > hashCodes).
+ * <p/>
+ * You are correct.
+ * <p/>
+ * Let's add putIdentity and getIdentity methods to Hashtable4 to fix.
+ * <p/>
+ * Please ignore for now. Most VMs I ever tested on produce unique values as
+ * long as an object is not gc'd.
+ * <p/>
+ * TODO FIXME
  */
 public class MigrationConnection {
-    
+
     private final Hashtable4 _referenceMap;
-    
-    MigrationConnection(){
+
+    MigrationConnection() {
         _referenceMap = new Hashtable4(1);
     }
-    
+
     public void mapReference(Object obj, YapObject ref) {
         _referenceMap.put(System.identityHashCode(obj), ref);
     }
 
-    public YapObject referenceFor(Object obj){
+    public YapObject referenceFor(Object obj) {
         int hcode = System.identityHashCode(obj);
-        YapObject ref = (YapObject)_referenceMap.get(hcode);
+        YapObject ref = (YapObject) _referenceMap.get(hcode);
         _referenceMap.remove(hcode);
         return ref;
     }
