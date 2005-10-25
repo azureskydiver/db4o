ec90d2c9a614decf0b8f4ba1edc2082785ec32ea
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2005-10-25 00:47:35 +0000

    draft nq chapter

---
+++ db4on/tutorial/db4o-tutorial-chapters/src/com/db4o/f1/chapter1/ComplexQuery.cs
@@ -0,0 +1,14 @@
+namespace com.db4o.f1.chapter1
+{
+    using com.db4o.query;
+
+    public class ComplexQuery : Predicate
+    {
+    	public boolean match(Pilot pilot)
+    	{
+	    	return pilot.Points>99
+	        		&& pilot.Points<199
+	        	||pilot.Name=="Rubens Barrichello";
+    	}
+    }
+}
