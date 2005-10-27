2c0e5aab5789c13aadbe7c671a90d2a0d79f88c0
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2005-10-27 22:12:58 +0000

    multiple runs for more stable results

--- db4onqopt/test/src/com/db4o/test/nativequery/cats/TestCatSpeed.java
+++ db4onqopt/test/src/com/db4o/test/nativequery/cats/TestCatSpeed.java
@@ -15,6 +15,7 @@ public class TestCatSpeed {
     
 	private final static String FILENAME="catspeed.yap";
     private final static int[] COUNT = {10000,100000};
+	private static final int NUMRUNS = 5;
 	
 	private final static Predicate[] PREDICATES={
 		new Predicate() {
@@ -69,28 +70,27 @@ public class TestCatSpeed {
 	}
 
 	public static void queryCats() {
-		for(int run=0;run<2;run++) {
-			boolean warmup=(run==0);
-			System.out.println(warmup ? "WARMING UP" : "TIMING");
-			for(int predIdx=0;predIdx<PREDICATES.length;predIdx++) {
-				if(!warmup) {
-					System.out.println("PREDICATE #"+(predIdx+1));
-				}
-				timeQuery(PREDICATES[predIdx],true,!warmup);
-				timeQuery(PREDICATES[predIdx],false,!warmup);
+		for (int predIdx = 0; predIdx < PREDICATES.length; predIdx++) {
+			long timeUnopt = 0;
+			long timeOpt = 0;
+			for (int run = 0; run <= NUMRUNS; run++) {
+				boolean warmup = (run == 0);
+				timeUnopt += timeQuery(PREDICATES[predIdx], false, warmup);
+				timeOpt += timeQuery(PREDICATES[predIdx], true, warmup);
 			}
+			System.out.println("PREDICATE #" + (predIdx + 1)+": "+(timeUnopt/NUMRUNS)+" / "+(timeOpt/NUMRUNS));
 		}
 	}
-	
-	public static void timeQuery(Predicate predicate,boolean optimize,boolean time) {
+
+	public static long timeQuery(Predicate predicate, boolean optimize,
+			boolean warmup) {
 		Db4o.configure().optimizeNativeQueries(optimize);
-		ObjectContainer db=Db4o.openFile(FILENAME);
-		long start=System.currentTimeMillis();
-		ObjectSet result=db.query(predicate);
-		if(time) {
-			System.out.println("Found "+result.size()+" results in "+(System.currentTimeMillis()-start)+" ms (opt: "+optimize+")");
-		}
+		ObjectContainer db = Db4o.openFile(FILENAME);
+		long start = System.currentTimeMillis();
+		db.query(predicate);
+		long time = (warmup ? 0 : System.currentTimeMillis() - start);
 		db.close();
+		return time;
 	}
 	
     public static void storeCats(int count){
