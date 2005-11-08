02368ac220529aaa99ed1f66bd87292ad90c1b4d
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2005-11-08 15:58:45 +0000

    fixed the tutorial (code and formatting)

--- db4on/tutorial/db4o-tutorial-chapters/src/com/db4o/f1/chapter6/EvenHistoryEvaluation.cs
+++ db4on/tutorial/db4o-tutorial-chapters/src/com/db4o/f1/chapter6/EvenHistoryEvaluation.cs
@@ -1,11 +1,14 @@
 using com.db4o.f1.chapter3;
 using com.db4o.query;
 	
-namespace com.db4o.f1.chapter6 {	
-	public class EvenHistoryEvaluation : Evaluation {
-	  public void evaluate(Candidate candidate) {
-	    Car car=(Car)candidate.getObject();
-	    candidate.include(car.GetHistory().Length%2==0);
-	  }
+namespace com.db4o.f1.chapter6
+{	
+	public class EvenHistoryEvaluation : Evaluation
+	{
+		public void evaluate(Candidate candidate)
+		{
+			Car car=(Car)candidate.getObject();
+			candidate.include(car.History.Count % 2 == 0);
+		}
 	}
 }
