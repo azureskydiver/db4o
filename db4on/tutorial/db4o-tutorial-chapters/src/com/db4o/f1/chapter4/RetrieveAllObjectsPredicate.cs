2a6eab2a65e181f21a427d861913f1c640af69de
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2005-11-08 19:08:01 +0000

    boolean -> bool

--- db4on/tutorial/db4o-tutorial-chapters/src/com/db4o/f1/chapter4/RetrieveAllObjectsPredicate.cs
+++ db4on/tutorial/db4o-tutorial-chapters/src/com/db4o/f1/chapter4/RetrieveAllObjectsPredicate.cs
@@ -3,7 +3,7 @@ namespace com.db4o.f1.chapter4
     using com.db4o.query;
 
 	public class RetrieveAllObjectsPredicate : Predicate {
-		public boolean match(object candidate){
+		public bool match(object candidate){
     		return true;
     	}
    	}
