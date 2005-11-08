07c00ff78a7e0fd5b8879387f30b2a43cb844cf2
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2005-11-08 15:58:45 +0000

    fixed the tutorial (code and formatting)

--- db4on/tutorial/db4o-tutorial-chapters/src/com/db4o/f1/chapter6/EvaluationExample.cs
+++ db4on/tutorial/db4o-tutorial-chapters/src/com/db4o/f1/chapter6/EvaluationExample.cs
@@ -1,44 +1,48 @@
 using System.IO;
-
-using com.db4o;
-using com.db4o.f1;
 using com.db4o.f1.chapter3;
 using com.db4o.query;
 
-namespace com.db4o.f1.chapter6 {
-	public class EvaluationExample : Util {
-	  public static void Main(string[] args) {
-	    File.Delete(Util.YapFileName);
-	    ObjectContainer db=Db4o.openFile(Util.YapFileName);
-	    try {
-	      storeCars(db);
-	      queryWithEvaluation(db);
-	    }
-	    finally {
-	      db.close();
-	    }
-	  }
-		
-	  public static void storeCars(ObjectContainer db) {
-	    Pilot pilot1=new Pilot("Michael Schumacher",100);
-	    Car car1=new Car("Ferrari");
-	    car1.Pilot=pilot1;
-	    car1.Snapshot();
-	    db.set(car1);
-	    Pilot pilot2=new Pilot("Rubens Barrichello",99);
-	    Car car2=new Car("BMW");
-	    car2.Pilot=pilot2;
-	    car2.Snapshot();
-	    car2.Snapshot();
-	    db.set(car2);
-	  }
-		
-	  public static void queryWithEvaluation(ObjectContainer db) {
-	    Query query=db.query();
-	    query.constrain(typeof(Car));
-	    query.constrain(new EvenHistoryEvaluation());
-	    ObjectSet result=query.execute();
-	    Util.listResult(result);
-	  }
+namespace com.db4o.f1.chapter6
+{
+	public class EvaluationExample : Util
+	{
+		public static void Main(string[] args)
+		{
+			File.Delete(Util.YapFileName);
+			ObjectContainer db = Db4o.openFile(Util.YapFileName);
+			try
+			{
+				storeCars(db);
+				queryWithEvaluation(db);
+			}
+			finally
+			{
+				db.close();
+			}
+		}
+
+		public static void storeCars(ObjectContainer db)
+		{
+			Pilot pilot1 = new Pilot("Michael Schumacher", 100);
+			Car car1 = new Car("Ferrari");
+			car1.Pilot = pilot1;
+			car1.Snapshot();
+			db.set(car1);
+			Pilot pilot2 = new Pilot("Rubens Barrichello", 99);
+			Car car2 = new Car("BMW");
+			car2.Pilot = pilot2;
+			car2.Snapshot();
+			car2.Snapshot();
+			db.set(car2);
+		}
+
+		public static void queryWithEvaluation(ObjectContainer db)
+		{
+			Query query = db.query();
+			query.constrain(typeof (Car));
+			query.constrain(new EvenHistoryEvaluation());
+			ObjectSet result = query.execute();
+			Util.listResult(result);
+		}
 	}
 }
