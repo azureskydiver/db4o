722c72f5966f2474b41501319f1e748caa99cc1b
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2005-11-12 18:33:49 +0000

    defining CF_2_0

--- db4on/core/src/core/com/db4o/YapStream2_0.cs
+++ db4on/core/src/core/com/db4o/YapStream2_0.cs
@@ -2,35 +2,30 @@
 
 namespace com.db4o
 {
-#if NET_2_0
+#if NET_2_0 || CF_2_0
+
+    using com.db4o.inside.query;
+    using com.db4o.query;
 	using System;
 	using System.Collections.Generic;
 	using System.Text;
 
     public partial class YapStream
     {
-        class GenericPredicateEvaluation<T> : DelegateEnvelope, com.db4o.query.Evaluation
-        {
-            public GenericPredicateEvaluation(Predicate<T> predicate) : base(predicate)
-            {
-            }
-
-            public void evaluate(com.db4o.query.Candidate candidate)
-            {
-                // use starting _ for PascalCase conversion purposes
-                Predicate<T> _predicate = (Predicate<T>)GetContent();
-                candidate.include(_predicate((T)candidate.getObject()));
-            }
-        }
-
         public IList<Extent> query<Extent>(Predicate<Extent> match)
         {
-            com.db4o.query.Query q = query();
-            q.constrain(typeof(Extent));
-            q.constrain(new GenericPredicateEvaluation<Extent>(match));
-            com.db4o.inside.query.QueryResult qr = ((QQuery)q).getQueryResult();
-            return new com.db4o.inside.query.GenericObjectSetFacade<Extent>(qr);
+            return getNativeQueryHandler().execute(match);
+        }
+
+        public IList<Extent> query<Extent>(System.Type extent)
+        {
+            QQuery q = (QQuery)query();
+            q.constrain(extent);
+            QueryResult qres = q.getQueryResult();
+            return new com.db4o.inside.query.GenericObjectSetFacade<Extent>(qres);
         }
     }
 #endif
 }
+
+
