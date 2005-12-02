36aef2ae4dc3d050c07aab2aa1ced8c21f07196c
Larysa_Visengeriyeva <Larysa_Visengeriyeva@915b37e7-0715-0410-8bc3-b14b59444072>
2005-12-02 15:16:07 +0000

    *** empty log message ***

---
+++ db4oj/core/src/com/db4o/reflect/self/SelfMethod.java
@@ -0,0 +1,18 @@
+package com.db4o.reflect.self;
+
+import com.db4o.reflect.ReflectClass;
+import com.db4o.reflect.ReflectMethod;
+
+public class SelfMethod implements ReflectMethod{
+
+	public Object invoke(Object onObject, Object[] parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ReflectClass getReturnType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
