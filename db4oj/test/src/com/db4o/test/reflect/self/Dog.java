ceef3140f45dafabe47e848fb2c5423a736766e3
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2005-12-03 13:26:20 +0000

    *** empty log message ***

--- db4oj/test/src/com/db4o/test/reflect/self/Dog.java
+++ db4oj/test/src/com/db4o/test/reflect/self/Dog.java
@@ -12,8 +12,8 @@ public class Dog implements SelfReflectable {
     
     private String _name;
     
-    public Dog(){
-        // for now: require default constructor
+    public Dog() {
+    	// require public no-args constructor
     }
     
     public Dog(String name){
@@ -21,7 +21,7 @@ public class Dog implements SelfReflectable {
     }
     
     public void configure(){
-        Db4o.configure().reflectWith(new SelfReflector());
+        Db4o.configure().reflectWith(new SelfReflector(new RegressionDogSelfReflectionRegistry()));
     }
     
     public void store(){
@@ -50,11 +50,11 @@ public class Dog implements SelfReflectable {
    private final static String[] FIELDNAMES={"_name"};
    private final static Class[] FIELDTYPES={String.class};
     
-	public String[] db4o$getFieldNames() {
+	public static String[] db4o$getFieldNames() {
 		return FIELDNAMES;
 	}
 
-	public Class db4o$getFieldType(String fieldName) {
+	public static Class db4o$getFieldType(String fieldName) {
 		for(int idx=0;idx<FIELDNAMES.length;idx++) {
 			if(FIELDNAMES[idx].equals(fieldName)) {
 				return FIELDTYPES[idx];
