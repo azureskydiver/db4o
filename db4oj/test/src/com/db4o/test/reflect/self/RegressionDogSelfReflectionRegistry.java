725575defda8a2ddfc7e9fa2804f8945e146aac8
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2005-12-03 13:26:20 +0000

    *** empty log message ***

---
+++ db4oj/test/src/com/db4o/test/reflect/self/RegressionDogSelfReflectionRegistry.java
@@ -0,0 +1,20 @@
+package com.db4o.test.reflect.self;
+
+import java.util.*;
+
+import com.db4o.*;
+import com.db4o.reflect.self.*;
+
+public class RegressionDogSelfReflectionRegistry implements SelfReflectionRegistry {
+	private final static Hashtable FIELDINFO;
+	
+	static {
+		FIELDINFO=new Hashtable(2);
+		FIELDINFO.put(Dog.class, new SelfField[]{new SelfField("_name",String.class)});
+		FIELDINFO.put(P1Object.class, new SelfField[]{});
+	}
+	
+	public SelfField[] fieldsFor(Class clazz) {
+		return (SelfField[])FIELDINFO.get(clazz);
+	}
+}
