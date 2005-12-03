82e37b4f2f122dd7e0ad55546d6e61c3517bc48c
Larysa_Visengeriyeva <Larysa_Visengeriyeva@915b37e7-0715-0410-8bc3-b14b59444072>
2005-12-03 14:42:53 +0000

    *** empty log message ***

--- unittests/src/com/db4o/reflect/self/SelfReflectTest.java
+++ unittests/src/com/db4o/reflect/self/SelfReflectTest.java
@@ -6,29 +6,54 @@ import com.db4o.reflect.*;
 
 public class SelfReflectTest extends TestCase {
 	private SelfReflector _reflector;
-	
+
 	protected void setUp() throws Exception {
-		_reflector=new SelfReflector(new UnitDogSelfReflectionRegistry());
+		_reflector = new SelfReflector(new UnitDogSelfReflectionRegistry());
 	}
-	
+
 	public void testReflectorClassRetrieval() {
-		assertSelfClass(Dog.class,_reflector.forClass(Dog.class));
-		assertSelfClass(Dog.class,_reflector.forName(Dog.class.getName()));
-		assertSelfClass(Dog.class,_reflector.forObject(new Dog("Laika")));
+		assertSelfClass(Dog.class, _reflector.forClass(Dog.class));
+		assertSelfClass(Dog.class, _reflector.forName(Dog.class.getName()));
+		assertSelfClass(Dog.class, _reflector.forObject(new Dog("Laika")));
 	}
-	
+
 	public void testSelfClass() {
-		SelfClass selfClass=new SelfClass(_reflector,Dog.class);
-		assertEquals(Dog.class.getName(),selfClass.getName());
-		assertEquals(Object.class.getName(),selfClass.getSuperclass().getName());
-		ReflectField[] fields=selfClass.getDeclaredFields();
-		assertEquals(1,fields.length);
+		SelfClass selfClass = selfclass();
+		assertEquals(Dog.class.getName(), selfClass.getName());
+		assertEquals(Object.class.getName(), selfClass.getSuperclass()
+				.getName());
+		ReflectField[] fields = selfClass.getDeclaredFields();
+		assertEquals(1, fields.length);
+		ReflectField field = selfClass.getDeclaredField("_name");
+		try {
+			assertEquals(Dog.class.getDeclaredField("_name"), field);
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		}
+		assertEquals(selfClass.reflector(), selfClass.getDelegate());
+		assertSame(_reflector, selfClass.reflector());
 	}
-	
-	private void assertSelfClass(Class jdkClass,ReflectClass reflectClass) {
-		SelfClass selfClass=(SelfClass)reflectClass;
+
+	public void testSelfField() {
+		SelfClass selfClass = selfclass();
+		ReflectField field = selfClass.getDeclaredField("_name");
+		SelfField selfField = new SelfField("_name", String.class);
+		assertEquals(field.getType(), selfField.getType());
+		assertEquals("_name", selfField.getName());
+	}
+
+	private SelfClass selfclass() {
+		SelfClass selfClass = new SelfClass(_reflector, Dog.class);
+		return selfClass;
+	}
+
+	private void assertSelfClass(Class jdkClass, ReflectClass reflectClass) {
+		SelfClass selfClass = (SelfClass) reflectClass;
 		assertEquals(jdkClass.getName(), selfClass.getName());
-		// TODO: Check semantics for reflector() - why does it return the parent?
+		// TODO: Check semantics for reflector() - why does it return the
+		// parent?
 		// assertSame(_reflector, selfClass.reflector());
 	}
 }
