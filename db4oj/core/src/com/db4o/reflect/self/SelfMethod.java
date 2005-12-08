d6c563976c8bd65c53f6c8b1ca08974865c5a38f
Larysa_Visengeriyeva <Larysa_Visengeriyeva@915b37e7-0715-0410-8bc3-b14b59444072>
2005-12-08 15:07:55 +0000

    for now we don't have any methods

--- db4oj/core/src/com/db4o/reflect/self/SelfMethod.java
+++ db4oj/core/src/com/db4o/reflect/self/SelfMethod.java
@@ -6,12 +6,10 @@ import com.db4o.reflect.ReflectMethod;
 public class SelfMethod implements ReflectMethod{
 
 	public Object invoke(Object onObject, Object[] parameters) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public ReflectClass getReturnType() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
