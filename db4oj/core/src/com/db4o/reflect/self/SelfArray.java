d69d84ee41b8c6409170f1433a164b4ce5cadd20
Larysa_Visengeriyeva <Larysa_Visengeriyeva@915b37e7-0715-0410-8bc3-b14b59444072>
2005-12-19 13:12:17 +0000

    SelfArray added

---
+++ db4oj/core/src/com/db4o/reflect/self/SelfArray.java
@@ -0,0 +1,57 @@
+package com.db4o.reflect.self;
+
+import com.db4o.reflect.*;
+
+public class SelfArray implements ReflectArray{
+
+	public int[] dimensions(Object arr) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int flatten(Object a_shaped, int[] a_dimensions, int a_currentDimension, Object[] a_flat, int a_flatElement) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Object get(Object onArray, int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ReflectClass getComponentType(ReflectClass a_class) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getLength(Object array) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean isNDimensional(ReflectClass a_class) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Object newInstance(ReflectClass componentType, int length) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object newInstance(ReflectClass componentType, int[] dimensions) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void set(Object onArray, int index, Object element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int shape(Object[] a_flat, int a_flatElement, Object a_shaped, int[] a_dimensions, int a_currentDimension) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
