a90a4c5c0094e70382ae48af71d01233eec36a03
Larysa_Visengeriyeva <Larysa_Visengeriyeva@915b37e7-0715-0410-8bc3-b14b59444072>
2005-12-20 13:14:44 +0000

    *** empty log message ***

--- unittests/src/com/db4o/reflect/self/SelfReflectTest.java
+++ unittests/src/com/db4o/reflect/self/SelfReflectTest.java
@@ -6,10 +6,11 @@ import com.db4o.reflect.*;
 
 public class SelfReflectTest extends TestCase {
 	private SelfReflectionRegistry _registry;
+
 	private SelfReflector _reflector;
 
 	protected void setUp() throws Exception {
-		_registry=new UnitDogSelfReflectionRegistry();
+		_registry = new UnitDogSelfReflectionRegistry();
 		_reflector = new SelfReflector(_registry);
 		_reflector.setParent(_reflector);
 	}
@@ -17,27 +18,29 @@ public class SelfReflectTest extends TestCase {
 	public void testReflectorClassRetrieval() {
 		assertSelfClass(Dog.class, _reflector.forClass(Dog.class));
 		assertSelfClass(Dog.class, _reflector.forName(Dog.class.getName()));
-		assertSelfClass(Dog.class, _reflector.forObject(new Dog("Laika",7,new Dog[0])));
+		assertSelfClass(Dog.class, _reflector.forObject(new Dog("Laika", 7,
+				new Dog[0])));
 	}
 
 	public void testSelfClass() throws Exception {
 		SelfClass selfClass = selfclass();
 		assertEquals(Dog.class.getName(), selfClass.getName());
-		assertEquals(Dog.class,selfClass.getJavaClass());
+		assertEquals(Dog.class, selfClass.getJavaClass());
 		assertFalse(selfClass.isAbstract());
 		assertFalse(selfClass.isInterface());
 		assertFalse(selfClass.isPrimitive());
 		assertNull(selfClass.getComponentType());
-		
-		SelfClass superClass = (SelfClass)selfClass.getSuperclass();
+
+		SelfClass superClass = (SelfClass) selfClass.getSuperclass();
 		assertEquals(Animal.class.getName(), superClass.getName());
-		assertEquals(Animal.class,superClass.getJavaClass());
-		assertEquals(Object.class.getName(),superClass.getSuperclass().getName());
+		assertEquals(Animal.class, superClass.getJavaClass());
+		assertEquals(Object.class.getName(), superClass.getSuperclass()
+				.getName());
 		assertNull(superClass.getSuperclass().getSuperclass());
 		assertTrue(superClass.isAbstract());
 		assertFalse(superClass.isInterface());
 		assertFalse(superClass.isPrimitive());
-		
+
 		assertTrue(selfClass.isAssignableFrom(selfClass));
 		assertTrue(superClass.isAssignableFrom(selfClass));
 		assertFalse(selfClass.isAssignableFrom(superClass));
@@ -45,66 +48,84 @@ public class SelfReflectTest extends TestCase {
 		assertTrue(selfClass.isInstance(new Dog()));
 		assertTrue(superClass.isInstance(new Dog()));
 		assertFalse(selfClass.isInstance(new Object()));
-		
+
 		ReflectField[] fields = selfClass.getDeclaredFields();
-		assertEquals(1, fields.length);
+		assertEquals(2, fields.length);
 		assertEquals(selfClass, selfClass.getDelegate());
 	}
 
 	public void testInterface() {
-		SelfClass interfaceClass=(SelfClass)_reflector.forClass(Being.class);
-		assertEquals(Being.class.getName(),interfaceClass.getName());
-		assertEquals(Being.class,interfaceClass.getJavaClass());
+		SelfClass interfaceClass = (SelfClass) _reflector.forClass(Being.class);
+		assertEquals(Being.class.getName(), interfaceClass.getName());
+		assertEquals(Being.class, interfaceClass.getJavaClass());
 		assertTrue(interfaceClass.isInterface());
 		assertTrue(interfaceClass.isAbstract());
 		assertFalse(interfaceClass.isPrimitive());
-		assertEquals(0,interfaceClass.getDeclaredFields().length);
-		assertEquals(0,interfaceClass.getDeclaredConstructors().length);
-		
-		SelfClass selfClass=selfclass();
+		assertEquals(0, interfaceClass.getDeclaredFields().length);
+		assertEquals(0, interfaceClass.getDeclaredConstructors().length);
+
+		SelfClass selfClass = selfclass();
 		assertTrue(interfaceClass.isAssignableFrom(selfClass));
 		assertFalse(selfClass.isAssignableFrom(interfaceClass));
 		assertTrue(interfaceClass.isInstance(new Dog()));
 	}
 
 	public void testPrimitive() {
-		Class[] primitives={Integer.class,Long.class,Short.class,Character.class,Byte.class,String.class};
-		for(int idx=0;idx<primitives.length;idx++) {
+		Class[] primitives = { Integer.class, Long.class, Short.class,
+				Character.class, Byte.class, String.class };
+		for (int idx = 0; idx < primitives.length; idx++) {
 			assertPrimitive(primitives[idx]);
 		}
 		assertFalse(_reflector.forClass(Object.class).isPrimitive());
 	}
-	
+
 	public void testSelfField() {
 		SelfClass selfClass = selfclass();
 		ReflectField field = selfClass.getDeclaredField("_age");
-		assertEquals(Integer.class.getName(),field.getType().getName());
+		assertEquals(Integer.class.getName(), field.getType().getName());
 		assertEquals("_age", field.getName());
+
+		assertTrue(selfClass.getDeclaredField("_age").isPublic());
+		assertFalse(selfClass.getDeclaredField("_age").isStatic());
+		assertFalse(selfClass.getDeclaredField("_age").isTransient());
+
+		assertTrue(selfClass.getDeclaredField("_parents").isPublic());
+		assertFalse(selfClass.getDeclaredField("_parents").isStatic());
+		assertFalse(selfClass.getDeclaredField("_parents").isTransient());
+
+		SelfClass superClass = (SelfClass) selfClass.getSuperclass();
+		assertTrue(superClass.getDeclaredField("_name").isPublic());
+		assertFalse(superClass.getDeclaredField("_name").isStatic());
+		assertFalse(superClass.getDeclaredField("_name").isTransient());
+
 	}
-	
+
 	public void testInstanceField() {
-		Dog laika=new Dog("Laika",7,new Dog[0]);
+		Dog laika = new Dog("Laika", 7, new Dog[0]);
 		ReflectField field = selfclass().getDeclaredField("_age");
-		Object value=field.get(laika);
-		assertEquals(new Integer(laika.age()),value);
+		Object value = field.get(laika);
+		assertEquals(new Integer(laika.age()), value);
 		field.set(laika, new Integer(8));
-		assertEquals(laika.age(),8);
-		assertEquals(field.get(laika),new Integer(8));
+		assertEquals(laika.age(), 8);
+		assertEquals(field.get(laika), new Integer(8));
 	}
 
 	public void testArray() {
-		SelfClass arrayClass=(SelfClass)_reflector.forClass(Dog[].class);
+		SelfClass arrayClass = (SelfClass) _reflector.forClass(Dog[].class);
 		assertTrue(arrayClass.isArray());
-		assertEquals(_reflector.forClass(Dog.class).getName(),arrayClass.getComponentType().getName());
-		
-		SelfArray arrayHandler=(SelfArray)_reflector.array();
-		assertEquals(_reflector.forClass(Dog.class).getName(),arrayHandler.getComponentType(arrayClass).getName());
-		Dog[] dogs=(Dog[])arrayHandler.newInstance(_reflector.forClass(Dog.class), 2);
-		assertEquals(2,dogs.length);
+		assertEquals(_reflector.forClass(Dog.class).getName(), arrayClass
+				.getComponentType().getName());
+
+		SelfArray arrayHandler = (SelfArray) _reflector.array();
+		assertEquals(_reflector.forClass(Dog.class).getName(), arrayHandler
+				.getComponentType(arrayClass).getName());
+		Dog[] dogs = (Dog[]) arrayHandler.newInstance(_reflector
+				.forClass(Dog.class), 2);
+		assertEquals(2, dogs.length);
 	}
-	
+
 	private SelfClass selfclass() {
-		SelfClass selfClass = new SelfClass(_reflector,_registry, Dog.class);
+		SelfClass selfClass = new SelfClass(_reflector, _registry, Dog.class);
 		return selfClass;
 	}
 
@@ -114,9 +135,9 @@ public class SelfReflectTest extends TestCase {
 	}
 
 	private void assertPrimitive(Class clazz) {
-		SelfClass primitiveClass=(SelfClass)_reflector.forClass(clazz);
+		SelfClass primitiveClass = (SelfClass) _reflector.forClass(clazz);
 		assertFalse(primitiveClass.isInterface());
 		assertFalse(primitiveClass.isAbstract());
-		assertTrue(clazz.getName(),primitiveClass.isPrimitive());
+		assertTrue(clazz.getName(), primitiveClass.isPrimitive());
 	}
 }
