531295b2c2d35d93dde87f5bcfa0d1bdd9af06b2
Larysa_Visengeriyeva <Larysa_Visengeriyeva@915b37e7-0715-0410-8bc3-b14b59444072>
2005-12-20 13:14:44 +0000

    *** empty log message ***

--- unittests/src/com/db4o/reflect/self/UnitDogSelfReflectionRegistry.java
+++ unittests/src/com/db4o/reflect/self/UnitDogSelfReflectionRegistry.java
@@ -4,32 +4,40 @@ import java.util.*;
 
 public class UnitDogSelfReflectionRegistry extends SelfReflectionRegistry {
 	private final static Hashtable CLASSINFO;
-	
+
 	static {
-		CLASSINFO=new Hashtable(2);
-		CLASSINFO.put(Animal.class, new ClassInfo(true,Object.class,new FieldInfo[]{new FieldInfo("_name",String.class,true,false,false)}));
-		CLASSINFO.put(Dog.class, new ClassInfo(false,Animal.class,new FieldInfo[]{new FieldInfo("_age",Integer.class,true,false,false)}));
+		CLASSINFO = new Hashtable(2);
+		CLASSINFO.put(Animal.class, new ClassInfo(true, Object.class,
+				new FieldInfo[] { new FieldInfo("_name", String.class, true,
+						false, false) }));
+		CLASSINFO.put(Dog.class,
+				new ClassInfo(false, Animal.class,
+						new FieldInfo[] {
+								new FieldInfo("_age", Integer.class, true,
+										false, false),
+								new FieldInfo("_parents", Dog[].class, true,
+										false, false) }));
 	}
-	
+
 	public ClassInfo infoFor(Class clazz) {
-		return (ClassInfo)CLASSINFO.get(clazz);
+		return (ClassInfo) CLASSINFO.get(clazz);
 	}
 
 	public Object arrayFor(Class clazz, int length) {
-		if(Dog.class.isAssignableFrom(clazz)) {
+		if (Dog.class.isAssignableFrom(clazz)) {
 			return new Dog[length];
 		}
-		if(Animal.class.isAssignableFrom(clazz)) {
+		if (Animal.class.isAssignableFrom(clazz)) {
 			return new Animal[length];
 		}
 		return null;
 	}
 
 	public Class componentType(Class clazz) {
-		if(Dog[].class.isAssignableFrom(clazz)) {
+		if (Dog[].class.isAssignableFrom(clazz)) {
 			return Dog.class;
 		}
-		if(Animal[].class.isAssignableFrom(clazz)) {
+		if (Animal[].class.isAssignableFrom(clazz)) {
 			return Animal.class;
 		}
 		return null;
