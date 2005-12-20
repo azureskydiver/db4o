2d9d8f2866b6199b7b439bf35bf3a2fb9f64614f
Larysa_Visengeriyeva <Larysa_Visengeriyeva@915b37e7-0715-0410-8bc3-b14b59444072>
2005-12-20 12:48:59 +0000

    SelfField, isPublic, isStatic, isTransient implemented

--- db4oj/core/src/com/db4o/reflect/self/SelfClass.java
+++ db4oj/core/src/com/db4o/reflect/self/SelfClass.java
@@ -6,24 +6,30 @@ import com.db4o.reflect.*;
 
 public class SelfClass implements ReflectClass {
 	private static final SelfField[] EMPTY_FIELDS = new SelfField[0];
+
 	private boolean _isAbstract;
+
 	private SelfField[] _fields;
+
 	private Reflector _parentReflector;
+
 	private SelfReflectionRegistry _registry;
-	
+
 	private Class _class;
+
 	private Class _superClass;
 
-//	public SelfClass() {
-//		super();
-//	}
+	// public SelfClass() {
+	// super();
+	// }
 
-	public SelfClass(Reflector parentReflector,SelfReflectionRegistry registry, Class clazz) {
-		_parentReflector=parentReflector;
+	public SelfClass(Reflector parentReflector,
+			SelfReflectionRegistry registry, Class clazz) {
+		_parentReflector = parentReflector;
 		_registry = registry;
 		_class = clazz;
 	}
-	
+
 	// TODO: Is this needed at all?
 	public Class getJavaClass() {
 		return _class;
@@ -34,49 +40,49 @@ public class SelfClass implements ReflectClass {
 	}
 
 	public ReflectClass getComponentType() {
-		if(!isArray()) {
+		if (!isArray()) {
 			return null;
 		}
 		return _parentReflector.forClass(_registry.componentType(_class));
 	}
 
 	public ReflectConstructor[] getDeclaredConstructors() {
-		if(isInterface()) {
+		if (isInterface()) {
 			return new SelfConstructor[0];
 		}
 		return new SelfConstructor[] { new SelfConstructor(_class) };
 	}
-	
+
 	public ReflectField[] getDeclaredFields() {
 		ensureClassInfoLoaded();
 		return _fields;
 	}
 
 	private void ensureClassInfoLoaded() {
-		if(_fields==null) {
-			ClassInfo classInfo=_registry.infoFor(_class);
-			if(classInfo==null) {
-				_fields=EMPTY_FIELDS;
+		if (_fields == null) {
+			ClassInfo classInfo = _registry.infoFor(_class);
+			if (classInfo == null) {
+				_fields = EMPTY_FIELDS;
 				return;
 			}
-			_superClass=classInfo.superClass();
-			_isAbstract=classInfo.isAbstract();
-			FieldInfo[] fieldInfo=classInfo.fieldInfo();
-			if(fieldInfo==null) {
-				_fields=EMPTY_FIELDS;
+			_superClass = classInfo.superClass();
+			_isAbstract = classInfo.isAbstract();
+			FieldInfo[] fieldInfo = classInfo.fieldInfo();
+			if (fieldInfo == null) {
+				_fields = EMPTY_FIELDS;
 				return;
 			}
-			_fields=new SelfField[fieldInfo.length];
-			for(int idx=0;idx<fieldInfo.length;idx++) {
-				_fields[idx]=selfFieldFor(fieldInfo[idx]);
+			_fields = new SelfField[fieldInfo.length];
+			for (int idx = 0; idx < fieldInfo.length; idx++) {
+				_fields[idx] = selfFieldFor(fieldInfo[idx]);
 			}
 		}
 	}
-	
+
 	public ReflectField getDeclaredField(String name) {
 		ensureClassInfoLoaded();
-		for(int idx=0;idx<_fields.length;idx++) {
-			if(_fields[idx].getName().equals(name)) {
+		for (int idx = 0; idx < _fields.length; idx++) {
+			if (_fields[idx].getName().equals(name)) {
 				return _fields[idx];
 			}
 		}
@@ -84,7 +90,8 @@ public class SelfClass implements ReflectClass {
 	}
 
 	private SelfField selfFieldFor(FieldInfo fieldInfo) {
-		return new SelfField(fieldInfo.name(),_parentReflector.forClass(fieldInfo.type()));
+		return new SelfField(fieldInfo.name(), _parentReflector
+				.forClass(fieldInfo.type()), this, _registry);
 	}
 
 	public ReflectClass getDelegate() {
@@ -101,9 +108,9 @@ public class SelfClass implements ReflectClass {
 		return _class.getName();
 	}
 
-	public ReflectClass getSuperclass() {	
+	public ReflectClass getSuperclass() {
 		ensureClassInfoLoaded();
-		if(_superClass==null) {
+		if (_superClass == null) {
 			return null;
 		}
 		return _parentReflector.forClass(_superClass);
@@ -111,7 +118,7 @@ public class SelfClass implements ReflectClass {
 
 	public boolean isAbstract() {
 		ensureClassInfoLoaded();
-		return _isAbstract||isInterface();
+		return _isAbstract || isInterface();
 	}
 
 	public boolean isArray() {
@@ -148,21 +155,21 @@ public class SelfClass implements ReflectClass {
 	public Object newInstance() {
 		try {
 			return _class.newInstance();
-        } catch(Exception e){
-            e.printStackTrace();
-        }
-        
-        // Specialized exceptions break conversion to .NET
-        
-//           
-//        
-//            
-//		} catch (InstantiationException e) {
-//			e.printStackTrace();
-//		} catch (IllegalAccessException e) {
-//			e.printStackTrace();
-//		}
-        
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Specialized exceptions break conversion to .NET
+
+		//           
+		//        
+		//            
+		// } catch (InstantiationException e) {
+		// e.printStackTrace();
+		// } catch (IllegalAccessException e) {
+		// e.printStackTrace();
+		// }
+
 		return null;
 	}
 
@@ -176,7 +183,7 @@ public class SelfClass implements ReflectClass {
 		// Class.newInstance()
 	}
 
-    // FIXME: remove. Reintroduced since OM depends on it - refactor OM.
+	// FIXME: remove. Reintroduced since OM depends on it - refactor OM.
 	public Object[] toArray(Object obj) {
 		return null;
 	}
