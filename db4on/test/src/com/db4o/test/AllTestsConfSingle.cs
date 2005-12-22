cfe9684ab065937765c0b64f6af3af0085f83c94
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2005-12-22 00:42:17 +0000

    update to survive pascalcase conversion

--- db4on/test/src/com/db4o/test/AllTestsConfSingle.cs
+++ db4on/test/src/com/db4o/test/AllTestsConfSingle.cs
@@ -1,92 +1,106 @@
 /* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
 
 using System;
+using System.Collections;
 using System.IO;
+using com.db4o.test.j4otest;
+using com.db4o.test.soda;
 
-namespace com.db4o.test {
-
-    public class AllTestsConfSingle {
-
-        /**
+namespace com.db4o.test
+{
+	public class AllTestsConfSingle
+	{
+		/**
          * new testcases here
          */
-        internal Type[] TESTS = new Type[]{
-            typeof(com.db4o.test.SimplestPossible),
-            //typeof(com.db4o.test.ExternalBlobs),
-            //typeof(com.db4o.test.soda.SodaNumberCoercion),
+
+		internal Type[] TESTS
+		{
+			// using property instead of array initializer
+			// so the camelCase->PascalCase converter does
+			// not get lost
+			get
+			{
+				ArrayList tests = new ArrayList();
+
+				tests.Add(typeof (SimplestPossible));
+				//tests.Add(typeof(com.db4o.test.ExternalBlobs));
+				//tests.Add(typeof(com.db4o.test.soda.SodaNumberCoercion));
 #if NET_2_0
-            typeof(net2.Net2GenericContainers),
-            typeof(net2.Net2GenericDictionary),
-			typeof(net2.Net2GenericList),
-			typeof(net2.Net2GenericOtherCollections),
+				tests.Add(typeof(net2.Net2GenericContainers));
+				tests.Add(typeof(net2.Net2GenericDictionary));
+				tests.Add(typeof(net2.Net2GenericList));
+				tests.Add(typeof(net2.Net2GenericOtherCollections));
 #endif
-            //typeof(com.db4o.test.nativequeries.NativeQueriesTestCase),
-            //typeof(com.db4o.test.nativequeries.NQRegressionTests),
-            typeof(com.db4o.test.j4otest.TypeNameTest),
-            typeof(com.db4o.test.soda.SodaNumberCoercion),
-         };
-       
-        /**
+				//tests.Add(typeof(com.db4o.test.nativequeries.NativeQueriesTestCase));
+				//tests.Add(typeof(com.db4o.test.nativequeries.NQRegressionTests));
+				tests.Add(typeof (TypeNameTest));
+				tests.Add(typeof (SodaNumberCoercion));
+				return (Type[]) tests.ToArray(typeof (Type));
+			}
+		}
+
+
+		/**
           * the number of test runs
           */
-        internal int RUNS = 1;
+		internal int RUNS = 1;
 
-        /**
+		/**
         * delete the database files
         */
-        internal bool DELETE_FILE = true;
-      
-        /**
+		internal bool DELETE_FILE = true;
+
+		/**
           * run the tests stand-alone
           */
-        internal bool SOLO = true;
-      
-        /**
+		internal bool SOLO = true;
+
+		/**
           * run the tests in client/server mode
           */
-        internal bool CLIENT_SERVER = false;
-      
-        /**
+		internal bool CLIENT_SERVER = false;
+
+		/**
           * run the client/server test against a remote server.
           * This requires AllTestsServer to be started on the other machine and
           * SERVER_HOSTNAME to be set correctly.
           */
-        internal bool REMOTE_SERVER = false;
+		internal bool REMOTE_SERVER = false;
 
-        /**
+		/**
          * the database file to be used for the server.
          */
-        public static String FILE_SERVER = "xt_serv.yap";
+		public static String FILE_SERVER = "xt_serv.yap";
 
-        /**
+		/**
          * the database file to be used stand-alone.
          */
-        public static String FILE_SOLO = "xt_solo.yap";
+		public static String FILE_SOLO = "xt_solo.yap";
 
-        /**
+		/**
          * the server host name.
          */
-        public static String SERVER_HOSTNAME = "localhost";
+		public static String SERVER_HOSTNAME = "localhost";
 
-        /**
+		/**
          * the server port.
          */
-        public static int SERVER_PORT = 4448;
+		public static int SERVER_PORT = 4448;
 
-        /**
+		/**
          * the db4o user.
          */
-        public static String DB4O_USER = "db4o";
+		public static String DB4O_USER = "db4o";
 
-        /**
+		/**
          * the db4o password.
          */
-        public static String DB4O_PASSWORD = "db4o";
+		public static String DB4O_PASSWORD = "db4o";
 
-        /**
+		/**
          * path to blobs held externally
          */
-        public static String BLOB_PATH = Path.Combine(Path.GetTempPath(), "BLOBS");
-
-    }
+		public static String BLOB_PATH = Path.Combine(Path.GetTempPath(), "BLOBS");
+	}
 }
