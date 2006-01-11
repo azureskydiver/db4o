ba8141f01d77d6e36e690f118f941a3e65d7042e
larysa_visengeriyeva <larysa_visengeriyeva@915b37e7-0715-0410-8bc3-b14b59444072>
2006-01-11 12:35:01 +0000

    bytecode comments added

--- db4onqopt/core/src/com/db4o/j2me/bloat/ClassEnhancer.java
+++ db4onqopt/core/src/com/db4o/j2me/bloat/ClassEnhancer.java
@@ -1,7 +1,7 @@
 package com.db4o.j2me.bloat;
 
-
 import EDU.purdue.cs.bloat.editor.ClassEditor;
+import EDU.purdue.cs.bloat.editor.Label;
 import EDU.purdue.cs.bloat.editor.MemberRef;
 import EDU.purdue.cs.bloat.editor.MethodEditor;
 import EDU.purdue.cs.bloat.editor.Opcode;
@@ -41,6 +41,7 @@ public class ClassEnhancer extends Enhancer {
 				"self_get", new Class[] { String.class }, new Class[0]);
 		MemberRef mr = methodRef(ce.getClass(), "self_get",
 				new Class[] { String.class }, Object.class);
+		me.addLabel(new Label(1));
 		// TODO: instructions:
 		/*
 		 * public Object self_get(String fieldName) {
@@ -50,6 +51,44 @@ public class ClassEnhancer extends Enhancer {
 		 * super.self_get(fieldName); }
 		 * 
 		 */
+		//		 access flags 1
+		// public self_get(String) : Object
+		// L0 (0)
+		// ALOAD 1: fieldName
+		// LDC "_age"
+		// INVOKEVIRTUAL String.equals(Object) : boolean
+		// IFEQ L1
+		// L2 (5)
+		// NEW Integer
+		// DUP
+		// ALOAD 0: this
+		// GETFIELD Dog._age : int
+		// INVOKESPECIAL Integer.<init>(int) : void
+		// ARETURN
+		// L1 (12)
+		// ALOAD 1: fieldName
+		// LDC "_parents"
+		// INVOKEVIRTUAL String.equals(Object) : boolean
+		// IFEQ L3
+		// L4 (17)
+		// ALOAD 0: this
+		// GETFIELD Dog._parents : Dog[]
+		// ARETURN
+		// L3 (21)
+		// ALOAD 1: fieldName
+		// LDC "_prices"
+		// INVOKEVIRTUAL String.equals(Object) : boolean
+		// IFEQ L5
+		// L6 (26)
+		// ALOAD 0: this
+		// GETFIELD Dog._prices : int[]
+		// ARETURN
+		// L5 (30)
+		// ALOAD 0: this
+		// ALOAD 1: fieldName
+		// INVOKESPECIAL Animal.self_get(String) : Object
+		//		    ARETURN
+		//		   L7 (35)
 	}
 
 	private void generateSelf_set(ClassEditor ce) {
@@ -58,6 +97,7 @@ public class ClassEnhancer extends Enhancer {
 						getType(Object.class) }, new Type[0]);
 		MemberRef mr = methodRef(ce.getClass(), "self_set", new Class[] {
 				String.class, Object.class }, void.class);
+		me.addLabel(new Label(1));
 		// TODO: instructions:
 		/*
 		 * 
@@ -67,10 +107,56 @@ public class ClassEnhancer extends Enhancer {
 		 * return; } if(fieldName.equals("_prices")) { _prices=(int[])value;
 		 * return; } super.self_set(fieldName,value); }
 		 */
+		// access flags 1
+		// public self_set(String,Object) : void
+		// L0 (0)
+		// ALOAD 1: fieldName
+		// LDC "_age"
+		// INVOKEVIRTUAL String.equals(Object) : boolean
+		// IFEQ L1
+		// L2 (5)
+		// ALOAD 0: this
+		// ALOAD 2: value
+		// CHECKCAST Integer
+		// INVOKEVIRTUAL Integer.intValue() : int
+		// PUTFIELD Dog._age : int
+		// L3 (11)
+		// RETURN
+		// L1 (13)
+		// ALOAD 1: fieldName
+		// LDC "_parents"
+		// INVOKEVIRTUAL String.equals(Object) : boolean
+		// IFEQ L4
+		// L5 (18)
+		// ALOAD 0: this
+		// ALOAD 2: value
+		// CHECKCAST Dog[]
+		// PUTFIELD Dog._parents : Dog[]
+		// L6 (23)
+		// RETURN
+		// L4 (25)
+		// ALOAD 1: fieldName
+		// LDC "_prices"
+		// INVOKEVIRTUAL String.equals(Object) : boolean
+		// IFEQ L7
+		// L8 (30)
+		// ALOAD 0: this
+		// ALOAD 2: value
+		// CHECKCAST int[]
+		// PUTFIELD Dog._prices : int[]
+		// L9 (35)
+		// RETURN
+		// L7 (37)
+		// ALOAD 0: this
+		// ALOAD 1: fieldName
+		// ALOAD 2: value
+		// INVOKESPECIAL Animal.self_set(String,Object) : void
+		//		   L10 (42)
+		//		    RETURN
+		//		   L11 (44)
 	}
 
+	public static void main(String[] args) {
 
-public static void main(String[] args) {
-	
-}
+	}
 }
--- db4onqopt/core/src/com/db4o/j2me/bloat/RegistryEnhancer.java
+++ db4onqopt/core/src/com/db4o/j2me/bloat/RegistryEnhancer.java
@@ -1,17 +1,20 @@
-package com.db4o.j2me.bloat;
+package com.db4o.bloat;
 
 import java.util.Hashtable;
 
 import EDU.purdue.cs.bloat.editor.ClassEditor;
 import EDU.purdue.cs.bloat.editor.FieldEditor;
+import EDU.purdue.cs.bloat.editor.Label;
+import EDU.purdue.cs.bloat.editor.LocalVariable;
 import EDU.purdue.cs.bloat.editor.MemberRef;
 import EDU.purdue.cs.bloat.editor.MethodEditor;
+import EDU.purdue.cs.bloat.editor.Opcode;
 import EDU.purdue.cs.bloat.editor.Type;
 import EDU.purdue.cs.bloat.reflect.Modifiers;
 
 import com.db4o.reflect.self.ClassInfo;
 
-public class RegistryEnhancer extends Enhancer{
+public class RegistryEnhancer extends Enhancer {
 
 	private void generateCLASSINFOField(ClassEditor ce) {
 		FieldEditor fe = createField(ce, 26, Type.getType(Hashtable.class),
@@ -27,6 +30,106 @@ public class RegistryEnhancer extends Enhancer{
 		 * new FieldInfo("_prices", int[].class, true, false, false), })); //
 		 * FIELDINFO.put(P1Object.class, new FieldInfo[]{}); }
 		 */
+//		 access flags 8
+		// static <clinit>() : void
+		// L0 (0)
+		// NEW Hashtable
+		// DUP
+		// ICONST_2
+		// INVOKESPECIAL Hashtable.<init>(int) : void
+		// PUTSTATIC RegressionDogSelfReflectionRegistry.CLASSINFO : Hashtable
+		// L1 (6)
+		// GETSTATIC RegressionDogSelfReflectionRegistry.CLASSINFO : Hashtable
+		// LDC Lcom/db4o/bloat/Animal;.class
+		// NEW ClassInfo
+		// DUP
+		// ICONST_1
+		// LDC Ljava/lang/Object;.class
+		// L2 (13)
+		// ICONST_1
+		// ANEWARRAY FieldInfo
+		// DUP
+		// ICONST_0
+		// L3 (18)
+		// NEW FieldInfo
+		// DUP
+		// LDC "_name"
+		// LDC Ljava/lang/String;.class
+		// ICONST_1
+		// L4 (24)
+		// ICONST_0
+		// ICONST_0
+		// INVOKESPECIAL FieldInfo.<init>(String,Class,boolean,boolean,boolean)
+		// : void
+		// AASTORE
+		// INVOKESPECIAL ClassInfo.<init>(boolean,Class,FieldInfo[]) : void
+		// L5 (30)
+		// INVOKEVIRTUAL Hashtable.put(Object,Object) : Object
+		// POP
+		// L6 (33)
+		// GETSTATIC RegressionDogSelfReflectionRegistry.CLASSINFO : Hashtable
+		// LDC Lcom/db4o/bloat/Dog;.class
+		// L7 (36)
+		// NEW ClassInfo
+		// DUP
+		// ICONST_0
+		// LDC Lcom/db4o/bloat/Animal;.class
+		// L8 (41)
+		// ICONST_3
+		// ANEWARRAY FieldInfo
+		// DUP
+		// ICONST_0
+		// L9 (46)
+		// NEW FieldInfo
+		// DUP
+		// LDC "_age"
+		// LDC Ljava/lang/Integer;.class
+		// ICONST_1
+		// L10 (52)
+		// ICONST_0
+		// ICONST_0
+		// L11 (55)
+		// INVOKESPECIAL FieldInfo.<init>(String,Class,boolean,boolean,boolean)
+		// : void
+		// AASTORE
+		// DUP
+		// ICONST_1
+		// L12 (60)
+		// NEW FieldInfo
+		// DUP
+		// LDC "_parents"
+		// LDC [Lcom/db4o/bloat/Dog;.class
+		// ICONST_1
+		// L13 (66)
+		// ICONST_0
+		// ICONST_0
+		// L14 (69)
+		// INVOKESPECIAL FieldInfo.<init>(String,Class,boolean,boolean,boolean)
+		// : void
+		// AASTORE
+		// DUP
+		// ICONST_2
+		// L15 (74)
+		// NEW FieldInfo
+		// DUP
+		// LDC "_prices"
+		// LDC [I.class
+		// ICONST_1
+		// L16 (80)
+		// ICONST_0
+		// ICONST_0
+		// L17 (83)
+		// INVOKESPECIAL FieldInfo.<init>(String,Class,boolean,boolean,boolean)
+		// : void
+		// AASTORE
+		// L18 (86)
+		// INVOKESPECIAL ClassInfo.<init>(boolean,Class,FieldInfo[]) : void
+		//		   L19 (88)
+		//		    INVOKEVIRTUAL Hashtable.put(Object,Object) : Object
+		//		    POP
+		//		   L20 (91)
+		//		    RETURN
+
 	}
 
 	private void generateInfoForMethod(ClassEditor ce) {
@@ -35,12 +138,32 @@ public class RegistryEnhancer extends Enhancer{
 				new Class[] { Class.class }, new Class[0]);
 		MemberRef mr = methodRef(ce.getClass(), "infoFor",
 				new Class[] { Class.class }, ClassInfo.class);
-		// TODO: inject instructions:
+//		 TODO: inject instructions:
 		/*
 		 * public ClassInfo infoFor(Class clazz) { return (ClassInfo)
 		 * CLASSINFO.get(clazz); }
 		 * 
 		 */
+		// access flags 1
+		// public infoFor(Class) : ClassInfo
+		// L0 (0)
+		// GETSTATIC RegressionDogSelfReflectionRegistry.CLASSINFO : Hashtable
+		// ALOAD 1: clazz
+		// INVOKEVIRTUAL Hashtable.get(Object) : Object
+		// CHECKCAST ClassInfo
+		// CHECKCAST ClassInfo
+		// ARETURN
+		// L1 (7)
+		me.addLabel(new Label(1));
+		me.addInstruction(Opcode.opc_getstatic, fieldRef(ce.name(),
+				Hashtable.class, "CLASSINFO"));
+		me.addInstruction(Opcode.opc_aload, new LocalVariable(1));
+		me.addInstruction(Opcode.opc_invokevirtual, methodRef(Hashtable.class,
+				"get", new Class[] { Object.class }, Object.class));
+		me.addInstruction(Opcode.opc_checkcast, getType(ClassInfo.class));
+		me.addInstruction(Opcode.opc_areturn);
+		me.commit();
+		
 	}
 
 	private void generateArrayForMethod(ClassEditor ce) {
@@ -49,6 +172,7 @@ public class RegistryEnhancer extends Enhancer{
 				new Class[0]);
 		MemberRef mr = methodRef(ce.getClass(), "arrayFor", new Class[] {
 				Class.class, Integer.class }, Object.class);
+		me.addLabel(new Label(1));
 		// TODO: inject instructions:
 		/*
 		 * public Object arrayFor(Class clazz, int length) { if
@@ -57,6 +181,33 @@ public class RegistryEnhancer extends Enhancer{
 		 * return super.arrayFor(clazz, length); }
 		 * 
 		 */
+		// access flags 1
+		// public arrayFor(Class,int) : Object
+		// L0 (0)
+		// LDC Lcom/db4o/bloat/Dog;.class
+		// ALOAD 1: clazz
+		// INVOKEVIRTUAL Class.isAssignableFrom(Class) : boolean
+		// IFEQ L1
+		// L2 (5)
+		// ILOAD 2: length
+		// ANEWARRAY Dog
+		// ARETURN
+		// L1 (9)
+		// LDC Lcom/db4o/bloat/Animal;.class
+		// ALOAD 1: clazz
+		// INVOKEVIRTUAL Class.isAssignableFrom(Class) : boolean
+		// IFEQ L3
+		// L4 (14)
+		// ILOAD 2: length
+		// ANEWARRAY Animal
+		// ARETURN
+		// L3 (18)
+		// ALOAD 0: this
+		// ALOAD 1: clazz
+		// ILOAD 2: length
+		// INVOKESPECIAL SelfReflectionRegistry.arrayFor(Class,int) : Object
+		// ARETURN
+		// L5 (24)
 	}
 
 	private void generateComponentTypeMethod(ClassEditor ce) {
@@ -64,6 +215,7 @@ public class RegistryEnhancer extends Enhancer{
 				"componentType", new Class[] { Class.class }, new Class[0]);
 		MemberRef mr = methodRef(ce.getClass(), "componentType",
 				new Class[] { Class.class }, Class.class);
+		me.addLabel(new Label(1));
 		// TODO:inject instructions:
 		/*
 		 * public Class componentType(Class clazz) { if
@@ -71,6 +223,30 @@ public class RegistryEnhancer extends Enhancer{
 		 * (Animal[].class.isAssignableFrom(clazz)) { return Animal.class; }
 		 * return super.componentType(clazz); }
 		 */
+		// access flags 1
+		// public componentType(Class) : Class
+		// L0 (0)
+		// LDC [Lcom/db4o/bloat/Dog;.class
+		// ALOAD 1: clazz
+		// INVOKEVIRTUAL Class.isAssignableFrom(Class) : boolean
+		// IFEQ L1
+		// L2 (5)
+		// LDC Lcom/db4o/bloat/Dog;.class
+		// ARETURN
+		// L1 (8)
+		// LDC [Lcom/db4o/bloat/Animal;.class
+		// ALOAD 1: clazz
+		// INVOKEVIRTUAL Class.isAssignableFrom(Class) : boolean
+		// IFEQ L3
+		// L4 (13)
+		// LDC Lcom/db4o/bloat/Animal;.class
+		// ARETURN
+		// L3 (16)
+		// ALOAD 0: this
+		// ALOAD 1: clazz
+		// INVOKESPECIAL SelfReflectionRegistry.componentType(Class) : Class
+		// ARETURN
+		// L5 (21)
 	}
 
 	public static void main(String[] args) {
