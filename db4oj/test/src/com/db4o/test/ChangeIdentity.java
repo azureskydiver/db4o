ebc77d23194630cb8df8ccd45084e26b988d06c0
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-01-20 17:02:58 +0000

    Added a hack test case that changes the identity of a database file.

---
+++ db4oj/test/src/com/db4o/test/ChangeIdentity.java
@@ -0,0 +1,47 @@
+/* Copyright (C) 2004 - 2005  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.test;
+
+import com.db4o.*;
+import com.db4o.ext.*;
+
+
+public class ChangeIdentity {
+    
+    public void test(){
+        
+        if(Test.isClientServer()){
+            return;
+        }
+        
+        ExtObjectContainer oc = Test.objectContainer();
+        oc.identity();
+        
+        Db4oDatabase db = oc.identity();
+        byte[] oldSignature = db.getSignature();
+        
+        YapFile yf = (YapFile)oc;
+        PBootRecord bootRecord = yf.bootRecord();
+        bootRecord.i_db = Db4oDatabase.generate();
+        bootRecord.setDirty();
+        bootRecord.store(1);
+        
+        Test.reOpen();
+        
+        oc = Test.objectContainer();
+        
+        db = oc.identity();
+        byte[] newSignature = db.getSignature();
+        
+        boolean same = true;
+        
+        for (int i = 0; i < oldSignature.length; i++) {
+            if(oldSignature[i] != newSignature[i]){
+                same =false;
+            }
+        }
+        
+        Test.ensure( ! same);
+    }
+
+}
