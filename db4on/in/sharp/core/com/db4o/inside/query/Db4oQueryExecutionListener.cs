32459229c6757db0a0963ac5c0fa02b8b6eff28b
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2006-01-20 14:20:48 +0000

    use events instead of the listener dialect

--- db4on/in/sharp/core/com/db4o/inside/query/Db4oQueryExecutionListener.cs
+++
@@ -1,8 +0,0 @@
-/* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
-namespace com.db4o.inside.query
-{
-	public interface Db4oQueryExecutionListener
-	{
-		void notifyQueryExecuted(object filter, string msg);
-	}
-}
