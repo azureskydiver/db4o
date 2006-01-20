91d0108bb139abb0b3c388e3110e92f82477df03
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2006-01-20 18:49:43 +0000

    removing unused files

--- db4obuild/src/pascalcase/FixUp.cs
+++
@@ -1,8 +0,0 @@
-// The pascal case converter fails to convert these classes but they are
-// referenced in AllTests.cs
-namespace com.db4o.test.cs
-{
-	public class CsAssemblyVersionChange
-	{
-	}
-}
