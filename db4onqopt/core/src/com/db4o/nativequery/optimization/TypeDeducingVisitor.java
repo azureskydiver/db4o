dfb0a3a1a56e557b857d862d010bc924a3fc8783
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-02-10 23:08:58 +0000

    pr: split up Java build time optimization visitor zoo into separate classes

---
+++ db4onqopt/core/src/com/db4o/nativequery/optimization/TypeDeducingVisitor.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.db4o.nativequery.optimization;
+
+import java.lang.reflect.*;
+
+import com.db4o.nativequery.expr.cmp.*;
+import com.db4o.nativequery.expr.cmp.field.*;
+
+class TypeDeducingVisitor implements ComparisonOperandVisitor {
+	private Class _predicateClass;
+	private Class _candidateClass;
+	private Class _clazz;
+	
+	public TypeDeducingVisitor(Class predicateClass, Class candidateClass) {
+		this._predicateClass = predicateClass;
+		this._candidateClass = candidateClass;
+		_clazz=null;
+	}
+
+	public void visit(PredicateFieldRoot root) {
+		_clazz=_predicateClass;
+	}
+
+	public void visit(CandidateFieldRoot root) {
+		_clazz=_candidateClass;
+	}
+
+	public void visit(StaticFieldRoot root) {
+		try {
+			_clazz=Class.forName(root.className());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Class operandClass() {
+		return _clazz;
+	}
+
+	public void visit(ArithmeticExpression operand) {
+	}
+
+	public void visit(ConstValue operand) {
+		_clazz=operand.value().getClass();
+	}
+
+	public void visit(FieldValue operand) {
+		operand.parent().accept(this);
+		try {
+			_clazz=fieldFor(_clazz,operand.fieldName()).getType();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void visit(ArrayAccessValue operand) {
+		operand.parent().accept(this);
+		_clazz=_clazz.getComponentType();
+	}
+	
+	Field fieldFor(Class clazz,String fieldName) {
+		while(clazz!=null) {
+			try {
+				return clazz.getDeclaredField(fieldName);
+			} catch (Exception e) {
+			}
+		}
+		return null;
+	}
+}
