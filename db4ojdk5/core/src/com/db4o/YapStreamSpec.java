ecd93e7b7096c8d144d3d1074d42aeada721c674
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-02-15 21:36:47 +0000

    pr: YapStreamSpec comments

--- db4ojdk5/core/src/com/db4o/YapStreamSpec.java
+++ db4ojdk5/core/src/com/db4o/YapStreamSpec.java
@@ -2,6 +2,12 @@ package com.db4o;
 
 import com.db4o.ext.*;
 
+/**
+ * Workaround to provide the Java 5 version with a hook to add ExtObjectContainer.
+ * (Generic method declarations won't match ungenerified YapStreamBase implementations
+ * otherwise and implementing it directly kills .NET conversion.)
+ * 
+ * @exclude
+ */
 public interface YapStreamSpec extends ExtObjectContainer {
-
 }
