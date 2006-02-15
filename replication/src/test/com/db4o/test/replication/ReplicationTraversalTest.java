51f70c2d971e75d746238f628174dffa85c7d183
albert_kwan <albert_kwan@915b37e7-0715-0410-8bc3-b14b59444072>
2006-02-15 07:35:04 +0000

    Tried to reproduce altana pharma bug "changed field not replicated in 2nd round with Oracle 10g" using Oracle 10g with StepByStepExample and HibernateReplicationFeaturesMain. The tests passed unfortunately. Encapsulated Replicated fields with name _* because _ is a illegal character for Oracle.

--- replication/src/test/com/db4o/test/replication/ReplicationTraversalTest.java
+++ replication/src/test/com/db4o/test/replication/ReplicationTraversalTest.java
@@ -2,36 +2,36 @@
 
 package com.db4o.test.replication;
 
-import com.db4o.inside.replication.*;
+import com.db4o.inside.replication.GenericReplicationSession;
 import com.db4o.replication.ReplicationSession;
 import com.db4o.test.Test;
-import com.db4o.test.replication.transients.*;
+import com.db4o.test.replication.transients.TransientReplicationProvider;
 
 public class ReplicationTraversalTest {
 
-    private final TransientReplicationProvider _peerA = new TransientReplicationProvider(new byte[]{0});
-    private final TransientReplicationProvider _peerB = new TransientReplicationProvider(new byte[]{1});
+	private final TransientReplicationProvider _peerA = new TransientReplicationProvider(new byte[]{0});
+	private final TransientReplicationProvider _peerB = new TransientReplicationProvider(new byte[]{1});
 
-    public void test() {
-        Replicated obj1 = new Replicated("1");
-        Replicated obj2 = new Replicated("2");
-        Replicated obj3 = new Replicated("3");
-        
-        obj1._link = obj2;
-        obj2._link = obj3;
-        obj3._link = obj1;
-        
+	public void test() {
+		Replicated obj1 = new Replicated("1");
+		Replicated obj2 = new Replicated("2");
+		Replicated obj3 = new Replicated("3");
+
+		obj1.setLink(obj2);
+		obj2.setLink(obj3);
+		obj3.setLink(obj1);
+
 		_peerA.transientProviderSpecificStore(obj1);
 		_peerA.transientProviderSpecificStore(obj2);
 		_peerA.transientProviderSpecificStore(obj3);
 
 		ReplicationSession replication = new GenericReplicationSession(_peerA, _peerB, null);
 		replication.replicate(obj1);
-		
+
 		Test.ensure(_peerA.activatedObjects().containsKey(obj1));
 		Test.ensure(_peerA.activatedObjects().containsKey(obj2));
 		Test.ensure(_peerA.activatedObjects().containsKey(obj3));
-    }
+	}
 
 
 }
