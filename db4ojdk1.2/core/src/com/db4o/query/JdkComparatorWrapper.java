011a6c515f3a70fbef74db7c39c478cab1ce6e83
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-02-16 19:55:04 +0000

    cr: adding @exclude to JdkComparatorWrapper

--- db4ojdk1.2/core/src/com/db4o/query/JdkComparatorWrapper.java
+++ db4ojdk1.2/core/src/com/db4o/query/JdkComparatorWrapper.java
@@ -1,7 +1,11 @@
+/* Copyright (C) 2004 - 2006 db4objects Inc.   http://www.db4o.com */
 package com.db4o.query;
 
 import java.util.*;
 
+/**
+ * @exclude
+ */
 public class JdkComparatorWrapper implements QueryComparator {
 	private Comparator _comparator;
 	
