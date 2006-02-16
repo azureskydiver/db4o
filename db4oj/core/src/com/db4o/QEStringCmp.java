7458826c3cb39c570b7bebd858eab0167540aba6
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-02-16 19:55:22 +0000

    cr: Adding @exclude to QEStringSmp, Removing no longer needed SafeSynIoAdapter, documenting QueryComparator

--- db4oj/core/src/com/db4o/QEStringCmp.java
+++ db4oj/core/src/com/db4o/QEStringCmp.java
@@ -1,5 +1,10 @@
+/* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
+
 package com.db4o;
 
+/**
+ * @exclude
+ */
 public abstract class QEStringCmp extends QEAbstract {
 	private boolean caseSensitive;
 
