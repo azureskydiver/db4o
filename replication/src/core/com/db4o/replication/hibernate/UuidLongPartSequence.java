a96b042d3ccd4edf29d98312342f1406d21b6f07
albert_kwan <albert_kwan@915b37e7-0715-0410-8bc3-b14b59444072>
2006-02-16 11:22:44 +0000

    ak:Increases RDBMS compatibility.Extracted UuidLongPartGenerator from HibernateReplicationProviderImpl. UuidLongPartGenerator is now managed by Hibernate instead of RAW SQL.

---
+++ replication/src/core/com/db4o/replication/hibernate/UuidLongPartSequence.java
@@ -0,0 +1,23 @@
+package com.db4o.replication.hibernate;
+
+public class UuidLongPartSequence {
+	public static final String TABLE_NAME = "uuid_long_part_sequence";
+
+	private long current;
+
+	public UuidLongPartSequence() {
+		current = UuidLongPartGenerator.MIN_SEQ_NO;
+	}
+
+	public long getCurrent() {
+		return current;
+	}
+
+	public void setCurrent(long current) {
+		this.current = current;
+	}
+
+	public void increment() {
+		current++;
+	}
+}
