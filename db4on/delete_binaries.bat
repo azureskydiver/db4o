a551e8ab62ece80cdaf3586769c7bbaab814bd1e
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-02-17 16:13:55 +0000

    cr: delete_binaries.bat added to make it more convenient to switch between VS and .NET versions

---
+++ db4on/delete_binaries.bat
@@ -0,0 +1,9 @@
+REM This batch helps to switch between VS.NET 2003 and VS.NET 2005
+
+del core\bin /S /Q
+del core\obj /S /Q
+del test\bin /S /Q
+del test\obj /S /Q
+del tools\bin /S /Q
+del tools\obj /S /Q
+
