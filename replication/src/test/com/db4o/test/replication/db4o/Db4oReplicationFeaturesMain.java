a17a405c9897d387ef5fa2200ac5e5d3cfd37afd
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-02-23 20:22:37 +0000

    cr: Changed Db4oReplicationFeaturesMain back to using the standard two db4o providers. One of the reasons: The test suite should terminate correctly without leaving over MemoryFile weak reference collection threads.

--- replication/src/test/com/db4o/test/replication/db4o/Db4oReplicationFeaturesMain.java
+++ replication/src/test/com/db4o/test/replication/db4o/Db4oReplicationFeaturesMain.java
@@ -2,36 +2,25 @@
 
 package com.db4o.test.replication.db4o;
 
-import com.db4o.ObjectContainer;
-import com.db4o.ext.ExtDb4o;
-import com.db4o.ext.MemoryFile;
 import com.db4o.inside.replication.TestableReplicationProvider;
 import com.db4o.replication.db4o.Db4oReplicationProvider;
+import com.db4o.test.*;
 import com.db4o.test.replication.ReplicationFeaturesMain;
 
 public class Db4oReplicationFeaturesMain extends ReplicationFeaturesMain {
-	private final ObjectContainer cA = ExtDb4o.openMemoryFile(new MemoryFile());
-	private final ObjectContainer cB = ExtDb4o.openMemoryFile(new MemoryFile());
+    
+    protected TestableReplicationProvider prepareProviderA() {
+        return new Db4oReplicationProvider(Test.objectContainer());
+    }
 
-	private final Db4oReplicationProvider pA = new Db4oReplicationProvider(cA);
-	private final Db4oReplicationProvider pB = new Db4oReplicationProvider(cB);
+    protected TestableReplicationProvider prepareProviderB() {
+        return Db4oReplicationTestUtil.providerB();
+    }
 
-	protected TestableReplicationProvider prepareProviderA() {
-		return pA;
-	}
-
-	protected TestableReplicationProvider prepareProviderB() {
-		return pB;
-	}
-
-	public void test() {
-		super.test();
-	}
+    public void test() {
+        super.test();
+    }
 
 	protected void clean() {
-		pA.closeIfOpened();
-		pB.closeIfOpened();
-		cA.close();
-		cB.close();
 	}
 }
