d95057a013a70c714aa60b30b787a575d649fa31
albert_kwan <albert_kwan@915b37e7-0715-0410-8bc3-b14b59444072>
2006-03-01 07:18:15 +0000

    ak: Make HibernateDb4oListTest to pass

---
+++ replication/src/test/com/db4o/test/replication/db4o/hibernate/Db4oHibernateListTest.java
@@ -0,0 +1,30 @@
+package com.db4o.test.replication.db4o.hibernate;
+
+import com.db4o.inside.replication.TestableReplicationProviderInside;
+import com.db4o.replication.db4o.Db4oReplicationProvider;
+import com.db4o.replication.hibernate.HibernateReplicationProviderImpl;
+import com.db4o.test.Test;
+import com.db4o.test.replication.collections.ListContent;
+import com.db4o.test.replication.collections.ListHolder;
+import com.db4o.test.replication.collections.ListTest;
+import com.db4o.test.replication.hibernate.HibernateConfigurationFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Db4oHibernateListTest extends ListTest {
+
+	protected TestableReplicationProviderInside prepareProviderA() {
+		return new Db4oReplicationProvider(Test.objectContainer());
+	}
+
+	protected TestableReplicationProviderInside prepareProviderB() {
+		Configuration configuration = HibernateConfigurationFactory.createNewDbConfig();
+		configuration.addClass(ListHolder.class);
+		configuration.addClass(ListContent.class);
+		return new HibernateReplicationProviderImpl(configuration, "A");
+	}
+
+	public void test() {
+		super.test();
+	}
+
+}
