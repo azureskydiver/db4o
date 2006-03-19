252cede4afb4b5d7860f34c664ff1d82f4519b94
albert_kwan <albert_kwan@915b37e7-0715-0410-8bc3-b14b59444072>
2006-03-19 15:35:38 +0000

    ak: ProviderTestHsqlRefAsTable  passed

--- replication/src/test/com/db4o/test/replication/hibernate/ref_as_table/hsql/ProviderTestHsqlRefAsTable.java
+++ replication/src/test/com/db4o/test/replication/hibernate/ref_as_table/hsql/ProviderTestHsqlRefAsTable.java
@@ -1,12 +1,12 @@
 package com.db4o.test.replication.hibernate.ref_as_table.hsql;
 
 import com.db4o.inside.replication.TestableReplicationProviderInside;
-import com.db4o.test.replication.hibernate.HibernateUtil;
+import com.db4o.replication.hibernate.impl.ref_as_table.RefAsTableReplicationProvider;
 import com.db4o.test.replication.hibernate.ref_as_columns.hsql.ProviderTestRefAsColumns;
 
 public class ProviderTestHsqlRefAsTable extends ProviderTestRefAsColumns {
 	protected TestableReplicationProviderInside prepareSubject() {
-		return HibernateUtil.refAsTableProviderA();
+		return new RefAsTableReplicationProvider(newCfg());
 	}
 
 	public void testReplicationProvider() {
