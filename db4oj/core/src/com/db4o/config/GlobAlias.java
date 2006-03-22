91726fe4cf2f107846eab55b66ecbb460c8c2be4
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2006-03-22 18:17:50 +0000

    rbo+pr: glob aliases (access .net data using java classes for instance)

---
+++ db4oj/core/src/com/db4o/config/GlobAlias.java
@@ -0,0 +1,57 @@
+package com.db4o.config;
+
+public class GlobAlias implements Alias {
+	
+	private GlobPattern _storedPattern;
+	private GlobPattern _runtimePattern;
+
+	public GlobAlias(String storedPattern, String runtimePattern) {
+		if (null == storedPattern) throw new IllegalArgumentException("storedPattern");
+		if (null == runtimePattern) throw new IllegalArgumentException("runtimePattern");
+		
+		_storedPattern = new GlobPattern(storedPattern);
+		_runtimePattern = new GlobPattern(runtimePattern);
+	}
+
+	public String resolve(String runtimeType) {
+		String match = _runtimePattern.matches(runtimeType);
+		return match != null
+			? _storedPattern.inject(match)
+			: null;
+	}
+	
+	static class GlobPattern {
+		private String _head;
+		private String _tail;
+
+		public GlobPattern(String pattern) {
+			String[] parts = split(pattern);
+			
+			_head = parts[0];
+			_tail = parts[1];
+		}
+
+		public String inject(String s) {
+			return _head + s + _tail; 
+		}
+
+		public String matches(String s) {
+			if (!s.startsWith(_head) || !s.endsWith(_tail)) return null;
+			return s.substring(_head.length(), s.length()-_tail.length());
+		}
+
+		private void invalidPattern() {
+			throw new  IllegalArgumentException("pattern");
+		}
+		
+		String[] split(String pattern) {
+			int index = pattern.indexOf('*');
+			if (-1 == index) invalidPattern();
+			return new String[] {
+					pattern.substring(0, index),
+					pattern.substring(index+1)
+			};
+		}
+	}
+
+}
--- db4ojdk1.2/test/src/com/db4o/test/aliases/ClassAliasesTestCase.java
+++ db4ojdk1.2/test/src/com/db4o/test/aliases/ClassAliasesTestCase.java
@@ -1,9 +1,11 @@
 package com.db4o.test.aliases;
 
+import java.io.*;
+
 import com.db4o.*;
-import com.db4o.config.TypeAlias;
-import com.db4o.reflect.ReflectClass;
+import com.db4o.config.*;
 import com.db4o.test.Test;
+import com.db4o.test.lib.File4;
 
 public class ClassAliasesTestCase {
 
@@ -19,14 +21,108 @@ public class ClassAliasesTestCase {
 				new TypeAlias("com.db4o.test.aliases.Person1",
 						"com.db4o.test.aliases.Person2"));
 		
-		ReflectClass rc = container.ext().reflector().forClass(Person2.class);
-		System.out.println(rc.getName());
-		
+		assertData(container);
+	}
+
+	private void assertData(ObjectContainer container) {
 		ObjectSet os = container.query(Person2.class);
 		
 		Test.ensureEquals(2, os.size());
 		ensureContains(os, new Person2("Homer Simpson"));
 		ensureContains(os, new Person2("John Cleese"));
+	}
+	
+	public void testAccessingDotnetFromJava() throws Exception {
+		generateDotnetData();
+		ObjectContainer container = openDotnetDataFile();
+		container.ext().configure().alias(
+				new GlobAlias(
+						"com.db4o.test.aliases.*, MyAssembly",
+						"com.db4o.test.aliases.*"));
+//				new TypeAlias(
+//						Person2.class.getName() + ", MyAssembly",
+//						Person2.class.getName()));
+		try {
+			assertData(container);
+		} finally {
+			container.close();
+		}
+	}
+
+	private ObjectContainer openDotnetDataFile() {
+		return Db4o.openFile(getDotnetDataFilePath());
+	}
+
+	private String getDotnetDataFilePath() {
+		return buildTempPath("dotnet.yap");
+	}
+
+	private String buildTempPath(String fname) {
+		return new File(getTempPath(), fname).getAbsolutePath();
+	}
+
+	private String getTempPath() {
+		return System.getProperty("java.io.tmpdir");
+	}
+
+	private void generateDotnetData() throws IOException {
+		new File(getDotnetDataFilePath()).delete();
+		String assembly = generateAssembly();
+		exec(assembly + " " + getDotnetDataFilePath());
+	}
+
+	private String generateAssembly() throws IOException {
+		String code = "namespace com.db4o.test.aliases {" +
+			"class Person2 { string _name; public Person2(string name) { _name = name; }}" +
+			"class Program {" +
+				"static void Main(string[] args) {" +
+					"string fname = args[0];" +
+					"using (ObjectContainer container = Db4o.openFile(fname)) {" +
+						"container.set(new Person2(\"Homer Simpson\"));" +
+						"container.set(new Person2(\"John Cleese\"));" +
+					"}" +
+					"System.Console.WriteLine(\"success\");" + 
+				"}" +
+			"}" +
+		"}";
+		
+		String srcFile = buildTempPath("MyAssembly.cs");
+		writeFile(srcFile, code);
+		String exePath = buildTempPath("MyAssembly.exe");
+
+		new File4(db4odllPath()).copy(buildTempPath("db4o.dll"));
+		String cmdLine = "csc /target:exe /r:" + db4odllPath() + " /out:" + exePath + " " + srcFile;
+		exec(cmdLine);
+		return exePath;
+	}
+
+	private String exec(String cmdLine) throws IOException {
+		Process p = Runtime.getRuntime().exec(cmdLine);
+		return readStdOut(p);
+	}
+
+	private String db4odllPath() throws IOException {
+		return new File("../db4obuild/dist/dll/net/db4o.dll").getCanonicalPath();
+	}
+
+	private void writeFile(String fname, String contents) throws IOException {
+		FileWriter writer = new FileWriter(fname);
+		try {
+			writer.write(contents);
+		} finally {
+			writer.close();
+		}
+	}
+
+	private String readStdOut(Process p) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		StringWriter writer = new StringWriter();
+		String line = null;
+		while (null != (line = reader.readLine())) {
+			writer.write(line);
+			writer.write("\n");
+		}
+		return writer.toString();
 	}
 
 	private void ensureContains(ObjectSet actual, Object expected) {
