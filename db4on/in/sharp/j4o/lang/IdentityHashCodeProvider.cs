c96da44301150f9d4c5e0acaa9328cad2f002352
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2006-03-31 17:32:42 +0000

    rbo: use reflection for now to avoid multi AppDomain issues because of a runtime bug

--- db4on/in/sharp/j4o/lang/IdentityHashCodeProvider.cs
+++ db4on/in/sharp/j4o/lang/IdentityHashCodeProvider.cs
@@ -7,7 +7,7 @@ namespace j4o.lang
 {
 	public class IdentityHashCodeProvider
 	{
-#if !CF_1_0 && !CF_2_0
+#if NET_2_0 || MONO
 		public static int identityHashCode(object obj)
 		{
 			return System.Runtime.CompilerServices.RuntimeHelpers.GetHashCode(obj);
@@ -25,6 +25,7 @@ namespace j4o.lang
 		{
 			Assembly assembly = typeof(object).Assembly;
 
+	#if CF_1_0 || CF_2_0
 			// CompactFramework
 			try
 			{
@@ -38,7 +39,7 @@ namespace j4o.lang
 			catch (Exception e)
 			{
 			}
-
+	#endif
 			// We may be running the CF app on .NET Framework 1.1
 			// for profiling, let's give that a chance
 			try
@@ -53,11 +54,6 @@ namespace j4o.lang
 			catch (Exception e)
 			{
 			}
-
-			// and for completeness sake, let's provide .NET Framework 1.0
-			// compliance also so we can debug the CF app there too
-			return (typeof(object)).GetMethod("GetHashCode");
-
 			return null;
 		}
 #endif
