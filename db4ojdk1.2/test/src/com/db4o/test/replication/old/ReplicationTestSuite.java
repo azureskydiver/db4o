a8cb78cd8c4309999955b917b8487eaece88a3cd
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-04-12 14:33:32 +0000

    cr: GetByUUID moved to db4oj

--- db4ojdk1.2/test/src/com/db4o/test/replication/old/ReplicationTestSuite.java
+++ db4ojdk1.2/test/src/com/db4o/test/replication/old/ReplicationTestSuite.java
@@ -6,7 +6,6 @@ public class ReplicationTestSuite extends TestSuite{
 
 	public Class[] tests() {
 		return new Class[] {
-            GetByUUID.class,
             MigrateFromNull.class,
             ReplicateDb4oList.class,
 			// ReplicationFeaturesMain.class,
