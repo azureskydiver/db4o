79ec9c6363b23a120faa066e7788526aa6783edb
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2006-04-21 23:42:22 +0000

    rbo: formatting cleanup after refactoring

--- db4oj/core/src/com/db4o/foundation/HashtableIntEntry.java
+++ db4oj/core/src/com/db4o/foundation/HashtableIntEntry.java
@@ -2,47 +2,48 @@
 
 package com.db4o.foundation;
 
-
 /**
  * 
  */
 class HashtableIntEntry implements DeepClone {
-    
-    int i_key;
-    Object i_object;
-    HashtableIntEntry i_next;
-    
-    protected HashtableIntEntry() {}
-    
-    HashtableIntEntry(int a_hash, Object a_object) {
-        i_key = a_hash;
-        i_object = a_object;
-    }
 
-    public Object deepClone(Object obj) {
-    	return deepCloneInternal(new HashtableIntEntry(), obj);
-    }
-    
-    protected HashtableIntEntry deepCloneInternal(HashtableIntEntry hie,Object obj) {
-        hie.i_key=i_key;
-        hie.i_next=i_next;
-        if(i_object instanceof DeepClone) {
-        	hie.i_object = ((DeepClone)i_object).deepClone(obj);
-        }
-        else {
-        	hie.i_object = i_object;
-        }
-        if(i_next != null){
-            hie.i_next = (HashtableIntEntry)i_next.deepClone(obj);    
-        }
-        return hie;
-    }
+	int i_key;
+
+	Object i_object;
+
+	HashtableIntEntry i_next;
+
+	HashtableIntEntry(int a_hash, Object a_object) {
+		i_key = a_hash;
+		i_object = a_object;
+	}
+
+	protected HashtableIntEntry() {
+	}
+
+	public void acceptKeyVisitor(Visitor4 visitor) {
+		visitor.visit(new Integer(i_key));
+	}
+
+	public Object deepClone(Object obj) {
+		return deepCloneInternal(new HashtableIntEntry(), obj);
+	}
 
 	public boolean sameKeyAs(HashtableIntEntry other) {
 		return i_key == other.i_key;
 	}
 
-	public void acceptKeyVisitor(Visitor4 visitor) {
-		visitor.visit(new Integer(i_key));
+	protected HashtableIntEntry deepCloneInternal(HashtableIntEntry entry, Object obj) {
+		entry.i_key = i_key;
+		entry.i_next = i_next;
+		if (i_object instanceof DeepClone) {
+			entry.i_object = ((DeepClone) i_object).deepClone(obj);
+		} else {
+			entry.i_object = i_object;
+		}
+		if (i_next != null) {
+			entry.i_next = (HashtableIntEntry) i_next.deepClone(obj);
+		}
+		return entry;
 	}
 }
