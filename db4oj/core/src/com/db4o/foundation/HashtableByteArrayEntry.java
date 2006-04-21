02a7c0e6c58f96be0937199ba09c47a64e8e4916
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2006-04-21 23:42:22 +0000

    rbo: formatting cleanup after refactoring

--- db4oj/core/src/com/db4o/foundation/HashtableByteArrayEntry.java
+++ db4oj/core/src/com/db4o/foundation/HashtableByteArrayEntry.java
@@ -8,26 +8,27 @@ class HashtableByteArrayEntry extends HashtableObjectEntry {
 	public HashtableByteArrayEntry(byte[] bytes, Object value) {
 		super(hash(bytes), bytes, value);
 	}
-	
-	public boolean hasKey(Object key) {
-		return key instanceof byte[]
-			? areEqual((byte[])i_objectKey, (byte[])key)
-			: false;
-	}
-	
-	static int hash(byte[] bytes) {
-        int ret = 0;
-        for (int i = 0; i < bytes.length; i++) {
-            ret = ret * 31 + bytes[i]; 
-        }
-        return ret;
-    }
 
-	static boolean areEqual(byte[] lhs, byte[] rhs) {		
+	public boolean hasKey(Object key) {
+		if (key instanceof byte[]) {
+			return areEqual((byte[]) i_objectKey, (byte[]) key);
+		}
+		return false;
+	}
+
+	static int hash(byte[] bytes) {
+		int ret = 0;
+		for (int i = 0; i < bytes.length; i++) {
+			ret = ret * 31 + bytes[i];
+		}
+		return ret;
+	}
+
+	static boolean areEqual(byte[] lhs, byte[] rhs) {
 		if (rhs.length != lhs.length) return false;
 		for (int i = 0; i < rhs.length; i++) {
 			if (rhs[i] != lhs[i]) return false;
 		}
-		return true;;
-	}    		
+		return true;
+	}
 }
