4dcce1ee85c313c5bde72a884d6ee50b7aaa21b4
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-04-24 20:21:34 +0000

    pr: Updating OM version

--- objectmanager/src/com/db4o/browser/gui/standalone/StandaloneBrowser.java
+++ objectmanager/src/com/db4o/browser/gui/standalone/StandaloneBrowser.java
@@ -45,7 +45,7 @@ import com.swtworkbench.community.xswt.metalogger.*;
 public class StandaloneBrowser implements IControlFactory {
     
     public static final String APPNAME = "db4o Object Manager";
-    public static final String VERSION = "1.7";
+    public static final String VERSION = "1.8";
     public static final String LOGFILE = ".objectmanager.log";
     public static final String LOGCONFIG = ".objectmanager.logconfig";
     
