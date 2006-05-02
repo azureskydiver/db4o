6620a91f20bb0087168ea1d6f90a44c03d6a1126
albert_kwan <albert_kwan@915b37e7-0715-0410-8bc3-b14b59444072>
2006-05-02 17:46:03 +0000

    ak: removed the wrap calls

--- replication/src/core/com/db4o/replication/hibernate/HibernateReplication.java
+++ replication/src/core/com/db4o/replication/hibernate/HibernateReplication.java
@@ -7,8 +7,20 @@ import com.db4o.replication.Replication;
 import com.db4o.replication.ReplicationConflictException;
 import com.db4o.replication.ReplicationEventListener;
 import com.db4o.replication.ReplicationSession;
+import com.db4o.replication.db4o.Db4oReplicationProvider;
+import com.db4o.replication.hibernate.impl.HibernateReplicationProviderImpl;
 
 public class HibernateReplication {
+	/**
+	 * begins a replication session between db4o and Hibernate, no conflict
+	 * resolver.
+	 *
+	 * @throws ReplicationConflictException when conflicts occur
+	 * @see ReplicationEventListener
+	 */
+	public static ReplicationSession begin(ObjectContainer oc, Configuration cfg) {
+		return begin(oc, cfg, null);
+	}
 
 	/**
 	 * begins a replication session between Hibernate and Hibernate, no conflict
@@ -18,32 +30,20 @@ public class HibernateReplication {
 	 * @see ReplicationEventListener
 	 */
 	public static ReplicationSession begin(Configuration cfg1, Configuration cfg2) {
-		return HibernateReplication.begin(cfg1, cfg2, null);
-	}
-
-	/**
-	 * begins a replication session between db4o and Hibernate, no conflict
-	 * resolver.
-	 *
-	 * @throws ReplicationConflictException when conflicts occur
-	 * @see ReplicationEventListener
-	 */
-	public static ReplicationSession begin(ObjectContainer oc, Configuration cfg) {
-		return HibernateReplication.begin(oc, cfg, null);
-	}
-
-	/**
-	 * begins a replication session between Hibernate and Hibernate
-	 */
-	public static ReplicationSession begin(Configuration cfg1, Configuration cfg2, ReplicationEventListener listener) {
-		return Replication.begin(Replication.wrap(cfg1), Replication.wrap(cfg2), listener);
+		return begin(cfg1, cfg2, null);
 	}
 
 	/**
 	 * begins a replication session between db4o and Hibernate
 	 */
 	public static ReplicationSession begin(ObjectContainer oc, Configuration cfg, ReplicationEventListener listener) {
-		return Replication.begin(Replication.wrap(oc), Replication.wrap(cfg), listener);
+		return Replication.begin(new Db4oReplicationProvider(oc), new HibernateReplicationProviderImpl(cfg), listener);
 	}
 
+	/**
+	 * begins a replication session between Hibernate and Hibernate
+	 */
+	public static ReplicationSession begin(Configuration cfg1, Configuration cfg2, ReplicationEventListener listener) {
+		return Replication.begin(new HibernateReplicationProviderImpl(cfg1), new HibernateReplicationProviderImpl(cfg2), listener);
+	}
 }
