e7e9b4436b6c2d7a6bd7c4a7b8ccba872e2998d7
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-05-05 22:33:22 +0000

    cr: A little bit of daily BTree work

--- db4oj/test/src/com/db4o/test/inside/btree/BTreeIntIndex.java
+++ db4oj/test/src/com/db4o/test/inside/btree/BTreeIntIndex.java
@@ -3,6 +3,7 @@
 package com.db4o.test.inside.btree;
 
 import com.db4o.*;
+import com.db4o.foundation.*;
 import com.db4o.inside.btree.*;
 import com.db4o.test.*;
 
@@ -10,24 +11,33 @@ import com.db4o.test.*;
 public class BTreeIntIndex {
     
     public void test(){
-        
         BTree btree = createBTree();
-        
         addValues(btree);
         
-        
+        expect(btree, SORTED);
         
         
     }
     
     private void addValues(BTree btree){
-        
         Transaction trans = trans(); 
-        
-        for (int i = 0; i < values.length; i++) {
-            btree.add(trans, new Integer(values[i]));
+        for (int i = 0; i < VALUES.length; i++) {
+            btree.add(trans, new Integer(VALUES[i]));
         }
-        
+    }
+    
+    private void expect(BTree btree, final int[] values){
+        final int[] cursor = new int[] {0};
+        btree.traverseKeys(trans(), new Visitor4() {
+            public void visit(Object obj) {
+                // Test.ensure(((Integer)obj).intValue() == values[cursor[0]]);
+                
+                System.out.println(obj);
+                
+                cursor[0] ++;
+            }
+        });
+        Test.ensure(cursor[0] == values.length);
     }
     
     private Transaction trans(){
@@ -46,9 +56,13 @@ public class BTreeIntIndex {
     }
     
     
-    static int[] values = {3, 234, 55, 87, 2, 1, 101, 59, 70, 300, 288};
+    static final int[] VALUES = {3, 234, 55, 87, 2, 1, 101, 59, 70};
     
-    static int[] sorted = {1, 2, 3, 55, 59, 70, 87, 101, 234, 288, 300};
+    static final int[] SORTED = {1, 2, 3, 55, 59, 70, 87, 101, 234};
+    
+//    static final int[] VALUES = {3, 234, 55, 87, 2, 1, 101, 59, 70, 300, 288};
+//    
+//    static final int[] SORTED = {1, 2, 3, 55, 59, 70, 87, 101, 234, 288, 300};
     
     
     
