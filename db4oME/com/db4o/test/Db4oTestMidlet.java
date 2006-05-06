7120c31d555538bd74b51993bb7d55a018e5c09d
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-05-06 17:43:30 +0000

    lv/pr: First successful run storing/retrieving dogs on J2ME with intermediate database shutdown

--- db4oME/com/db4o/test/Db4oTestMidlet.java
+++ db4oME/com/db4o/test/Db4oTestMidlet.java
@@ -36,24 +36,37 @@ public class Db4oTestMidlet extends MIDlet {
 			RecordStore.deleteRecordStore("test.yap");
 		} catch (Exception e) {
 		}
-		System.out.println(IntClassGetter.getIntClass());
+                String[] stores=RecordStore.listRecordStores();
+                System.out.println("Found "+(stores!=null ? stores.length : 0)+" stores");
+                if(stores!=null) {
+                    for (int storeIdx = 0; storeIdx < stores.length; storeIdx++) {
+                        System.out.println(stores[storeIdx]);
+                    }
+                }
 		IoAdapter io =
-			new RecordStoreIoAdapter(256);
+			new RecordStoreIoAdapter(1024);
 			//new MemoryIoAdapter();
 		Db4o.configure().io(io);
-        Db4o.configure().reflectWith(new SelfReflector(new RegressionDogSelfReflectionRegistry()));
-		ObjectContainer db=Db4o.openFile("test.yap");
+                Db4o.configure().reflectWith(new SelfReflector(new RegressionDogSelfReflectionRegistry()));
+                Db4o.configure().objectClass(Animal.class).objectField("_name").indexed(true);
+                long start=System.currentTimeMillis();
+                ObjectContainer db=Db4o.openFile("test.yap");
 		for(int i=0;i<100;i++) {
 			db.set(new Dog("Laika"+i,i));
 		}
 		db.commit();
-//		db.close();
-//		db=Db4o.openFile("test.yap");
+		db.close();
+                System.out.println("Storing 100 dogs took "+(System.currentTimeMillis()-start)+" ms");
+		db=Db4o.openFile("test.yap");
+                start=System.currentTimeMillis();
 		ObjectSet result=db.query(Dog.class);
-		System.out.println(result.size());
+		System.out.println("Found "+result.size()+" dogs");
+                System.out.println("Query took "+(System.currentTimeMillis()-start)+" ms");
+                start=System.currentTimeMillis();
 		while(result.hasNext()) {
-			System.out.println(result.next());
+			result.next();
 		}
+                System.out.println("Activation took "+(System.currentTimeMillis()-start)+" ms");
 		db.close();
 	}
 
--- db4oME/com/db4o/test/RecordStoreIoAdapter.java
+++ db4oME/com/db4o/test/RecordStoreIoAdapter.java
@@ -11,8 +11,6 @@ public class RecordStoreIoAdapter extends IoAdapter {
 	
 	private RecordStore _recordStore;
 	private int _recordSize;
-	private Vector _recordIDs;
-	private long _length;
 	
 	private long _curPos;
 	private byte[] _page;
@@ -23,11 +21,11 @@ public class RecordStoreIoAdapter extends IoAdapter {
 	
 	private RecordStoreIoAdapter(String path,int recordSize) throws RecordStoreException {
 		_recordStore=RecordStore.openRecordStore(path,true);
+                // TODO: if recordSize<=0, read first record and assume its size
 		_recordSize=recordSize;
-		_recordIDs=new Vector();
-		_length=0;
 		_curPos=0;
 		_page=new byte[_recordSize];
+                System.out.println(path+" has "+_recordStore.getNumRecords()+" records");
 	}
 	
 	public void close() throws IOException {
@@ -52,7 +50,12 @@ public class RecordStoreIoAdapter extends IoAdapter {
 	}
 
 	public long getLength() throws IOException {
-		return _length;
+            try {
+                return _recordStore.getNumRecords()*_recordSize;
+            } 
+            catch (RecordStoreNotOpenException ex) {
+                throw new IOException(ex.getMessage());
+            }
 	}
 
 	public IoAdapter open(String path, boolean lockFile, long initialLength) throws IOException {
@@ -64,7 +67,6 @@ public class RecordStoreIoAdapter extends IoAdapter {
 	}
 
 	public int read(byte[] bytes, int length) throws IOException {
-		//System.out.println("READ "+_curPos+","+length);
 		try {
 			int stillToRead=length;
 			int recIdx=recordIndexForPosition(_curPos);
@@ -73,7 +75,6 @@ public class RecordStoreIoAdapter extends IoAdapter {
 				int curRead=loadRecordByIndex(recIdx);
 				int nowToRead=min(stillToRead,curRead-curOffset);
 				System.arraycopy(_page, curOffset, bytes, length-stillToRead, nowToRead);
-				//System.out.println(" readPage "+recIdx+","+curOffset+","+nowToRead);
 				stillToRead-=nowToRead;
 				curOffset=0;
 				recIdx++;
@@ -92,23 +93,21 @@ public class RecordStoreIoAdapter extends IoAdapter {
 	}
 
 	public void write(byte[] buffer, int length) throws IOException {
-		//System.out.println("WRITE "+_curPos+","+length);
 		try {
 			int stillToWrite=length;
 			int recIdx=recordIndexForPosition(_curPos);
-			while(_recordIDs.size()<=recIdx) {
+			while(_recordStore.getNumRecords()<=recIdx) {
 				addPage();
 			}
 			int curOffset=recordOffsetForPosition(_curPos);
 			while(stillToWrite>0) {
-				if(_recordIDs.size()<=recIdx) {
+				if(_recordStore.getNumRecords()<=recIdx) {
 					addPage();
 				}
 				int curRead=loadRecordByIndex(recIdx);
 				int nowToWrite=min(stillToWrite,curRead-curOffset);
 				System.arraycopy(buffer, length-stillToWrite, _page, curOffset, nowToWrite);
 				_recordStore.setRecord(recordIDByIndex(recIdx), _page, 0, _recordSize);
-				//System.out.println(" writePage "+recIdx+","+curOffset+","+nowToWrite);
 				stillToWrite-=nowToWrite;
 				curOffset=0;
 				recIdx++;
@@ -119,9 +118,7 @@ public class RecordStoreIoAdapter extends IoAdapter {
 	}
 
 	private void addPage() throws RecordStoreNotOpenException, RecordStoreException, RecordStoreFullException {
-		int recID=_recordStore.addRecord(_page, 0, _recordSize);
-		_recordIDs.addElement(new Integer(recID));
-		System.out.println(" addPage "+(_recordIDs.size()-1)+":"+recID);
+		_recordStore.addRecord(_page, 0, _recordSize);
 	}
 	
 	private int recordIndexForPosition(long pos) {
@@ -133,13 +130,11 @@ public class RecordStoreIoAdapter extends IoAdapter {
 	}
 
 	private int loadRecordByIndex(int recIdx) throws RecordStoreException {
-		int read=_recordStore.getRecord(recordIDByIndex(recIdx),_page,0);
-		//System.out.println(" read "+read+" for "+recIdx);
-		return read;
+            return _recordStore.getRecord(recordIDByIndex(recIdx),_page,0);
 	}
 
 	private int recordIDByIndex(int recIdx) {
-		return ((Integer)_recordIDs.elementAt(recIdx)).intValue();
+		return recIdx+1;
 	}
 	
 	private int min(int a,int b) {
@@ -147,7 +142,7 @@ public class RecordStoreIoAdapter extends IoAdapter {
 	}
 	
 	public void log() throws RecordStoreException {
-		for(int i=0;i<_recordIDs.size();i++) {
+		for(int i=0;i<_recordStore.getNumRecords();i++) {
 			loadRecordByIndex(i);
 			System.out.println(i);
 			for(int j=0;j<_page.length;j++) {
