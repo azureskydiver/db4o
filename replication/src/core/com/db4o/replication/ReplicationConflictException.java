a6fbc02c1346fc65432551784de7a286516633bc
albert_kwan <albert_kwan@915b37e7-0715-0410-8bc3-b14b59444072>
2006-05-12 07:22:23 +0000

    ak: make sure the build works!

--- replication/src/core/com/db4o/replication/ReplicationConflictException.java
+++ replication/src/core/com/db4o/replication/ReplicationConflictException.java
@@ -4,6 +4,15 @@ package com.db4o.replication;
 
 import com.db4o.ext.Db4oException;
 
+/**
+ * Thrown when a conflict occurs and no ReplicationEventListener is specified.
+ *
+ * @author Albert Kwan
+ * @author Klaus Wuestefeld
+ * @version 1.2
+ * @see ReplicationEventListener
+ * @since dRS 1.2
+ */
 public class ReplicationConflictException extends Db4oException {
 	public ReplicationConflictException(String message) {
 		super(message);
--- replication/src/test/com/db4o/test/replication/ReplicationTestSuite.java
+++ replication/src/test/com/db4o/test/replication/ReplicationTestSuite.java
@@ -1,12 +1,15 @@
 package com.db4o.test.replication;
 
 import com.db4o.test.TestSuite;
+import com.db4o.test.other.ObjectVersionTest;
+import com.db4o.test.replication.collections.Db4oListTest;
+import com.db4o.test.replication.collections.ListTest;
 import com.db4o.test.replication.collections.SimpleArrayTest;
+import com.db4o.test.replication.collections.map.MapTest;
 import com.db4o.test.replication.hibernate.ReplicationConfiguratorTest;
 import com.db4o.test.replication.hibernate.TablesCreatorTest;
-import com.db4o.test.replication.r0tor4.R0to4Runner;
 import com.db4o.test.replication.provider.ReplicationProviderTest;
-import com.db4o.test.other.ObjectVersionTest;
+import com.db4o.test.replication.r0tor4.R0to4Runner;
 
 public class ReplicationTestSuite extends TestSuite {
 	public Class[] tests() {
@@ -15,18 +18,26 @@ public class ReplicationTestSuite extends TestSuite {
 
 	private Class[] all() {
 		return new Class[]{
+				//Simple
 				Simplest.class,
 				ReplicationEventTest.class,
 				ReplicationConfiguratorTest.class,
 				ReplicationProviderTest.class,
-
-				//ListTest.class,
-				//Db4oListTest.class,
-				//MapTest.class,
 				ReplicationAfterDeletionTest.class,
 				SimpleArrayTest.class,
 				SimpleParentChild.class,
-				//SingleTypeCollectionReplicationTest.class,
+
+				//Collection
+				ListTest.class,
+				//Db4oListTest.class,
+				MapTest.class,
+				SingleTypeCollectionReplicationTest.class,
+				ArrayReplicationTest.class,
+				MixedTypesCollectionReplicationTest.class,
+				
+				//Complex
+				R0to4Runner.class,
+				ReplicationFeaturesMain.class,
 
 				//General
 				CollectionHandlerImplTest.class,
@@ -34,19 +45,12 @@ public class ReplicationTestSuite extends TestSuite {
 				TablesCreatorTest.class,
 				GetByUUID.class,
 				DatabaseUnicityTest.class,
-
-				R0to4Runner.class,
-				ArrayReplicationTest.class,
-				MixedTypesCollectionReplicationTest.class,
-				ReplicationFeaturesMain.class,
-
-				//Db4oObjectUpdateTest
 		};
 	}
 
 	private Class[] one() {
 		return new Class[]{
-			ObjectVersionTest.class,
+				ObjectVersionTest.class,
 		};
 	}
 }
