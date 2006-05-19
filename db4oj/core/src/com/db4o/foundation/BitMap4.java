23989b7d635c16f355e50157b67be99efe35e2fe
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2006-05-19 00:32:03 +0000

    rbo: fixed conversion issues

--- db4oj/core/src/com/db4o/foundation/BitMap4.java
+++ db4oj/core/src/com/db4o/foundation/BitMap4.java
@@ -11,10 +11,9 @@ public class BitMap4 {
     private final byte[] _bits;
     
     public BitMap4(int numBits){
-        int byteCount = (numBits + 7) / 8 ;
-        _bits = new byte[byteCount];
+        _bits = new byte[byteCount(numBits)];
     }
-    
+
     /** "readFrom  buffer" constructor **/
     public BitMap4(byte[] buffer, int pos, int numBits){
         this(numBits);
@@ -22,31 +21,38 @@ public class BitMap4 {
     }
     
     public boolean isTrue(int bit) {
-        int offSetInArray = bit / 8;
-        int offSetInByte = bit % 8;
-        return ((_bits[offSetInArray]>>>offSetInByte)&1)!=0;
+        return ((_bits[arrayOffset(bit)]>>>byteOffset(bit))&1)!=0;
     }
-    
+
     public int marshalledLength(){
         return _bits.length;
     }
     
     public void setFalse(int bit){
-        int offSetInArray = bit / 8;
-        int offSetInByte = bit % 8;
-        byte mask=(byte)(1 << offSetInByte);
-        _bits[offSetInArray]&= ~mask;
+        _bits[arrayOffset(bit)] &= (byte)~bitMask(bit);
     }
     
     public void setTrue(int bit){
-        int offSetInArray = bit / 8;
-        int offSetInByte = bit % 8;
-        byte mask=(byte)(1 << offSetInByte);
-        _bits[offSetInArray]|=mask;
+        _bits[arrayOffset(bit)] |= bitMask(bit);
     }
     
     public void writeTo(byte[] bytes, int pos){
         System.arraycopy(_bits, 0, bytes, pos, _bits.length);
     }
+    
+	private byte byteOffset(int bit) {
+		return (byte)(bit % 8);
+	}
 
+	private int arrayOffset(int bit) {
+		return bit / 8;
+	}
+	
+	private byte bitMask(int bit) {
+		return (byte)(1 << byteOffset(bit));
+	}
+	
+	private int byteCount(int numBits) {
+		return (numBits + 7) / 8;
+	}
 }
