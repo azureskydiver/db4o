647735e83bd4f2e6d9fe3b98040727b525f7f0c9
rodrigo_de_oliveira <rodrigo_de_oliveira@915b37e7-0715-0410-8bc3-b14b59444072>
2006-05-26 20:16:47 +0000

    rbo: correct assembly information

--- db4on/instrumentation/Db4oAdmin.Tests/Properties/AssemblyInfo.cs
+++ db4on/instrumentation/Db4oAdmin.Tests/Properties/AssemblyInfo.cs
@@ -2,32 +2,16 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("CFNativeQueriesEnabler.Tests")]
+[assembly: AssemblyTitle("Db4oAdmin.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Flying Circuits")]
-[assembly: AssemblyProduct("CFNativeQueriesEnabler.Tests")]
-[assembly: AssemblyCopyright("Copyright © Flying Circuits 2006")]
+[assembly: AssemblyCompany("db4objects, Inc.")]
+[assembly: AssemblyProduct("db4o - database for objects")]
+[assembly: AssemblyCopyright("Copyright © db4objects, Inc. 2006")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b5fb702b-c4bf-4394-88d4-7baa06db84fe")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
