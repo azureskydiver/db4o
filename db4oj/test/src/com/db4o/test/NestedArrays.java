ab92866fb9b795472543f9a01bcbb0afb717ee20
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-05-27 23:22:38 +0000

    cr: Test case NestedArrays to debug and time the changed marshaller format. The DEPTH parameter in NestedArrays was 7 for the results I posted to the forums. It is now back to 5 so the test runs faster in the regression test setup. It could be potentially extremely slow with our old Client/Server setup. Changing MarshallerFamily.LEGACY to false won't help for that either, since it will send a byte array with 10k back and forth. Enjoy!

---
+++ db4oj/test/src/com/db4o/test/NestedArrays.java
@@ -0,0 +1,113 @@
+/* Copyright (C) 2004 - 2006  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.test;
+
+import java.io.*;
+
+import com.db4o.*;
+
+
+public class NestedArrays {
+    
+    public Object _object;
+    
+    public Object[] _objectArray;
+    
+    private static final int DEPTH = 5;
+    
+    private static final int ELEMENTS = 3;
+    
+    private static final String FILE = "nestedArrays.yap";
+    
+    
+    public NestedArrays(){
+        
+    }
+    
+    
+    public static void main(String[] arguments) {
+        
+        new File(FILE).delete();
+        ObjectContainer oc = Db4o.openFile(FILE);
+        NestedArrays nr = new NestedArrays();
+        nr.storeOne();
+        
+        long storeStart = System.currentTimeMillis();
+        oc.set(nr);
+        long storeStop = System.currentTimeMillis();
+        oc.commit();
+        long commitStop = System.currentTimeMillis();
+        
+        oc.close();
+        Db4o.configure().activationDepth(0);
+        oc = Db4o.openFile(FILE);
+        long loadStart = System.currentTimeMillis();
+        nr = (NestedArrays)oc.get(new NestedArrays()).next();
+        oc.activate(nr, Integer.MAX_VALUE);
+        long loadStop = System.currentTimeMillis();
+        
+        oc.close();
+        
+        long store = storeStop - storeStart;
+        long commit = commitStop - storeStop;
+        long load = loadStop - loadStart;
+        
+        System.out.println(Db4o.version() +  " running com.db4o.test.NestedArrays");
+        System.out.println("store: " + store + "ms");
+        System.out.println("commit: " + commit + "ms");
+        System.out.println("load: " + load + "ms");
+        
+    }
+    
+    
+    public void storeOne(){
+        
+        _object = new Object[ELEMENTS];
+        fill((Object[])_object, DEPTH);
+        
+        _objectArray = new Object[ELEMENTS];
+        fill(_objectArray, DEPTH);
+    }
+    
+    private void fill(Object[] arr, int depth){
+        
+        if(depth <= 0){
+            arr[0] = "somestring";
+            arr[1] = new Integer(10);
+            return;
+        }
+        
+        depth --;
+        
+        for (int i = 0; i < ELEMENTS; i++) {
+            arr[i] = new Object[ELEMENTS];
+            fill((Object[])arr[i], depth );
+        }
+    }
+    
+    public void testOne(){
+        Test.objectContainer().activate(this, Integer.MAX_VALUE);
+        
+        check((Object[])_object, DEPTH);
+        
+        check((Object[])_objectArray, DEPTH);
+        
+        
+    }
+    
+    private void check(Object[] arr, int depth){
+        if(depth <= 0){
+            Test.ensure(arr[0].equals("somestring"));
+            Test.ensure(arr[1].equals(new Integer(10)));
+            return;
+        }
+        
+        depth --;
+        
+        for (int i = 0; i < ELEMENTS; i++) {
+            check((Object[])arr[i], depth );
+        }
+        
+    }
+    
+}
