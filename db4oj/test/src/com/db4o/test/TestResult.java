482afea1a54b0aa841bcda3cefcf73723469e67f
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-06-01 14:25:28 +0000

    pr: quickhack to allow ant regression tests to fail after the suite is run

---
+++ db4oj/test/src/com/db4o/test/TestResult.java
@@ -0,0 +1,29 @@
+package com.db4o.test;
+
+public class TestResult {
+	private int _numFailures;
+	private long _timeTaken;
+	private int _numAssertions;
+
+	public TestResult(int numAssertions,int numFailures, long timeTaken) {
+		_numAssertions=numAssertions;
+		_numFailures = numFailures;
+		_timeTaken=timeTaken;
+	}
+
+	public int numFailures() {
+		return _numFailures;
+	}
+
+	public long timeTaken() {
+		return _timeTaken;
+	}
+
+	public int numAssertions() {
+		return _numAssertions;
+	}
+	
+	public String toString() {
+		return _numFailures+" failures, "+_numAssertions+" assertions, "+_timeTaken+" ms";
+	}
+}
