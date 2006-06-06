f7e0f92ded81d2f5304e7d83ac6b2cfa86e0f9d1
larysa_visengeriyeva <larysa_visengeriyeva@915b37e7-0715-0410-8bc3-b14b59444072>
2006-06-06 18:48:13 +0000

    lv:JDK_5#extendConfiguration() modified,

--- db4ojdk5/core/src/com/db4o/JDK_5.java
+++ db4ojdk5/core/src/com/db4o/JDK_5.java
@@ -3,35 +3,39 @@
 package com.db4o;
 
 import com.db4o.config.*;
+import com.db4o.config.annotations.Annotate;
 import com.db4o.reflect.*;
 import com.db4o.reflect.jdk.*;
 
-
 class JDK_5 extends JDK_1_4 {
-    
-    private static final String ENUM_CLASSNAME = "java.lang.Enum"; 
-    
-    private static ReflectClass enumClass;
-    
-    public Config4Class extendConfiguration(ReflectClass clazz,Configuration config,Config4Class classConfig) {
-    	Class javaClazz=JdkReflector.toNative(clazz);
-    	return null;
-    }
 
-    boolean isEnum(Reflector reflector, ReflectClass claxx) {
-        
-        if(claxx == null){
-            return false;
-        }
-        
-        if(enumClass == null){
-            try {
-                enumClass = reflector.forClass(Class.forName(ENUM_CLASSNAME));
-            } catch (ClassNotFoundException e) {
-                return false;
-            }
-        }
-        
-        return enumClass.isAssignableFrom(claxx);
-    }
+	private static final String ENUM_CLASSNAME = "java.lang.Enum";
+
+	private static ReflectClass enumClass;
+
+	public ObjectClass extendConfiguration(ReflectClass clazz,
+			Configuration config, ObjectClass classConfig) {
+		Class javaClazz = JdkReflector.toNative(clazz);
+
+		return new Annotate(javaClazz, config, classConfig)
+				.reflectAnnotations(javaClazz);
+	}
+
+	boolean isEnum(Reflector reflector, ReflectClass claxx) {
+
+		if (claxx == null) {
+			return false;
+		}
+
+		if (enumClass == null) {
+			try {
+				enumClass = reflector.forClass(Class.forName(ENUM_CLASSNAME));
+			} catch (ClassNotFoundException e) {
+				return false;
+			}
+		}
+
+		return enumClass.isAssignableFrom(claxx);
+	}
+
 }
