4b67f6766abddf66df891fde1724f3b07938ce04
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-06-22 14:23:12 +0000

    cr: Client/Server Performance degrade reason in 5.4 tracked down and fixed: The classOnlyQuery was expecting a BTree on the client. Further to that I made sure that the special client QueryResult implementation is always used where it can be used. This should really speed up things a lot since it sends objects in batches.

--- db4oj/core/src/com/db4o/Db4oVersion.java
+++ db4oj/core/src/com/db4o/Db4oVersion.java
@@ -6,7 +6,7 @@ package com.db4o;
 * @exclude
 */
 public class Db4oVersion {
-    public static final String NAME = "5.4.010";
+    public static final String NAME = "5.4.011";
     public static final int MAJOR = 5;
     public static final int MINOR = 4;
 }
