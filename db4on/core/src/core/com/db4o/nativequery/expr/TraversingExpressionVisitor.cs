15898faee67f93e3c3e558f7ec5fb82cb53f1195
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2006-06-29 22:11:33 +0000

    Fixed NQ runtime optimization for comparisons against enum constants

--- db4on/core/src/core/com/db4o/nativequery/expr/TraversingExpressionVisitor.cs
+++ db4on/core/src/core/com/db4o/nativequery/expr/TraversingExpressionVisitor.cs
@@ -1,6 +1,7 @@
 namespace com.db4o.nativequery.expr
 {
 	public class TraversingExpressionVisitor : com.db4o.nativequery.expr.ExpressionVisitor
+		, com.db4o.nativequery.expr.cmp.ComparisonOperandVisitor
 	{
 		public virtual void Visit(com.db4o.nativequery.expr.AndExpression expression)
 		{
@@ -22,11 +23,66 @@ namespace com.db4o.nativequery.expr
 		public virtual void Visit(com.db4o.nativequery.expr.ComparisonExpression expression
 			)
 		{
+			expression.Left().Accept(this);
+			expression.Right().Accept(this);
 		}
 
 		public virtual void Visit(com.db4o.nativequery.expr.NotExpression expression)
 		{
 			expression.Expr().Accept(this);
+		}
+
+		public virtual void Visit(com.db4o.nativequery.expr.cmp.ArithmeticExpression operand
+			)
+		{
+			operand.Left().Accept(this);
+			operand.Right().Accept(this);
+		}
+
+		public virtual void Visit(com.db4o.nativequery.expr.cmp.ConstValue operand)
+		{
+		}
+
+		public virtual void Visit(com.db4o.nativequery.expr.cmp.FieldValue operand)
+		{
+			operand.Parent().Accept(this);
+		}
+
+		public virtual void Visit(com.db4o.nativequery.expr.cmp.field.CandidateFieldRoot 
+			root)
+		{
+		}
+
+		public virtual void Visit(com.db4o.nativequery.expr.cmp.field.PredicateFieldRoot 
+			root)
+		{
+		}
+
+		public virtual void Visit(com.db4o.nativequery.expr.cmp.field.StaticFieldRoot root
+			)
+		{
+		}
+
+		public virtual void Visit(com.db4o.nativequery.expr.cmp.ArrayAccessValue operand)
+		{
+			operand.Parent().Accept(this);
+			operand.Index().Accept(this);
+		}
+
+		public virtual void Visit(com.db4o.nativequery.expr.cmp.MethodCallValue value)
+		{
+			value.Parent().Accept(this);
+			VisitArgs(value);
+		}
+
+		protected virtual void VisitArgs(com.db4o.nativequery.expr.cmp.MethodCallValue value
+			)
+		{
+			com.db4o.nativequery.expr.cmp.ComparisonOperand[] args = value.Args();
+			for (int i = 0; i < args.Length; ++i)
+			{
+				args[i].Accept(this);
+			}
 		}
 	}
 }
