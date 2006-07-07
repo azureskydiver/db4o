c4f9010bd9b68c877bd173fd23e1d0aa6b8bcb61
tetyana_loskutova <tetyana_loskutova@915b37e7-0715-0410-8bc3-b14b59444072>
2006-07-07 16:02:58 +0000

    Tutorial for index tuning chapter. VB

--- db4on/tutorial/db4o-tutorial-console/src/App.cs
+++ db4on/tutorial/db4o-tutorial-console/src/App.cs
@@ -13,6 +13,11 @@ namespace db4otutorialrunner
 			com.db4o.f1.chapter4.InheritanceExample.Main(args);
 			com.db4o.f1.chapter5.ClientServerExample.Main(args);
 			com.db4o.f1.chapter6.TranslatorExample.Main(args);
+			com.db4o.f1.chapter21.IndexedExample.fillUpDB();
+			com.db4o.f1.chapter21.IndexedExample.noIndex();
+			com.db4o.f1.chapter21.IndexedExample.fullIndex();
+			com.db4o.f1.chapter21.IndexedExample.pilotIndex();
+			com.db4o.f1.chapter21.IndexedExample.pointsIndex();
 		}
 	}
 }
