1b9db13fb102ac662a60e2cfdb8d3a722e3f9ca6
tetyana_loskutova <tetyana_loskutova@915b37e7-0715-0410-8bc3-b14b59444072>
2006-07-07 16:02:58 +0000

    Tutorial for index tuning chapter. VB

--- db4on/core/src/core/com/db4o/Db4oVersion.cs
+++ db4on/core/src/core/com/db4o/Db4oVersion.cs
@@ -3,10 +3,10 @@ namespace com.db4o
 	/// <exclude></exclude>
 	public class Db4oVersion
 	{
-		public static readonly string NAME = "5.4.012";
+		public static readonly string NAME = "5.5.1";
 
 		public const int MAJOR = 5;
 
-		public const int MINOR = 4;
+		public const int MINOR = 5;
 	}
 }
