b6f4109b32d847fe5d689d1f6a0b524ca4d91e34
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-07-26 16:25:08 +0000

    cr: Recent tutorial changes rolled back to 2885, so the continous build keeps on building a full tutorial and documentation. I have a local backup of the thinned out tutorial to be able to reapply, once our reference documentation is ready for deployment.

---
+++ tutorial/src/com/tutorial/f1/chapter31/IndexDiagListener.java
@@ -0,0 +1,19 @@
+package com.tutorial.f1.chapter31;
+
+import com.db4o.diagnostic.*;
+
+public class IndexDiagListener  implements DiagnosticListener
+{
+	   public void onDiagnostic(Diagnostic d) {
+		   if (d.getClass().equals(LoadedFromClassIndex.class)){
+	        System.out.println(d.toString());
+		   }
+		   /*if (d.getClass().getName().equals("com.db4o.diagnostic.LoadedFromClassIndex")){
+			   System.out.println("Class name match "+ d.getClass().getName());
+		       System.out.println(d.toString());
+		   } else {
+			   System.out.println("Class name = "+ d.getClass().getName());
+			   
+		   }*/
+	    }
+}
