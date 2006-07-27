d4f4d4f9510dc37adbacb21fed2a02e714a679ad
jbevain <jbevain@915b37e7-0715-0410-8bc3-b14b59444072>
2006-07-27 10:58:52 +0000

    implement objectOnNew

--- db4oj/core/src/com/db4o/events/impl/EventRegistryImpl.java
+++ db4oj/core/src/com/db4o/events/impl/EventRegistryImpl.java
@@ -13,6 +13,7 @@ public class EventRegistryImpl implements Callbacks, EventRegistry {
 	private final Event4Impl _objectCanActivate = new Event4Impl();
 	
 	private final Event4Impl _objectOnActivate = new Event4Impl();
+	private final Event4Impl _objectOnNew = new Event4Impl();
 
 	// Callbacks implementation
 	public void onQueryFinished(Query query) {
@@ -40,6 +41,10 @@ public class EventRegistryImpl implements Callbacks, EventRegistry {
 	public void objectOnActivate(Object obj) {
 		_objectOnActivate.trigger(new ObjectEventArgsImpl(obj));
 	}
+	
+	public void objectOnNew(Object obj) {
+		_objectOnNew.trigger(new ObjectEventArgsImpl(obj));
+	}
 
 	// EventRegistry implementation
 	public Event4 queryFinished() {
@@ -60,5 +65,9 @@ public class EventRegistryImpl implements Callbacks, EventRegistry {
 
 	public Event4 objectOnActivate() {
 		return _objectOnActivate;
+	}
+
+	public Event4 objectOnNew() {
+		return _objectOnNew;
 	}
 }
