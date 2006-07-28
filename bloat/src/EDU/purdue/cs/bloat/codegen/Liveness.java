e73659b5772b9583d4a597068f0511ae219f3046
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-07-28 23:23:06 +0000

    switched bsd style license comments to lgpl style license comments

--- bloat/src/EDU/purdue/cs/bloat/codegen/Liveness.java
+++ bloat/src/EDU/purdue/cs/bloat/codegen/Liveness.java
@@ -2,26 +2,20 @@
  * All files in the distribution of BLOAT (Bytecode Level Optimization and
  * Analysis tool for Java(tm)) are Copyright 1997-2001 by the Purdue
  * Research Foundation of Purdue University.  All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * <p>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that this entire copyright notice is duplicated in all
- * such copies, and that any documentation, announcements, and other
- * materials related to such distribution and use acknowledge that the
- * software was developed at Purdue University, West Lafayette, IN by
- * Antony Hosking, David Whitlock, and Nathaniel Nystrom.  No charge
- * may be made for copies, derivations, or distributions of this
- * material without the express written consent of the copyright
- * holder.  Neither the name of the University nor the name of the
- * author may be used to endorse or promote products derived from this
- * material without specific prior written permission.  THIS SOFTWARE
- * IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR ANY PARTICULAR PURPOSE.
- *
- * <p>
- * Java is a trademark of Sun Microsystems, Inc.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 package EDU.purdue.cs.bloat.codegen;
