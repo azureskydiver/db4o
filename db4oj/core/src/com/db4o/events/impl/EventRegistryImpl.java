7c1331a2931ac3be96cd6807dd065f0d99bf9a65
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2006-07-29 18:46:35 +0000

    renamed fields to match event name

--- db4oj/core/src/com/db4o/events/impl/EventRegistryImpl.java
+++ db4oj/core/src/com/db4o/events/impl/EventRegistryImpl.java
@@ -14,17 +14,17 @@ public class EventRegistryImpl implements Callbacks, EventRegistry {
 	private final Event4Impl _queryStarted = new Event4Impl();
 	private final Event4Impl _queryFinished = new Event4Impl();
 	
-	private final Event4Impl _objectCanNew = new Event4Impl();
-	private final Event4Impl _objectCanActivate = new Event4Impl();
-	private final Event4Impl _objectCanUpdate = new Event4Impl();
-	private final Event4Impl _objectCanDelete = new Event4Impl();
-	private final Event4Impl _objectCanDeactivate = new Event4Impl();
+	private final Event4Impl _creating = new Event4Impl();
+	private final Event4Impl _activating = new Event4Impl();
+	private final Event4Impl _updating = new Event4Impl();
+	private final Event4Impl _deleting = new Event4Impl();
+	private final Event4Impl _deactivating = new Event4Impl();
 	
-	private final Event4Impl _objectOnNew = new Event4Impl();
-	private final Event4Impl _objectOnActivate = new Event4Impl();
-	private final Event4Impl _objectOnUpdate = new Event4Impl();
-	private final Event4Impl _objectOnDelete = new Event4Impl();
-	private final Event4Impl _objectOnDeactivate = new Event4Impl();
+	private final Event4Impl _created = new Event4Impl();
+	private final Event4Impl _activated = new Event4Impl();
+	private final Event4Impl _updated = new Event4Impl();
+	private final Event4Impl _deleted = new Event4Impl();
+	private final Event4Impl _deactivated = new Event4Impl();
 
 	// Callbacks implementation
 	public void onQueryFinished(Query query) {
@@ -46,43 +46,43 @@ public class EventRegistryImpl implements Callbacks, EventRegistry {
 	}
 	
 	public boolean objectCanNew(Object obj) {
-		return triggerCancellableEvent(_objectCanNew, obj);
+		return triggerCancellableEvent(_creating, obj);
 	}
 	
 	public boolean objectCanActivate(Object obj) {
-		return triggerCancellableEvent(_objectCanActivate, obj);
+		return triggerCancellableEvent(_activating, obj);
 	}
 	
 	public boolean objectCanUpdate(Object obj) {
-		return triggerCancellableEvent(_objectCanUpdate, obj);
+		return triggerCancellableEvent(_updating, obj);
 	}
 	
 	public boolean objectCanDelete(Object obj) {
-		return triggerCancellableEvent(_objectCanDelete, obj);
+		return triggerCancellableEvent(_deleting, obj);
 	}
 	
 	public boolean objectCanDeactivate(Object obj) {
-		return triggerCancellableEvent(_objectCanDeactivate, obj);
+		return triggerCancellableEvent(_deactivating, obj);
 	}
 	
 	public void objectOnActivate(Object obj) {
-		triggerObjectEvent(_objectOnActivate, obj);
+		triggerObjectEvent(_activated, obj);
 	}
 	
 	public void objectOnNew(Object obj) {
-		triggerObjectEvent(_objectOnNew, obj);
+		triggerObjectEvent(_created, obj);
 	}
 	
 	public void objectOnUpdate(Object obj) {
-		triggerObjectEvent(_objectOnUpdate, obj);
+		triggerObjectEvent(_updated, obj);
 	}
 	
 	public void objectOnDelete(Object obj) {
-		triggerObjectEvent(_objectOnDelete, obj);		
+		triggerObjectEvent(_deleted, obj);		
 	}	
 
 	public void objectOnDeactivate(Object obj) {
-		triggerObjectEvent(_objectOnDeactivate, obj);
+		triggerObjectEvent(_deactivated, obj);
 	}
 
 	// EventRegistry implementation
@@ -95,42 +95,42 @@ public class EventRegistryImpl implements Callbacks, EventRegistry {
 	}
 	
 	public Event4 creating() {
-		return _objectCanNew;
+		return _creating;
 	}
 	
 	public Event4 activating() {
-		return _objectCanActivate;
+		return _activating;
 	}
 	
 	public Event4 updating() {
-		return _objectCanUpdate;
+		return _updating;
 	}
 	
 	public Event4 deleting() {
-		return _objectCanDelete;
+		return _deleting;
 	}
 	
 	public Event4 deactivating() {
-		return _objectCanDeactivate;
+		return _deactivating;
 	}
 	
 	public Event4 created() {
-		return _objectOnNew;
+		return _created;
 	}
 	
 	public Event4 activated() {
-		return _objectOnActivate;
+		return _activated;
 	}
 	
 	public Event4 updated() {
-		return _objectOnUpdate;
+		return _updated;
 	}
 
 	public Event4 deleted() {
-		return _objectOnDelete;
+		return _deleted;
 	}
 
 	public Event4 deactivated() {
-		return _objectOnDeactivate;
+		return _deactivated;
 	}
 }
