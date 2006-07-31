57c09206179cb865256506853fbad26d6782c75c
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2006-07-31 18:53:53 +0000

    COR-9 - keep track of activated objects during query execution

--- db4oj/test/src/com/db4o/db4ounit/QueryStatisticsTestCase.java
+++ db4oj/test/src/com/db4o/db4ounit/QueryStatisticsTestCase.java
@@ -3,6 +3,7 @@
 package com.db4o.db4ounit;
 
 import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
 import com.db4o.events.*;
 import com.db4o.foundation.StopWatch;
 import com.db4o.query.Query;
@@ -16,52 +17,99 @@ import db4ounit.db4o.fixtures.Db4oSolo;
 public class QueryStatisticsTestCase extends Db4oTestCase {
 	
 	private static final int ITEM_COUNT = 10000;
+	private QueryTracker _tracker;
 
 	protected void store() {
 		for (int i=0; i<ITEM_COUNT; ++i) {
 			db().set(new Item(i));
 		}
 	}
+	
+	public void setUp() throws Exception {
+		super.setUp();
+		
+		_tracker = new QueryTracker();		
+		_tracker.connect(db());
+	}
+	
+	public void tearDown() throws Exception {
+		
+		_tracker.disconnect();
+		
+		super.tearDown();
+	}
 
 	/**
 	 * Keeps track of query statistics.
 	 */
-	static class QueryTracker implements EventListener4 {
+	static class QueryTracker {	
 		
-		StopWatch _watch = new StopWatch();
-		EventRegistry _registry = null;
-
-		public void onEvent(Event4 e, EventArgs args) {
-			if (e == _registry.queryStarted()) {
+		private EventRegistry _registry = null;
+		
+		private int _activationCount;
+		
+		private final StopWatch _watch = new StopWatch();
+		
+		private final EventListener4 _queryStarted = new EventListener4() {			
+			public void onEvent(Event4 e, EventArgs args) {
+				_activationCount = 0;
 				_watch.start();
-			} else if (e == _registry.queryFinished()) {
+			}			
+		};
+		
+		private final EventListener4 _queryFinished = new EventListener4() {
+			public void onEvent(Event4 e, EventArgs args) {
 				_watch.stop();
 			}
-		}
+		};
 		
+		private final EventListener4 _activated = new EventListener4() {
+			public void onEvent(Event4 e, EventArgs args) {
+				++_activationCount;
+			};
+		};
+
 		public long executionTime() {
 			return _watch.elapsed();
+		}
+		
+		public int activationCount() {
+			return _activationCount;
 		}
 
 		public void connect(ObjectContainer container) {
 			_registry = EventRegistryFactory.forObjectContainer(container);
-			_registry.queryStarted().addListener(this);
-			_registry.queryFinished().addListener(this);
+			_registry.queryStarted().addListener(_queryStarted);
+			_registry.queryFinished().addListener(_queryFinished);
+			_registry.activated().addListener(_activated);
 		}
 		
 		public void disconnect() {
 			if (null != _registry) {
-				_registry.queryStarted().removeListener(this);
-				_registry.queryFinished().removeListener(this);
+				_registry.queryStarted().removeListener(_queryStarted);
+				_registry.queryFinished().removeListener(_queryFinished);
+				_registry.activated().removeListener(_activated);
 				_registry = null;
 			}
 		}
 	}
+	
+	public void testActivationCount() {
+		
+		Query q = db().query();		
+		q.constrain(Item.class);
+		
+		ObjectSet result = q.execute();
+		
+		Assert.areEqual(0, _tracker.activationCount());		
+		result.next();		
+		Assert.areEqual(1, _tracker.activationCount());
+		result.next();
+		Assert.areEqual(2, _tracker.activationCount());
+		
+	}
 
 	public void testExecutionTime() {
-		
-		QueryTracker listener = new QueryTracker();		
-		listener.connect(db());
 		
 		Query q = db().query();		
 		q.constrain(Item.class);		
@@ -69,8 +117,8 @@ public class QueryStatisticsTestCase extends Db4oTestCase {
 		long started = System.currentTimeMillis();		
 		q.execute();		
 		long elapsed = System.currentTimeMillis() - started;
-		Assert.isTrue(listener.executionTime() > 0);
-		Assert.isTrue(listener.executionTime() <= elapsed);
+		Assert.isTrue(_tracker.executionTime() > 0);
+		Assert.isTrue(_tracker.executionTime() <= elapsed);
 	}
 
 	public static void main(String[] args) {
