369019f58ae2de8bf1a7949883ffdbe72bfdf7a0
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-08-03 22:28:36 +0000

    cr: New test case for readding deleted objects.

---
+++ db4oj/test/src/com/db4o/db4ounit/assorted/AllTests.java
@@ -0,0 +1,29 @@
+/* Copyright (C) 2004 - 2006 db4objects Inc. http://www.db4o.com */
+
+package com.db4o.db4ounit.assorted;
+
+import db4ounit.TestRunner;
+import db4ounit.TestSuite;
+import db4ounit.TestSuiteBuilder;
+import db4ounit.db4o.Db4oTestCase;
+import db4ounit.db4o.Db4oTestSuiteBuilder;
+import db4ounit.db4o.fixtures.Db4oSolo;
+
+
+public class AllTests extends Db4oTestCase implements TestSuiteBuilder {
+
+	public TestSuite build() {
+		return new Db4oTestSuiteBuilder(
+				fixture(),
+				new Class[] {
+                    ReaddCascadedDelete.class
+				}).build();
+	}
+	
+	public static void main(String[] args) {
+        new TestRunner(
+                new Db4oTestSuiteBuilder(
+                        new Db4oSolo(),
+                        AllTests.class)).run();
+    }
+}
