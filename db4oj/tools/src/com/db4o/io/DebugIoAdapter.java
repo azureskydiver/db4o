b694aadd833c84fd8fd0738bc2949115b4f4de3e
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-08-05 20:04:46 +0000

    cr: Simple extension of VanillaIoAdapter, helped me to debug, maybe it's good for someone else too.

---
+++ db4oj/tools/src/com/db4o/io/DebugIoAdapter.java
@@ -0,0 +1,35 @@
+/* Copyright (C) 2006  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.io;
+
+import java.io.*;
+
+
+public class DebugIoAdapter extends VanillaIoAdapter{
+    
+    public static int counter;
+    
+    private static final int[] RANGE_OF_INTEREST = new int[] {0, 20};
+    
+
+    public DebugIoAdapter(IoAdapter delegateAdapter){
+        super(delegateAdapter);
+    }
+    
+    protected DebugIoAdapter(IoAdapter delegateAdapter, String path, boolean lockFile, long initialLength) throws IOException {
+        super(delegateAdapter.open(path, lockFile, initialLength));
+    }
+
+    public IoAdapter open(String path, boolean lockFile, long initialLength) throws IOException {
+        return new DebugIoAdapter(new RandomAccessFileAdapter(),  path, lockFile, initialLength);
+    }
+    
+    public void seek(long pos) throws IOException {
+        if(pos >= RANGE_OF_INTEREST[0] && pos <= RANGE_OF_INTEREST[1]){
+            counter ++;
+            System.out.println("seek: " + pos + "  counter: " + counter);
+        }
+        super.seek(pos);
+    }
+
+}
