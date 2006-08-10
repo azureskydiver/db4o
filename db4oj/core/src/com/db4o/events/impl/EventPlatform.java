c326f22105ec8af1cf72e9c3e73b90c8952c0357
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2006-08-10 21:37:02 +0000

    simpler version

--- db4oj/core/src/com/db4o/events/impl/EventPlatform.java
+++ db4oj/core/src/com/db4o/events/impl/EventPlatform.java
@@ -3,28 +3,23 @@
 package com.db4o.events.impl;
 
 import com.db4o.events.CancellableObjectEventArgs;
-import com.db4o.events.EventArgs;
 import com.db4o.events.ObjectEventArgs;
 import com.db4o.events.QueryEventArgs;
 import com.db4o.query.Query;
 
 class EventPlatform {
 
-	protected static void triggerQueryEvent(Event4Impl e, Query q) {
-		triggerEvent(e, new QueryEventArgs(q));
+	public static void triggerQueryEvent(Event4Impl e, Query q) {
+		e.trigger(new QueryEventArgs(q));
 	}
 	
-	protected static boolean triggerCancellableObjectEventArgs(Event4Impl e, Object o) {
-		CancellableObjectEventArgs coea = new CancellableObjectEventArgs(o);
-		triggerEvent(e, coea);
-		return !coea.isCancelled();
+	public static boolean triggerCancellableObjectEventArgs(Event4Impl e, Object o) {
+		CancellableObjectEventArgs args = new CancellableObjectEventArgs(o);
+		e.trigger(args);
+		return !args.isCancelled();
 	}
 	
-	protected static void triggerObjectEvent(Event4Impl e, Object o) {
-		triggerEvent(e, new ObjectEventArgs(o));
-	}
-	
-	private static void triggerEvent(Event4Impl e, EventArgs ea) {
-		e.trigger(ea);
+	public static void triggerObjectEvent(Event4Impl e, Object o) {
+		e.trigger(new ObjectEventArgs(o));
 	}
 }
