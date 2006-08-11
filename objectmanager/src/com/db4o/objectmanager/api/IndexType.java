75b5f46622ac935e309e1156e6ead347687fb76b
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-08-11 16:49:40 +0000

    unmodifiable list of enum members

--- objectmanager/src/com/db4o/objectmanager/api/IndexType.java
+++ objectmanager/src/com/db4o/objectmanager/api/IndexType.java
@@ -12,12 +12,9 @@ public final class IndexType {
 	public final static IndexType FLAT=new IndexType("Flat");
 	public final static IndexType BTREE=new IndexType("BTree");
 	
-	private final static IndexType[] ALL={FLAT,BTREE};
+	public final static List ALL=
+			Collections.unmodifiableList(Arrays.asList(new IndexType[]{FLAT,BTREE}));
 
-	public static List all() {
-		return Arrays.asList(ALL);
-	}
-	
 	private String _name;
 	
 	private IndexType(String name) {
