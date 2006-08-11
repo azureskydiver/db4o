62ccc9d0947d1a9777e5cadafe31b811f0292be9
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-08-11 16:34:52 +0000

    reverted to JDK1.4 style

--- objectmanager/src/com/db4o/objectmanager/api/IndexType.java
+++ objectmanager/src/com/db4o/objectmanager/api/IndexType.java
@@ -1,11 +1,30 @@
 package com.db4o.objectmanager.api;
 
+import java.util.*;
+
 /**
  * User: treeder
  * Date: Aug 7, 2006
  * Time: 10:40:11 AM
  */
-public enum IndexType {
-    Flat,
-    Btree
+public final class IndexType {
+
+	public final static IndexType FLAT=new IndexType("Flat");
+	public final static IndexType BTREE=new IndexType("BTree");
+	
+	private final static IndexType[] ALL={FLAT,BTREE};
+
+	public static List all() {
+		return Arrays.asList(ALL);
+	}
+	
+	private String _name;
+	
+	private IndexType(String name) {
+		_name=name;
+	}
+	
+	public String name() {
+		return _name;
+	}
 }
