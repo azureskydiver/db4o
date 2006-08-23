8c1c9a50ee2f6b2896ea212039c931e3161dcffd
albert_kwan <albert_kwan@915b37e7-0715-0410-8bc3-b14b59444072>
2006-08-23 11:25:29 +0000

    ak: change class to class_name, remove drs prefix of column

--- replication/src/test/com/db4o/test/replication/AllTestsReplication.java
+++ replication/src/test/com/db4o/test/replication/AllTestsReplication.java
@@ -42,11 +42,11 @@ public class AllTestsReplication extends AllTests {
 		// In C/S, you can't run all combinations together, it causes db4o connection to timeout.
 
 		//db4o();
-		transients();
+		//transients();
 		//hsql();
 		//db4otransient();
 		//hsqlDb4o();
-		//db4oHsql();
+		db4oHsql();
 
 		//oracle();
 		//mysql();
