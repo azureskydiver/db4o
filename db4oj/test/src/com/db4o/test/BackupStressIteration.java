5f17951b02ef74997d535ee1feea5cca2063a210
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-08-24 15:55:38 +0000

    cr: #COR-114 Stress test that documents failing backup.

---
+++ db4oj/test/src/com/db4o/test/BackupStressIteration.java
@@ -0,0 +1,24 @@
+/* Copyright (C) 2006  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.test;
+
+
+public class BackupStressIteration {
+    
+    public int _iteration;
+    
+    
+    public BackupStressIteration() {
+     
+    }
+
+    public void setIteration(int iteration) {
+        _iteration = iteration;
+    }
+
+
+    public int getIteration() {
+        return _iteration;
+    }
+
+}
