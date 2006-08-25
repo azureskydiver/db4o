9859800db0de4217830b76f475a687bbd9b17d99
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2006-08-25 17:31:08 +0000

    removed unused from File4 and simplified the API a bit (having File4.copy as a static method will also make it easier to convert the test cases)

--- db4oj/test/src/com/db4o/test/lib/File4.java
+++ db4oj/test/src/com/db4o/test/lib/File4.java
@@ -3,64 +3,22 @@
 package com.db4o.test.lib;
 
 import java.io.*;
-import java.util.zip.*;
 
-/**
- * sorry, very very old code, recently included for crash simulator,
- * needs fix and look over
- */
-public class File4 extends java.io.File {
-    public File4(String path) {
-        super(path);
-    }
+public class File4 {
 
-    public File4(String path, String file) {
-        this(assemble(path, file));
-    }
-
-    public static String assemble(String pathName, String fileName) {
-        return String4._right(pathName, separator)
-            ? pathName + fileName
-            : pathName + separator + fileName;
-    }
-
-    public File4 copy(String toPath, String filter) {
+    public static void copy(String source, String target) {
         try {
-            new File4(toPath).mkdirs();
-            new File4(toPath).delete();
-
-            if (isDirectory()) {
-                String[] files = list();
-                if (files != null) {
-                    for (int i = 0; i < files.length; i++) {
-                        if (filter == null || String4._right(files[i], filter)) {
-                            File4 child = new File4(getAbsolutePath(), files[i]);
-                            child.copy(assemble(toPath, files[i]), filter);
-                        }
-                    }
-                }
+        	java.io.File sourceFile = new java.io.File(source);
+        	
+            java.io.File targetFile = new java.io.File(target);
+			targetFile.mkdirs();
+            targetFile.delete();
+            
+            if (sourceFile.isDirectory()) {
+                copyDirectory(sourceFile, targetFile);
             } else {
-
-                final int bufferSize = 64000;
-
-                RandomAccessFile rafIn = new RandomAccessFile(getAbsolutePath(), "r");
-                RandomAccessFile rafOut = new RandomAccessFile(toPath, "rw");
-                long len = rafIn.length();
-                byte[] bytes = new byte[bufferSize];
-
-                while (len > 0) {
-                    len -= bufferSize;
-                    if (len < 0) {
-                        bytes = new byte[(int) (len + bufferSize)];
-                    }
-                    rafIn.read(bytes);
-                    rafOut.write(bytes);
-                }
-
-                rafIn.close();
-                rafOut.close();
+                copyFile(sourceFile, targetFile);
             }
-            return new File4(toPath);
         } catch (Exception e) {
             System.out.println("File.copy failed.");
             e.printStackTrace();
@@ -68,140 +26,40 @@ public class File4 extends java.io.File {
         }
     }
 
-    public File4 copy(String toPath) {
-        return copy(toPath, null);
+	public static void copyFile(File source, File target) throws IOException {
+		final int bufferSize = 64000;
+
+		RandomAccessFile rafIn = new RandomAccessFile(source.getAbsolutePath(), "r");
+		RandomAccessFile rafOut = new RandomAccessFile(target.getAbsolutePath(), "rw");
+		long len = rafIn.length();
+		byte[] bytes = new byte[bufferSize];
+
+		while (len > 0) {
+		    len -= bufferSize;
+		    if (len < 0) {
+		        bytes = new byte[(int) (len + bufferSize)];
+		    }
+		    rafIn.read(bytes);
+		    rafOut.write(bytes);
+		}
+
+		rafIn.close();
+		rafOut.close();
+	}
+
+	private static void copyDirectory(File source, File target) throws IOException {
+		String[] files = source.list();
+		if (files != null) {
+		    for (int i = 0; i < files.length; i++) {
+		        copy(assemble(source.getAbsolutePath(), files[i]),
+		        	assemble(target.getAbsolutePath(), files[i]));
+		    }
+		}
+	}
+	
+    private static String assemble(String pathName, String fileName) {
+        return String4._right(pathName, java.io.File.separator)
+            ? pathName + fileName
+            : pathName + java.io.File.separator + fileName;
     }
-
-    public boolean delete() {
-        return delete(null);
-    }
-
-    public boolean delete(String filter) {
-        String[] files = list();
-        if (files != null) {
-            for (int i = 0; i < files.length; i++) {
-                File4 child = new File4(getAbsolutePath(), files[i]);
-                if (child.isDirectory() || filter == null || String4._right(files[i], filter)) {
-                    child.delete(filter);
-                }
-                new File4(getAbsolutePath(), files[i]).delete(filter);
-            }
-        }
-        if (filter == null || String4._right(getName(), filter)) {
-        	return super.delete();
-        }else{
-        	return false;
-        }
-    }
-    
-    public String directory(){
-    	String absolute = getAbsolutePath(); 
-    	if(isDirectory()){
-    		return absolute;
-    	}
-    	String4 s4 = new String4(absolute);
-    	s4.splitRight(separator);
-    	return s4.toString();
-    }
-
-    public String list(String filter) {
-        StringBuffer sb = new StringBuffer();
-        list(sb, filter);
-        return sb.toString();
-    }
-
-    private void list(StringBuffer sb, String filter) {
-        if (isDirectory()) {
-            String[] files = list();
-            if (files != null) {
-                for (int i = 0; i < files.length; i++) {
-                    File4 child = new File4(getAbsolutePath(), files[i]);
-                    if (child.isDirectory()
-                        || filter == null
-                        || String4._right(files[i], filter)) {
-                        child.list(sb, filter);
-                    }
-                }
-            }
-        } else {
-            sb.append(getAbsolutePath());
-            sb.append("\r\n");
-        }
-    }
-
-    public void replace(String a_replace, String a_with) {
-        String path = getAbsolutePath();
-        String4 str = null;
-        RandomAccessFile raf = null;
-        int len = 0;
-        byte l_bytes[] = null;
-        try {
-            if (!exists()) {
-                throw new Exception();
-            }
-            raf = new RandomAccessFile(path, "rw");
-        } catch (Exception e) {
-            System.out.println("Failed to open file: " + path);
-            return;
-        }
-
-        try {
-            len = (int) raf.length();
-            l_bytes = new byte[len];
-            raf.read(l_bytes, 0, len);
-            raf.close();
-        } catch (Exception e) {
-            System.out.println("File read denied:" + path);
-            return;
-        }
-
-        str = new String4(new String(l_bytes));
-        str.replace(a_replace, a_with);
-        l_bytes = str.getString().getBytes();
-
-        try {
-            new File4(path).delete();
-        } catch (Exception e) {
-            System.out.println("File access denied: " + path);
-            return;
-        }
-
-        try {
-            raf = new RandomAccessFile(path, "rw");
-            raf.write(l_bytes);
-            raf.close();
-        } catch (Exception e) {
-            System.out.println("File access denied: " + path);
-            return;
-        }
-    }
-    
-    public void unzip() throws IOException, FileNotFoundException{
-    	
-    	int BLOCK_LENGTH = 8000;
-    	int read;
-    	byte[] bytes = new byte[BLOCK_LENGTH];
-    	
-    	ZipInputStream zis = new ZipInputStream(new FileInputStream(this));
-    	ZipEntry ze = zis.getNextEntry();
-    	while(ze != null){
-    		File file = new File(this.getParent(), ze.getName());
-            parentFile(file).mkdirs();
-    		FileOutputStream fos = new FileOutputStream(file);
-    		while((read = zis.read(bytes,0,BLOCK_LENGTH)) > 0){
-    			fos.write(bytes,0, read);
-    		}
-    		fos.flush();
-    		fos.close();
-    		ze = zis.getNextEntry();
-    	}
-    	zis.close();
-    }
-    
-    private File parentFile(File forFile) {
-        String path = forFile.getParent();
-        if (path == null) return null;
-        return new File(path);
-     }
-
 }
