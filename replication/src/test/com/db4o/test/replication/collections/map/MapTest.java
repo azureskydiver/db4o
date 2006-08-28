812f95c7420e200e7f4234a79635e005c3fb4d3e
albert_kwan <albert_kwan@915b37e7-0715-0410-8bc3-b14b59444072>
2006-08-28 08:59:12 +0000

    ak: #DRS-8 transients passed MapTest

--- replication/src/test/com/db4o/test/replication/collections/map/MapTest.java
+++ replication/src/test/com/db4o/test/replication/collections/map/MapTest.java
@@ -9,36 +9,139 @@ import com.db4o.test.Test;
 import com.db4o.test.replication.ReplicationTestCase;
 
 public class MapTest extends ReplicationTestCase {
-
+	static final MapKey[] EXP_2_KEYS = new MapKey[]{new MapKey("key1"), new MapKey("key2")};
+	
+	static final MapContent[] EXP_2_VALUES = new MapContent[]{new MapContent("c1"), new MapContent("c2")};
+	static final MapContent[] EXP_2_VALUES_UPDATED = new MapContent[]{new MapContent("co1"), new MapContent("co2")};
+	
+	static final MapKey[] EXP_3_KEYS = new MapKey[]{new MapKey("key1"), new MapKey("key2"), new MapKey("key3")};
+	static final MapContent[] EXP_3_VALUES = new MapContent[]{new MapContent("co1"),new MapContent("co2"), new MapContent("co3")};
+	
 	protected void actualTest() {
 
 		storeMapToProviderA();
 
 		replicateAllToProviderBFirstTime();
 
-		//modifyInProviderB();
+		modifyInProviderB();
 
-		//replicateAllStep2();
+		replicateAllStep2();
 
-		//addElementInProviderA();
+		addElementInProviderA();
 
-		//replicateHolderStep3();
+		replicateHolderStep3();
+	}
+	
+	protected MapHolder createMapHolder() {
+		MapHolder mh = new MapHolder("h1");
+		mh.setMap(new HashMap());
+		return mh;
 	}
 	
 	private void storeMapToProviderA() {
+		newStore();
+	}
 
+	private void oldStore() {
 		MapHolder mh = createMapHolder();
+		
 		MapContent mc1 = new MapContent("c1");
 		MapContent mc2 = new MapContent("c2");
-		mh.put(new MapKey("key1"), mc1);
+		final MapKey key1 = new MapKey("key1");
+		_providerA.storeNew(key1);
+		mh.put(key1, mc1);
+		
 		mh.put(new MapKey("key2"), mc2);
 		_providerA.storeNew(mh);
 		_providerA.commit();
 
-		final MapKey[] keys = new MapKey[]{new MapKey("key1"), new MapKey("key2")};
-		final MapContent[] values = new MapContent[]{new MapContent("c1"), new MapContent("c2")};
-		ensureContent(_providerA, "h1", keys, 
-				values);
+		ensureContent(_providerA, "h1", EXP_2_KEYS, EXP_2_VALUES);
+	}
+
+	private void newStore() {
+		final MapKey key1 = new MapKey("key1");
+		_providerA.storeNew(key1);
+		
+		final MapKey key2 = new MapKey("key2");
+		_providerA.storeNew(key2);
+		
+		MapContent c1 = new MapContent("c1");
+		_providerA.storeNew(c1);
+		
+		MapContent c2 = new MapContent("c2");
+		_providerA.storeNew(c2);
+
+		MapHolder mh = createMapHolder();
+		mh.put(key1, c1);
+		mh.put(key2, c2);
+
+		_providerA.storeNew(mh);
+		_providerA.commit();
+
+		ensureContent(_providerA, "h1", EXP_2_KEYS, EXP_2_VALUES);
+	}
+
+	private void replicateAllToProviderBFirstTime() {
+		replicateClasses(_providerA, _providerB, new Class[]{MapKey.class, MapContent.class, MapHolder.class});
+				
+		ensureContent(_providerA, "h1", EXP_2_KEYS, EXP_2_VALUES);
+		ensureContent(_providerB, "h1", EXP_2_KEYS, EXP_2_VALUES);
+	}
+
+	
+
+	private void modifyInProviderB() {
+		MapHolder mh = (MapHolder) getOneInstance(_providerB, MapHolder.class);
+
+		mh.setName("h2");
+		MapContent mc1 = (MapContent) mh.getMap().get(new MapKey("key1"));
+		MapContent mc2 = (MapContent) mh.getMap().get(new MapKey("key2"));
+		mc1.name= "co1";
+		mc2.name= "co2";
+
+		_providerB.update(mc1);
+		_providerB.update(mc2);
+		
+		_providerB.update(mh.getMap());
+		_providerB.update(mh);
+
+		_providerB.commit();
+
+		
+		ensureContent(_providerB, "h2", EXP_2_KEYS, EXP_2_VALUES_UPDATED);
+	}
+
+	private void replicateAllStep2() {
+		replicateAll(_providerB, _providerA);
+		
+		ensureContent(_providerA, "h2", EXP_2_KEYS, EXP_2_VALUES_UPDATED);
+		ensureContent(_providerB, "h2", EXP_2_KEYS, EXP_2_VALUES_UPDATED);
+	}
+
+	private void addElementInProviderA() {
+		MapHolder mh = (MapHolder) getOneInstance(_providerA, MapHolder.class);
+		mh.setName("h3");
+		
+		final MapKey key3 = new MapKey("key3");
+		_providerA.storeNew(key3);
+		
+		MapContent mc3 = new MapContent("co3");
+		_providerA.storeNew(mc3);
+		
+		mh.getMap().put(key3, mc3);
+
+		_providerA.update(mh.getMap());
+		_providerA.update(mh);
+		_providerA.commit();
+
+		ensureContent(_providerA, "h3", EXP_3_KEYS, EXP_3_VALUES);
+	}
+
+	private void replicateHolderStep3() {
+		replicateClass(_providerA, _providerB, MapHolder.class);
+		
+		ensureContent(_providerA, "h3", EXP_3_KEYS, EXP_3_VALUES);
+		ensureContent(_providerB, "h3", EXP_3_KEYS, EXP_3_VALUES);
 	}
 
 	private void ensureContent(TestableReplicationProviderInside provider,
@@ -59,107 +162,9 @@ public class MapTest extends ReplicationTestCase {
 		
 		for (MapContent value : values)
 			ensure(map.containsValue(value), value + " not found in " + provider);
-	}
-
-	private void storeMapToProviderAOLD() {
-
-		MapHolder mh = createMapHolder();
-		MapContent mc1 = new MapContent("c1");
-		MapContent mc2 = new MapContent("c2");
-		mh.put("key1", mc1);
-		mh.put("key2", mc2);
-		_providerA.storeNew(mh);
-		_providerA.commit();
-
-		ensureContent(_providerA, new String[]{"h1"}, new String[]{"key1", "key2"}, new String[]{"c1", "c2"});
-	}
-
-	protected MapHolder createMapHolder() {
-		MapHolder mh = new MapHolder("h1");
-		mh.setMap(new HashMap());
-		return mh;
-	}
-
-	private void replicateAllToProviderBFirstTime() {
-		replicateAll(_providerA, _providerB);
-
-		ensureContent(_providerA, "h1", new MapKey[]{new MapKey("key1"), new MapKey("key2")}, 
-				new MapContent[]{new MapContent("c1"), new MapContent("c2")});
 		
-		ensureContent(_providerB, "h1", new MapKey[]{new MapKey("key1"), new MapKey("key2")}, 
-				new MapContent[]{new MapContent("c1"), new MapContent("c2")});
-	}
-
-	private void modifyInProviderB() {
-
-		MapHolder mh = (MapHolder) getOneInstance(_providerB, MapHolder.class);
-
-		mh.setName("h2");
-		MapContent mc1 = (MapContent) mh.getMap().get("key1");
-		MapContent mc2 = (MapContent) mh.getMap().get("key2");
-		mc1.name= "co1";
-		mc2.name="co2";
-
-		_providerB.update(mc1);
-		_providerB.update(mc2);
-		_providerB.update(mh.getMap());
-		_providerB.update(mh);
-
-		_providerB.commit();
-
-		ensureContent(_providerB, new String[]{"h2"}, new String[]{"key1", "key2"}, new String[]{"co1", "co2"});
-	}
-
-	private void replicateAllStep2() {
-		replicateAll(_providerB, _providerA);
-
-		ensureContent(_providerA, new String[]{"h2"}, new String[]{"key1", "key2"}, new String[]{"co1", "co2"});
-		ensureContent(_providerB, new String[]{"h2"}, new String[]{"key1", "key2"}, new String[]{"co1", "co2"});
-	}
-
-	private void addElementInProviderA() {
-
-		MapHolder mh = (MapHolder) getOneInstance(_providerA, MapHolder.class);
-		mh.setName("h3");
-		MapContent mc3 = new MapContent("co3");
-		_providerA.storeNew(mc3);
-		mh.getMap().put("key3", mc3);
-
-		_providerA.update(mh.getMap());
-		_providerA.update(mh);
-		_providerA.commit();
-
-		ensureContent(_providerA, new String[]{"h3"}, new String[]{"key1", "key2", "key3"}, new String[]{"co1", "co2", "co3"});
-	}
-
-	private void replicateHolderStep3() {
-		replicateClass(_providerA, _providerB, MapHolder.class);
-
-		ensureContent(_providerA, new String[]{"h3"}, new String[]{"key1", "key2", "key3"}, new String[]{"co1", "co2", "co3"});
-		ensureContent(_providerB, new String[]{"h3"}, new String[]{"key1", "key2", "key3"}, new String[]{"co1", "co2", "co3"});
-	}
-
-	private void ensureContent(TestableReplicationProviderInside provider, String[] holderNames, String[] keyNames, String[] valueNames) {
-		int holderCount = holderNames.length;
-		ensureInstanceCount(provider, MapHolder.class, holderCount);
-
-		// After dropping generating uuid for collection, it does not
-		//  make sense to count collection because collection is never reused
-		// ensureInstanceCount(provider, Map.class, holderCount);
-
-		int i = 0;
-		ObjectSet objectSet = provider.getStoredObjects(MapHolder.class);
-		while (objectSet.hasNext()) {
-			MapHolder lh = (MapHolder) objectSet.next();
-			Test.ensure(holderNames[i].equals(lh.getName()));
-
-			Map Map = lh.getMap();
-			for (int j = 0; j < keyNames.length; j++) {
-				MapContent mc = (MapContent) Map.get(keyNames[j]);
-				final String name = mc.name;
-				Test.ensure(valueNames[j].equals(name));
-			}
-		}
+		for (int i = 0; i < keys.length; i++)
+			ensureEquals(values[i], map.get(keys[i]));
 	}
 
 	public void test() {
