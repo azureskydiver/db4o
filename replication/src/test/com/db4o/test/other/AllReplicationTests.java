9a5324ce7b67a111c4fb9dc3f720dd09ce5f5773
peter_vandermerwe <peter_vandermerwe@915b37e7-0715-0410-8bc3-b14b59444072>
2006-08-31 11:38:15 +0000

    PvdM : Convert BugDrs8 to use db4ounit

--- replication/src/test/com/db4o/test/other/AllReplicationTests.java
+++ replication/src/test/com/db4o/test/other/AllReplicationTests.java
@@ -11,52 +11,52 @@ import db4ounit.TestRunner;
 import db4ounit.TestSuite;
 import db4ounit.TestSuiteBuilder;
 
-
-public class AllReplicationTests extends DrsTestCase implements TestSuiteBuilder {
+public class AllReplicationTests extends DrsTestCase implements
+		TestSuiteBuilder {
 
 	public TestSuite build() {
-		return new DrsTestSuiteBuilder(
-				a(), b(), new Class[] {
-					// Simple
-					TheSimplest.class,
-					ReplicationEventTest.class,
-					ReplicationProviderTest.class,
-					ReplicationAfterDeletionTest.class,
-					SimpleArrayTest.class,
-					SimpleParentChild.class,
-					GetByUUID.class,
-					
-					// Collection
-					ArrayReplicationTest.class,
-					// CollectionUuidTest.class,
-					ListTest.class,
-					Db4oListTest.class,
-					MapTest.class,
-					SingleTypeCollectionReplicationTest.class,
-					MixedTypesCollectionReplicationTest.class,
-					
-					// Complex
-					R0to4Runner.class,
-					ReplicationFeaturesMain.class,
-					
-					// General
-					CollectionHandlerImplTest.class,
-					ReplicationTraversalTest.class,
-					DatabaseUnicityTest.class
-					
-				}).build();
+		return new DrsTestSuiteBuilder(a(), b(), new Class[] {
+				// Bugs
+				BugDrs8.class,
+
+				// // Simple
+				TheSimplest.class,
+				ReplicationEventTest.class,
+				ReplicationProviderTest.class,
+				ReplicationAfterDeletionTest.class,
+				SimpleArrayTest.class,
+				SimpleParentChild.class,
+				GetByUUID.class,
+
+				// Collection
+				MapTest.class,
+				ArrayReplicationTest.class,
+				// CollectionUuidTest.class,
+				ListTest.class, Db4oListTest.class, MapTest.class,
+				SingleTypeCollectionReplicationTest.class,
+				MixedTypesCollectionReplicationTest.class,
+
+				// Complex
+				R0to4Runner.class, ReplicationFeaturesMain.class,
+
+				// General
+				CollectionHandlerImplTest.class,
+				ReplicationTraversalTest.class, DatabaseUnicityTest.class
+
+		}).build();
 	}
 
 	public static void main(String[] args) {
-		new TestRunner(
-				new DrsTestSuiteBuilder(
-						new Db4oClientServerDrsFixture("db4o-cs-a", 0xdb40), new Db4oClientServerDrsFixture("db4o-cs-b", 4455),
-						AllReplicationTests.class)).run();
 
-		new TestRunner(
-				new DrsTestSuiteBuilder(
-						new Db4oDrsFixture("db4o-a"), new Db4oDrsFixture("db4o-b"),
-						AllReplicationTests.class)).run();
+//		new TestRunner(new DrsTestSuiteBuilder(new Db4oClientServerDrsFixture(
+//				"db4o-cs-a", 0xdb40), new Db4oClientServerDrsFixture(
+//				"db4o-cs-b", 4455), AllReplicationTests.class)).run();
 
+		new TestRunner(new DrsTestSuiteBuilder(new Db4oDrsFixture("db4o-a"),
+				new Db4oDrsFixture("db4o-b"), AllReplicationTests.class)).run();
+
+//		new TestRunner(new DrsTestSuiteBuilder(new Db4oDrsFixture("db4o-a"),
+//				new Db4oClientServerDrsFixture("db4o-cs-b", 4455),
+//				AllReplicationTests.class)).run();
 	}
 }
