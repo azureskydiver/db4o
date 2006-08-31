40dcd5fac7aaa50953f6401456b1fbd5ab48a6bc
albert_kwan <albert_kwan@915b37e7-0715-0410-8bc3-b14b59444072>
2006-08-31 14:53:59 +0000

    ak: #COR-194

---
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/collections/map/MapHolder.java
@@ -0,0 +1,20 @@
+package com.db4o.db4ounit.collections.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapHolder {
+
+	public String name;
+
+	public Map map;
+
+	public MapHolder(String name) {
+		this.name = name;
+		this.map = new HashMap();
+	}
+
+	public String toString() {
+		return "name = " + name + ", map = " + map;
+	}
+}
