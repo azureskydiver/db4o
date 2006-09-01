1381fda0dd20606f903be0c07dbe4ac8d5401d2a
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-01 15:20:59 +0000

    pvdm + cr: #DRS-12 Kyalami circuit as a first performance test for dRS performance.

---
+++ replicationpolepos/src/org/polepos/drs/Db4oDb4oCar.java
@@ -0,0 +1,22 @@
+/* Copyright (C) 2006  db4objects Inc.  http://www.db4o.com */
+
+package org.polepos.drs;
+
+import com.db4o.test.replication.db4ounit.fixtures.*;
+
+
+/**
+ * @exclude
+ */
+public class Db4oDb4oCar extends DrsCar{
+    
+    public Db4oDb4oCar () {
+        super(new Db4oDrsFixture("poleposA"), new Db4oDrsFixture("poleposB"));
+    }
+    
+    @Override
+    public String name() {
+        return "db4o-db4o";
+    }
+
+}
