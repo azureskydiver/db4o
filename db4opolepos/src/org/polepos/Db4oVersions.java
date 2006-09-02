3728e17ba34a55ef654d98a1fc595b5f608cfb6b
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-02 23:18:25 +0000

    cr: db4o poleposition additions to be able to run new circuits locally first

---
+++ db4opolepos/src/org/polepos/Db4oVersions.java
@@ -0,0 +1,99 @@
+/* 
+This file is part of the PolePosition database benchmark
+http://www.polepos.org
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+MA  02111-1307, USA. */
+
+
+package org.polepos;
+
+import java.io.*;
+import java.lang.reflect.*;
+import java.net.*;
+
+import org.polepos.framework.*;
+import org.polepos.teams.db4o.*;
+
+
+public class Db4oVersions {
+    
+    private static String guessWorkSpace() {
+        File absoluteFile = new File(new File("lib").getAbsolutePath());
+        String workspace = absoluteFile.getParentFile().getParentFile().getAbsolutePath();
+        System.out.println("Guessed workspace:\n" + workspace + "\n");
+        return workspace;
+    }
+    
+    public static Team[] teams(){
+        
+        String workSpacePath = guessWorkSpace();
+        
+        String jar55 = "db4o-5.5-java5.jar";
+        
+        return new Team[]{
+            workspaceTeam(workSpacePath),
+            db4oTeam(workSpacePath, jar55, new int[]{} ),
+        };
+    }
+    
+    private static Team workspaceTeam(String workspace){
+        return db4oTeam(workspace, null, new int[]{} );
+    }
+    
+    private static Team db4oTeam(String workspace, String jarName, int[] options) {
+        
+        try {
+            Team team = null;
+            
+            if(jarName == null){
+                team = (Team)Class.forName(Db4oTeam.class.getName()).newInstance();
+            }else{
+                String[] prefixes={"com.db4o.","org.polepos.teams.db4o."};
+                
+                URL classURL=new File("bin").toURL();
+                URL jarURL=new File("lib/"+jarName).toURL();
+                
+                URL poleposClassURL=new File(workspace + "/polepos/bin").toURL();
+                
+                // System.out.println(classURL+" , "+jarURL + " , " + poleposClassURL);
+                
+                ClassLoader loader=new VersionClassLoader(new URL[]{poleposClassURL, classURL, jarURL},prefixes);
+                team = (Team)loader.loadClass(Db4oTeam.class.getName()).newInstance();
+                
+            }
+            team.configure(options);
+            
+            Object[] drivers = NewDrivers.drivers();
+            
+            for (int i = 0; i < drivers.length; i++) {
+                String driverName = drivers[i].getClass().getName();
+                invoke(team, "addDriver", driverName);
+            }
+            
+            return team;
+        } catch (Exception exc) {
+            exc.printStackTrace();
+            return null;
+        }
+    }
+    
+    private static void invoke(Object onObject, String methodName, Object param) throws Exception{
+        Class clazz = onObject.getClass();
+        Method method = clazz.getMethod(methodName, param.getClass());
+        method.invoke(onObject, new Object[]{param});
+    }
+
+}
