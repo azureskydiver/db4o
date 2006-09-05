55b098648b74409b3e96ab45a49a76ae19db7d8e
jbevain <jbevain@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-05 19:42:30 +0000

    pr+jb: COR-199, revert behaviour on storing naked objects to that of 5.3: throwing ObjectNotStorableException

--- db4oj/test/src/com/db4o/db4ounit/assorted/AllTests.java
+++ db4oj/test/src/com/db4o/db4ounit/assorted/AllTests.java
@@ -14,6 +14,7 @@ public class AllTests extends Db4oTestSuite {
 		return new Class[] {
 			ReAddCascadedDeleteTestCase.class,
             BackupStressTestCase.class,
+            NakedObjectTestCase.class,
 		};
 	}
 }
