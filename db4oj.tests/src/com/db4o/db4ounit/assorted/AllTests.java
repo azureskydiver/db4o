16eef3112fa57aa5be8e2472b8a2bcab5221c52f
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-11 18:03:00 +0000

    rbo+pr: converted GetByUUID to db4ounit and moved to com.db4o.db4ounit.assorted

--- db4oj.tests/src/com/db4o/db4ounit/assorted/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/assorted/AllTests.java
@@ -13,6 +13,7 @@ public class AllTests extends Db4oTestSuite {
 	protected Class[] testCases() {
 		return new Class[] {
             BackupStressTestCase.class,
+            GetByUUID.class,
             NakedObjectTestCase.class,
             ReAddCascadedDeleteTestCase.class,
             SimplestPossibleTestCase.class,
