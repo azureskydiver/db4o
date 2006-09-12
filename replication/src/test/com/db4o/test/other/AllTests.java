a0b1ff9910e205164b52f7fc8195c232323f6dc3
peter_vandermerwe <peter_vandermerwe@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-12 15:19:20 +0000

    #DRS-4 Get AllTests.cs to be starting point of the tests

--- replication/src/test/com/db4o/test/other/AllTests.java
+++ replication/src/test/com/db4o/test/other/AllTests.java
@@ -6,13 +6,42 @@ public class AllTests extends DrsTestSuite {
 	
 	protected Class[] testCases() {
 		return new Class[] {
-			CollectionUuidTest.class,
-			GetByUUID.class
+				// Bugs
+				BugDrs8.class,
+
+				// Simple
+				TheSimplest.class,
+				ReplicationEventTest.class,
+				ReplicationProviderTest.class,
+				ReplicationAfterDeletionTest.class,
+				SimpleArrayTest.class,
+				SimpleParentChild.class,
+				GetByUUID.class,
+
+				// Collection
+				MapTest.class,
+				ArrayReplicationTest.class,
+				//CollectionUuidTest.class,
+				ListTest.class, Db4oListTest.class, MapTest.class,
+				SingleTypeCollectionReplicationTest.class,
+				MixedTypesCollectionReplicationTest.class,
+
+				// Complex
+				R0to4Runner.class, ReplicationFeaturesMain.class,
+
+				// General
+				CollectionHandlerImplTest.class,
+				ReplicationTraversalTest.class, DatabaseUnicityTest.class
 		};
 	}
 	
 	public static void main(String[] args) {
-		 new AllTests().runDb4oDb4o();
+
+		new AllTests().runDb4oDb4o();
+		new AllTests().rundb4oCS();
+		new AllTests().runCSdb4o();
+		new AllTests().runCSCS();
+		 
 	}
 
 }
