70dae93d5e9501e8b439f55cf2118f75fe0aee10
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-12 18:41:28 +0000

    testing field index keys with primitive nulls in search of the NullWrapperProblem

--- db4oj.tests/src/com/db4o/db4ounit/btree/BTreeNullKeyTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/btree/BTreeNullKeyTestCase.java
@@ -1,5 +1,6 @@
 package com.db4o.db4ounit.btree;
 
+import com.db4o.*;
 import com.db4o.inside.btree.*;
 
 import db4ounit.*;
@@ -29,8 +30,34 @@ public class BTreeNullKeyTestCase extends BTreeTestCaseBase {
 			add(keys[idx]);
 		}
 		commit();
+
 		BTreeRange range = _btree.search(trans(), null);
 		Assert.areEqual(2,range.size());
+
+		BTreeNodeSearchResult lower=_btree.searchLeaf(trans(), null,SearchTarget.LOWEST);
+		BTreeNodeSearchResult higher=_btree.searchLeaf(trans(), null,SearchTarget.HIGHEST);
+		range=lower.createIncludingRange(higher);
+		Assert.areEqual(2,range.size());		
+	}
+
+	public void testMultipleNullFieldIndexKeys() {
+		YapStream stream=trans().stream();
+		FieldIndexKeyHandler keyHandler = new FieldIndexKeyHandler(stream,new YInt(stream));
+		BTree btree=new BTree(trans(), 0, keyHandler, null, 7, stream.configImpl().bTreeCacheHeight());
+		
+		final Integer[] keys = new Integer[] { new Integer(1), null, new Integer(2), null, new Integer(3) };
+		for (int idx = 0; idx < keys.length; idx++) {
+			btree.add(trans(),new FieldIndexKey(42,keys[idx]));
+		}
+		commit();
+
+		BTreeRange range = btree.search(trans(), new FieldIndexKey(42,null));
+		Assert.areEqual(2,range.size());
+
+		BTreeNodeSearchResult lower=btree.searchLeaf(trans(), new FieldIndexKey(42,null),SearchTarget.LOWEST);
+		BTreeNodeSearchResult higher=btree.searchLeaf(trans(), new FieldIndexKey(42,null),SearchTarget.HIGHEST);
+		range=lower.createIncludingRange(higher);
+		Assert.areEqual(2,range.size());		
 	}
 
 	public static void main(String[] args) {
