834fb3f24cbb63b2db7c33aa96871190f09add7d
peter_vandermerwe <peter_vandermerwe@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-12 15:21:41 +0000

    #DRS-4 C# version of AllTests.  Only run the tests that can be converted to C# at the moment

---
+++ replication/drs.net/drs.tests.in/com/db4o/test/other/AllTests.cs
@@ -0,0 +1,18 @@
+namespace com.db4o.test.other
+{
+    public class AllTests : com.db4o.test.replication.db4ounit.DrsTestSuite
+    {
+        protected override System.Type[] TestCases()
+        {
+            return new System.Type[] { typeof(TheSimplest), typeof(GetByUUID) };
+        }
+
+        public static void Main(string[] args)
+        {
+            new com.db4o.test.other.AllTests().RunDb4oDb4o();
+            new com.db4o.test.other.AllTests().Rundb4oCS();
+            new com.db4o.test.other.AllTests().RunCSdb4o();
+            new com.db4o.test.other.AllTests().RunCSCS();
+        }
+    }
+}
