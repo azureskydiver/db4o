859849c3e0fb666beb1e435ee0c0d274fd65b1da
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-14 23:24:03 +0000

    unused imports

--- db4oj/core/src/com/db4o/inside/btree/BTreeRemove.java
+++ db4oj/core/src/com/db4o/inside/btree/BTreeRemove.java
@@ -1,8 +1,7 @@
 /* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
 package com.db4o.inside.btree;
 
-import com.db4o.*;
-import com.db4o.foundation.*;
+import com.db4o.Transaction;
 
 /**
  * @exclude
