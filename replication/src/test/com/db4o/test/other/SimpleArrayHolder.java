b1ff3c04f67a296791313cf4a35cc855c9c00205
albert_kwan <albert_kwan@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-15 10:28:06 +0000

    ak: deprecating old tests

---
+++ replication/src/test/com/db4o/test/other/SimpleArrayHolder.java
@@ -0,0 +1,49 @@
+/* Copyright (C) 2004 - 2005  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.test.other;
+
+public class SimpleArrayHolder {
+    
+    private String name;
+    
+    private SimpleArrayContent[] arr;
+    
+    public SimpleArrayHolder() {
+        
+    }
+    
+    public SimpleArrayHolder(String name) {
+        this.name = name;
+    }
+
+    
+    public SimpleArrayContent[] getArr() {
+        return arr;
+    }
+    
+    public void setArr(SimpleArrayContent[] arr) {
+        this.arr = arr;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void add(SimpleArrayContent sac){
+        if(arr == null){
+            arr = new SimpleArrayContent[]{sac};
+            return;
+        }
+        SimpleArrayContent[] temp = arr;
+        arr = new SimpleArrayContent[temp.length + 1];
+        System.arraycopy(temp, 0, arr, 0, temp.length);
+        arr[temp.length] = sac;
+    }
+    
+    
+
+}
