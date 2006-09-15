654073bb791ea84ddb845a400820535dda6b6bdf
albert_kwan <albert_kwan@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-15 11:28:35 +0000

    ak: #COR-211 Created this test case to show C/S unable to return the UUID of a ArrayList

--- db4ojdk1.2/test/src/com/db4o/db4ounit/jdk12/collections/CollectionUuidTest.java
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/jdk12/collections/CollectionUuidTest.java
@@ -3,9 +3,14 @@ package com.db4o.db4ounit.jdk12.collections;
 import java.util.ArrayList;
 
 import com.db4o.Db4o;
+import com.db4o.db4ounit.jdk12.collections.map.SimpleMapTestCase;
 
 import db4ounit.Assert;
+import db4ounit.TestRunner;
 import db4ounit.extensions.AbstractDb4oTestCase;
+import db4ounit.extensions.Db4oTestSuiteBuilder;
+import db4ounit.extensions.fixtures.Db4oClientServer;
+import db4ounit.extensions.fixtures.Db4oInMemory;
 
 public class CollectionUuidTest extends AbstractDb4oTestCase {	
 	
@@ -16,7 +21,18 @@ public class CollectionUuidTest extends AbstractDb4oTestCase {
 	public void test() {
 		ArrayList list = new ArrayList();
 		db().set(list);
-		db().commit();
 		Assert.isNotNull(db().getObjectInfo(list).getUUID());
 	}
+	
+	public static void main(String[] args) {
+        new TestRunner(
+                new Db4oTestSuiteBuilder(
+                        new Db4oInMemory(),
+                        CollectionUuidTest.class)).run();
+        
+        new TestRunner(
+                new Db4oTestSuiteBuilder(
+                        new Db4oClientServer("Db4oClientServer.yap",0xdb40),
+                        CollectionUuidTest.class)).run();
+    }
 }
