22cddf17a3174f7ad6938d3fc7c7dd27bb014de3
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-17 18:23:29 +0000

    cr: #COR-1 No more MarshallerFamilies before BTree.FIELD_INDEXES! Let's roll !

--- db4oj.tests/src/com/db4o/db4ounit/fieldindex/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/fieldindex/AllTests.java
@@ -2,8 +2,6 @@
 
 package com.db4o.db4ounit.fieldindex;
 
-import com.db4o.inside.marshall.*;
-
 import db4ounit.extensions.Db4oTestSuite;
 
 public class AllTests extends Db4oTestSuite {
@@ -23,11 +21,11 @@ public class AllTests extends Db4oTestSuite {
 	            StringIndexTestCase.class,
 		};
 		Class[] tests=neutral;
-        if(MarshallerFamily.BTREE_FIELD_INDEX ){
-        	tests=new Class[fieldBased.length+neutral.length];
-        	System.arraycopy(neutral, 0, tests, 0, neutral.length);
-        	System.arraycopy(fieldBased, 0, tests, neutral.length, fieldBased.length);
-        }
+        
+    	tests=new Class[fieldBased.length+neutral.length];
+    	System.arraycopy(neutral, 0, tests, 0, neutral.length);
+    	System.arraycopy(fieldBased, 0, tests, neutral.length, fieldBased.length);
+            
         return tests;
     }
 }
