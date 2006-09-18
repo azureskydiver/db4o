43b14344eb35d325b5f79960349c04febe0df71c
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-18 10:52:39 +0000

    cr: Updated AssemblyInfo content. Can we generate these files in /in automatically?

--- db4on/in/sharp/compact/AssemblyInfo.cs
+++ db4on/in/sharp/compact/AssemblyInfo.cs
@@ -3,14 +3,14 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("db4o - database for objects")]
-[assembly: AssemblyDescription("db4o 5.0.001 M1 SneakPreview .NET")]
+[assembly: AssemblyDescription("db4o 5.7.001 .NET CompactFramework")]
 [assembly: AssemblyConfiguration(".NET CompactFramework")]
 [assembly: AssemblyCompany("db4objects Inc., San Mateo, CA, USA")]
 [assembly: AssemblyProduct("db4o - database for objects")]
-[assembly: AssemblyCopyright("db4o 2005")]
+[assembly: AssemblyCopyright("db4o 2006")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]	
-[assembly: AssemblyVersion("5.0.001")]
+[assembly: AssemblyVersion("5.7.001")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
