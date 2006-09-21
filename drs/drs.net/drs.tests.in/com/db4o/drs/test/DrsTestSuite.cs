214c90a3679bba8a452eb4d05ada63f01ddedcb5
peter_vandermerwe <peter_vandermerwe@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-21 08:17:11 +0000

    PvdM : #DRS-4 : Added to new drs folder structure

---
+++ drs/drs.net/drs.tests.in/com/db4o/drs/test/DrsTestSuite.cs
@@ -0,0 +1,29 @@
+namespace com.db4o.drs.test
+{
+    /// <exclude></exclude>
+    public abstract class DrsTestSuite : com.db4o.drs.test.DrsTestCase, Db4oUnit.TestSuiteBuilder
+    {
+        public virtual Db4oUnit.TestSuite Build()
+        {
+            return new com.db4o.drs.test.DrsTestSuiteBuilder(A(), B(), TestCases()).Build();
+        }
+
+        protected virtual System.Type[] TestCases()
+        {
+            return All();
+        }
+
+        protected virtual System.Type[] One()
+        {
+            return new System.Type[] { typeof(TheSimplest) };
+        }
+
+        protected virtual System.Type[] All()
+        {
+            return new System.Type[] { typeof(com.db4o.drs.test.TheSimplest), 
+			                           typeof(ReplicationEventTest), 
+			                           typeof(SimpleArrayTest), 
+			                           typeof(SimpleParentChild) };
+        }
+    }
+}
