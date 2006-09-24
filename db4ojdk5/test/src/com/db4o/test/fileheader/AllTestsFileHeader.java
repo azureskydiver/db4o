1a38792bb4932714de1b02dc6f032179aa0f3e2a
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-24 13:54:41 +0000

    cr: #COR-227 Just one simple Client/Server test makes sense to be able to test YapObjectCarrier.

--- db4ojdk5/test/src/com/db4o/test/fileheader/AllTestsFileHeader.java
+++ db4ojdk5/test/src/com/db4o/test/fileheader/AllTestsFileHeader.java
@@ -2,12 +2,14 @@
 
 package com.db4o.test.fileheader;
 
+import com.db4o.db4ounit.common.assorted.*;
 import com.db4o.test.*;
 
 public class AllTestsFileHeader extends AllTestsJdk1_2 {
 
     public static void main(String[] args) {
         runSolo(new FileHeaderTestSuite());
+        new SimplestPossibleTestCase().runClientServer();
     }
 
 }
