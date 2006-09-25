573d18669e1bc43beb91eeca33574fa702df3aff
tetyana_loskutova <tetyana_loskutova@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-25 07:19:57 +0000

    tl: #COR-91 Utility methods: peek persisted

---
+++ db4on/reference/vb/db4o-reference-chapters/src/com/db4odoc/f1/persist/Change2.vb
@@ -0,0 +1,35 @@
+' Copyright (C) 2004 - 2006 db4objects Inc. http://www.db4o.com 
+Imports System.Threading
+
+Namespace com.db4odoc.f1.persist
+    Public Class Change2
+        Dim _thread As Thread
+        Dim _running As Boolean
+        Dim _car As Car
+
+        Public Sub Init(ByVal car As Car)
+            _car = car
+            _running = True
+            Dim threadStart As ThreadStart = New ThreadStart(AddressOf Run)
+            _thread = New Thread(threadStart)
+            _thread.Start()
+        End Sub
+
+        Private Sub Run()
+            While (_running)
+                _car.Temperature = GetConditionerTemperature()
+                Thread.Sleep(10)
+            End While
+        End Sub
+
+        Private Function GetConditionerTemperature() As Integer
+            Return 17
+        End Function
+
+        Public Sub Kill()
+            _running = False
+        End Sub
+
+    End Class
+End Namespace
+
