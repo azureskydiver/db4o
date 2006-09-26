1866b142c4c3537d1b8f286b94362613220f2609
tetyana_loskutova <tetyana_loskutova@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-26 17:46:17 +0000

    tl: #COR-91 oops - also omitted (Utility methods: peek persisted)

---
+++ db4on/reference/db4o-reference-chapters/src/com/db4odoc/f1/persist/Change2.cs
@@ -0,0 +1,44 @@
+/* Copyright (C) 2004 - 2006 db4objects Inc. http://www.db4o.com */
+using System.Threading;
+
+namespace com.db4odoc.f1.persist
+{
+	public class Change2
+	{
+		protected Thread _thread;
+		protected bool   _running;
+		Car _car;
+		
+		public Change2()
+		{
+		}
+
+		public void Init(Car car)
+		{
+			this._car = car;
+			_running = true;
+			ThreadStart threadStart = new ThreadStart(Run);
+			_thread = new Thread(threadStart);
+			_thread.Start();
+		}
+
+		private void Run()
+		{
+			while(_running)
+			{
+				_car.Temperature = GetConditionerTemperature();
+				Thread.Sleep(10);
+			}
+		}
+		
+		private int GetConditionerTemperature()
+		{
+			return 17;
+		}
+
+		public void Stop()
+		{ 
+			_running = false;
+		}
+	}
+}
