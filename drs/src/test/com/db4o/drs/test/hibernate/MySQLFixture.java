4acd25cded8607d61d6469272d16bd8f5102318d
peter_vandermerwe <peter_vandermerwe@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-28 09:55:08 +0000

    Correct location of MySQL.cfg.xml file

--- drs/src/test/com/db4o/drs/test/hibernate/MySQLFixture.java
+++ drs/src/test/com/db4o/drs/test/hibernate/MySQLFixture.java
@@ -10,7 +10,7 @@ public class MySQLFixture extends RdbmsFixture {
 	}
 
 	public void open()  {
-		config = createConfig().configure("com/db4o/test/drs/hibernate/MySQL.cfg.xml");
+		config = createConfig().configure("com/db4o/drs/test/hibernate/MySQL.cfg.xml");
 		_provider = new HibernateReplicationProviderImpl(config, _name);
 	}
 }
