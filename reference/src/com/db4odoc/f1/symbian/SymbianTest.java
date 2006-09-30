4618ff2e3d43748c2374a07d37463c59eb148aae
tetyana_loskutova <tetyana_loskutova@915b37e7-0715-0410-8bc3-b14b59444072>
2006-09-30 16:08:51 +0000

    tl: #COR-99 Platforms documentation: Symbian

---
+++ reference/src/com/db4odoc/f1/symbian/SymbianTest.java
@@ -0,0 +1,92 @@
+/* Copyright (C) 2004 - 2006 db4objects Inc. http://www.db4o.com */
+
+package com.db4odoc.f1.symbian;
+
+
+import java.io.File;
+import java.io.IOException;
+
+
+import com.db4o.Db4o;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+
+
+
+public class SymbianTest {
+
+	public static final String YAPFILENAME = "formula1.yap";
+	
+	public static void main(String[] args) throws IOException {
+		setObjects();
+		setObjectsSymbian();
+		getObjects();
+		getObjectsSymbian();
+	}
+	
+	public static void setObjects(){
+		System.out.println("\nSetting objects using RandomAccessFileAdapter");
+		new File(YAPFILENAME).delete();
+		Db4o.configure().io(new com.db4o.io.RandomAccessFileAdapter());
+		try {
+			ObjectContainer db = Db4o.openFile(YAPFILENAME);
+			try {
+				db.set(new SymbianTest());
+			} finally {
+				db.close();
+			}
+		} catch (Exception ex){
+			System.out.println("Exception accessing file: " + ex.getMessage());
+		}
+	}
+
+	public static void setObjectsSymbian(){
+		System.out.println("\nSetting objects using SymbianIoAdapter");
+		new File(YAPFILENAME).delete();
+		Db4o.configure().io(new com.db4o.io.SymbianIoAdapter());
+		try {
+			ObjectContainer db = Db4o.openFile(YAPFILENAME);
+			try {
+				db.set(new SymbianTest());
+			} finally {
+				db.close();
+			}
+		} catch (Exception ex){
+			System.out.println("Exception accessing file: " + ex.getMessage());
+		}
+	}
+
+	public static void getObjects(){
+		System.out.println("\nRetrieving objects using RandomAccessFileAdapter");
+		Db4o.configure().io(new com.db4o.io.RandomAccessFileAdapter());
+		try {
+			ObjectContainer db = Db4o.openFile(YAPFILENAME);
+			try {
+				 ObjectSet result=db.get(new Object());
+				 System.out.println("Objects in the database: " + result.size());
+			} finally {
+				db.close();
+			}
+		} catch (Exception ex){
+			System.out.println("Exception accessing file: " + ex.getMessage());
+		}
+	}
+	
+	public static void getObjectsSymbian(){
+		System.out.println("\nRetrieving objects using SymbianIoAdapter");
+		Db4o.configure().io(new com.db4o.io.SymbianIoAdapter());
+		try {
+			ObjectContainer db = Db4o.openFile(YAPFILENAME);
+			try {
+				 ObjectSet result=db.get(new Object());
+				 System.out.println("Objects in the database: " + result.size());
+			} finally {
+				db.close();
+			}
+		} catch (Exception ex){
+			System.out.println("Exception accessing file: " + ex.getMessage());
+		}
+	}
+
+}
+
