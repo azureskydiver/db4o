77eeafe0375d90939e27ddce09d96fb2014e12ba
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2006-10-01 13:45:31 +0000

    Andrew: supplement some test cases for c/s concurrency.

--- db4ocs/test/src/com/db4o/test/CascadeDeleteArray.java
+++ db4ocs/test/src/com/db4o/test/CascadeDeleteArray.java
@@ -3,6 +3,8 @@
 package com.db4o.test;
 
 import com.db4o.Db4o;
+import com.db4o.ObjectSet;
+import com.db4o.config.Configuration;
 import com.db4o.ext.ExtObjectContainer;
 import com.db4o.test.config.TestConfigure;
 import com.db4o.test.persistent.SimpleObject;
@@ -17,7 +19,8 @@ public class CascadeDeleteArray extends ClientServerTestCase {
 
 	private int TOTAL_COUNT = TestConfigure.CONCURRENCY_THREAD_COUNT;
 
-	public void configure() {
+	public void configure(Configuration config) {
+		super.configure(config);
 		Db4o.configure().objectClass(this).cascadeOnDelete(true);
 	}
 
@@ -26,28 +29,60 @@ public class CascadeDeleteArray extends ClientServerTestCase {
 		for (int i = 0; i < TOTAL_COUNT; ++i) {
 			elements[i] = new SimpleObject("testString" + i, i);
 		}
-
+		oc.set(this);
 	}
 
-	public void conc(ExtObjectContainer oc) {
+	public void concDelete(ExtObjectContainer oc) {
 		int size = Db4oUtil.occurrences(oc, SimpleObject.class);
 		if (size != TOTAL_COUNT && size != 0) {
 			Assert.fail("Error size " + size + "! The size should either be "
 					+ TOTAL_COUNT + " or 0");
 		}
 		if (size == 0) {
-			oc.delete(CascadeDeleteArray.class);
+			CascadeDeleteArray cda = (CascadeDeleteArray) Db4oUtil.getOne(oc, this);
+			oc.delete(cda);
 			Db4oUtil.assertOccurrences(oc, SimpleObject.class, 0);
 			oc.rollback();
 			Db4oUtil.assertOccurrences(oc, SimpleObject.class, 0);
 		} else {
-			oc.delete(CascadeDeleteArray.class);
+			CascadeDeleteArray cda = (CascadeDeleteArray) Db4oUtil.getOne(oc, this);
+			oc.delete(cda);
 			Db4oUtil.assertOccurrences(oc, SimpleObject.class, 0);
 			oc.rollback();
 			Db4oUtil.assertOccurrences(oc, SimpleObject.class, TOTAL_COUNT);
-			oc.commit();
-			oc.delete(CascadeDeleteArray.class);
+			oc.delete(cda);
 			Db4oUtil.assertOccurrences(oc, SimpleObject.class, 0);
 		}
 	}
+	
+	public void checkDelete(ExtObjectContainer oc) {
+		Db4oUtil.assertOccurrences(oc, SimpleObject.class, 0);
+	}
+
+	public void test() {
+		int total = 10;
+		ExtObjectContainer[] ocs = new ExtObjectContainer[total];
+		ObjectSet[] oss = new ObjectSet[total];
+		for (int i = 0; i < total; i++) {
+			ocs[i] = fixture().db();
+			oss[i] = ocs[i].query(SimpleObject.class);
+			Assert.areEqual(TOTAL_COUNT, oss[i].size());
+		}
+		for (int i = 0; i < total; i++) {
+			Db4oUtil.deleteObjectSet(ocs[i], oss[i]);
+		}
+		ExtObjectContainer oc = fixture().db();
+		try {
+			Db4oUtil.assertOccurrences(oc, SimpleObject.class, TOTAL_COUNT);
+			ocs[0].commit();
+			Db4oUtil.assertOccurrences(oc, SimpleObject.class, 0);
+			for (int i = 1; i < total; i++) {
+				ocs[i].close();
+			}
+			Db4oUtil.assertOccurrences(oc, SimpleObject.class, 0);
+		} finally {
+			oc.close();
+		}
+	}
+
 }
