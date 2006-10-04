e5d4b1c8c1a0d87afdd8fc4762f21767bb2fc574
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-10-04 17:08:54 +0000

    test case for COR-240 (disabled)

--- db4oj.tests/src/com/db4o/db4ounit/jre11/assorted/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/jre11/assorted/AllTests.java
@@ -14,6 +14,7 @@ public class AllTests extends Db4oTestSuite {
 		return new Class[] {
             NullWrapperTestCase.class,
             UUIDMigrationTestCase.class,
+            StoreNumberTestCase.class,
 		};
 	}
 }
