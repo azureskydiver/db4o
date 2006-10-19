7f2d6a93beff1e155039efedb661f05d1696ca28
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-10-19 23:19:13 +0000

    convert more array test cases

--- db4oj.tests/src/com/db4o/db4ounit/common/types/arrays/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/types/arrays/AllTests.java
@@ -12,6 +12,9 @@ public class AllTests extends Db4oTestSuite {
 
 	protected Class[] testCases() {
 		return new Class[] {
+				ArrayNOrderTestCase.class,
+				ByteArrayTestCase.class,
+				NestedArraysTestCase.class,
 				SimpleStringArrayTestCase.class,
 				SimpleTypeArrayInUntypedVariableTestCase.class,
 				TypedArrayInObjectTestCase.class,
