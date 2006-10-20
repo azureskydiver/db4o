0e4e6096b3613d5f14ab056e376128588382026f
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-10-20 12:17:41 +0000

    Don't immediately throw away old style regression tests that have been converted to db4ounit, but collect them in a specific suite as a safety net against test bugs introduced during conversion.

---
+++ db4oj.tests/src/com/db4o/test/AllTestsLegacy.java
@@ -0,0 +1,41 @@
+/* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
+
+package com.db4o.test;
+
+import com.db4o.test.legacy.*;
+
+/**
+ * This suite contains all the old style regression tests that have been converted to
+ * db4ounit. It should serve as a safety net against test bugs introduced during
+ * conversion.
+ */
+public class AllTestsLegacy extends AllTests {
+
+    public static void main(String[] args) {
+        new AllTestsLegacy(new String[]{}).runWithException();
+    }
+    
+    public AllTestsLegacy(String[] testcasenames) {
+    	super(testcasenames);
+    }
+    protected void addTestSuites(TestSuite suites) {
+        suites.add(this);
+	}
+    
+    public Class[] tests(){
+        return new Class[] {
+        		ArrayNOrder.class,
+        		Book.class,
+        		ByteArray.class,
+        		CreateIndex.class,
+        		GetByUUID.class,
+        		MultiDelete.class,
+        		NestedArrays.class,
+        		SimpleTypeArrayInUntypedVariable.class,
+        		TypedArrayInObject.class,
+        		TypedDerivedArray.class,
+        };
+    }
+
+
+}
