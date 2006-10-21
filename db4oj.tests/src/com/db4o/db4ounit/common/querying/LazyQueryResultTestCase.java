406c56c5ec598f2f4598b17596302e8b9cd0c7f0
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-10-21 13:07:32 +0000

    cr: #COR-209 Specific test cases for the two QueryResult implementations, allowing to run them separate and detached without changing anything in the core.

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/querying/LazyQueryResultTestCase.java
@@ -0,0 +1,18 @@
+/* Copyright (C) 2006  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.db4ounit.common.querying;
+
+import com.db4o.inside.query.*;
+
+
+public class LazyQueryResultTestCase extends QueryResultTestCase{
+
+	public static void main(String[] args) {
+		new LazyQueryResultTestCase().runSolo();
+	}
+
+	protected QueryResult newQueryResult() {
+		return new LazyQueryResult(trans());
+	}
+
+}
