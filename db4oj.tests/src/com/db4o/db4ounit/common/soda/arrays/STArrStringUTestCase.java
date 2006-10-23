07c3e7c33f43c53b64d08504aeffcbb82daa7ed8
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-10-23 23:02:23 +0000

    started to convert soda tests to db4ounit (more food for defragmenting fixture)

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/soda/arrays/STArrStringUTestCase.java
@@ -0,0 +1,68 @@
+/* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
+
+package com.db4o.db4ounit.common.soda.arrays;
+
+import com.db4o.db4ounit.common.soda.util.*;
+import com.db4o.query.*;
+
+public class STArrStringUTestCase extends SodaBaseTestCase {
+	
+	public class Data{
+		public Object[] _strArr;
+
+		public Data(Object[] strArr) {
+			this._strArr = strArr;
+		}
+	}
+	
+	protected Object[] createData() {
+		return new Object[]{
+			new Data(null),
+			new Data(new Object[] {null}),
+			new Data(new Object[] {null, null}),
+			new Data(new Object[] {"foo", "bar", "fly"}),
+			new Data(new Object[] {null, "bar", "wohay", "johy"})
+		};
+	}
+
+	public void testDefaultContainsOne(){
+		Query q = newQuery();
+		q.constrain(new Data(new Object[] {"bar"}));
+		expect(q, new int[] {3,4});
+	}
+
+	public void testDefaultContainsTwo(){
+		Query q = newQuery();
+		q.constrain(new Data(new Object[] {"foo", "bar"}));
+		expect(q, new int[] {3});
+	}
+	
+	public void testDescendOne(){
+		Query q = newQuery(Data.class);
+		q.descend("_strArr").constrain("bar");
+		expect(q, new int[] {3,4});
+	}
+	
+	public void testDescendTwo(){
+		Query q = newQuery(Data.class);
+		Query qElements = q.descend("_strArr");
+		qElements.constrain("foo");
+		qElements.constrain("bar");
+		expect(q, new int[] {3});
+	}
+	
+	public void testDescendOneNot(){
+		Query q = newQuery(Data.class);
+		q.descend("_strArr").constrain("bar").not();
+		expect(q, new int[] {0,1,2});
+	}
+	
+	public void testDescendTwoNot(){
+		Query q = newQuery(Data.class);
+		Query qElements = q.descend("_strArr");
+		qElements.constrain("foo").not();
+		qElements.constrain("bar").not();
+		expect(q, new int[] {0,1,2});
+	}
+	
+}
---
+++ db4oj.tests/src/com/db4o/db4ounit/common/soda/util/SodaBaseTestCase.java
@@ -0,0 +1,33 @@
+/* Copyright (C) 2006  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.db4ounit.common.soda.util;
+
+import com.db4o.query.*;
+
+import db4ounit.extensions.*;
+
+public abstract class SodaBaseTestCase extends AbstractDb4oTestCase {
+
+	protected Object[] _array;
+
+	protected void db4oSetupBeforeStore() throws Exception {
+		_array=createData();
+	}
+
+	public void store() {
+		Object[] data=createData();
+		for (int idx = 0; idx < data.length; idx++) {
+			db().set(data[idx]);
+		}
+	}
+	
+	protected abstract Object[] createData();
+
+    protected void expect(Query query, int[] indices) {
+    	Object[] data=new Object[indices.length];
+    	for (int idx = 0; idx < indices.length; idx++) {
+			data[idx]=_array[indices[idx]];
+		}
+        SodaTestUtil.expect(query, data, false);
+    }
+}
