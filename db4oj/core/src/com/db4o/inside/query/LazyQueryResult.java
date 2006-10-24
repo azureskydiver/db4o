e6d75e5cb9769cbd967a2b19d6ed0b2c5a42da7c
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-10-24 11:11:48 +0000

    cr: #COR-209 Direct access to BTree for class-only-query passes unit test.

--- db4oj/core/src/com/db4o/inside/query/LazyQueryResult.java
+++ db4oj/core/src/com/db4o/inside/query/LazyQueryResult.java
@@ -5,6 +5,8 @@ package com.db4o.inside.query;
 import com.db4o.*;
 import com.db4o.ext.*;
 import com.db4o.foundation.*;
+import com.db4o.inside.btree.*;
+import com.db4o.inside.classindex.*;
 import com.db4o.query.*;
 
 
@@ -14,6 +16,8 @@ import com.db4o.query.*;
 public class LazyQueryResult implements QueryResult {
 	
 	private final Transaction _transaction;
+	
+	private Iterable4 _iterable;
 	
 	public LazyQueryResult(Transaction trans) {
 		_transaction = trans;
@@ -28,11 +32,27 @@ public class LazyQueryResult implements QueryResult {
 	}
 
 	public IntIterator4 iterateIDs() {
-		throw new NotImplementedException();
+		if(_iterable == null){
+			throw new IllegalStateException();
+		}
+		return new IntIterator4Adaptor(_iterable.iterator());
 	}
-
+	
 	public void loadFromClassIndex(YapClass clazz) {
-		throw new NotImplementedException();
+		ClassIndexStrategy index = clazz.index();
+		if(! (index instanceof BTreeClassIndexStrategy)){
+			throw new IllegalStateException();
+		}
+		final BTree btree = ((BTreeClassIndexStrategy)index).btree();
+		_iterable = new Iterable4() {
+			public Iterator4 iterator() {
+				return new BTreeRangeSingle(transaction(), btree, btree.firstPointer(transaction()), null).keys();
+			}
+		};
+	}
+	
+	public Transaction transaction(){
+		return _transaction;
 	}
 
 	public void loadFromClassIndexes(YapClassCollectionIterator iterator) {
