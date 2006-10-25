56941fd967aae1a475882ca2d61f2bf5f74dda0e
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-10-25 15:28:32 +0000

    cr: #COR-267 This is a platform-dependant test, it may run OK for .NET, moving to jre11.staging.

--- db4oj.tests/src/com/db4o/db4ounit/jre11/staging/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/jre11/staging/AllTests.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2004 - 2006 db4objects Inc. http://www.db4o.com */
 
-package com.db4o.db4ounit.common.staging;
+package com.db4o.db4ounit.jre11.staging;
 
 import db4ounit.extensions.Db4oTestSuite;
 
