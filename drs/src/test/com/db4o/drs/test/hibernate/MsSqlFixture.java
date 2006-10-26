1feebb4c8ae652a38b0a449107d20cb496c24baf
albert_kwan <albert_kwan@915b37e7-0715-0410-8bc3-b14b59444072>
2006-10-26 08:41:31 +0000

    ak: #DRS-25 Microsoft SQL Server 2005 is good

---
+++ drs/src/test/com/db4o/drs/test/hibernate/MsSqlFixture.java
@@ -0,0 +1,17 @@
+/* Copyright (C) 2004 - 2006 db4objects Inc. http://www.db4o.com */
+
+package com.db4o.drs.test.hibernate;
+
+import com.db4o.drs.hibernate.impl.HibernateReplicationProviderImpl;
+
+public class MsSqlFixture extends RdbmsFixture {
+	public MsSqlFixture(String name) {
+		super(name);
+		
+	}
+
+	public void open()  {
+		config = createConfig().configure("com/db4o/drs/test/hibernate/MsSql.cfg.xml");
+		_provider = new HibernateReplicationProviderImpl(config, _name);
+	}
+}
