a13ae8e35e7dbfb4048afd0131cba013e8b7820e
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2006-10-30 13:34:30 +0000

    PMFD is java only for now

--- db4oj.tests/src/com/db4o/db4ounit/jre11/tools/defragment/RunTestsDefrag.java
+++ db4oj.tests/src/com/db4o/db4ounit/jre11/tools/defragment/RunTestsDefrag.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2006  db4objects Inc.  http://www.db4o.com */
 
-package com.db4o.db4ounit.common.tools.defragment;
+package com.db4o.db4ounit.jre11.tools.defragment;
 
 import db4ounit.*;
 import db4ounit.extensions.*;
