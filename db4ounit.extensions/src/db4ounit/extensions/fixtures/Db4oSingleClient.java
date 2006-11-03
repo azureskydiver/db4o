c40fea36713ddaa557b11f2de3107fb96e974f9d
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2006-11-03 15:40:04 +0000

    Andrew: Modify Db4oSingleClient#accept(Class clazz) method. A c/s test should must implement Db4oTestCase and not implement OptOutCS.

--- db4ounit.extensions/src/db4ounit/extensions/fixtures/Db4oSingleClient.java
+++ db4ounit.extensions/src/db4ounit/extensions/fixtures/Db4oSingleClient.java
@@ -8,48 +8,58 @@ import com.db4o.Db4o;
 import com.db4o.ext.ExtObjectContainer;
 
 import db4ounit.TestException;
-
+import db4ounit.extensions.Db4oTestCase;
 
 public class Db4oSingleClient extends AbstractClientServerDb4oFixture {
-    
-    private ExtObjectContainer _objectContainer;
 
-    public Db4oSingleClient(ConfigurationSource config,String fileName, int port) {
-        super(config,fileName, port);
-    }
-    
-    public Db4oSingleClient(ConfigurationSource config){
-        super(config);
-    }
+	private ExtObjectContainer _objectContainer;
 
-    public Db4oSingleClient(){
-        this(new IndependentConfigurationSource());
-    }
+	public Db4oSingleClient(ConfigurationSource config, String fileName,
+			int port) {
+		super(config, fileName, port);
+	}
 
-    public void close() throws Exception {
-        _objectContainer.close();
-        super.close();
-    }
+	public Db4oSingleClient(ConfigurationSource config) {
+		super(config);
+	}
 
-    public void open() throws Exception {
-        super.open();
-        try {
-            _objectContainer = Db4o.openClient(config(),HOST, PORT, USERNAME, PASSWORD).ext();
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new TestException(e);
-        }
-    }
-    
+	public Db4oSingleClient() {
+		this(new IndependentConfigurationSource());
+	}
+
+	public void close() throws Exception {
+		_objectContainer.close();
+		super.close();
+	}
+
+	public void open() throws Exception {
+		super.open();
+		try {
+			_objectContainer = Db4o.openClient(config(), HOST, PORT, USERNAME,
+					PASSWORD).ext();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new TestException(e);
+		}
+	}
+
+	/**
+	 * Does not accept a clazz which is assignable from OptOutCS, or not
+	 * assignable from Db4oTestCase.
+	 * 
+	 * @return returns false if the clazz is assignable from OptOutCS, or not
+	 *         assignable from Db4oTestCase. Otherwise, returns true.
+	 */
 	public boolean accept(Class clazz) {
-		if((OptOutCS.class.isAssignableFrom(clazz))){
+		if ((OptOutCS.class.isAssignableFrom(clazz))
+				|| !Db4oTestCase.class.isAssignableFrom(clazz)) {
 			return false;
 		}
 		return true;
 	}
 
-    public ExtObjectContainer db() {
-        return _objectContainer;
-    }
+	public ExtObjectContainer db() {
+		return _objectContainer;
+	}
 
 }
