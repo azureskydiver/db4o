c17a3fc250656b05e263de50f014c61298832ff2
albert_kwan <albert_kwan@915b37e7-0715-0410-8bc3-b14b59444072>
2006-11-08 16:26:22 +0000

    ak: Db4objects.Drs

---
+++ drs/drs.net/drs.tests.in/Db4objects/Drs/Test/ReplicationTraversalTest.cs
@@ -0,0 +1,27 @@
+namespace Db4objects.Drs.Test
+{
+    public class ReplicationTraversalTest : Db4objects.Drs.Test.DrsTestCase
+    {
+
+        public virtual void Test()
+        {
+            Db4objects.Drs.Test.Replicated obj1 = new Db4objects.Drs.Test.Replicated("1");
+            Db4objects.Drs.Test.Replicated obj2 = new Db4objects.Drs.Test.Replicated("2");
+            Db4objects.Drs.Test.Replicated obj3 = new Db4objects.Drs.Test.Replicated("3");
+            obj1.SetLink(obj2);
+            obj2.SetLink(obj3);
+            obj3.SetLink(obj1);
+            A().Provider().StoreNew(obj1);
+            ReplicateClass(A().Provider(), B().Provider(), typeof(Replicated));
+            EnsureContains(A().Provider(), obj1);
+            EnsureContains(A().Provider(), obj2);
+            EnsureContains(A().Provider(), obj3);
+        }
+
+        protected virtual void EnsureContains(Db4objects.Drs.Inside.TestableReplicationProviderInside provider, Replicated obj)
+        {
+            ObjectSet objectSet = provider.GetStoredObjects(typeof(Replicated));
+            Db4oUnit.Assert.IsTrue(objectSet.Contains(obj));
+        }
+    }
+}
---
+++ drs/drs.net/drs.tests.in/Db4objects/Drs/Test/SingleTypeCollectionReplicationTest.cs
@@ -0,0 +1,33 @@
+namespace Db4objects.Drs.Test
+{
+    public class SingleTypeCollectionReplicationTest : Db4objects.Drs.Test.DrsTestCase
+    {
+        protected virtual void ActualTest()
+        {
+            Db4objects.Drs.Test.CollectionHolder h1 = new Db4objects.Drs.Test.CollectionHolder();
+            h1.ht["1"] = "One";
+            h1.set["2"] = "two";
+            h1.list.Add("three");
+            A().Provider().StoreNew(h1);
+            A().Provider().Commit();
+            Db4objects.Db4o.DrsReplicationSession replication = new Db4objects.Drs.Inside.GenericReplicationSession(A().Provider(), B().Provider());
+            Db4objects.Db4o.ObjectSet objectSet = A().Provider().ObjectsChangedSinceLastReplication();
+            while (objectSet.HasNext())
+            {
+                replication.Replicate(objectSet.Next());
+            }
+            replication.Commit();
+            Db4objects.Db4o.ObjectSet it = B().Provider().GetStoredObjects(typeof(Db4objects.Drs.Test.CollectionHolder));
+            Db4oUnit.Assert.IsTrue(it.HasNext());
+            Db4objects.Drs.Test.CollectionHolder replica = (Db4objects.Drs.Test.CollectionHolder)it.Next();
+            Db4oUnit.Assert.AreEqual("One", replica.ht["1"]);
+            Db4oUnit.Assert.IsTrue(replica.set.Contains("2"));
+            Db4oUnit.Assert.AreEqual("three", replica.list[0]);
+        }
+
+        public virtual void Test()
+        {
+            ActualTest();
+        }
+    }
+}
