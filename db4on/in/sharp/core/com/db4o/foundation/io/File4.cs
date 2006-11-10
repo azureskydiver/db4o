b5b1d63b8847ff13fa894bc9d6f8c70ec950dc15
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2006-11-10 21:35:38 +0000

    rbo+jbe: fixed the old .net build because it's going to distributed as the legacy version (a compatibility layer over Db4objects.Db4o.dll would probably not fulfill our users expectations)

---
+++ db4on/in/sharp/core/com/db4o/foundation/io/File4.cs
@@ -0,0 +1,21 @@
+﻿/* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
+using System.IO;
+
+namespace com.db4o.foundation.io
+{
+    public class File4
+    {
+        public static void Delete(string file)
+        {
+            if (File.Exists(file))
+            {
+                File.Delete(file);
+            }
+        }
+
+        public static void Copy(string from, string to)
+        {
+            File.Copy(from, to, true);
+        }
+    }
+}
