2c5c05101b742b82e237bea8fc611f09b3c5f382
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-11-11 12:29:45 +0000

    added comments and convenience constructor

--- db4oj/tools/src/com/db4o/tools/defragment/AvailableClassFilter.java
+++ db4oj/tools/src/com/db4o/tools/defragment/AvailableClassFilter.java
@@ -4,14 +4,37 @@ package com.db4o.tools.defragment;
 
 import com.db4o.*;
 
+/**
+ * Filter that accepts only YapClass instances whose corresponding Java class is
+ * currently known.
+ */
 public class AvailableClassFilter implements YapClassFilter {
 	private ClassLoader _loader;
-	
+
+	/**
+	 * Will accept only classes that are known to the classloader that loaded
+	 * this class.
+	 */
+	public AvailableClassFilter() {
+		this(AvailableClassFilter.class.getClassLoader());
+	}
+
+	/**
+	 * Will accept only classes that are known to the given classloader.
+	 * 
+	 * @param loader The classloader to check class names against
+	 */
 	public AvailableClassFilter(ClassLoader loader) {
-		super();
 		_loader = loader;
 	}
 
+	/**
+	 * Will accept only classes whose corresponding platform class is known
+	 * to the configured classloader.
+	 * 
+	 * @param yapClass The YapClass instance to be checked
+	 * @return true if the corresponding platform class is known to the configured classloader, false otherwise
+	 */
 	public boolean accept(YapClass yapClass) {
 		try {
 			_loader.loadClass(yapClass.getName());
