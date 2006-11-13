a84599aa387fceb809ac51e80721292b53db916c
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-11-13 11:18:07 +0000

    COR-115: factored away mapClassIDs() method

--- db4oj/core/src/com/db4o/defragment/AbstractContextIDMapping.java
+++ db4oj/core/src/com/db4o/defragment/AbstractContextIDMapping.java
@@ -7,10 +7,20 @@ import com.db4o.foundation.*;
 
 /**
  * Base class for defragment ID mappings.
+ * 
+ * @see Defragment
  */
 abstract class AbstractContextIDMapping implements ContextIDMapping {
 
 	private Hashtable4	_classIDs = new Hashtable4();
+
+	public final void mapIDs(int origID, int mappedID, boolean isClassID) {
+		if(isClassID) {
+			mapClassIDs(origID, mappedID);
+			return;
+		}
+		mapNonClassIDs(origID, mappedID);
+	}
 
 	protected int mappedClassID(int origID) {
 		Object obj = _classIDs.get(origID);
@@ -20,8 +30,9 @@ abstract class AbstractContextIDMapping implements ContextIDMapping {
 		return ((Integer)obj).intValue();
 	}
 
-	public void mapClassIDs(int oldID, int newID) {
+	private void mapClassIDs(int oldID, int newID) {
 		_classIDs.put(oldID,new Integer(newID));
 	}
 
+	protected abstract void mapNonClassIDs(int origID,int mappedID);
 }
