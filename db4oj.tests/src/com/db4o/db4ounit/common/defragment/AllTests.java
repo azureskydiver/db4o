075e0608477836f9b67eb04e798d931887055fd2
jbevain <jbevain@915b37e7-0715-0410-8bc3-b14b59444072>
2006-11-13 15:19:17 +0000

    Move fast defrag tests from .jre11 to .common so they are executed on .net as well.

--- db4oj.tests/src/com/db4o/db4ounit/common/defragment/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/defragment/AllTests.java
@@ -1,10 +1,14 @@
 /* Copyright (C) 2006  db4objects Inc.  http://www.db4o.com */
 
-package com.db4o.db4ounit.jre11.defragment;
+package com.db4o.db4ounit.common.defragment;
 
 import db4ounit.extensions.*;
 
 public class AllTests extends Db4oTestSuite {
+	
+	public static void main(String[] args) {
+		new AllTests().runSolo();
+    }
 
 	protected Class[] testCases() {
 		return new Class[] {
--- db4oj.tests/src/com/db4o/db4ounit/common/defragment/Db4oDefragSolo.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/defragment/Db4oDefragSolo.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2006  db4objects Inc.  http://www.db4o.com */
 
-package com.db4o.db4ounit.jre11.defragment;
+package com.db4o.db4ounit.common.defragment;
 
 import java.io.*;
 
