47f4e7e559078f51b8aa902902ba5161c144ce69
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2006-11-14 13:36:53 +0000

    rbo+jbe: Db4o => Db4oFactory

---
+++ db4on/in/sharp/core/com/db4o/Db4o.cs
@@ -0,0 +1,9 @@
+namespace com.db4o
+{
+    /// <summary>Same as <see cref="com.db4o.Db4oFactory">com.db4o.Db4oFactory</see> but
+    /// with old name.</summary>
+    /// <seealso cref="com.db4o.Db4oFactory">com.db4o.Db4oFactory class</seealso>
+    public class Db4o : Db4oFactory
+    {
+    }
+}
