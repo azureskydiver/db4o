51d5cdf8f42fe8e01ec9d75b3dc713aba467eff0
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-11-18 17:06:54 +0000

    quickfix for COR-288, to be revised, see Jira comment(s)

--- db4oj.tests/src/com/db4o/db4ounit/common/reflect/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/reflect/AllTests.java
@@ -9,6 +9,7 @@ public class AllTests extends Db4oTestSuite {
 
 	protected Class[] testCases() {
 		return new Class[] {
+			GenericReflectorStateTest.class,
 			ReflectArrayTestCase.class,
 		};
 	}
