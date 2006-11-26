92d1a89f3fc6a3f2b6acde08af424f876ddcc4bb
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-11-26 22:47:49 +0000

    test case for COR-296 (temporarily disabled)

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/defragment/SlotDefragmentConstants.java
@@ -0,0 +1,12 @@
+/* Copyright (C) 2006  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.db4ounit.common.defragment;
+
+public abstract class SlotDefragmentConstants {
+	
+	public final static String FILENAME="defrag.yap";
+	public final static String BACKUPFILENAME=FILENAME+".backup";
+
+	private SlotDefragmentConstants() {
+	}
+}
