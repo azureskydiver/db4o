f01efd3563eedac1938fe531688b731816d186ca
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2006-11-27 19:23:01 +0000

    moved test.interfaces to db4ounit.common.interfaces

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/interfaces/AllTests.java
@@ -0,0 +1,14 @@
+/* Copyright (C) 2004 - 2006 db4objects Inc. http://www.db4o.com */
+
+package com.db4o.db4ounit.common.interfaces;
+
+import db4ounit.extensions.Db4oTestSuite;
+
+
+public class AllTests extends Db4oTestSuite {
+
+	protected Class[] testCases() {
+		return new Class[] { InterfaceTestCase.class };
+	} 
+
+}
