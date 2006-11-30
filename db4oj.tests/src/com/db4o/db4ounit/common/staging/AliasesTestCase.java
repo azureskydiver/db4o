345a7eb6be58cabf019f8affd136c634aeef7e49
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-11-30 17:35:25 +0000

    cr: Added db4ounit test case that shows broken Aliases.

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/staging/AliasesTestCase.java
@@ -0,0 +1,67 @@
+/* Copyright (C) 2006  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.db4ounit.common.staging;
+
+import com.db4o.config.*;
+import com.db4o.test.lib.*;
+
+import db4ounit.*;
+import db4ounit.extensions.*;
+
+
+/**
+ * @exclude
+ */
+public class AliasesTestCase extends AbstractDb4oTestCase {
+	
+	
+	public static class AFoo{
+		
+		public String foo;
+		
+	}
+	
+	public static class ABar extends AFoo{
+		
+		public String bar;
+		
+	}
+	
+	public static class BFoo{
+		
+		public String foo;
+		
+	}
+	
+	public static class BBar extends AFoo{
+		
+		public String bar;
+		
+	}
+	
+
+	public static void main(String[] args) {
+		new AliasesTestCase().runSolo();
+	}
+	
+	public void store(){
+		ABar bar = new ABar();
+		bar.foo = "foo";
+		bar.bar = "bar";
+	}
+	
+	private WildcardAlias createAlias(){
+		String className = reflector().forObject(new ABar()).getName();
+		String storedPattern = Strings.replace(className, "ABar", "A*");
+		String runtimePattern = Strings.replace(className, "ABar", "B*");
+		return new WildcardAlias(storedPattern, runtimePattern);
+	}
+	
+	public void test(){
+		db().configure().addAlias(createAlias());
+		BBar bar = (BBar) retrieveOnlyInstance(BBar.class);
+		Assert.areEqual("foo", bar.foo);
+		Assert.areEqual("bar", bar.bar);
+	}
+
+}
