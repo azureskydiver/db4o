f324a257a3b2b4d432e71c5d9b45b9714ad6a756
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2006-12-01 23:45:32 +0000

    cr: Something is going wrong in the ClassMeta unmarshalling and it completely stalls the server, the client and my test cases. This is a temporary fix to get them to run again.

--- db4oj/core/src/com/db4o/cs/messages/MClassMeta.java
+++ db4oj/core/src/com/db4o/cs/messages/MClassMeta.java
@@ -2,10 +2,7 @@
 
 package com.db4o.cs.messages;
 
-import com.db4o.Transaction;
-import com.db4o.YapClass;
-import com.db4o.YapStream;
-import com.db4o.YapWriter;
+import com.db4o.*;
 import com.db4o.cs.ClassMeta;
 import com.db4o.cs.YapServerThread;
 import com.db4o.reflect.generic.GenericClass;
@@ -14,24 +11,30 @@ public class MClassMeta extends MsgObject {
 	public boolean processAtServer(YapServerThread serverThread) {
 		YapStream stream = stream();
 		unmarshall();
-		ClassMeta classMeta = (ClassMeta) stream().unmarshall(_payLoad);
-		GenericClass genericClass = stream.getClassMetaHelper().classMetaToGenericClass(stream().reflector(), classMeta);
-		if (genericClass != null) {
-			synchronized (streamLock()) {
-				Transaction trans = stream.getSystemTransaction();
-				YapWriter returnBytes = new YapWriter(trans, 0);
-
-				YapClass yapClass = stream.produceYapClass(genericClass);
-				if (yapClass != null) {
-					stream.checkStillToSet();
-					yapClass.setStateDirty();
-					yapClass.write(trans);
-					trans.commit();
-					returnBytes = stream
-							.readWriterByID(trans, yapClass.getID());
-					serverThread.write(Msg.OBJECT_TO_CLIENT.getWriter(returnBytes));
-					return true;
+		try{
+			ClassMeta classMeta = (ClassMeta) stream().unmarshall(_payLoad);
+			GenericClass genericClass = stream.getClassMetaHelper().classMetaToGenericClass(stream().reflector(), classMeta);
+			if (genericClass != null) {
+				synchronized (streamLock()) {
+					Transaction trans = stream.getSystemTransaction();
+					YapWriter returnBytes = new YapWriter(trans, 0);
+	
+					YapClass yapClass = stream.produceYapClass(genericClass);
+					if (yapClass != null) {
+						stream.checkStillToSet();
+						yapClass.setStateDirty();
+						yapClass.write(trans);
+						trans.commit();
+						returnBytes = stream
+								.readWriterByID(trans, yapClass.getID());
+						serverThread.write(Msg.OBJECT_TO_CLIENT.getWriter(returnBytes));
+						return true;
+					}
 				}
+			}
+		}catch(Exception e){
+			if(Debug.atHome){
+				e.printStackTrace();
 			}
 		}
 		serverThread.write(Msg.FAILED);
