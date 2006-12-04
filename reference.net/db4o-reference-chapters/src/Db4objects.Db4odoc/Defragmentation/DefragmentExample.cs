91358ac00262a5a11e47f77b01f0bf79d5da237b
tetyana_loskutova <tetyana_loskutova@915b37e7-0715-0410-8bc3-b14b59444072>
2006-12-04 12:32:35 +0000

    tl: Old changes + query modes examples

---
+++ reference.net/db4o-reference-chapters/src/Db4objects.Db4odoc/Defragmentation/DefragmentExample.cs
@@ -0,0 +1,31 @@
+using System;
+using Db4objects.Db4o.Defragment;
+
+namespace Db4objects.Db4odoc.src.Db4objects.Db4odoc.Defragmentation
+{
+    class DefragmentExample
+    {
+
+        public static void Main(string[] args)
+        {
+            RunDefragment();
+        }
+        // end Main
+
+        public static void RunDefragment()
+        {
+            DefragmentConfig config = new DefragmentConfig("sample.yap", "sample.bap");
+            config.ForceBackupDelete(true);
+            config.StoredClassFilter(new AvailableTypeFilter());
+            try
+            {
+                Defragment.Defrag(config);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+        // end RunDefragment
+    }
+}
