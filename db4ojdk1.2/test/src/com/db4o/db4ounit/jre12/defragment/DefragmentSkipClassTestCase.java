cdda23b748e0c925a06cfd3ce59ca128127622a0
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-12-14 16:10:25 +0000

    split defragment test case - ExcludingClassLoader cannot be used with JDK1.1

---
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/defragment/DefragmentSkipClassTestCase.java
@@ -0,0 +1,47 @@
+/* Copyright (C) 2006  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.db4ounit.jre12.defragment;
+
+import java.io.*;
+import java.util.*;
+
+import com.db4o.db4ounit.common.defragment.*;
+import com.db4o.defragment.*;
+import com.db4o.test.util.*;
+
+import db4ounit.*;
+
+/**
+ * This one tests common, non-jdk1.2 specific functionality, but requires an
+ * ExcludingClassLoader which doesn't work on JDK < 1.2.
+ */
+public class DefragmentSkipClassTestCase implements TestLifeCycle {
+
+	public void testSkipsClass() throws Exception {
+		DefragmentConfig defragConfig = SlotDefragmentFixture.defragConfig(true);
+		Defragment.defrag(defragConfig);
+		SlotDefragmentFixture.assertDataClassKnown(true);
+
+		defragConfig = SlotDefragmentFixture.defragConfig(true);
+		defragConfig.storedClassFilter(new AvailableClassFilter());
+		Defragment.defrag(defragConfig);
+		SlotDefragmentFixture.assertDataClassKnown(true);
+
+		defragConfig = SlotDefragmentFixture.defragConfig(true);
+		Vector excluded=new Vector();
+		excluded.add(SlotDefragmentFixture.Data.class.getName());
+		ExcludingClassLoader loader=new ExcludingClassLoader(getClass().getClassLoader(),excluded);
+		defragConfig.storedClassFilter(new AvailableClassFilter(loader));
+		Defragment.defrag(defragConfig);
+		SlotDefragmentFixture.assertDataClassKnown(false);
+	}
+
+	public void setUp() throws Exception {
+		new File(SlotDefragmentConstants.FILENAME).delete();
+		new File(SlotDefragmentConstants.BACKUPFILENAME).delete();
+		SlotDefragmentFixture.createFile(SlotDefragmentConstants.FILENAME);
+	}
+
+	public void tearDown() throws Exception {
+	}
+}
