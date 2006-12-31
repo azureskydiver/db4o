8507054ba58643ef7981834c5947ebdfa6363b14
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2006-12-31 10:46:01 +0000

    another vector defragment test case

--- db4oj.tests/src/com/db4o/db4ounit/jre11/defragment/SlotDefragmentVectorUUIDTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/jre11/defragment/SlotDefragmentVectorUUIDTestCase.java
@@ -6,13 +6,14 @@ import java.io.*;
 import java.util.*;
 
 import com.db4o.*;
+import com.db4o.config.*;
 import com.db4o.db4ounit.common.defragment.*;
 import com.db4o.defragment.*;
 import com.db4o.query.*;
 
 import db4ounit.*;
 
-public class SlotDefragmentVectorTestCase implements TestCase {
+public class SlotDefragmentVectorUUIDTestCase implements TestCase {
 
 	public static class Holder {
 		public Vector _vector;
@@ -21,17 +22,6 @@ public class SlotDefragmentVectorTestCase implements TestCase {
 			this._vector = vector;
 		}
 	}
-
-	public static class Data {
-		public int _id;
-
-		public Data(int id) {
-			this._id = id;
-		}
-	}
-
-	private static final int NUMVECTORS = 10;
-	private static final int NUMENTRIES = 10;
 	
 	public void testVectorDefragment() throws Exception {
 		store();
@@ -40,11 +30,11 @@ public class SlotDefragmentVectorTestCase implements TestCase {
 	}
 
 	private void query() {
-		ObjectContainer db=Db4o.openFile(Db4o.newConfiguration(),SlotDefragmentTestConstants.FILENAME);
+		ObjectContainer db=openDatabase();
 		Query query=db.query();
 		query.constrain(Holder.class);
 		ObjectSet result=query.execute();
-		Assert.areEqual(NUMVECTORS+1,result.size());
+		Assert.areEqual(1,result.size());
 		db.close();
 	}
 
@@ -56,16 +46,14 @@ public class SlotDefragmentVectorTestCase implements TestCase {
 
 	private void store() {
 		new File(SlotDefragmentTestConstants.FILENAME).delete();
-		ObjectContainer db=Db4o.openFile(Db4o.newConfiguration(),SlotDefragmentTestConstants.FILENAME);
-		for(int vectorIdx=0;vectorIdx<NUMVECTORS;vectorIdx++) {
-			Vector vector=new Vector(NUMENTRIES);
-			for(int entryIdx=0;entryIdx<NUMENTRIES;entryIdx++) {
-				Object obj=(entryIdx%2==0 ? (Object)new Data(entryIdx) : (Object)String.valueOf(entryIdx));
-				vector.add(obj);
-			}
-			db.set(new Holder(vector));
-		}
-		db.set(new Holder(new Vector(0)));
+		ObjectContainer db=openDatabase();
+		db.set(new Holder(new Vector()));
 		db.close();
+	}
+
+	private ObjectContainer openDatabase() {
+		Configuration config = Db4o.newConfiguration();
+		config.generateUUIDs(Integer.MAX_VALUE);
+		return Db4o.openFile(config,SlotDefragmentTestConstants.FILENAME);
 	}
 }
