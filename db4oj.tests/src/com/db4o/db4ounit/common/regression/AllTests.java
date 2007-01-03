19820d18ceeb8edb2156ee800fca2651bc4e90ed
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-01-03 11:55:31 +0000

    #COR-57 testcase (disabled for now)

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/regression/AllTests.java
@@ -0,0 +1,20 @@
+/* Copyright (C) 2004 - 2006 db4objects Inc. http://www.db4o.com */
+
+package com.db4o.db4ounit.common.regression;
+
+import db4ounit.extensions.Db4oTestSuite;
+
+
+public class AllTests extends Db4oTestSuite {
+
+	protected Class[] testCases() {
+		return new Class[] {
+			COR57TestCase.class,
+		};
+	}
+	
+	public static void main(String[] args) {
+		new AllTests().runSolo();
+	}
+
+}
