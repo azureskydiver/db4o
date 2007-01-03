9677bb6aa83497184b6ec440819a5ef131fb49df
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-01-03 11:55:31 +0000

    #COR-57 testcase (disabled for now)

--- db4oj.tests/src/com/db4o/db4ounit/common/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/AllTests.java
@@ -27,6 +27,7 @@ public class AllTests extends Db4oTestSuite {
 			com.db4o.db4ounit.common.header.AllTests.class,
 			com.db4o.db4ounit.common.interfaces.AllTests.class,
 			com.db4o.db4ounit.common.reflect.AllTests.class,
+			com.db4o.db4ounit.common.regression.AllTests.class,
 			com.db4o.db4ounit.common.querying.AllTests.class,
 			com.db4o.db4ounit.common.soda.AllTests.class,
 			com.db4o.db4ounit.common.types.AllTests.class,
