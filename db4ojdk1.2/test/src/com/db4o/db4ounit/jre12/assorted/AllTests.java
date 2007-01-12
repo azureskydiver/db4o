6d6314f088cf2a0d06b4435e7a7efd92c6ced905
jbevain <jbevain@915b37e7-0715-0410-8bc3-b14b59444072>
2007-01-12 16:10:53 +0000

    Test for final fields.

--- db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/assorted/AllTests.java
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/assorted/AllTests.java
@@ -4,6 +4,7 @@ import db4ounit.extensions.*;
 public class AllTests extends Db4oTestSuite {
 	protected Class[] testCases() {
 		return new Class[] {
+				FinalFieldTestCase.class,
 				TranslatorStoredClassesTestCase.class,
 		};
 	}
