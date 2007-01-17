0329dea99a536d7ae0b41ed4020d378fe97699c1
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2007-01-17 16:12:10 +0000

    disable final fields test case - final fields won't work for JDK1.3-1.4 :(

--- db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/assorted/AllTests.java
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/assorted/AllTests.java
@@ -4,7 +4,7 @@ import db4ounit.extensions.*;
 public class AllTests extends Db4oTestSuite {
 	protected Class[] testCases() {
 		return new Class[] {
-				FinalFieldTestCase.class,
+				//FinalFieldTestCase.class,
 				TranslatorStoredClassesTestCase.class,
 		};
 	}
