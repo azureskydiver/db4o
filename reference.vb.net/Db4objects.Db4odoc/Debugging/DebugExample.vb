ea3e66702827a24f0739c9ef44aa37d8192c4b5e
tetyana_loskutova <tetyana_loskutova@915b37e7-0715-0410-8bc3-b14b59444072>
2007-01-18 18:08:23 +0000

    tl: #COR-89 Adding examples on how to configure the debug messages output

--- reference.vb.net/Db4objects.Db4odoc/Debugging/DebugExample.vb
+++ reference.vb.net/Db4objects.Db4odoc/Debugging/DebugExample.vb
@@ -10,11 +10,14 @@ Namespace Db4objects.Db4odoc.Debugging
 
         Public Shared Sub Main(ByVal args() As String)
             SetCars()
+            SetCarsWithFileOutput()
         End Sub
         ' end Main
 
         Public Shared Sub SetCars()
+            ' Set the debug message levet to the maximum
             Db4oFactory.Configure().MessageLevel(3)
+            ' Do some db4o operations
             File.Delete(YapFileName)
             Dim db As IObjectContainer = Db4oFactory.OpenFile(YapFileName)
             Try
@@ -33,6 +36,37 @@ Namespace Db4objects.Db4odoc.Debugging
             Db4oFactory.Configure().MessageLevel(0)
         End Sub
         ' end SetCars
+
+        Public Shared Sub SetCarsWithFileOutput()
+            ' Create StreamWriter for a file
+            Dim f As FileInfo = New FileInfo("Debug.txt")
+            Dim debugWriter As StreamWriter = f.CreateText()
+
+            ' Redirect debug output to the specified writer
+            Db4oFactory.Configure().SetOut(debugWriter)
+
+            ' Set the debug message levet to the maximum
+            Db4oFactory.Configure().MessageLevel(3)
+            ' Do some db4o operations
+            File.Delete(YapFileName)
+            Dim db As IObjectContainer = Db4oFactory.OpenFile(YapFileName)
+            Try
+                Dim car1 As Car = New Car("BMW")
+                db.Set(car1)
+                Dim car2 As Car = New Car("Ferrari")
+                db.Set(car2)
+                db.Deactivate(car1, 2)
+                Dim query As IQuery = db.Query()
+                query.Constrain(GetType(Car))
+                Dim results As IObjectSet = query.Execute()
+                ListResult(results)
+            Finally
+                db.Close()
+                debugWriter.Close()
+            End Try
+            Db4oFactory.Configure().MessageLevel(0)
+        End Sub
+        ' end SetCarsWithFileOutput
 
         Public Shared Sub ListResult(ByVal result As IObjectSet)
             Console.WriteLine(result.Count)
