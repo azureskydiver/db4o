a2483a0e79fcf5484929c8e59e3bd0212c9d63bc
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-01-19 13:09:10 +0000

    removed some extra space

--- db4oj/core/src/com/db4o/inside/fieldindex/FieldIndexProcessorResult.java
+++ db4oj/core/src/com/db4o/inside/fieldindex/FieldIndexProcessorResult.java
@@ -8,7 +8,6 @@ import com.db4o.inside.btree.*;
 
 public class FieldIndexProcessorResult {
 	
-	
 	public static final FieldIndexProcessorResult NO_INDEX_FOUND = new FieldIndexProcessorResult(null);
 
 	public static final FieldIndexProcessorResult FOUND_INDEX_BUT_NO_MATCH = new FieldIndexProcessorResult(null);
