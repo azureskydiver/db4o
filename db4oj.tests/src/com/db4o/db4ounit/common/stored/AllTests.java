5736f045d562fc1ae5f66ecf6db24d7017277bda
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-01-23 12:21:49 +0000

    fix the test case to also run on .net (ReflectClass#getName includes the assembly name)

--- db4oj.tests/src/com/db4o/db4ounit/common/stored/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/stored/AllTests.java
@@ -6,6 +6,10 @@ import db4ounit.extensions.*;
 
 public class AllTests extends Db4oTestSuite {
 
+	public static void main(String[] args) {
+		new AllTests().runSolo();
+	}
+	
 	protected Class[] testCases() {
 		return new Class[] {
 				ArrayStoredTypeTestCase.class,
