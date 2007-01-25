2c55960ed282a5ab63ea9b77bf786598d476180a
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-01-25 23:21:49 +0000

    az + cr: Functionality to be able to turn the class index off per class as a configuration setting.

--- db4oj.tests/src/com/db4o/db4ounit/common/classindex/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/classindex/AllTests.java
@@ -13,7 +13,8 @@ public class AllTests extends Db4oTestSuite {
 	protected Class[] testCases() {
         
         return new Class[] {
-        		ClassIndexTestCase.class,
+        	ClassIndexOffTestCase.class,
+        	ClassIndexTestCase.class,
 		};
     }
 }
