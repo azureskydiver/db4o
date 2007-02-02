8fcc7d8ba41f1051dec7fb0957140c692f395873
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-02-02 17:25:33 +0000

    cr + pr: #COR-74  Packages renamed. All .inside to .internal. All internal packages moved below .internal

--- db4oj/core/src/com/db4o/inside/fieldindex/IndexedNodeWithRange.java
+++
@@ -1,9 +0,0 @@
-/* Copyright (C) 2006   db4objects Inc.   http://www.db4o.com */
-
-package com.db4o.inside.fieldindex;
-
-import com.db4o.inside.btree.*;
-
-public interface IndexedNodeWithRange extends IndexedNode {
-	BTreeRange getRange();
-}
