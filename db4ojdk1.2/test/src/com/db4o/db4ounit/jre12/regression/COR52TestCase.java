e2a2b70734f8979cc8bbf3ed561a8a724594cbab
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-02-09 08:42:21 +0000

    Andrew: fixed COR-52. The bug is caused because db4o queries password from database, and if the global activation depth is set to 0, the password field is not initialized. Configuring the mininum activation depth of User.class to 1 solves this problem. The problem only occurs in db4oj>=1.2, which means we have another bug in db4oj query behaviour. :) Will raise a separated JIRA issue for the query problem.

---
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/regression/COR52TestCase.java
@@ -0,0 +1,33 @@
+/* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
+
+package com.db4o.db4ounit.jre12.regression;
+
+import java.io.File;
+
+import com.db4o.Db4o;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectServer;
+import com.db4o.internal.Config4Impl;
+
+import db4ounit.TestCase;
+
+public class COR52TestCase implements TestCase {
+	private static final String TEST_FILE = "COR52.dat";
+	public void test() throws Exception {
+		int originalActivationDepth = ((Config4Impl) Db4o.configure())
+				.activationDepth();
+		Db4o.configure().activationDepth(0);
+		ObjectServer server = Db4o.openServer(TEST_FILE, 1111);
+		try {
+			server.grantAccess("db4o", "db4o");
+			ObjectContainer oc = Db4o.openClient("localhost", 1111, "db4o",
+					"db4o");
+			oc.close();
+		} finally {
+			Db4o.configure().activationDepth(originalActivationDepth);
+			new File(TEST_FILE).delete();
+			server.close();
+		}
+
+	}
+}
