ae34c0e5be5f84d3b5066905a2ed91deebd2fa79
albert_kwan <albert_kwan@915b37e7-0715-0410-8bc3-b14b59444072>
2007-02-12 14:58:36 +0000

    ak: Extracted ClientServerReplicationProvider out of Db4oReplicationProvider.

---
+++ drs/src/core/com/db4o/drs/db4o/ClientServerReplicationProvider.java
@@ -0,0 +1,20 @@
+/* Copyright (C) 2004 - 2005  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.drs.db4o;
+
+import com.db4o.ObjectContainer;
+
+class ClientServerReplicationProvider extends FileReplicationProvider {
+	public ClientServerReplicationProvider(ObjectContainer objectContainer) {		 
+		super(objectContainer, "null");
+	}
+
+	public ClientServerReplicationProvider(ObjectContainer objectContainer, String name) {
+		super(objectContainer, name);
+		System.out.println("ClientServerReplicationProvider");
+	}
+	
+	protected void refresh(Object obj) {
+		_stream.refresh(obj, 1);
+	}
+}
