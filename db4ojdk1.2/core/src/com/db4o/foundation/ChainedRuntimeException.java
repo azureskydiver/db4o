b2faec4cd757c15a9a16916a74390303947edd74
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-03-03 16:40:09 +0000

    exception hierarchy refactoring so we can support exception chaining on every platform

---
+++ db4ojdk1.2/core/src/com/db4o/foundation/ChainedRuntimeException.java
@@ -0,0 +1,21 @@
+/* Copyright (C) 2004 - 2006 db4objects Inc. http://www.db4o.com */
+
+package com.db4o.foundation;
+
+
+/**
+ * Just delegates to the platform chaining mechanism.
+ */
+public abstract class ChainedRuntimeException extends RuntimeException {
+	
+	public ChainedRuntimeException() {
+	}
+	
+	public ChainedRuntimeException(String msg) {
+		this(msg, null);
+	}
+	
+	public ChainedRuntimeException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
