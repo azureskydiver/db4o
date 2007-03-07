04736baf0c759b5c3ff3234bf4370602cc8f9318
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-03-07 23:34:43 +0000

    fixed PreventMultiOpenTestCase on .net

--- db4oj/core/src/com/db4o/ext/DatabaseFileLockedException.java
+++ db4oj/core/src/com/db4o/ext/DatabaseFileLockedException.java
@@ -2,10 +2,18 @@
 
 package com.db4o.ext;
 
+import com.db4o.foundation.ChainedRuntimeException;
+
 /**
  * this Exception is thrown during any of the db4o open calls
  * if the database file is locked by another process.
  * @see com.db4o.Db4o#openFile
  */
-public class DatabaseFileLockedException extends RuntimeException{
+public class DatabaseFileLockedException extends ChainedRuntimeException {
+	public DatabaseFileLockedException() {
+	}
+	
+	public DatabaseFileLockedException(Exception cause) {
+		super(cause.getMessage(), cause);
+	}
 }
