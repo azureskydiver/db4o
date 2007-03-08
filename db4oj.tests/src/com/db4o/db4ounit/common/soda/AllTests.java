e575cfb0704f07176a56da8ef6622045a832a7e9
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-03-08 17:07:42 +0000

    cr + pr: #COR-18 fix for multiple sort

--- db4oj.tests/src/com/db4o/db4ounit/common/soda/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/soda/AllTests.java
@@ -18,6 +18,7 @@ public class AllTests extends Db4oTestSuite {
 		return new Class[]{
 				com.db4o.db4ounit.common.soda.arrays.AllTests.class,
 				CollectionIndexedJoinTestCase.class,
+				SortMultipleTestCase.class,
 				STBooleanTestCase.class,				
 				STBooleanWUTestCase.class,
 				STByteTestCase.class,
