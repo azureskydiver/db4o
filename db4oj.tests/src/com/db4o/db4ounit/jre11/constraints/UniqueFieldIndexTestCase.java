91072777147e5310386a75d2a306ae02aabb7c80
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-03-12 19:01:24 +0000

    cr: #COR-178 test case that demonstrates the behaviour of the new unique constraints.

---
+++ db4oj.tests/src/com/db4o/db4ounit/jre11/constraints/UniqueFieldIndexTestCase.java
@@ -0,0 +1,53 @@
+/* Copyright (C) 2007  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.db4ounit.jre11.constraints;
+
+import com.db4o.config.*;
+import com.db4o.constraints.*;
+
+import db4ounit.*;
+import db4ounit.extensions.*;
+
+
+public class UniqueFieldIndexTestCase extends AbstractDb4oTestCase{
+	
+	public static void main(String[] arguments) {
+		new UniqueFieldIndexTestCase().runSolo();
+	}
+	
+	public static class Item {
+		
+		public String	_str;
+
+		public Item(){
+		}
+		
+		public Item(String str){
+			_str = str;
+		}
+		
+	}
+	
+	protected void configure(Configuration config) {
+		super.configure(config);
+		config.objectClass(Item.class).objectField("_str").indexed(true);
+		config.add(new UniqueFieldValueConstraint(Item.class, "_str"));
+	}
+	
+	protected void store() throws Exception {
+		store(new Item("1"));
+		store(new Item("2"));
+		store(new Item("3"));
+	}
+	
+	public void testNewViolates(){
+		store(new Item("2"));
+		Assert.expect(UniqueFieldValueConstraintViolationException.class, new CodeBlock() {
+			public void run() throws Exception {
+				db().commit();
+			}
+		});
+		db().rollback();
+	}
+
+}
