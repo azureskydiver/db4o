0f7181517c8d5141101637bd21216b8877cfd869
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-03-14 23:35:10 +0000

    cr: #COR-178 Constraints for unique fields now work for C/S, #COR-437 C/S commit now is synchronous

---
+++ db4oj.tests/src/com/db4o/db4ounit/jre11/assorted/ClientServerThrowsOnCommitTestCase.java
@@ -0,0 +1,39 @@
+/* Copyright (C) 2007  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.db4ounit.jre11.assorted;
+
+import com.db4o.events.*;
+import com.db4o.ext.*;
+
+import db4ounit.*;
+import db4ounit.extensions.*;
+import db4ounit.extensions.fixtures.*;
+
+
+public class ClientServerThrowsOnCommitTestCase extends AbstractDb4oTestCase implements OptOutSolo{
+	
+	public static void main(String[] arguments) {
+		new ClientServerThrowsOnCommitTestCase().runClientServer();
+	}
+	
+	public static class ExpectedException extends Db4oException{
+		public ExpectedException() {
+			super("");
+		}
+	}
+
+	public void test(){
+		final EventListener4 listener = new EventListener4() {
+			public void onEvent(Event4 e, EventArgs args) {
+				throw new ExpectedException();
+			}
+		};
+		EventRegistryFactory.forObjectContainer(fileSession()).committing().addListener(listener);
+		Assert.expect(ExpectedException.class, new CodeBlock() {
+			public void run() throws Exception {
+				db().commit();
+			}
+		});
+		EventRegistryFactory.forObjectContainer(fileSession()).committing().removeListener(listener);
+	}
+}
