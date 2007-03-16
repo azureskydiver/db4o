d70332f725a8f3cae0465290fd507e191b4e9053
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-03-16 03:53:22 +0000

    az: check in test case for fatal error on native query. It's ignored now since we still swallow exceptions during nq.

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/fatalerror/Item.java
@@ -0,0 +1,14 @@
+/* Copyright (C) 2004 - 2006 db4objects Inc. http://www.db4o.com */
+package com.db4o.db4ounit.common.fatalerror;
+
+public class Item {
+	public String str;
+
+	public Item(String s) {
+		str = s;
+	}
+	
+	public String toString() {
+		return str;
+	}
+}
---
+++ db4oj.tests/src/com/db4o/db4ounit/common/fatalerror/NativeQueryTestCase.java
@@ -0,0 +1,43 @@
+/* Copyright (C) 2004 - 2006 db4objects Inc. http://www.db4o.com */
+package com.db4o.db4ounit.common.fatalerror;
+
+import com.db4o.ObjectSet;
+import com.db4o.ext.DatabaseFileLockedException;
+import com.db4o.query.Predicate;
+
+import db4ounit.Assert;
+import db4ounit.CodeBlock;
+import db4ounit.extensions.AbstractDb4oTestCase;
+
+public class NativeQueryTestCase extends AbstractDb4oTestCase {
+
+	public static void main(String[] args) {
+		new NativeQueryTestCase().runSoloAndClientServer();
+	}
+
+	protected void store() throws Exception {
+		store(new Item("hello"));
+	}
+
+	public void _test() {
+		Assert.expect(NQError.class, new CodeBlock() {
+			public void run() throws Exception {
+				Predicate fatalErrorPredicate = new FatalErrorPredicate();
+				db().query(fatalErrorPredicate);
+			}
+		});
+		Assert.isTrue(db().isClosed());
+	}
+
+	public static class FatalErrorPredicate extends Predicate {
+		public boolean match(Item item) {
+			throw new NQError("nq error!");
+		}
+	}
+	
+	public static class NQError extends Error {
+		public NQError(String msg) {
+			super(msg);
+		}
+	}
+}
