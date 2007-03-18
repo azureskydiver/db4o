d5d642946274fe5792f8d55d67a42925de550dfa
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-03-18 14:40:03 +0000

    people should install sandcastle from now on

--- db4obuild/lib/sandcastle/Presentation/vs2005/copyHavana.bat
+++
@@ -1,8 +0,0 @@
-copy "%DXROOT%\Presentation\vs2005\hxs\test.HxC" Output
-copy "%DXROOT%\Presentation\vs2005\hxs\test.HxF" Output
-copy "%DXROOT%\Presentation\vs2005\hxs\test_A.HxK" Output
-copy "%DXROOT%\Presentation\vs2005\hxs\test_B.HxK" Output
-copy "%DXROOT%\Presentation\vs2005\hxs\test_F.HxK" Output
-copy "%DXROOT%\Presentation\vs2005\hxs\test_K.HxK" Output
-copy "%DXROOT%\Presentation\vs2005\hxs\test_N.HxK" Output
-copy "%DXROOT%\Presentation\vs2005\hxs\test_S.HxK" Output
