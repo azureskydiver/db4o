737ed011d863166b2a48a40047dc7c8b701a3805
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-03-23 14:40:39 +0000

    az + cr: Adjusting API docs

--- db4oj/core/src/com/db4o/io/UncheckedIOException.java
+++ db4oj/core/src/com/db4o/io/UncheckedIOException.java
@@ -1,7 +1,12 @@
+/* Copyright (C) 2007 db4objects Inc.  http://www.db4o.com */
+
 package com.db4o.io;
 
 import com.db4o.foundation.ChainedRuntimeException;
 
+/**
+ * @exclude
+ */
 public class UncheckedIOException extends ChainedRuntimeException {
 
 	public UncheckedIOException() {
