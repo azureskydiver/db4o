1f097ea3a63818e2eb50a344e99eec995fe66de8
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-03-25 12:48:32 +0000

    Ported database identity issue from 6.1

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/ext/Db4oDatabaseTestCase.java
@@ -0,0 +1,18 @@
+package com.db4o.db4ounit.common.ext;
+
+import com.db4o.ext.Db4oDatabase;
+
+import db4ounit.*;
+
+public class Db4oDatabaseTestCase implements TestCase {
+	
+	public void testGenerate() {
+		Db4oDatabase db1 = Db4oDatabase.generate();
+		Db4oDatabase db2 = Db4oDatabase.generate();
+		Db4oDatabase db3 = Db4oDatabase.generate();
+		Assert.areNotEqual(db1, db2);
+		Assert.areNotEqual(db1, db3);
+		Assert.areNotEqual(db2, db3);
+	}
+
+}
