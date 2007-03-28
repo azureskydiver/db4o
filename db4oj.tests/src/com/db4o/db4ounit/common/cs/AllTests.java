c6a9c1517b694fa76b5bc99aaf0e271de9693362
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-03-28 14:33:27 +0000

    az + cr: #COR-433 Separate CS test package.

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/cs/AllTests.java
@@ -0,0 +1,23 @@
+/* Copyright (C) 2004 - 2006 db4objects Inc. http://www.db4o.com */
+
+package com.db4o.db4ounit.common.cs;
+
+
+
+import com.db4o.db4ounit.common.assorted.*;
+
+import db4ounit.extensions.*;
+
+public class AllTests extends Db4oTestSuite {
+
+	public static void main(String[] args) {
+		new AllTests().runClientServer();
+    }
+
+	protected Class[] testCases() {
+		return new Class[] {
+            ClientServerPingTestCase.class,
+            ServerRevokeAccessTestCase.class,
+		};
+	}
+}
