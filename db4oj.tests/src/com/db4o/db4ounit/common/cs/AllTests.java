365dd99159a850230cf8aae7c0dc1324824c998b
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-03-31 22:37:20 +0000

    az: test case for server-side timeout.

--- db4oj.tests/src/com/db4o/db4ounit/common/cs/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/cs/AllTests.java
@@ -17,6 +17,7 @@ public class AllTests extends Db4oTestSuite {
             ClientServerPingTestCase.class,
             SendMessageToClientTestCase.class,
             ServerRevokeAccessTestCase.class,
+            ServerTimeoutTestCase.class,
 		};
 	}
 }
