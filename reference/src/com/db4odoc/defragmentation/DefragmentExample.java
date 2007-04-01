ed4871d105c7f45dd956d1b80eeda92c16057f77
tetyana_loskutova <tetyana_loskutova@915b37e7-0715-0410-8bc3-b14b59444072>
2007-04-01 15:57:29 +0000

    tl: updated defragment example #COR-354

--- reference/src/com/db4odoc/defragmentation/DefragmentExample.java
+++ reference/src/com/db4odoc/defragmentation/DefragmentExample.java
@@ -2,24 +2,64 @@
 
 package com.db4odoc.defragmentation;
 
+import java.io.IOException;
+
+import com.db4o.Db4o;
+import com.db4o.defragment.AvailableClassFilter;
+import com.db4o.defragment.Defragment;
+import com.db4o.defragment.DefragmentConfig;
+import com.db4o.defragment.DefragmentInfo;
+import com.db4o.defragment.DefragmentListener;
+import com.db4o.defragment.TreeIDMapping;
+
 
 public class DefragmentExample {
 
+	private static final String DB_FILE = "test.db4o";
+	private static final String BACKUP_FILE = "test.bap";
+	
 	public static void main(String[] args){
-		runDefragment();
+		simplestDefragment();
+		configuredDefragment();
+		defragmentWithListener();
 	}
 	// end main
 	
-	public static void runDefragment(){
-		com.db4o.defragment.DefragmentConfig config=
-			new com.db4o.defragment.DefragmentConfig("sample.yap","sample.bap");
+	private static void simplestDefragment(){
+		try {
+	    	Defragment.defrag(DB_FILE);
+	    } catch (IOException ex){
+	    	System.out.println(ex.toString());
+	    }
+	}
+	// end simplestDefragment
+	
+	private static void configuredDefragment(){
+		DefragmentConfig config=new DefragmentConfig(DB_FILE, BACKUP_FILE, new TreeIDMapping());
+		config.objectCommitFrequency(5000);
+		config.db4oConfig(Db4o.cloneConfiguration());
 		config.forceBackupDelete(true);
-		config.storedClassFilter(new com.db4o.defragment.AvailableClassFilter());
+		config.storedClassFilter(new AvailableClassFilter());
+		config.upgradeFile(DB_FILE + ".upg");
 	    try {
-	    	com.db4o.defragment.Defragment.defrag(config);
+	    	Defragment.defrag(config);
 	    } catch (Exception ex){
 	    	System.out.println(ex.toString());
 	    }
 	}
-	// end runDefragment
+	// end configuredDefragment
+	
+	private static void defragmentWithListener(){
+		DefragmentConfig config=new DefragmentConfig(DB_FILE);
+		try {
+	    	Defragment.defrag(config, new DefragmentListener() {
+				public void notifyDefragmentInfo(DefragmentInfo info) {
+					System.err.println(info);
+				}
+			});
+	    } catch (Exception ex){
+	    	System.out.println(ex.toString());
+	    }
+	}
+	// end defragmentWithListener
 }
