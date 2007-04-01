5825cce25733c12072fdfecda805f91c22c14b75
tetyana_loskutova <tetyana_loskutova@915b37e7-0715-0410-8bc3-b14b59444072>
2007-04-01 15:57:57 +0000

    tl: updated defragment example #COR-354

--- reference.net/db4o-reference-chapters/src/Db4objects.Db4odoc/Defragmentation/DefragmentExample.cs
+++ reference.net/db4o-reference-chapters/src/Db4objects.Db4odoc/Defragmentation/DefragmentExample.cs
@@ -1,22 +1,43 @@
+/* Copyright (C) 2004 - 2006 db4objects Inc. http://www.db4o.com */
 using System;
+using Db4objects.Db4o;
 using Db4objects.Db4o.Defragment;
 
 namespace Db4objects.Db4odoc.Defragmentation
 {
     class DefragmentExample
     {
+        private const string DB_FILE = "test.db4o";
+        private const string BACKUP_FILE = "test.bap";
 
         public static void Main(string[] args)
         {
-            RunDefragment();
+            SimplestDefragment();
+            ConfiguredDefragment();
+            DefragmentWithListener();
         }
         // end Main
 
-        public static void RunDefragment()
+        public static void SimplestDefragment()
         {
-            DefragmentConfig config = new DefragmentConfig("sample.yap", "sample.bap");
+            try
+            {
+                Defragment.Defrag(DB_FILE);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+        // end SimplestDefragment
+
+        public static void ConfiguredDefragment()
+        {
+            DefragmentConfig config = new DefragmentConfig(DB_FILE, BACKUP_FILE , new TreeIDMapping());
+            config.ObjectCommitFrequency(5000);
+            config.Db4oConfig(Db4oFactory.CloneConfiguration());
             config.ForceBackupDelete(true);
-            config.StoredClassFilter(new AvailableTypeFilter());
+            config.UpgradeFile(DB_FILE + ".upg");
             try
             {
                 Defragment.Defrag(config);
@@ -26,6 +47,29 @@ namespace Db4objects.Db4odoc.Defragmentation
                 System.Console.WriteLine(ex.Message);
             }
         }
-        // end RunDefragment
+        // end ConfiguredDefragment
+
+        public static void DefragmentWithListener()
+        {
+            DefragmentConfig config = new DefragmentConfig(DB_FILE, BACKUP_FILE);
+            try
+            {
+                Defragment.Defrag(config, new DefragmentListener());
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+        // end DefragmentWithListener
     }
+
+    public class DefragmentListener : IDefragmentListener
+    {
+        void IDefragmentListener.NotifyDefragmentInfo(DefragmentInfo info)
+        {
+            throw new System.Exception("The method or operation is not implemented.");
+        }
+    }
+    // end DefragmentListener
 }
