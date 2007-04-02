5a07fd2c013e480e478fdc056a97225a55b948c5
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-04-02 14:13:55 +0000

    Added jdk 5 enum test cases to db4ounit test suite; Added orphan ComparatorSortTestCase to db4ounit test suite;

--- db4ojdk5/test/src/com/db4o/db4ounit/jre5/AllTestsDb4oUnitJdk5.java
+++ db4ojdk5/test/src/com/db4o/db4ounit/jre5/AllTestsDb4oUnitJdk5.java
@@ -7,12 +7,14 @@ import db4ounit.extensions.Db4oTestSuite;
 public class AllTestsDb4oUnitJdk5 extends Db4oTestSuite {
 
 	public static void main(String[] args) {
-		System.exit(new AllTestsDb4oUnitJdk5().runSoloAndClientServer());
+		System.exit(new AllTestsDb4oUnitJdk5().runSolo());
 	}
 
 	@Override
 	protected Class[] testCases() {
 		return new Class[] {
+			com.db4o.db4ounit.jre5.enums.AllTests.class,
+			com.db4o.db4ounit.jre5.generics.AllTests.class,
 			com.db4o.db4ounit.jre12.AllTestsJdk1_2.class,
 			IndexedAnnotationTestCase.class,
 		};
