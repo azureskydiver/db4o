38595d6d0fe5552a2ca9a4fe0f5ccf260c6515b5
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-04-02 13:45:10 +0000

    use RandomAccessFileAdapter until all issues with CachedIoAdapter are sorted out

--- db4oj/core/src/com/db4o/internal/Config4Impl.java
+++ db4oj/core/src/com/db4o/internal/Config4Impl.java
@@ -127,7 +127,8 @@ public final class Config4Impl implements Configuration, DeepClone,
     
 	private final static KeySpec WEAK_REFERENCES=new KeySpec(true);
     
-	private final static KeySpec IOADAPTER=new KeySpec(new CachedIoAdapter(new RandomAccessFileAdapter()));
+	//private final static KeySpec IOADAPTER=new KeySpec(new CachedIoAdapter(new RandomAccessFileAdapter()));
+	private final static KeySpec IOADAPTER=new KeySpec(new RandomAccessFileAdapter());
     
     	// NOTE: activate this config to trigger the defragment failure
     	//= new NIOFileAdapter(512,3);
