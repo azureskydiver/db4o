148e8551b45c9a429724a7bc29004bfa1800c387
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-04-05 18:18:30 +0000

    ComparatorSortTestCase fix for the build; There are two versions of ComparatorSortTestCase, one in db4oj.tests for jdk < 5 and another in db4ojdk5 for jdk5+, they have to have the same exact name so the db4ojdk5 overwrites the db4oj.tests one during the jdk5 build

--- db4ojdk5/test/src/com/db4o/db4ounit/common/assorted/ComparatorSortTestCase.java
+++ db4ojdk5/test/src/com/db4o/db4ounit/common/assorted/ComparatorSortTestCase.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2004 - 2006  db4objects Inc.  http://www.db4o.com */
 
-package com.db4o.db4ounit.jre5.generics;
+package com.db4o.db4ounit.common.assorted;
 
 import com.db4o.ObjectSet;
 import com.db4o.config.Configuration;
