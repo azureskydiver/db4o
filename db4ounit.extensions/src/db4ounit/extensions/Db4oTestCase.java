506adbacd2b29fc3244aabbf2028a3e7d9bed4c6
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-04-16 08:15:39 +0000

    az: Removed db() method in Db4oTestCase interface, which is unnecesasry, and added copyright info.

--- db4ounit.extensions/src/db4ounit/extensions/Db4oTestCase.java
+++ db4ounit.extensions/src/db4ounit/extensions/Db4oTestCase.java
@@ -1,9 +1,8 @@
+/* Copyright (C) 2004 - 2007  db4objects Inc.  http://www.db4o.com */
 package db4ounit.extensions;
-
-import com.db4o.ext.*;
 
 import db4ounit.*;
 
 public interface Db4oTestCase extends TestCase, TestLifeCycle {
-	ExtObjectContainer db() throws Exception;
+	
 }
