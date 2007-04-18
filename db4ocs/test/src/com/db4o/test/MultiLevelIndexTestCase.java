814d3a46136118f2949ea6f5177591f83bce3020
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-04-18 15:07:58 +0000

    az: #UNIT-9 Migrated some concurrency test cases.

--- db4ocs/test/src/com/db4o/test/MultiLevelIndexTestCase.java
+++ db4ocs/test/src/com/db4o/test/MultiLevelIndexTestCase.java
@@ -1,18 +1,21 @@
-/* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
+/* Copyright (C) 2004 - 2007   db4objects Inc.   http://www.db4o.com */
 package com.db4o.test;
 
 import com.db4o.*;
 import com.db4o.config.*;
 import com.db4o.ext.*;
 import com.db4o.query.*;
-import com.db4o.test.config.*;
 
 import db4ounit.*;
 import db4ounit.extensions.*;
 
-public class MultiLevelIndex extends AbstractDb4oTestCase {
+public class MultiLevelIndexTestCase extends Db4oClientServerTestCase {
 
-	public MultiLevelIndex _child;
+	public static void main(String[] args) {
+		new MultiLevelIndexTestCase().runConcurrency();
+	}
+
+	public MultiLevelIndexTestCase _child;
 
 	public int _i;
 
@@ -23,43 +26,43 @@ public class MultiLevelIndex extends AbstractDb4oTestCase {
 		config.objectClass(this).objectField("_i").indexed(true);
 	}
 
-	public void store(ExtObjectContainer oc) {
-		store1(oc, 3);
-		store1(oc, 2);
-		store1(oc, 5);
-		store1(oc, 1);
+	public void store() {
+		store(3);
+		store(2);
+		store(5);
+		store(1);
 		for (int i = 6; i < 103; i++) {
-			store1(oc, i);
+			store(i);
 		}
 	}
 
-	private void store1(ExtObjectContainer oc, int val) {
-		MultiLevelIndex root = new MultiLevelIndex();
+	private void store(int val) {
+		MultiLevelIndexTestCase root = new MultiLevelIndexTestCase();
 		root._i = val;
-		root._child = new MultiLevelIndex();
+		root._child = new MultiLevelIndexTestCase();
 		root._child._level = 1;
 		root._child._i = -val;
-		oc.set(root);
+		store(root);
 	}
 
 	public void conc1(ExtObjectContainer oc) {
 		Query q = oc.query();
-		q.constrain(MultiLevelIndex.class);
+		q.constrain(MultiLevelIndexTestCase.class);
 		q.descend("_child").descend("_i").constrain(new Integer(-102));
 		ObjectSet objectSet = q.execute();
 		Assert.areEqual(1, objectSet.size());
-		MultiLevelIndex mli = (MultiLevelIndex) objectSet.next();
+		MultiLevelIndexTestCase mli = (MultiLevelIndexTestCase) objectSet.next();
 		Assert.areEqual(102, mli._i);
 	}
 
 	public void conc2(ExtObjectContainer oc, int seq) {
-		oc.configure().objectClass(MultiLevelIndex.class).cascadeOnUpdate(true);
+		oc.configure().objectClass(MultiLevelIndexTestCase.class).cascadeOnUpdate(true);
 		Query q = oc.query();
-		q.constrain(MultiLevelIndex.class);
+		q.constrain(MultiLevelIndexTestCase.class);
 		q.descend("_child").descend("_i").constrain(new Integer(seq - 102));
 		ObjectSet objectSet = q.execute();
 		Assert.areEqual(1, objectSet.size());
-		MultiLevelIndex mli = (MultiLevelIndex) objectSet.next();
+		MultiLevelIndexTestCase mli = (MultiLevelIndexTestCase) objectSet.next();
 		Assert.areEqual(102 - seq, mli._i);
 		mli._child._i = -(seq + 201);
 		oc.set(mli);
@@ -67,12 +70,11 @@ public class MultiLevelIndex extends AbstractDb4oTestCase {
 
 	public void check2(ExtObjectContainer oc) {
 		Query q = oc.query();
-		q.constrain(MultiLevelIndex.class);
+		q.constrain(MultiLevelIndexTestCase.class);
 		q.descend("_child").descend("_i").constrain(new Integer(-200))
 				.smaller();
 		ObjectSet objectSet = q.execute();
-		Assert.areEqual(TestConfigure.CONCURRENCY_THREAD_COUNT, objectSet
-				.size());
+		Assert.areEqual(threadCount(), objectSet.size());
 	}
 
 }
