0bd2bc6fdb77ac4cb53edbf11b11da22bfdc7ab4
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-04-30 14:33:05 +0000

    cr: #COR-568 Since the new BTreeFreespaceManager needs two integers for the two BTrees instead of just one address, I wrote a generic persistence service to map one single int to multiple ints. This is pretty much the simplest possible demo for how PersistentBase can be used.

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/assorted/PersistentIntegerArrayTestCase.java
@@ -0,0 +1,33 @@
+/* Copyright (C) 2007  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.db4ounit.common.assorted;
+
+import com.db4o.internal.*;
+
+import db4ounit.*;
+import db4ounit.extensions.*;
+import db4ounit.extensions.fixtures.*;
+
+
+/**
+ * @exclude
+ */
+public class PersistentIntegerArrayTestCase extends AbstractDb4oTestCase implements OptOutCS{
+	
+	public static void main(String[] arguments) {
+		new PersistentIntegerArrayTestCase().runSolo();
+	}
+	
+	public void test() throws Exception{
+		int[] original = new int[] {10,99,77};
+		PersistentIntegerArray arr = new PersistentIntegerArray(original);
+		arr.write(systemTrans());
+		int id = arr.getID();
+		reopen();
+		arr = new PersistentIntegerArray(id);
+		arr.read(systemTrans());
+		int[] copy = arr.array();
+		ArrayAssert.areEqual(original, copy);
+	}
+	
+}
