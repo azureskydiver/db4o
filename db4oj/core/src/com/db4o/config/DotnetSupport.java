c0a4161950808ad550cd22d4826033186198d4c7
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-05-02 21:32:39 +0000

    COR-595 - DotnetSupport configuration item that adds the required aliases to support reading a .net generated data file from java

---
+++ db4oj/core/src/com/db4o/config/DotnetSupport.java
@@ -0,0 +1,37 @@
+package com.db4o.config;
+
+import com.db4o.StaticClass;
+import com.db4o.StaticField;
+import com.db4o.ext.Db4oDatabase;
+import com.db4o.internal.ObjectContainerBase;
+
+/**
+ * Adds the basic configuration settings required to access a
+ * .net generated database from java.
+ * 
+ * The configuration only makes sure that database files can be
+ * successfuly open and things like UUIDs can be successfuly
+ * retrieved.
+ * 
+ * @sharpen.ignore
+ */
+public class DotnetSupport implements ConfigurationItem {
+
+	public void prepare(Configuration configuration) {
+		configuration.addAlias(
+				new TypeAlias(
+						"Db4objects.Db4o.Ext.Db4oDatabase, Db4objects.Db4o",
+						Db4oDatabase.class.getName()));
+		configuration.addAlias(
+				new TypeAlias(
+						"Db4objects.Db4o.StaticField, Db4objects.Db4o",
+						StaticField.class.getName()));
+		configuration.addAlias(
+				new TypeAlias(
+						"Db4objects.Db4o.StaticClass, Db4objects.Db4o",
+						StaticClass.class.getName()));
+	}
+	
+	public void apply(ObjectContainerBase container) {
+	}
+}
