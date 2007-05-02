7ed23f684fbca784d51bef9177f96ee522d0a307
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2007-05-02 03:54:20 +0000

    COR-592: first implementation of bind method

---
+++ db4otaj/src/com/db4o/ta/instrumentation/TransparentActivationInstrumentationConstants.java
@@ -0,0 +1,10 @@
+package com.db4o.ta.instrumentation;
+
+public abstract class TransparentActivationInstrumentationConstants {
+
+	public final static String ACTIVATOR_FIELD_NAME = "_db4o$$ta$$activator";
+	public final static String BIND_METHOD_NAME = "_db4o$$ta$$bind";
+	
+	private TransparentActivationInstrumentationConstants() {}
+
+}
--- db4otaj/test/com/db4o/ta/instrumentation/test/TransparentActivationClassLoaderTestCase.java
+++ db4otaj/test/com/db4o/ta/instrumentation/test/TransparentActivationClassLoaderTestCase.java
@@ -1,9 +1,12 @@
 package com.db4o.ta.instrumentation.test;
 
+import java.lang.reflect.*;
 import java.net.*;
 
+import com.db4o.*;
 import com.db4o.ta.*;
 import com.db4o.ta.instrumentation.*;
+import com.db4o.ta.internal.*;
 
 import db4ounit.*;
 
@@ -20,6 +23,22 @@ public class TransparentActivationClassLoaderTestCase implements TestLifeCycle {
 		Assert.areEqual(CLASS_NAME, clazz.getName());
 		Assert.areNotSame(ORIG_CLASS, clazz);
 		Assert.isTrue(Activatable.class.isAssignableFrom(clazz));
+		Field activatorField = clazz.getDeclaredField(TransparentActivationInstrumentationConstants.ACTIVATOR_FIELD_NAME);
+		Assert.areEqual(Activator.class, activatorField.getType());
+		assertFieldModifier(activatorField, Modifier.PRIVATE);
+		assertFieldModifier(activatorField, Modifier.TRANSIENT);
+		Method bindMethod = clazz.getDeclaredMethod(TransparentActivationInstrumentationConstants.BIND_METHOD_NAME, new Class[]{ObjectContainer.class});
+		Assert.isTrue((bindMethod.getModifiers() & Modifier.PUBLIC) > 0);
+		
+		activatorField.setAccessible(true);
+		Object obj = clazz.newInstance();
+		Assert.isNull(activatorField.get(obj));
+		bindMethod.invoke(obj, new Object[]{ null });
+		Assert.isNotNull(activatorField.get(obj));
+	}
+
+	private void assertFieldModifier(Field activatorField, int modifier) {
+		Assert.isTrue((activatorField.getModifiers()&modifier)>0);
 	}
 	
 	public void testOtherClassIsNotInstrumented() throws Exception {
@@ -36,7 +55,6 @@ public class TransparentActivationClassLoaderTestCase implements TestLifeCycle {
 		ClassFilter filter = new ByNameClassFilter(CLASS_NAME);
 		_loader = new TransparentActivationClassLoader(urls, baseLoader, filter);
 	}
-
 
 	public void tearDown() throws Exception {
 	}
