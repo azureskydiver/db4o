8522e90829ecc8500e334f341c2334b86bdda690
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-05-03 16:49:39 +0000

    az: #COR-456 Unchecked db4o IOException.

--- db4oj/core/src/com/db4o/Db4oIOException.java
+++ db4oj/core/src/com/db4o/Db4oIOException.java
@@ -1,10 +1,10 @@
 /* Copyright (C) 2007 db4objects Inc. http://www.db4o.com */
 package com.db4o;
 
-import com.db4o.ext.*;
+import com.db4o.foundation.*;
 
-public class Db4oIOException extends Db4oException {
+public class Db4oIOException extends ChainedRuntimeException {
 	public Db4oIOException(Throwable e) {
-		super(e);
+		super(e.getMessage(), e);
 	}
 }
