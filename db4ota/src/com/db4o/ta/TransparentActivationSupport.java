4583d6dc696e05f32a4f2cc154c2c7fd3c23680f
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-05-10 14:05:43 +0000

    Fixed c# conversion issues: * event handlers must be specified as anonymous classes; * class hierarchy accessibility must be consistent (derived classes cannot increase accessibility level);

--- db4ota/src/com/db4o/ta/TransparentActivationSupport.java
+++ db4ota/src/com/db4o/ta/TransparentActivationSupport.java
@@ -8,59 +8,68 @@ import com.db4o.reflect.*;
 
 public class TransparentActivationSupport implements ConfigurationItem {
 
-	public void apply(final ObjectContainerBase container) {
-		container.configure().activationDepth(0);
-		
-		EventRegistry factory = EventRegistryFactory.forObjectContainer(container);
-		factory.instantiated().addListener(
-			new EventListener4() {
-				public void onEvent(Event4 e, EventArgs args) {
-					ObjectEventArgs oea = (ObjectEventArgs)args;
-					if (oea.object() instanceof Activatable) {
-						((Activatable)oea.object()).bind(container);
-					}
-				}
-			});
-		factory.classRegistered().addListener(new TAClassRegistrationListener(container));
-	}
-	
 	public void prepare(Configuration configuration) {
 		// Nothing to do...
 	}
 
-	private static final class TAClassRegistrationListener implements EventListener4 {
-		private final ObjectContainerBase _container;
-		private final ReflectClass _activatableClass;
+	public void apply(final ObjectContainerBase container) {
+		container.configure().activationDepth(0);
 
-		private TAClassRegistrationListener(ObjectContainerBase container) {
+		EventRegistry factory = EventRegistryFactory
+				.forObjectContainer(container);
+
+		factory.instantiated().addListener(new EventListener4() {
+			public void onEvent(Event4 e, EventArgs args) {
+				ObjectEventArgs oea = (ObjectEventArgs) args;
+				if (oea.object() instanceof Activatable) {
+					((Activatable) oea.object()).bind(container);
+				}
+			}
+		});
+
+		final TADiagnosticProcessor processor = new TADiagnosticProcessor(container);
+		factory.classRegistered().addListener(new EventListener4() {
+			public void onEvent(Event4 e, EventArgs args) {
+				ClassEventArgs cea = (ClassEventArgs) args;
+				processor.onClassRegistered(cea.classMetadata());
+			}
+		});
+	}
+
+	private final class TADiagnosticProcessor {
+		private final ObjectContainerBase _container;
+
+		public TADiagnosticProcessor(ObjectContainerBase container) {
 			_container = container;
-			_activatableClass = container.reflector().forClass(Activatable.class);
 		}
 
-		public void onEvent(Event4 e, EventArgs args) {
-			ClassEventArgs cea = (ClassEventArgs) args;
-			ClassMetadata clazz = cea.classMetadata();
-//			if(Platform4.isDb4oClass(clazz.getName())) {
-//				return;
-//			}
+		public void onClassRegistered(ClassMetadata clazz) {
+			// if(Platform4.isDb4oClass(clazz.getName())) {
+			// return;
+			// }
 			ReflectClass reflectClass = clazz.classReflector();
-			if(_activatableClass.isAssignableFrom(reflectClass)) {
+			if (activatableClass().isAssignableFrom(reflectClass)) {
 				return;
 			}
-			if(hasOnlyPrimitiveFields(reflectClass)) {
+			if (hasOnlyPrimitiveFields(reflectClass)) {
 				return;
 			}
-			NotTransparentActivationEnabled diagnostic = new NotTransparentActivationEnabled(clazz);
+			NotTransparentActivationEnabled diagnostic = new NotTransparentActivationEnabled(
+					clazz);
 			DiagnosticProcessor processor = _container.handlers()._diagnosticProcessor;
 			processor.onDiagnostic(diagnostic);
 		}
 
+		private ReflectClass activatableClass() {
+			return _container.reflector().forClass(Activatable.class);
+		}
+
 		private boolean hasOnlyPrimitiveFields(ReflectClass clazz) {
 			ReflectClass curClass = clazz;
-			while(curClass != null) {
+			while (curClass != null) {
 				ReflectField[] fields = curClass.getDeclaredFields();
 				for (int fieldIdx = 0; fieldIdx < fields.length; fieldIdx++) {
-					if(!fields[fieldIdx].getFieldType().isPrimitive()) {
+					if (!fields[fieldIdx].getFieldType().isPrimitive()) {
 						return false;
 					}
 				}
@@ -68,6 +77,5 @@ public class TransparentActivationSupport implements ConfigurationItem {
 			}
 			return true;
 		}
-	}
-	
+	};
 }
