a5827e0d2d28515b70ec9736ce303628892b3ea4
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-05-10 12:47:08 +0000

    com.db4o.collections => com.db4o.ta.tests.collections

--- db4otaj/test/com/db4o/ta/instrumentation/test/integration/TransparentActivationSampleMain.java
+++ db4otaj/test/com/db4o/ta/instrumentation/test/integration/TransparentActivationSampleMain.java
@@ -4,10 +4,10 @@ import java.io.*;
 import java.util.*;
 
 import com.db4o.*;
-import com.db4o.collections.*;
 import com.db4o.config.*;
 import com.db4o.reflect.jdk.*;
 import com.db4o.ta.*;
+import com.db4o.ta.test.collections.*;
 
 import db4ounit.extensions.*;
 
