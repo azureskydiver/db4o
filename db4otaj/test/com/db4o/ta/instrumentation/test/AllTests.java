13a31453b785fd80dd6664cca827d205df79b72b
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-05-10 12:48:15 +0000

    AllTests.main

--- db4otaj/test/com/db4o/ta/instrumentation/test/AllTests.java
+++ db4otaj/test/com/db4o/ta/instrumentation/test/AllTests.java
@@ -5,6 +5,10 @@ import com.db4o.ta.instrumentation.test.integration.*;
 import db4ounit.extensions.*;
 
 public class AllTests extends Db4oTestSuite {
+	
+	public static void main(String[] args) {
+		new AllTests().runSolo();
+	}
 
 	protected Class[] testCases() {
 		return new Class[] {
