5830275036d595554b0338608697a75129d62cc2
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-05-15 21:10:09 +0000

    fixed text encoding issue caused by previous commit

--- db4oj.tests/src/com/db4o/db4ounit/common/btree/BTreeAssert.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/btree/BTreeAssert.java
@@ -1,4 +1,4 @@
-﻿/* Copyright (C) 2007   db4objects Inc.   http://www.db4o.com */
+/* Copyright (C) 2007   db4objects Inc.   http://www.db4o.com */
 package com.db4o.db4ounit.common.btree;
 
 import com.db4o.db4ounit.common.foundation.IntArrays4;
--- db4oj.tests/src/com/db4o/db4ounit/jre11/btree/BTreeNullKeyTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/jre11/btree/BTreeNullKeyTestCase.java
@@ -1,4 +1,4 @@
-﻿/* Copyright (C) 2007   db4objects Inc.   http://www.db4o.com */
+/* Copyright (C) 2007   db4objects Inc.   http://www.db4o.com */
 package com.db4o.db4ounit.jre11.btree;
 
 import com.db4o.db4ounit.common.btree.BTreeTestCaseBase;
