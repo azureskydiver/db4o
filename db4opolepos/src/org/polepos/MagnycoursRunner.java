8472689f4241145db82fe3faf0e2e8acfefdc474
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-05-17 16:08:40 +0000

    cr: added the Silverstone circuit

--- db4opolepos/src/org/polepos/MagnycoursRunner.java
+++ db4opolepos/src/org/polepos/MagnycoursRunner.java
@@ -1,8 +1,27 @@
-/* Copyright (C) 2007   db4objects Inc.   http://www.db4o.com */
+/* 
+This file is part of the PolePosition database benchmark
+http://www.polepos.org
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+MA  02111-1307, USA. */
+
 
 package org.polepos;
 
 import org.polepos.circuits.magnycours.*;
+import org.polepos.db4o.*;
 import org.polepos.framework.*;
 import org.polepos.runner.db4o.*;
 import org.polepos.teams.db4o.*;
