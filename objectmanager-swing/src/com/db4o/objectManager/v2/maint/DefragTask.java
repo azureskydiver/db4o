04f4f8dfdde79403c55115ddf98dc5886689a259
norberto_goussies <norberto_goussies@915b37e7-0715-0410-8bc3-b14b59444072>
2007-05-30 12:40:52 +0000

    ng: #OMR-88 using db4o configuration in defrag configuration

--- objectmanager-swing/src/com/db4o/objectManager/v2/maint/DefragTask.java
+++ objectmanager-swing/src/com/db4o/objectManager/v2/maint/DefragTask.java
@@ -1,5 +1,8 @@
 package com.db4o.objectManager.v2.maint;
 
+import com.db4o.Db4o;
+import com.db4o.config.Configuration;
+import com.db4o.config.DotnetSupport;
 import com.db4o.defragment.DefragmentConfig;
 import com.db4o.defragment.Defragment;
 import com.db4o.objectmanager.model.Db4oConnectionSpec;
@@ -16,11 +19,14 @@ public class DefragTask {
 	private static final int DEFAULT_OBJECT_COMMIT_FREQUENCY = 500000;
 
 	public DefragTask(Db4oConnectionSpec connectionSpec) throws IOException {
-		DefragmentConfig config = new DefragmentConfig(connectionSpec.getFullPath());
-		config.forceBackupDelete(true);
-		config.objectCommitFrequency(DEFAULT_OBJECT_COMMIT_FREQUENCY);
+		DefragmentConfig defragConfig = new DefragmentConfig(connectionSpec.getFullPath());
+
+		defragConfig.db4oConfig(connectionSpec.newConfiguration());
+		
+		defragConfig.forceBackupDelete(true);
+		defragConfig.objectCommitFrequency(DEFAULT_OBJECT_COMMIT_FREQUENCY);
 		//if(true)throw new IOException("test");
-		Defragment.defrag(config);
+		Defragment.defrag(defragConfig);
 
 	}
 }
