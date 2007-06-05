bdcdbc7f89ce6b184a1aa2c911611b932cfd436c
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2007-06-05 17:05:16 +0000

    Initial checkin OSGi core bundle module

---
+++ osgi-projects/db4o_osgi/src/com/db4o/osgi/Db4oActivator.java
@@ -0,0 +1,21 @@
+package com.db4o.osgi;
+
+import java.util.*;
+
+import org.osgi.framework.*;
+
+public class Db4oActivator implements BundleActivator {
+
+	public final static String BUNDLE_ID = "db4o_osgi";
+	
+	public void start(BundleContext context) throws Exception {
+		context.registerService(
+				Db4oService.class.getName(), 
+				new Db4oServiceFactory(), 
+				new Hashtable());		
+	}
+	
+	public void stop(BundleContext context) throws Exception {
+	}
+
+}
