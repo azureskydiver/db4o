c84f1dbc5271b252c3b134ff2b1108c757912a17
norberto_goussies <norberto_goussies@915b37e7-0715-0410-8bc3-b14b59444072>
2007-06-06 18:42:11 +0000

    ng : added System#exit call at Db4oTestActivator#start

--- osgi-projects/db4o_osgi_test/src/com/db4o/osgi/test/Db4oTestActivator.java
+++ osgi-projects/db4o_osgi_test/src/com/db4o/osgi/test/Db4oTestActivator.java
@@ -4,15 +4,28 @@ import java.util.*;
 
 import org.osgi.framework.*;
 
+import db4ounit.TestRunner;
+import db4ounit.TestSuite;
+import db4ounit.extensions.Db4oTestSuiteBuilder;
+
 public class Db4oTestActivator implements BundleActivator {
 
 	public static final String BUNDLE_ID = "db4o_osgi_test";
+	  private static final String FILENAME = "db4ounit.osgi.db4o";
 
 	public void start(BundleContext context) {
 		context.registerService(
 				Db4oTestService.class.getName(), 
 				new Db4oTestServiceImpl(context), 
-				new Hashtable());		
+				new Hashtable());
+				
+		
+		Db4oOSGiBundleFixture fixture = new Db4oOSGiBundleFixture(context, FILENAME);
+		Class testClass = 
+				com.db4o.db4ounit.jre12.AllTestsJdk1_2.class;
+		TestSuite suite = new Db4oTestSuiteBuilder(fixture, testClass).build();
+		System.exit((new TestRunner(suite).run()) > 0 ? 1 : 0);
+		
 	}
 
 	public void stop(BundleContext ctx) {
