993d0e1d42107a0daf0580c304ba144f7cee1562
richard_liang <richard_liang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-06-12 15:16:51 +0000

    rl: add AssemblyInfo to dRS.NET

---
+++ drs/overlay/drs.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿/* Copyright (C)  2007  db4objects Inc.  http://www.db4o.com */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Db4objects.Drs.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Db4objects, Inc.")]
+[assembly: AssemblyProduct("Db4objects.Drs.Test")]
+[assembly: AssemblyCopyright("Copyright \u00A9 Db4objects, Inc. 2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("6.3.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
