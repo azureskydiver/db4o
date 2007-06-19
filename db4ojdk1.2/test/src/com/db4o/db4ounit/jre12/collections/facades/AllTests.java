2ee817a9dfb90d8dca13936a56a659c228e978c9
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-06-19 03:28:29 +0000

    az: #COR-642 Tests for FastList (not completed yet).

---
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/collections/facades/AllTests.java
@@ -0,0 +1,19 @@
+/* Copyright (C) 2006  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.db4ounit.jre12.collections.facades;
+
+import db4ounit.extensions.*;
+
+public class AllTests extends Db4oTestSuite {
+    
+    public static void main(String[] arguments) {
+        new AllTests().runAll();
+    }
+    
+	protected Class[] testCases() {
+		return new Class[] {
+			FastListTestCase.class,
+		};
+	}
+
+}
