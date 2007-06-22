558e493f292e301c75ae20354480284b76d40c76
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-06-22 12:01:38 +0000

    az: The  SimpleTimer can exit quickly now. It improved the speed of executing the whole test suite.

--- db4oj/core/src/com/db4o/foundation/SimpleTimer.java
+++ db4oj/core/src/com/db4o/foundation/SimpleTimer.java
@@ -7,36 +7,46 @@ package com.db4o.foundation;
  * @exclude
  */
 public final class SimpleTimer implements Runnable {
-	
+
 	private final Runnable _runnable;
+
 	private final int _interval;
+
 	private final String _name;
-	
+
+	private Lock4 _lock;
+
 	public volatile boolean stopped = false;
-	
-	public SimpleTimer(Runnable runnable, int interval, String name){
+
+	public SimpleTimer(Runnable runnable, int interval, String name) {
 		_runnable = runnable;
 		_interval = interval;
 		_name = name;
+		_lock = new Lock4();
 	}
-	
+
 	public void start() {
 		Thread thread = new Thread(this);
 		thread.setDaemon(true);
 		thread.setName(_name);
 		thread.start();
 	}
-	
-	public void stop(){
+
+	public void stop() {
 		stopped = true;
-	}
-	
-	public void run() {
-		while(! stopped){
-			Cool.sleepIgnoringInterruption(_interval);
-            if(! stopped){
-				_runnable.run();
-            }
+		synchronized (_lock) {
+			_lock.awake();
 		}
-    }
+	}
+
+	public void run() {
+		while (!stopped) {
+			synchronized (_lock) {
+				_lock.snooze(_interval);
+			}
+			if (!stopped) {
+				_runnable.run();
+			}
+		}
+	}
 }
