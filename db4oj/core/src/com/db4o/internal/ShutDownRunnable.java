f87806af1f784deb5d1be00dc79346765857aec4
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-06-29 13:14:57 +0000

    az: use Collection4 _containers as a field.

--- db4oj/core/src/com/db4o/internal/ShutDownRunnable.java
+++ db4oj/core/src/com/db4o/internal/ShutDownRunnable.java
@@ -1,22 +1,35 @@
-/* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
+/* Copyright (C) 2004 - 2007  db4objects Inc.   http://www.db4o.com */
 
 package com.db4o.internal;
 
 import com.db4o.foundation.*;
 
 
-class ShutDownRunnable extends Collection4 implements Runnable {
-	// FIXME: aggregate Collection4 instead of extending it
+class ShutDownRunnable implements Runnable {
+	
+	private Collection4 _containers = new Collection4();
 	
 	public volatile boolean dontRemove = false;
+
+	public void ensure(PartialObjectContainer container) {
+		_containers.ensure(container);	
+	}
 	
+	public void remove(PartialObjectContainer container) {
+		_containers.remove(container);
+	}
+
 	public void run(){
 		dontRemove = true;
-		Collection4 copy=new Collection4(this);
+		Collection4 copy= new Collection4(_containers);
 		Iterator4 i = copy.iterator();
 		while(i.moveNext()){
 			((ObjectContainerBase)i.current()).shutdownHook();
 		}
+	}
+
+	public int size() {
+		return _containers.size();
 	}
 	
 }
