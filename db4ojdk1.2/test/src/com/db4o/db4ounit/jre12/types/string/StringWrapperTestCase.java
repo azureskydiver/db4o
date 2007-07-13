0453963ba08fc3cfbc665bd70988befed2767d02
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-07-13 11:22:22 +0000

    az: move the test to jdk1.2 since java.lang.Comparable is since 1.2.

--- db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/types/string/StringWrapperTestCase.java
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/types/string/StringWrapperTestCase.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2007  db4objects Inc.  http://www.db4o.com */
 
-package com.db4o.db4ounit.common.types.string;
+package com.db4o.db4ounit.jre12.types.string;
 
 import db4ounit.*;
 import db4ounit.extensions.*;
