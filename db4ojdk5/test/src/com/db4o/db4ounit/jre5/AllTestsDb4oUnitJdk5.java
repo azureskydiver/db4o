5959fa3b95a639bc03987e9ab2b97a72b23014ff
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-07-13 00:24:31 +0000

    az: refined the test hierarchy.

--- db4ojdk5/test/src/com/db4o/db4ounit/jre5/AllTestsDb4oUnitJdk5.java
+++ db4ojdk5/test/src/com/db4o/db4ounit/jre5/AllTestsDb4oUnitJdk5.java
@@ -14,8 +14,9 @@ public class AllTestsDb4oUnitJdk5 extends Db4oTestSuite {
 	protected Class[] testCases() {
 		return new Class[] {
 			com.db4o.db4ounit.jre5.enums.AllTests.class,
+			com.db4o.db4ounit.jre5.annotation.AllTests.class,
+			com.db4o.db4ounit.jre5.generic.AllTests.class,
 			com.db4o.db4ounit.jre12.AllTestsJdk1_2.class,
-			com.db4o.db4ounit.jre5.annotation.IndexedAnnotationTestCase.class,
 		};
 	}
 
