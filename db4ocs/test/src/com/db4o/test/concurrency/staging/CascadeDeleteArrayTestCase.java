3e09099a238c80bfb681c8574e01c8d1fb4ffffb
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-07-23 11:27:38 +0000

    az: move failed test cases into staging package.

--- db4ocs/test/src/com/db4o/test/concurrency/staging/CascadeDeleteArrayTestCase.java
+++ db4ocs/test/src/com/db4o/test/concurrency/staging/CascadeDeleteArrayTestCase.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2004 - 2007  db4objects Inc.   http://www.db4o.com */
 
-package com.db4o.test.concurrency;
+package com.db4o.test.concurrency.staging;
 
 import com.db4o.*;
 import com.db4o.config.*;
