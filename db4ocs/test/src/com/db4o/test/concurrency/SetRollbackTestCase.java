dff95cd4695b8e5e0aa0be4968e69bff508d36d3
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-07-24 01:02:59 +0000

    az: #INF-197 integrate concurrency tests into db4oj.tests.

--- db4ocs/test/src/com/db4o/test/concurrency/SetRollbackTestCase.java
+++ db4ocs/test/src/com/db4o/test/concurrency/SetRollbackTestCase.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2004 - 2007  db4objects Inc.   http://www.db4o.com */
 
-package com.db4o.test.concurrency.regression;
+package com.db4o.test.concurrency;
 
 import com.db4o.ext.*;
 import com.db4o.test.persistent.*;
@@ -14,32 +14,6 @@ public class SetRollbackTestCase extends Db4oClientServerTestCase {
 		new SetRollbackTestCase().runConcurrency();
 	}
 	
-	/*
-	 * regression test: http://developer.db4o.com/forums/thread/29298.aspx
-	 */
-	public void testSetRollback() {
-		ExtObjectContainer oc1 = openNewClient();
-		ExtObjectContainer oc2 = openNewClient();
-		try {
-			for (int i = 0; i < 1000; i++) {
-				SimpleObject obj1 = new SimpleObject("oc " + i, i);
-				SimpleObject obj2 = new SimpleObject("oc2 " + i, i);
-				oc1.set(obj1);
-				oc2.set(obj2);
-				oc2.rollback();
-				obj2 = new SimpleObject("oc2.2 " + i, i);
-				oc2.set(obj2);
-			}
-			oc1.commit();
-			oc2.rollback();
-			Assert.areEqual(1000, oc1.query(SimpleObject.class).size());
-			Assert.areEqual(1000, oc2.query(SimpleObject.class).size());
-		} finally {
-			oc1.close();
-			oc2.close();
-		}
-	}
-
 	public void concSetRollback(ExtObjectContainer oc, int seq) {
 		if (seq % 2 == 0) { // if the thread sequence is even, store something
 			for (int i = 0; i < 1000; i++) {
