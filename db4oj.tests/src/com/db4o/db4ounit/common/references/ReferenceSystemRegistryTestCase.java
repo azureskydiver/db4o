fb18860cd8482649acac1a08dffa9bc99e32a17a
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-07-31 22:40:50 +0000

    cr + ng: #COR-666 Created a naive ReferenceSystemRegistry implementation.

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/references/ReferenceSystemRegistryTestCase.java
@@ -0,0 +1,72 @@
+/* Copyright (C) 2007  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.db4ounit.common.references;
+
+import com.db4o.internal.*;
+
+import db4ounit.*;
+import db4ounit.extensions.*;
+
+
+public class ReferenceSystemRegistryTestCase implements TestLifeCycle {
+
+    private ReferenceSystemRegistry _registry;
+    private ReferenceSystem _referenceSystem1;
+    private ReferenceSystem _referenceSystem2;
+    
+    private static int TEST_ID = 5;
+
+    public void setUp() throws Exception {
+        _registry = new ReferenceSystemRegistry();
+        _referenceSystem1 = new TransactionalReferenceSystem();
+        _referenceSystem2 = new TransactionalReferenceSystem();
+        _registry.addReferenceSystem(_referenceSystem1);
+        _registry.addReferenceSystem(_referenceSystem2);
+    }
+    
+    public void tearDown() throws Exception {
+        
+    }
+    
+    public void testRemoveId(){
+        addTestReference();
+        _registry.removeId(TEST_ID);
+        assertTestReferenceNotPresent();
+    }
+    
+    public void testRemoveObject(){
+        ObjectReference testReference = addTestReference();
+        _registry.removeObject(testReference.getObject());
+        assertTestReferenceNotPresent();
+    }
+    
+    public void testRemoveReference(){
+        ObjectReference testReference = addTestReference();
+        _registry.removeReference(testReference);
+        assertTestReferenceNotPresent();
+    }
+    
+    public void testRemoveReferenceSystem(){
+        addTestReference();
+        _registry.removeReferenceSystem(_referenceSystem1);
+        _registry.removeId(TEST_ID);
+        Assert.isNotNull(_referenceSystem1.referenceForId(TEST_ID));
+        Assert.isNull(_referenceSystem2.referenceForId(TEST_ID));
+    }
+
+    private void assertTestReferenceNotPresent() {
+        Assert.isNull(_referenceSystem1.referenceForId(TEST_ID));
+        Assert.isNull(_referenceSystem2.referenceForId(TEST_ID));
+    }
+
+    private ObjectReference addTestReference() {
+        ObjectReference ref = new ObjectReference(TEST_ID);
+        ref.setObject(new Object());
+        _referenceSystem1.addExistingReference(ref);
+        _referenceSystem2.addExistingReference(ref);
+        return ref;
+    }
+    
+    
+
+}
--- db4oj.tests/src/com/db4o/db4ounit/common/references/ReferenceSystemTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/references/ReferenceSystemTestCase.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2007  db4objects Inc.  http://www.db4o.com */
 
-package com.db4o.db4ounit.common.assorted;
+package com.db4o.db4ounit.common.references;
 
 import com.db4o.db4ounit.common.btree.*;
 import com.db4o.internal.*;
