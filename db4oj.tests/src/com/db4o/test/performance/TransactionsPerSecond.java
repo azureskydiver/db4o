7b6ff41ec990dff896481693f7c819d221f69123
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-08-05 15:09:55 +0000

    cr: Quick TransactionsPerSecond hack to allow measuring upper limit.

---
+++ db4oj.tests/src/com/db4o/test/performance/TransactionsPerSecond.java
@@ -0,0 +1,51 @@
+/* Copyright (C) 2007  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.test.performance;
+
+import java.io.*;
+import com.db4o.*;
+
+public class TransactionsPerSecond {
+    
+    public static void main(String[] args) {
+        new TransactionsPerSecond().run();
+    }
+    
+    public static class Item{
+        public int _int;
+        public Item(){
+        }
+        public Item(int int_){
+            _int = int_;
+        }
+    }
+    
+    private static final String FILE = "tps.db4o";
+    
+    private static final long TOTAL_COUNT = 5000;
+    
+    public void run(){
+        new File(FILE).delete();
+        
+        ObjectContainer objectContainer = Db4o.openFile(FILE).ext();
+        
+        long start = System.currentTimeMillis();
+        
+        for (int i = 0; i < TOTAL_COUNT; i++) {
+            objectContainer.set(new Item(i));
+            objectContainer.commit();
+        }
+        
+        long stop = System.currentTimeMillis();
+        long duration = stop - start;
+        objectContainer.close();
+        
+        System.out.println("Time to store " + TOTAL_COUNT + " objects: " + duration + "ms");
+        
+        double seconds = ((double)duration) / ((double)1000); 
+        double tps = TOTAL_COUNT / seconds;
+        
+        System.out.println("Transactions per second: " + tps);
+    }
+
+}
