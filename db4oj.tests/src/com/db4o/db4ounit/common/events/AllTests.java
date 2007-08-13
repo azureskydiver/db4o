9e9f2a655d4741277cd70071c1e9d484427f23a2
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-08-13 13:54:59 +0000

    System.exit is not c# friendly

--- db4oj.tests/src/com/db4o/db4ounit/common/events/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/events/AllTests.java
@@ -4,6 +4,10 @@ package com.db4o.db4ounit.common.events;
 import db4ounit.extensions.*;
 
 public class AllTests extends Db4oTestSuite {
+	
+	public static void main(String[] args) {
+		new AllTests().runAll();
+	}
 
 	protected Class[] testCases() {
 		return new Class[] {
