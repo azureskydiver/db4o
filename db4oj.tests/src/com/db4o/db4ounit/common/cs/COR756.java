35d8d541319bed86585d142b192888cf5d8e8833
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-08-15 20:49:12 +0000

    test doesn't build on jdk5 because of differences between the db4oj and db4ojdk5 definitions of Predicate differ

--- db4oj.tests/src/com/db4o/db4ounit/common/cs/COR756.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/cs/COR756.java
@@ -7,40 +7,32 @@ import db4ounit.Assert;
 
 public class COR756 extends ClientServerTestCaseBase  {
 	
-	
 	public static void main(String[] args) {
 		new COR756().runAll();
 	}
 	
-	public class A
-	{
-	  public boolean GetAllB (B b)
-	  {
+	public class A {
+	  public boolean getAllB(B b) {
 	    return this == b.a;
 	  }
 	}
 
-	public class B
-	{
+	public class B {
 	  public A a;
 	}
 	
-	public void _testClientServer(){
-		final A a = new A();
-		final B b = new B();
-		b.a = a;
-		ObjectContainer client = client();
-	    client.set(b);
-	    Assert.areEqual(
-	    client.query(new Predicate(){
-	    	public boolean appliesTo(Object candidate) {
-	    		B b = (B)candidate;
-	    		return a.GetAllB(b);
-	    	};
-	    	public Class extentType() {
-	    		return B.class;
-	    	};
-	    } ).size(), 1);
+	public void _testClientServer() {
+//		final A a = new A();
+//		final B b = new B();
+//		b.a = a;
+//		ObjectContainer client = client();
+//	    client.set(b);
+//	    Assert.areEqual(
+//	    client.query(new Predicate(){
+//	    	public boolean match(B candidate) {
+//	    		return a.getAllB(candidate);
+//	    	};
+//	    } ).size(), 1);
 		
 	}
 
