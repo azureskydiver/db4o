cc24ff4f87dee9e63cf3da0dd56e4a3f2004c58d
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-08-15 14:07:34 +0000

    rbo+pr: A test that always fails can be very useful sometimes. True story.

---
+++ db4ounit/src/db4ounit/FailingTest.java
@@ -0,0 +1,24 @@
+package db4ounit;
+
+/**
+ * A test that always fails with a specific exception.
+ */
+public class FailingTest implements Test {
+
+	private final Throwable _error;
+	private final String _label;
+
+	public FailingTest(String label, Throwable error) {
+		_label = label;
+		_error = error;
+	}
+
+	public String getLabel() {
+		return _label;
+	}
+
+	public void run(TestResult result) {
+		result.testFailed(this, _error);
+	}
+
+}
