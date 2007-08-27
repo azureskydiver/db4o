a57c926e1e48bdbe237e80a9d2fee234f2e9c6d5
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-08-27 10:45:16 +0000

    cr: #COR-772 keeping documentation up-to-date

--- db4oj/core/src/com/db4o/marshall/ReadBuffer.java
+++ db4oj/core/src/com/db4o/marshall/ReadBuffer.java
@@ -6,13 +6,32 @@ package com.db4o.marshall;
  * a buffer interface with methods to read.
  */
 public interface ReadBuffer {
-
+    
+    /**
+     * reads a byte from the buffer.
+     * @return the byte
+     */
     byte readByte();
     
+    /**
+     * reads an array of bytes from the buffer.
+     * The length of the array that is passed as a parameter specifies the
+     * number of bytes that are to be read. The passed bytes buffer parameter
+     * is directly filled.  
+     * @param bytes the byte array to read the bytes into.
+     */
     void readBytes(byte[] bytes);
 
+    /**
+     * reads an int from the buffer.
+     * @return the int
+     */
     int readInt();
     
+    /**
+     * reads a long from the buffer.
+     * @return the long
+     */
     long readLong();
 
 }
