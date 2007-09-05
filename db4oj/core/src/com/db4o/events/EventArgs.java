9c01cfde9d11c9e2a2ccc8595a139a2c43184a4c
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-09-05 11:23:13 +0000

    cr: Removed duplicated comment.

--- db4oj/core/src/com/db4o/events/EventArgs.java
+++ db4oj/core/src/com/db4o/events/EventArgs.java
@@ -7,10 +7,6 @@ package com.db4o.events;
  * Base class for event arguments.
  * @sharpen.ignore
  */
-/**
- * Base class for event arguments.
- * @sharpen.ignore
- */
 public class EventArgs {
 	
 	/**
