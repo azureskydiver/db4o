309daf6763037dd6c6e3b8d649fee92b3c255db1
richard_liang <richard_liang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-09-05 07:06:57 +0000

    rl: port fix of COR-854 to trunk

--- db4obuild/src/com/db4o/devtools/ant/AssemblyInfo.java
+++ db4obuild/src/com/db4o/devtools/ant/AssemblyInfo.java
@@ -1,20 +1,32 @@
 package com.db4o.devtools.ant;
 
+import java.util.*;
+
 public enum AssemblyInfo {
-    DB4O ("db4o", "db4o - database for objects"),
-    DRS  ("dRS", "dRS - db4o Replication System");
-    
-		public static final String COPYRIGHT = "db4o 2005";
-		public static final String COMPANY = "db4objects Inc., San Mateo, CA, USA";
-    
-    private final String title; 
-    private final String product; 
-    
+    DB4O("db4o", "db4o - database for objects"), DRS("dRS",
+            "dRS - db4o Replication System");
+
+    private static Calendar cal = Calendar.getInstance(Locale.US);
+
+    public static final String COPYRIGHT = "db4o 2005 - "
+            + cal.get(Calendar.YEAR);
+
+    public static final String COMPANY = "db4objects Inc., San Mateo, CA, USA";
+
+    private final String title;
+
+    private final String product;
+
     AssemblyInfo(String title, String product) {
-    		this.title = title;
+        this.title = title;
         this.product = product;
     }
-    
-    public String title()   { return title; }
-    public String product()   { return product; }
+
+    public String title() {
+        return title;
+    }
+
+    public String product() {
+        return product;
+    }
 }
