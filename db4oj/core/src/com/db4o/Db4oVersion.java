f620d4a39ffbb8be498d1463c80d30c5e01d0710
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-09-17 13:50:12 +0000

    cr: ITERATION and REVISION fields should be in SVN.

--- db4oj/core/src/com/db4o/Db4oVersion.java
+++ db4oj/core/src/com/db4o/Db4oVersion.java
@@ -1,4 +1,4 @@
-/* Copyright (C) 2005   db4objects Inc.   http://www.db4o.com */
+/* Copyright (C) 2007   db4objects Inc.   http://www.db4o.com */
 
 package com.db4o;
 
@@ -6,7 +6,9 @@ package com.db4o;
 * @exclude
 */
 public class Db4oVersion {
-    public static final String NAME = "6.4.000";
+    public static final String NAME = "6.4.9.7583";
     public static final int MAJOR = 6;
     public static final int MINOR = 4;
+    public static final int ITERATION = 9;
+    public static final int REVISION = 7583;
 }
