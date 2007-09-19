ce3335eecd87c948868d45b2e8d89cdb510dbffb
norberto_goussies <norberto_goussies@915b37e7-0715-0410-8bc3-b14b59444072>
2007-09-19 11:50:37 +0000

    ng : #COR-866 DoubleHandlerUpdateTestCase

--- db4oj.tests/src/com/db4o/db4ounit/jre11/migration/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/jre11/migration/AllTests.java
@@ -2,7 +2,8 @@
 
 package com.db4o.db4ounit.jre11.migration;
 
-import db4ounit.extensions.Db4oTestSuite;
+import db4ounit.*;
+import db4ounit.extensions.*;
 
 
 public class AllTests extends Db4oTestSuite {
@@ -18,6 +19,9 @@ public class AllTests extends Db4oTestSuite {
 			LongMigrationTestCase.class,
 			ShortMigrationTestCase.class,
 		};
+	}
+	public static void main(String[] args) {
+		new AllTests().runAll();
 	}
 
 }
