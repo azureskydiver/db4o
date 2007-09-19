f33d4de33e1bb63e44e1defc9f5dcab6ef1de3f7
norberto_goussies <norberto_goussies@915b37e7-0715-0410-8bc3-b14b59444072>
2007-09-19 11:50:25 +0000

    ng : #COR-866 DoubleHandlerUpdateTestCase

--- db4oj/core/src/com/db4o/Db4oVersion.java
+++ db4oj/core/src/com/db4o/Db4oVersion.java
@@ -6,9 +6,9 @@ package com.db4o;
 * @exclude
 */
 public class Db4oVersion {
-    public static final String NAME = "6.4.9.7583";
+    public static final String NAME = "6.4.9.7598";
     public static final int MAJOR = 6;
     public static final int MINOR = 4;
     public static final int ITERATION = 9;
-    public static final int REVISION = 7583;
+    public static final int REVISION = 7598;
 }
