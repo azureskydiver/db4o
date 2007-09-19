2f0d3fb466bd579f87b713800ad06ee1457a0efe
norberto_goussies <norberto_goussies@915b37e7-0715-0410-8bc3-b14b59444072>
2007-09-19 11:50:30 +0000

    ng : #COR-866 DoubleHandlerUpdateTestCase

--- db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/handlers/Db4oVersionRunner.java
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/handlers/Db4oVersionRunner.java
@@ -38,6 +38,7 @@ public class Db4oVersionRunner extends Db4oTestSuite {
 
     protected Class[] testCases() {
         return new Class[] { 
+			DoubleHandlerUpdateTestCase.class,
             IntHandlerUpdateTestCase.class, 
             LongHandlerUpdateTestCase.class,
             StringHandlerUpdateTestCase.class,
