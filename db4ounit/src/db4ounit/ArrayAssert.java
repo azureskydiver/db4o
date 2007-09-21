d54e04afd96809f9cb0ba68f6fdb022435c12855
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-09-21 13:07:06 +0000

    cr: Checking the correct type of array in areEqual

--- db4ounit/src/db4ounit/ArrayAssert.java
+++ db4ounit/src/db4ounit/ArrayAssert.java
@@ -33,6 +33,7 @@ public class ArrayAssert {
 		if (expected == actual) return;
 		if (expected == null || actual == null) Assert.areSame(expected, actual);
 		Assert.areEqual(expected.length, actual.length);
+		Assert.areSame(expected.getClass(), actual.getClass());
 	    for (int i = 0; i < expected.length; i++) {
 	        Assert.areEqual(expected[i], actual[i], indexMessage(i));
 	    }
