40ebd92cc7b12272636b5117562a173d2f42dc34
richard_liang <richard_liang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-09-24 10:23:44 +0000

    rl: run migration test separately

--- db4ojdk1.2/test/src/com/db4o/db4ounit/common/migration/AllTests.java
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/common/migration/AllTests.java
@@ -3,6 +3,10 @@ package com.db4o.db4ounit.common.migration;
 import db4ounit.extensions.*;
 
 public class AllTests extends Db4oTestSuite {
+    
+    public static void main(String[] args) {
+        System.exit(new AllTests().runSolo());
+    }
 	protected Class[] testCases() {
 		return new Class[]{
 			Db4oMigrationTestSuite.class,
