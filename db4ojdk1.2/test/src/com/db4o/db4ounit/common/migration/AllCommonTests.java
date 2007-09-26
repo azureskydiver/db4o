f1f53b6b4834a67c32e58026747d97ee1f28d6c7
richard_liang <richard_liang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-09-26 07:59:43 +0000

    rl: COR-869 - DateTimeHandlerUpdateTestCase

--- db4ojdk1.2/test/src/com/db4o/db4ounit/common/migration/AllCommonTests.java
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/common/migration/AllCommonTests.java
@@ -2,10 +2,10 @@ package com.db4o.db4ounit.common.migration;
 
 import db4ounit.extensions.*;
 
-public class AllTests extends Db4oTestSuite {
+public class AllCommonTests extends Db4oTestSuite {
     
     public static void main(String[] args) {
-        System.exit(new AllTests().runSolo());
+        System.exit(new AllCommonTests().runSolo());
     }
 	protected Class[] testCases() {
 		return new Class[]{
