9593609acdabb2e656183f9a1141ae092867b473
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-09-28 15:59:19 +0000

    az: move staging concurrency tests from db4ocs to db4oj.tests.

---
+++ db4oj.tests/src/com/db4o/db4ounit/jre11/concurrency/staging/AllTests.java
@@ -0,0 +1,24 @@
+/* Copyright (C) 2004 - 2007  db4objects Inc.   http://www.db4o.com */
+
+package com.db4o.db4ounit.jre11.concurrency.staging;
+
+import db4ounit.extensions.*;
+
+public class AllTests extends Db4oConcurrenyTestSuite {
+	
+	public static void main(String[] args) {
+		new AllTests().runConcurrency();
+	}
+
+	protected Class[] testCases() {
+		return new Class[] {
+			CascadeDeleteArrayTestCase.class,
+			CascadeToHashtableTestCase.class,
+			ComparatorSortTestCase.class,
+			CustomActivationDepthTestCase.class,
+			HashtableTestCase.class,
+			SwitchingFilesFromClientTestCase.class,
+		};
+	}
+
+}
