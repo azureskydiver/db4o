1aac9e71f57c84ebfe1ac33289d332247ae9ed51
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-02 12:44:59 +0000

    cr: Knowing whether we run on Java or .NET helps a lot.

---
+++ db4ounit/src/db4ounit/util/PlatformInformation.java
@@ -0,0 +1,19 @@
+/* Copyright (C) 2007  db4objects Inc.  http://www.db4o.com */
+
+package db4ounit.util;
+
+
+/**
+ * @sharpen.ignore
+ */
+public class PlatformInformation {
+    
+    public static boolean isJava(){
+        return true;
+    }
+    
+    public static boolean isDotNet(){
+        return false;
+    }
+
+}
