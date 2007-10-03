4e3b9e129eb7a3f2044bdb3fe107489ab42c8c7c
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-03 13:12:56 +0000

    az: #COR-899 removed ArrayList4 from db4ojdk5.

--- db4ojdk5/test/src/com/db4o/db4ounit/jre5/AllTestsDb4oUnitJdk5.java
+++ db4ojdk5/test/src/com/db4o/db4ounit/jre5/AllTestsDb4oUnitJdk5.java
@@ -15,7 +15,6 @@ public class AllTestsDb4oUnitJdk5 extends Db4oTestSuite {
 		return new Class[] {
 			com.db4o.db4ounit.jre5.annotation.AllTests.class,
 			com.db4o.db4ounit.jre5.enums.AllTests.class,
-			com.db4o.db4ounit.jre5.collections.AllTests.class,
 			com.db4o.db4ounit.jre5.generic.AllTests.class,
 			com.db4o.db4ounit.jre12.AllTestsJdk1_2.class,
 		};
