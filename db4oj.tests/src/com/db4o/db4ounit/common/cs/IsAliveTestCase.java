9fa4386adf1db7f8450509fe6fd514345ed9f074
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-03 13:36:51 +0000

    COR-898: extend test case

--- db4oj.tests/src/com/db4o/db4ounit/common/cs/IsAliveTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/cs/IsAliveTestCase.java
@@ -1,15 +1,59 @@
 package com.db4o.db4ounit.common.cs;
 
+import com.db4o.*;
+import com.db4o.config.*;
+import com.db4o.foundation.io.*;
 import com.db4o.internal.cs.*;
 
 import db4ounit.*;
-import db4ounit.extensions.*;
-import db4ounit.extensions.fixtures.*;
 
-public class IsAliveTestCase  extends Db4oClientServerTestCase implements OptOutAllButNetworkingCS {
+public class IsAliveTestCase  implements TestLifeCycle {
+	
+	private final static String USERNAME = "db4o";
+	private final static String PASSWORD = "db4o";
+	
+	private String filePath;
 
-	public void test() {
-		Assert.isTrue(((ClientObjectContainer)db()).isAlive());
+	public void testIsAlive() {
+		ObjectServer server = openServer();
+		int port = server.ext().port();
+		ClientObjectContainer client = openClient(port);
+		Assert.isTrue(client.isAlive());
+		client.close();
+		server.close();
+	}
+
+	public void testIsNotAlive() {
+		ObjectServer server = openServer();
+		int port = server.ext().port();
+		ClientObjectContainer client = openClient(port);
+		server.close();
+		Assert.isFalse(client.isAlive());
+		client.close();
+	}
+
+	public void setUp() throws Exception {
+		filePath = Path4.getTempFileName();
+		File4.delete(filePath);
+	}
+
+	public void tearDown() throws Exception {
+		File4.delete(filePath);
 	}
 	
+	private Configuration config() {
+		return Db4o.newConfiguration();
+	}
+
+	private ObjectServer openServer() {
+		ObjectServer server = Db4o.openServer(config(), filePath, -1);
+		server.grantAccess(USERNAME, PASSWORD);
+		return server;
+	}
+
+	private ClientObjectContainer openClient(int port) {
+		ClientObjectContainer client = (ClientObjectContainer) Db4o.openClient(config(), "localhost", port, USERNAME, PASSWORD);
+		return client;
+	}
+
 }
