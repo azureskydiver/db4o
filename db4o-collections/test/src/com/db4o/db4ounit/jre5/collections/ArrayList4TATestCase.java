f2f59ad7975f0ac0627a0ce21dc1162b28669d6a
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-04 10:48:22 +0000

    az: #COR-899 more tests.

--- db4o-collections/test/src/com/db4o/db4ounit/jre5/collections/ArrayList4TATestCase.java
+++ db4o-collections/test/src/com/db4o/db4ounit/jre5/collections/ArrayList4TATestCase.java
@@ -238,6 +238,157 @@ public class ArrayList4TATestCase extends AbstractDb4oTestCase {
 		Assert.isTrue(list.isEmpty());
 	}
 
+	public void testIterator() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		Iterator iter = list.iterator();
+		int count = 0;
+		while (iter.hasNext()) {
+			Integer i = (Integer) iter.next();
+			Assert.areEqual(count, i.intValue());
+			++count;
+		}
+		Assert.areEqual(CAPACITY, count);
+
+		list.clear();
+		iter = list.iterator();
+		Assert.isFalse(iter.hasNext());
+	}
+
+	public void testLastIndexOf() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		Assert.areEqual(0, list.indexOf(new Integer(0)));
+		Assert.areEqual(CAPACITY / 2, list.lastIndexOf(new Integer(
+				CAPACITY / 2)));
+		Assert.areEqual(CAPACITY / 3, list.lastIndexOf(new Integer(
+				CAPACITY / 3)));
+		Assert.areEqual(CAPACITY / 4, list.lastIndexOf(new Integer(
+				CAPACITY / 4)));
+
+		Assert.areEqual(-1, list.lastIndexOf(new Integer(-1)));
+		Assert.areEqual(-1, list.lastIndexOf(new Integer(CAPACITY)));
+
+		// returns false because current data doesn't contain null.
+		// Quotes from j.u.List spec: More formally, returns the lowest index i
+		// such that (o==null ? get(i)==null : o.equals(get(i))), or -1 if there
+		// is no such index.
+		Assert.areEqual(-1, list.lastIndexOf(null));
+
+		list.add(new Integer(0));
+		list.add(new Integer(CAPACITY / 2));
+		list.add(new Integer(CAPACITY / 3));
+		list.add(new Integer(CAPACITY / 4));
+
+		Assert.areEqual(CAPACITY, list.lastIndexOf(new Integer(0)));
+		Assert.areEqual(CAPACITY + 1, list.lastIndexOf(new Integer(
+				CAPACITY / 2)));
+		Assert.areEqual(CAPACITY + 2, list.lastIndexOf(new Integer(
+				CAPACITY / 3)));
+		Assert.areEqual(CAPACITY + 3, list.lastIndexOf(new Integer(
+				CAPACITY / 4)));
+
+		Assert.areEqual(-1, list.lastIndexOf(new Integer(-1)));
+		Assert.areEqual(-1, list.lastIndexOf(new Integer(CAPACITY)));
+
+		// returns false because current data doesn't contain null.
+		// Quotes from j.u.List spec: More formally, returns the lowest index i
+		// such that (o==null ? get(i)==null : o.equals(get(i))), or -1 if there
+		// is no such index.
+		Assert.areEqual(-1, list.lastIndexOf(null));
+	}
+
+	public void testRemove_Object() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		list.remove(new Integer(0));
+		Assert.areEqual(new Integer(1), list.get(0));
+
+		Assert.areEqual(CAPACITY - 1, list.size());
+
+		list.remove(new Integer(43));
+		Assert.areEqual(new Integer(44), list.get(42));
+		Assert.areEqual(new Integer(42), list.get(41));
+		Assert.areEqual(CAPACITY - 2, list.size());
+
+		for (int i = 0; i < CAPACITY - 2; ++i) {
+			list.remove(list.get(0));
+			Assert.areEqual(CAPACITY - 3 - i, list.size());
+		}
+		Assert.isTrue(list.isEmpty());
+	}
+
+	public void testRemove_I() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		list.remove(0);
+		Assert.areEqual(new Integer(1), list.get(0));
+		Assert.isFalse(list.contains(new Integer(0)));
+		Assert.areEqual(CAPACITY - 1, list.size());
+
+		list.remove(42);
+		Assert.areEqual(new Integer(44), list.get(42));
+		Assert.areEqual(new Integer(42), list.get(41));
+		Assert.isFalse(list.contains(new Integer(43)));
+		Assert.areEqual(CAPACITY - 2, list.size());
+
+		for (int i = 0; i < CAPACITY - 2; ++i) {
+			list.remove(0);
+			Assert.areEqual(CAPACITY - 3 - i, list.size());
+		}
+		Assert.isTrue(list.isEmpty());
+	}
+
+	public void testRemoveAll() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		Vector<Integer>v = new Vector<Integer>();
+
+		list.removeAll(v);
+		Assert.areEqual(CAPACITY, list.size());
+
+		v.add(new Integer(0));
+		v.add(new Integer(42));
+		list.removeAll(v);
+		Assert.isFalse(list.contains(new Integer(0)));
+		Assert.isFalse(list.contains(new Integer(42)));
+		Assert.areEqual(CAPACITY - 2, list.size());
+
+		v.add(new Integer(1));
+		v.add(new Integer(2));
+		list.removeAll(v);
+		Assert.isFalse(list.contains(new Integer(1)));
+		Assert.isFalse(list.contains(new Integer(2)));
+		Assert.areEqual(CAPACITY - 4, list.size());
+
+		for (int i = 0; i < CAPACITY; ++i) {
+			v.add(new Integer(i));
+		}
+		list.removeAll(v);
+		Assert.isTrue(list.isEmpty());
+	}
+
+	public void testRetainAll() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		Vector <Integer>v = new Vector<Integer>();
+		v.add(new Integer(0));
+		v.add(new Integer(42));
+
+		boolean ret = list.retainAll(list);
+		Assert.isFalse(ret);
+		Assert.areEqual(100, list.size());
+		for (int i = 0; i < CAPACITY; ++i) {
+			Assert.isTrue(list.contains(new Integer(i)));
+		}
+
+		ret = list.retainAll(v);
+		Assert.isTrue(ret);
+		Assert.areEqual(2, list.size());
+		list.contains(new Integer(0));
+		list.contains(new Integer(42));
+
+		ret = list.retainAll(v);
+		Assert.isFalse(ret);
+		list.contains(new Integer(0));
+		list.contains(new Integer(42));
+	}
+
+
 	public void testSet() throws Exception {
 		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
 		Integer element = new Integer(1);
@@ -283,7 +434,227 @@ public class ArrayList4TATestCase extends AbstractDb4oTestCase {
 		}
 	}
 
-
+	public void testToArray() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		Object[] array = list.toArray();
+		Assert.areEqual(CAPACITY, array.length);
+		for(int i = 0; i < CAPACITY; ++i) {
+			Integer element = (Integer) array[i];
+			Assert.areEqual(new Integer(i), element);
+		}
+		
+		list.clear();
+		array = list.toArray();
+		Assert.areEqual(0, array.length);
+	}
+	
+	public void testToArray_LObject() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		Object[] array1;
+		Object[] array2 = new Integer[CAPACITY];
+		array1 = list.toArray(array2);
+		Assert.areSame(array1, array2);
+		Assert.areEqual(CAPACITY, array2.length);
+		for(int i = 0; i < CAPACITY; ++i) {	
+			Integer element = (Integer) array2[i];
+			Assert.areEqual(new Integer(i), element);
+		}
+		
+		list.clear();
+		
+		array1 = new Integer[0];
+		array2 = new Integer[CAPACITY];
+		array1 = list.toArray(array2);
+		Assert.areSame(array1, array2);
+		Assert.areEqual(CAPACITY, array1.length);
+		
+		array2 = new Integer[0];
+		array1 = list.toArray(array2);
+		Assert.areEqual(0, array1.length);
+	}
+	
+	public void testToString() throws Exception {
+		ArrayList4<Object> list = new ArrayList4<Object>();
+		
+		Assert.areEqual("[]",list.toString());
+		
+		list.add(new Integer(1));
+		list.add(new Integer(2));
+		Assert.areEqual("[1, 2]",list.toString());
+		
+		list.add(list);
+		list.add(3);
+		Assert.areEqual("[1, 2, (this Collection), 3]",list.toString());
+	}
+	
+	public void testTrimToSize_EnsureCapacity() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		list.ensureCapacity(CAPACITY*2);
+		Assert.areEqual(CAPACITY, list.size());
+		for(int i = 0; i < CAPACITY; ++i) {
+			Integer element = (Integer) list.get(i);
+			Assert.areEqual(new Integer(i), element);
+		}
+		
+		list.trimToSize();
+		Assert.areEqual(CAPACITY, list.size());
+		for(int i = 0; i < CAPACITY; ++i) {
+			Integer element = (Integer) list.get(i);
+			Assert.areEqual(new Integer(i), element);
+		}
+	}
+	
+	public void testTrimToSize_Remove() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		
+		for (int i = CAPACITY-1; i >= 10 ; i--) {
+			list.remove(i);
+		}
+		Assert.areEqual(10, list.size());
+		for(int i = 0; i < 10; ++i) {
+			Integer element = (Integer) list.get(i);
+			Assert.areEqual(new Integer(i), element);
+		}
+	}
+	
+	public void testTrimToSize_Iterator() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		final Iterator<Integer> iterator = list.iterator();
+		list.trimToSize();
+		Assert.expect(ConcurrentModificationException.class, new CodeBlock(){
+			public void run() throws Throwable {
+				iterator.next();
+			}
+		});
+	}
+	
+	public void testEnsureCapacity_Iterator() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		final Iterator<Integer> iterator = list.iterator();
+		list.ensureCapacity(CAPACITY*2);
+		Assert.expect(ConcurrentModificationException.class, new CodeBlock(){
+			public void run() throws Throwable {
+				iterator.next();
+			}
+		});
+	}
+	
+	public void testClear_Iterator() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		final Iterator<Integer> iterator = list.iterator();
+		list.clear();
+		Assert.expect(ConcurrentModificationException.class, new CodeBlock(){
+			public void run() throws Throwable {
+				iterator.next();
+			}
+		});
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public void testClone() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		list.add(null);
+		ArrayList4<Integer> cloned = (ArrayList4<Integer>)list.clone();
+		for (int i = 0; i < CAPACITY; i++) {
+			Assert.areSame(list.get(i), cloned.get(i));
+		}
+	}
+	
+	public void testEquals() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		Assert.isFalse(list.equals(null));
+		Assert.isFalse(list.equals(new Integer(1)));
+		Assert.isTrue(list.equals(list));
+		Vector<Integer> v = new Vector<Integer>(list);
+		Assert.isTrue(list.equals(v));
+		v = new Vector<Integer>();
+		Assert.isFalse(list.equals(v));
+		Assert.isTrue(list.equals(list.clone()));
+	}
+	
+	public void testIteratorNext_NoSuchElementException() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		final Iterator<Integer> iterator = list.iterator();
+		Assert.expect(NoSuchElementException.class, new CodeBlock(){
+			public void run() throws Throwable {
+				while(true){iterator.next();}
+			}
+		});
+	}
+	
+	public void testIteratorNext_ConcurrentModificationException() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		final Iterator<Integer> iterator = list.iterator();
+		Assert.expect(NoSuchElementException.class, new CodeBlock(){
+			public void run() throws Throwable {
+				while(true){iterator.next();}
+			}
+		});
+		list.clear();
+		Assert.expect(ConcurrentModificationException.class, new CodeBlock(){
+			public void run() throws Throwable {
+				iterator.next();
+			}
+		});
+		
+	}
+	
+	public void testIteratorNext() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		final Iterator<Integer> iterator = list.iterator();
+		int i = 0;
+		while (iterator.hasNext()) {
+			Integer e1 = iterator.next();
+			Assert.areSame(e1, list.get(i));
+			i++;
+		}
+	}
+	
+	public void testIteratorRemove() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		final Iterator<Integer> iterator = list.iterator();
+		int i = CAPACITY-1;
+		while (iterator.hasNext()) {
+			Integer e1 = iterator.next();
+			Assert.areSame(e1, list.get(0));
+			Assert.areEqual(new Integer(CAPACITY-1), list.get(i));
+			iterator.remove();
+			Assert.areEqual(i, list.size());
+			i--;
+		}
+	}
+	
+	public void testRemove_IllegalStateException() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		final Iterator<Integer> iterator = list.iterator();
+		Assert.expect(IllegalStateException.class, new CodeBlock(){
+			public void run() throws Throwable {
+				iterator.remove();
+			}
+		});
+		
+		iterator.next();
+		
+		Assert.expect(IllegalStateException.class, new CodeBlock(){
+			public void run() throws Throwable {
+				iterator.remove();
+				iterator.remove();
+			}
+		});
+	}
+	
+	public void testIteratorRemove_ConcurrentModificationException() throws Exception {
+		final ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		final Iterator<Integer> iterator = list.iterator();
+		iterator.next();
+		list.clear();
+		Assert.expect(ConcurrentModificationException.class, new CodeBlock(){
+			public void run() throws Throwable {
+				iterator.remove();
+			}
+		});
+	}
 	@SuppressWarnings("unchecked")
 	private ArrayList4<Integer> retrieveAndAssertNullArrayList4() {
 		ArrayList4<Integer> list = (ArrayList4<Integer>) retrieveOnlyInstance(ArrayList4.class);
