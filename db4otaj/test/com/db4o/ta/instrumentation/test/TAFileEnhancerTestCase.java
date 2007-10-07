f7b54d40fac17abe77fa7c019c90440403ce873d
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-07 16:11:23 +0000

    cr: TAFileEnhancerTestCase doesn't compile and broke the build.

--- db4otaj/test/com/db4o/ta/instrumentation/test/TAFileEnhancerTestCase.java
+++ db4otaj/test/com/db4o/ta/instrumentation/test/TAFileEnhancerTestCase.java
@@ -8,56 +8,62 @@ import com.db4o.instrumentation.*;
 import com.db4o.query.*;
 import com.db4o.ta.*;
 import com.db4o.ta.instrumentation.*;
-import com.db4o.test.util.*;
+
+/* FIXME: com.db4o.test is not visible for db4otaj project */
+// import com.db4o.test.util.*;
 
 import db4ounit.*;
 
 public class TAFileEnhancerTestCase implements TestCase {
+    
+/* FIXME: Code doesn't compile from db4otaj. It's not JDK 1.1 compliant.  */
 	
-	private final static Class INSTRUMENTED_CLAZZ = ToBeInstrumentedWithFieldAccess.class;
-	
-	private final static Class NOT_INSTRUMENTED_CLAZZ = NotToBeInstrumented.class;
-
-	public void test() throws Exception {
-		final String srcDir = Path4.combine(Path4.getTempPath(), "tafileinstr/source");
-		File4.mkdirs(srcDir);
-		final String targetDir = Path4.combine(Path4.getTempPath(), "tafileinstr/target");
-		File4.mkdirs(targetDir);
-
-		final Class[] clazzes = { INSTRUMENTED_CLAZZ, NOT_INSTRUMENTED_CLAZZ };
-		
-		for (int clazzIdx = 0; clazzIdx < clazzes.length; clazzIdx++) {
-			copyClassFile(srcDir, clazzes[clazzIdx]);
-		}
-		
-		ClassFilter filter = new ByNameClassFilter(new String[]{ INSTRUMENTED_CLAZZ.getName() });
-		Db4oFileEnhancer enhancer = new Db4oFileEnhancer(new InjectTransparentActivationEdit(filter));
-		enhancer.enhance(srcDir, targetDir, new String[]{}, "");
-		
-		ExcludingClassLoader excludingLoader = new ExcludingClassLoader(getClass().getClassLoader(), clazzes);
-		URLClassLoader loader = new URLClassLoader(new URL[] { new File(targetDir).toURI().toURL() }, excludingLoader);
-		
-		Class instrumented = loader.loadClass(INSTRUMENTED_CLAZZ.getName());
-		Assert.isTrue(Activatable.class.isAssignableFrom(instrumented));
-		Class uninstrumented = loader.loadClass(NOT_INSTRUMENTED_CLAZZ.getName());
-		Assert.isFalse(Activatable.class.isAssignableFrom(uninstrumented));
-	}
-
-	private void copyClassFile(String srcDir, Class clazz)
-			throws URISyntaxException, IOException {
-		File file = fileForClass(clazz);
-		String targetPath = Path4.combine(srcDir, clazz.getName().replace('.', '/') + ".class");
-		File4.delete(targetPath);
-		File4.copy(file.getCanonicalPath(), targetPath);
-	}
-
-	private File fileForClass(Class clazz) throws URISyntaxException,
-			IOException {
-		String clazzName = clazz.getName();
-		int dotIdx = clazzName.lastIndexOf('.');
-		String simpleName = clazzName.substring(dotIdx + 1);
-		URL url = clazz.getResource(simpleName + ".class");
-		// FIXME: toURI is since 1.4
-		return new File(url.toURI());
-	}
+//	private final static Class INSTRUMENTED_CLAZZ = ToBeInstrumentedWithFieldAccess.class;
+//	
+//	private final static Class NOT_INSTRUMENTED_CLAZZ = NotToBeInstrumented.class;
+//
+//	public void test() throws Exception {
+//		final String srcDir = Path4.combine(Path4.getTempPath(), "tafileinstr/source");
+//		File4.mkdirs(srcDir);
+//		final String targetDir = Path4.combine(Path4.getTempPath(), "tafileinstr/target");
+//		File4.mkdirs(targetDir);
+//
+//		final Class[] clazzes = { INSTRUMENTED_CLAZZ, NOT_INSTRUMENTED_CLAZZ };
+//		
+//		for (int clazzIdx = 0; clazzIdx < clazzes.length; clazzIdx++) {
+//			copyClassFile(srcDir, clazzes[clazzIdx]);
+//		}
+//		
+//		ClassFilter filter = new ByNameClassFilter(new String[]{ INSTRUMENTED_CLAZZ.getName() });
+//		Db4oFileEnhancer enhancer = new Db4oFileEnhancer(new InjectTransparentActivationEdit(filter));
+//		enhancer.enhance(srcDir, targetDir, new String[]{}, "");
+//		
+//		ExcludingClassLoader excludingLoader = new ExcludingClassLoader(getClass().getClassLoader(), clazzes);
+//		URLClassLoader loader = new URLClassLoader(new URL[] { new File(targetDir).toURI().toURL() }, excludingLoader);
+//		
+//		Class instrumented = loader.loadClass(INSTRUMENTED_CLAZZ.getName());
+//		Assert.isTrue(Activatable.class.isAssignableFrom(instrumented));
+//		Class uninstrumented = loader.loadClass(NOT_INSTRUMENTED_CLAZZ.getName());
+//		Assert.isFalse(Activatable.class.isAssignableFrom(uninstrumented));
+//	}
+//
+//	private void copyClassFile(String srcDir, Class clazz)
+//			throws URISyntaxException, IOException {
+//		File file = fileForClass(clazz);
+//		String targetPath = Path4.combine(srcDir, clazz.getName().replace('.', '/') + ".class");
+//		File4.delete(targetPath);
+//		File4.copy(file.getCanonicalPath(), targetPath);
+//	}
+//
+//	private File fileForClass(Class clazz) throws URISyntaxException,
+//			IOException {
+//		String clazzName = clazz.getName();
+//		int dotIdx = clazzName.lastIndexOf('.');
+//		String simpleName = clazzName.substring(dotIdx + 1);
+//		URL url = clazz.getResource(simpleName + ".class");
+//		// FIXME: toURI is since 1.4
+//		return new File(url.toURI());
+//	}
+    
+    
 }
