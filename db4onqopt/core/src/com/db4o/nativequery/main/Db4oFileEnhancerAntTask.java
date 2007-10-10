74ffa56db936284b5636d139189e512d2ece7038
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-10 23:54:57 +0000

    COR-839: move ant task to db4otools

--- db4onqopt/core/src/com/db4o/nativequery/main/Db4oFileEnhancerAntTask.java
+++ db4onqopt/core/src/com/db4o/nativequery/main/Db4oFileEnhancerAntTask.java
@@ -1,56 +1,12 @@
-/* Copyright (C) 2004 - 2006  db4objects Inc.  http://www.db4o.com */
+/* Copyright (C) 2007 db4objects Inc.  http://www.db4o.com */
 
 package com.db4o.nativequery.main;
 
-import java.util.*;
 
-import org.apache.tools.ant.*;
-import org.apache.tools.ant.types.*;
+public class Db4oFileEnhancerAntTask extends com.db4o.instrumentation.ant.Db4oFileEnhancerAntTask {
 
-import com.db4o.instrumentation.*;
-
-public class Db4oFileEnhancerAntTask extends Task {
-	private String srcDir;
-	private String targetDir;
-	private List classPath=new ArrayList();
-	private String packagePredicate;
-	private String editClassName = TranslateNQToSODAEdit.class.getName();
-
-	public void setEditClass(String editClassName) {
-		this.editClassName = editClassName;
-	}
-
-	public void setSrcdir(String srcDir) {
-		this.srcDir=srcDir;
+	public Db4oFileEnhancerAntTask() {
+		setEditClass(TranslateNQToSODAEdit.class.getName());
 	}
 	
-	public void setTargetdir(String targetDir) {
-		this.targetDir=targetDir;
-	}
-
-	public void addClasspath(Path path) {
-		classPath.add(path);
-	}
-
-	public void setPackagefilter(String packagePredicate) {
-		this.packagePredicate=packagePredicate;
-	}
-
-	public void execute() {
-		List paths=new ArrayList();
-		for (Iterator pathIter = classPath.iterator(); pathIter.hasNext();) {
-			Path path = (Path) pathIter.next();
-			System.err.println(path.size());
-			String[] curPaths=path.list();
-			for (int curPathIdx = 0; curPathIdx < curPaths.length; curPathIdx++) {
-				paths.add(curPaths[curPathIdx]);
-			}
-		}
-		try {
-			BloatClassEdit clazzEdit = (BloatClassEdit) Class.forName(editClassName).newInstance();
-			new Db4oFileEnhancer(clazzEdit).enhance(srcDir,targetDir,(String[])paths.toArray(new String[paths.size()]),(packagePredicate==null ? "" : packagePredicate));
-		} catch (Exception exc) {
-			throw new BuildException(exc);
-		}
-	}
 }
