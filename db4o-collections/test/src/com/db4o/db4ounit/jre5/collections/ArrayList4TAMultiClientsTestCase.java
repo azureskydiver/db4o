def89fbdaf8f6edc813f5f95fc2109435c20d684
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-10 13:56:19 +0000

    az: #COR-909 more strict result check, and refactored ArrayList4MultiClientTestCase

--- db4o-collections/test/src/com/db4o/db4ounit/jre5/collections/ArrayList4TAMultiClientsTestCase.java
+++ db4o-collections/test/src/com/db4o/db4ounit/jre5/collections/ArrayList4TAMultiClientsTestCase.java
@@ -16,62 +16,92 @@ public class ArrayList4TAMultiClientsTestCase extends ArrayList4TATestCaseBase i
 		new ArrayList4TAMultiClientsTestCase().runEmbeddedClientServer();
 	}
 
+	private static final ArrayList4Operation <Integer> _addOp = new ArrayList4Operation<Integer>() {
+		public void operate(ArrayList4<Integer> list) {
+			list.add(new Integer(ArrayList4Asserter.CAPACITY));
+		}	
+	};
+	
+	private static final ArrayList4Operation<Integer> _removeOp = new ArrayList4Operation<Integer>() {
+		public void operate(ArrayList4<Integer> list) {
+			list.remove(0);
+		}	
+	};	
+	
+	private static final ArrayList4Operation<Integer> _setOp = new ArrayList4Operation<Integer>() {
+		public void operate(ArrayList4<Integer> list) {
+			list.set(0, new Integer(1));
+		}	
+	};	
+	
 	public void testAddAdd() throws Exception {
-		ExtObjectContainer client1 = openNewClient();
-		ExtObjectContainer client2 = openNewClient();
-		ArrayList4<Integer> list1 = retrieveAndAssertNullArrayList4(client1);
-		ArrayList4<Integer> list2 = retrieveAndAssertNullArrayList4(client2);
-		int size = list1.size();
-		list1.add(new Integer(100));
-		list2.add(new Integer(200));
-		client1.set(list1);
-		client2.set(list2);
-		client1.commit();
-		client2.commit();
-		client1.close();
-		client2.close();
-		ArrayList4<Integer> list3 = retrieveAndAssertNullArrayList4();
-		Integer lastItem = list3.get(size);
-		Assert.areEqual(new Integer(200), lastItem);
+		ArrayList4Operation<Integer> anotherAddOp = new ArrayList4Operation<Integer>() {
+			public void operate(ArrayList4<Integer> list) {
+				list.add(new Integer(ArrayList4Asserter.CAPACITY + 42));
+			}	
+		};	
+		operate(anotherAddOp, _addOp);
+		checkAdd();
+	}
+
+	public void testSetAdd() throws Exception {
+		operate(_setOp, _addOp);
+		checkAdd();
+	}
+	
+	public void testRemoveAdd() throws Exception {
+		operate(_removeOp, _addOp);
+		checkAdd();
+	}
+	
+	private void checkAdd() throws Exception {
+		ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		Assert.areEqual(ArrayList4Asserter.CAPACITY + 1, list.size());
+		for (int i = 0; i <= ArrayList4Asserter.CAPACITY; ++i) {
+			Assert.areEqual(new Integer(i), list.get(i));
+		}
 	}
 
 	public void testAddRemove() throws Exception {
-		ExtObjectContainer client1 = openNewClient();
-		ExtObjectContainer client2 = openNewClient();
-		ArrayList4<Integer> list1 = retrieveAndAssertNullArrayList4(client1);
-		ArrayList4<Integer> list2 = retrieveAndAssertNullArrayList4(client2);
-		int size = list1.size();
-		list1.add(new Integer(101));
-		list2.remove(0);
-		Integer firstItem = list2.get(0);
-		client1.set(list1);
-		client2.set(list2);
-		client1.commit();
-		client2.commit();
-		client1.close();
-		client2.close();
-		ArrayList4<Integer> list3 = retrieveAndAssertNullArrayList4();
-		Assert.areEqual(size-1, list3.size());
-		Assert.areEqual(firstItem, list3.get(0));
+		operate(_addOp, _removeOp);
+		checkRemove();
+	}
+	
+	private void checkRemove() throws Exception {
+		ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		Assert.areEqual(ArrayList4Asserter.CAPACITY - 1, list.size());
+		for (int i = 0; i < ArrayList4Asserter.CAPACITY - 1; ++i) {
+			Assert.areEqual(new Integer(i + 1), list.get(i));
+		}
 	}
 
 	public void testAddSet() throws Exception {
+		operate(_addOp, _setOp);
+		checkSet();
+	}
+
+	private void checkSet() throws Exception {
+		ArrayList4<Integer> list = retrieveAndAssertNullArrayList4();
+		Assert.areEqual(ArrayList4Asserter.CAPACITY, list.size());
+		Assert.areEqual(new Integer(1), list.get(0));
+		for (int i = 1; i < ArrayList4Asserter.CAPACITY; ++i) {
+			Assert.areEqual(new Integer(i), list.get(i));
+		}
+	}
+	
+	private void operate(ArrayList4Operation <Integer> op1, ArrayList4Operation<Integer> op2) throws Exception {
 		ExtObjectContainer client1 = openNewClient();
 		ExtObjectContainer client2 = openNewClient();
 		ArrayList4<Integer> list1 = retrieveAndAssertNullArrayList4(client1);
 		ArrayList4<Integer> list2 = retrieveAndAssertNullArrayList4(client2);
-		int size = list1.size();
-		list1.add(new Integer(101));
-		list2.set(0, new Integer(1));
+		op1.operate(list1);
+		op2.operate(list2);
 		client1.set(list1);
 		client2.set(list2);
 		client1.commit();
 		client2.commit();
 		client1.close();
 		client2.close();
-		ArrayList4<Integer> list3 = retrieveAndAssertNullArrayList4();
-		Assert.areEqual(size, list3.size());
-		Assert.areEqual(new Integer(1), list3.get(0));
 	}
 
 }
