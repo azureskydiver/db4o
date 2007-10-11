1eeba7a032698bbde4af2a2dc6c27522c811ffb2
richard_liang <richard_liang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-11 02:55:16 +0000

    rl: COR-910

--- db4o-collections/test/src/com/db4o/db4ounit/jre5/collections/ArrayMap4TAMultiClientsTestCase.java
+++ db4o-collections/test/src/com/db4o/db4ounit/jre5/collections/ArrayMap4TAMultiClientsTestCase.java
@@ -154,4 +154,329 @@ public class ArrayMap4TAMultiClientsTestCase extends AbstractDb4oTestCase
         Assert.areEqual(10, map.size());
         ArrayMap4Asserter.checkMap(map, 0, 10);
     }
+    
+    @SuppressWarnings("unchecked")
+    public void testClearClone() {
+        ExtObjectContainer client1 = openNewClient();
+        ExtObjectContainer client2 = openNewClient();
+
+        ArrayMap4<String, Integer> map1 = retrieveOnlyInstance(client1);
+        ArrayMap4<String, Integer> map2 = retrieveOnlyInstance(client2);
+        
+        ArrayMap4Asserter.checkMap(map1, 0, 10);
+        ArrayMap4Asserter.checkMap(map2, 0, 10);
+        map1.clear();
+        ArrayMap4<String, Integer> clone = (ArrayMap4<String, Integer>) map2.clone();
+        client1.set(map1);
+        client2.delete(map2);
+        client2.set(clone);
+        client1.close();
+        client2.close();
+        
+        ArrayMap4<String, Integer> map = retrieveOnlyInstance();
+        Assert.areEqual(10, map.size());
+        ArrayMap4Asserter.checkMap(map, 0, 10);
+    }
+    
+    public void testPutPut() {
+        ExtObjectContainer client1 = openNewClient();
+        ExtObjectContainer client2 = openNewClient();
+
+        ArrayMap4<String, Integer> map1 = retrieveOnlyInstance(client1);
+        ArrayMap4<String, Integer> map2 = retrieveOnlyInstance(client2);
+        
+        ArrayMap4Asserter.checkMap(map1, 0, 10);
+        ArrayMap4Asserter.checkMap(map2, 0, 10);
+        
+        map1.put("11", Integer.valueOf(11 * 100));
+        map2.put("10", Integer.valueOf(10 * 100));
+        
+        client1.set(map1);
+        client2.set(map2);
+        client1.close();
+        client2.close();
+        
+        ArrayMap4<String, Integer> map = retrieveOnlyInstance();
+        ArrayMap4Asserter.checkMap(map, 0, 11);
+        Assert.isNull(map.get("11"));
+    }
+    
+    public void testPutPutAll() {
+        ExtObjectContainer client1 = openNewClient();
+        ExtObjectContainer client2 = openNewClient();
+
+        ArrayMap4<String, Integer> map1 = retrieveOnlyInstance(client1);
+        ArrayMap4<String, Integer> map2 = retrieveOnlyInstance(client2);
+        
+        ArrayMap4Asserter.checkMap(map1, 0, 10);
+        ArrayMap4Asserter.checkMap(map2, 0, 10);
+        
+        map1.put("100", Integer.valueOf(100 * 100));
+        
+        ArrayMap4<String, Integer> other = new ArrayMap4<String, Integer>();
+        for (int i = 10; i < 50; i++) {
+            other.put(String.valueOf(i), Integer.valueOf(i * 100));
+        }
+        map2.putAll(other);
+        
+        client1.set(map1);
+        client2.set(map2);
+        client1.close();
+        client2.close();
+        
+        ArrayMap4<String, Integer> map = retrieveOnlyInstance();
+        ArrayMap4Asserter.checkMap(map, 0, 50);
+        Assert.isNull(map.get("100"));
+    }
+    
+    public void testPutRemove() {
+        ExtObjectContainer client1 = openNewClient();
+        ExtObjectContainer client2 = openNewClient();
+
+        ArrayMap4<String, Integer> map1 = retrieveOnlyInstance(client1);
+        ArrayMap4<String, Integer> map2 = retrieveOnlyInstance(client2);
+        
+        ArrayMap4Asserter.checkMap(map1, 0, 10);
+        ArrayMap4Asserter.checkMap(map2, 0, 10);
+        
+        map1.put("11", Integer.valueOf(11 * 100));
+        Integer value = map2.remove("9");
+        Assert.areEqual(Integer.valueOf(900), value);
+        
+        client1.set(map1);
+        client2.set(map2);
+        client1.close();
+        client2.close();
+        
+        ArrayMap4<String, Integer> map = retrieveOnlyInstance();
+        ArrayMap4Asserter.checkMap(map, 0, 9);
+        Assert.isNull(map.get("9"));
+    }
+    
+    public void testPutClear() {
+        ExtObjectContainer client1 = openNewClient();
+        ExtObjectContainer client2 = openNewClient();
+
+        ArrayMap4<String, Integer> map1 = retrieveOnlyInstance(client1);
+        ArrayMap4<String, Integer> map2 = retrieveOnlyInstance(client2);
+        
+        ArrayMap4Asserter.checkMap(map1, 0, 10);
+        ArrayMap4Asserter.checkMap(map2, 0, 10);
+        
+        map1.put("11", Integer.valueOf(11 * 100));
+        map2.clear();
+        
+        client1.set(map1);
+        client2.set(map2);
+        client1.close();
+        client2.close();
+        
+        ArrayMap4<String, Integer> map = retrieveOnlyInstance();
+        ArrayMap4Asserter.checkClear(map);
+    }
+    
+    public void testPutAllClear() {
+        ExtObjectContainer client1 = openNewClient();
+        ExtObjectContainer client2 = openNewClient();
+
+        ArrayMap4<String, Integer> map1 = retrieveOnlyInstance(client1);
+        ArrayMap4<String, Integer> map2 = retrieveOnlyInstance(client2);
+        
+        ArrayMap4Asserter.checkMap(map1, 0, 10);
+        ArrayMap4Asserter.checkMap(map2, 0, 10);
+        
+        ArrayMap4<String, Integer> other = new ArrayMap4<String, Integer>();
+        for (int i = 10; i < 50; i++) {
+            other.put(String.valueOf(i), Integer.valueOf(i * 100));
+        }
+        map1.putAll(other);
+        map2.clear();
+        
+        client1.set(map1);
+        client2.set(map2);
+        client1.close();
+        client2.close();
+        
+        ArrayMap4<String, Integer> map = retrieveOnlyInstance();
+        ArrayMap4Asserter.checkClear(map);
+    }
+    
+    public void testPutAllPut() {
+        ExtObjectContainer client1 = openNewClient();
+        ExtObjectContainer client2 = openNewClient();
+
+        ArrayMap4<String, Integer> map1 = retrieveOnlyInstance(client1);
+        ArrayMap4<String, Integer> map2 = retrieveOnlyInstance(client2);
+        
+        ArrayMap4Asserter.checkMap(map1, 0, 10);
+        ArrayMap4Asserter.checkMap(map2, 0, 10);
+        
+        ArrayMap4<String, Integer> other = new ArrayMap4<String, Integer>();
+        for (int i = 10; i < 50; i++) {
+            other.put(String.valueOf(i), Integer.valueOf(i * 100));
+        }
+        map1.putAll(other);
+        map2.put("-1", Integer.valueOf(-1 * 100));
+        
+        client1.set(map1);
+        client2.set(map2);
+        client1.close();
+        client2.close();
+        
+        ArrayMap4<String, Integer> map = retrieveOnlyInstance();
+        ArrayMap4Asserter.checkMap(map, -1, 10);
+    }
+    
+    public void testPutAllPutAll() {
+        ExtObjectContainer client1 = openNewClient();
+        ExtObjectContainer client2 = openNewClient();
+
+        ArrayMap4<String, Integer> map1 = retrieveOnlyInstance(client1);
+        ArrayMap4<String, Integer> map2 = retrieveOnlyInstance(client2);
+        
+        ArrayMap4Asserter.checkMap(map1, 0, 10);
+        ArrayMap4Asserter.checkMap(map2, 0, 10);
+        
+        ArrayMap4<String, Integer> other1 = new ArrayMap4<String, Integer>();
+        for (int i = 30; i < 50; i++) {
+            other1.put(String.valueOf(i), Integer.valueOf(i * 100));
+        }
+        
+        ArrayMap4<String, Integer> other2 = new ArrayMap4<String, Integer>();
+        for (int i = 10; i < 30; i++) {
+            other2.put(String.valueOf(i), Integer.valueOf(i * 100));
+        }
+        
+        map1.putAll(other1);
+        map2.putAll(other2);
+        
+        client1.set(map1);
+        client2.set(map2);
+        client1.close();
+        client2.close();
+        
+        ArrayMap4<String, Integer> map = retrieveOnlyInstance();
+        ArrayMap4Asserter.checkMap(map, 0, 30);
+    }
+    
+    public void testPutAllRemove() {
+        ExtObjectContainer client1 = openNewClient();
+        ExtObjectContainer client2 = openNewClient();
+
+        ArrayMap4<String, Integer> map1 = retrieveOnlyInstance(client1);
+        ArrayMap4<String, Integer> map2 = retrieveOnlyInstance(client2);
+        
+        ArrayMap4Asserter.checkMap(map1, 0, 10);
+        ArrayMap4Asserter.checkMap(map2, 0, 10);
+        
+        ArrayMap4<String, Integer> other = new ArrayMap4<String, Integer>();
+        for (int i = 10; i < 50; i++) {
+            other.put(String.valueOf(i), Integer.valueOf(i * 100));
+        }
+        map1.putAll(other);
+        map2.remove("9");
+        
+        client1.set(map1);
+        client2.set(map2);
+        client1.close();
+        client2.close();
+        
+        ArrayMap4<String, Integer> map = retrieveOnlyInstance();
+        ArrayMap4Asserter.checkMap(map, 0, 9);
+    }
+    
+    public void testRemoveClear() {
+        ExtObjectContainer client1 = openNewClient();
+        ExtObjectContainer client2 = openNewClient();
+
+        ArrayMap4<String, Integer> map1 = retrieveOnlyInstance(client1);
+        ArrayMap4<String, Integer> map2 = retrieveOnlyInstance(client2);
+        
+        ArrayMap4Asserter.checkMap(map1, 0, 10);
+        ArrayMap4Asserter.checkMap(map2, 0, 10);
+        
+        map1.remove("9");
+        map2.clear();
+        
+        client1.set(map1);
+        client2.set(map2);
+        client1.close();
+        client2.close();
+        
+        ArrayMap4<String, Integer> map = retrieveOnlyInstance();
+        ArrayMap4Asserter.checkClear(map);
+    }
+    
+    public void testRemovePut() {
+        ExtObjectContainer client1 = openNewClient();
+        ExtObjectContainer client2 = openNewClient();
+
+        ArrayMap4<String, Integer> map1 = retrieveOnlyInstance(client1);
+        ArrayMap4<String, Integer> map2 = retrieveOnlyInstance(client2);
+        
+        ArrayMap4Asserter.checkMap(map1, 0, 10);
+        ArrayMap4Asserter.checkMap(map2, 0, 10);
+        
+        map1.remove("0");
+        map2.put("10", Integer.valueOf(10 * 100));
+        
+        client1.set(map1);
+        client2.set(map2);
+        client1.close();
+        client2.close();
+        
+        ArrayMap4<String, Integer> map = retrieveOnlyInstance();
+        ArrayMap4Asserter.checkMap(map, 0, 11);
+    }
+    
+    public void testRemovePutAll() {
+        ExtObjectContainer client1 = openNewClient();
+        ExtObjectContainer client2 = openNewClient();
+
+        ArrayMap4<String, Integer> map1 = retrieveOnlyInstance(client1);
+        ArrayMap4<String, Integer> map2 = retrieveOnlyInstance(client2);
+        
+        ArrayMap4Asserter.checkMap(map1, 0, 10);
+        ArrayMap4Asserter.checkMap(map2, 0, 10);
+        
+        Integer value = map1.remove("9");
+        Assert.areEqual(Integer.valueOf(900), value);
+        
+        ArrayMap4<String, Integer> other = new ArrayMap4<String, Integer>();
+        for (int i = 10; i < 50; i++) {
+            other.put(String.valueOf(i), Integer.valueOf(i * 100));
+        }
+        map2.putAll(other);
+        
+        client1.set(map1);
+        client2.set(map2);
+        client1.close();
+        client2.close();
+        
+        ArrayMap4<String, Integer> map = retrieveOnlyInstance();
+        ArrayMap4Asserter.checkMap(map, 0, 50);
+        Assert.isNull(map.get("100"));
+    }
+    
+    public void testRemoveRemove() {
+        ExtObjectContainer client1 = openNewClient();
+        ExtObjectContainer client2 = openNewClient();
+
+        ArrayMap4<String, Integer> map1 = retrieveOnlyInstance(client1);
+        ArrayMap4<String, Integer> map2 = retrieveOnlyInstance(client2);
+        
+        ArrayMap4Asserter.checkMap(map1, 0, 10);
+        ArrayMap4Asserter.checkMap(map2, 0, 10);
+        
+        map1.remove("0");
+        map2.remove("9");
+        
+        client1.set(map1);
+        client2.set(map2);
+        client1.close();
+        client2.close();
+        
+        ArrayMap4<String, Integer> map = retrieveOnlyInstance();
+        ArrayMap4Asserter.checkMap(map, 0, 9);
+    }
 }
