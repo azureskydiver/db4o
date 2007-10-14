44509911f92c82c7a0b1021d07dfe1e9166a159e
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-14 00:52:06 +0000

    COR-839: run filters and regexpes in or mode

--- db4otaj/src/com/db4o/ta/instrumentation/ant/TAAntClassEditFactory.java
+++ db4otaj/src/com/db4o/ta/instrumentation/ant/TAAntClassEditFactory.java
@@ -13,6 +13,7 @@ import com.db4o.ta.instrumentation.*;
 public class TAAntClassEditFactory extends ProjectComponent implements ClassEditFactory {
 
 	private final List _regExp = new ArrayList();
+	private final List _filters = new ArrayList();
 
 	public RegularExpression createRegexp() {
         RegularExpression regExp = new RegularExpression();
@@ -20,19 +21,46 @@ public class TAAntClassEditFactory extends ProjectComponent implements ClassEdit
         return regExp;
 	}
 	
+	public void add(ClassFilter classFilter) {
+		_filters.add(classFilter);
+	}
+	
 	public BloatClassEdit createEdit() {
-		ClassFilter filter = null;
-		if(_regExp.isEmpty()) {
-			filter = new AcceptAllClassesFilter();
+		final List filters = new ArrayList(2);
+		for (Iterator filterIter = _filters.iterator(); filterIter.hasNext();) {
+			ClassFilter filter = (ClassFilter) filterIter.next();
+			filters.add(filter);
 		}
-		else {
+		if(!_regExp.isEmpty()) {
 			Regexp[] regExp = new Regexp[_regExp.size()];
 			int idx = 0;
 			for (Iterator reIter = _regExp.iterator(); reIter.hasNext();) {
 				RegularExpression re = (RegularExpression) reIter.next();
 				regExp[idx++] = re.getRegexp(getProject());
 			}
-			filter = new AntRegExpClassFilter(regExp);
+			filters.add(new AntRegExpClassFilter(regExp));
+		}
+		ClassFilter filter = null;
+		switch(filters.size()) {
+			case 0:
+				filter = new AcceptAllClassesFilter();
+				break;
+			case 1:
+				filter = (ClassFilter) filters.get(0);
+				break;
+			default:
+				// TODO: belongs to CompositeClassFilter/ClassFilterBuilder/whatever
+				filter = new ClassFilter() {
+					public boolean accept(Class clazz) {
+						for (Iterator filterIter = filters.iterator(); filterIter.hasNext();) {
+							ClassFilter curFilter = (ClassFilter) filterIter.next();
+							if(curFilter.accept(clazz)) {
+								return true;
+							}
+						}
+						return false;
+					}
+				};
 		}
 		return new InjectTransparentActivationEdit(filter);
 	}
