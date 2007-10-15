a889e05365ba1a5c1da9f6f9b0b3f54b5d243a4d
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-15 02:12:20 +0000

    COR-839: refactor towards fileset for source folders in ant, superugly quickfix for ta/nq collision

---
+++ db4otools/src/com/db4o/instrumentation/file/DefaultFilePathRoot.java
@@ -0,0 +1,85 @@
+package com.db4o.instrumentation.file;
+
+import java.io.*;
+import java.util.*;
+
+public class DefaultFilePathRoot implements FilePathRoot {
+
+	private final String[] _rootDirs;
+	private final String _extension;
+	
+	public DefaultFilePathRoot(String[] rootDirs) {
+		this(rootDirs, "");
+	}
+
+	public DefaultFilePathRoot(String[] rootDirs, String extension) {
+		_rootDirs = rootDirs;
+		_extension = extension;
+	}
+
+	public Iterator files() {
+		return new FileSystemIterator(_rootDirs, _extension);
+	}
+
+	public String[] rootDirs() {
+		return _rootDirs;
+	}
+
+	private static class FileSystemIterator implements Iterator {
+		private final String _extension;
+		private LinkedList _stack = new LinkedList();
+
+		public FileSystemIterator(String[] roots, String extension) {
+			_extension = extension;
+			for (int rootIdx = 0; rootIdx < roots.length; rootIdx++) {
+				File root = new File(roots[rootIdx]);
+				push(new FileWithRoot(root, root));
+			}
+			advanceQueue();
+		}
+
+		public boolean hasNext() {
+			return !_stack.isEmpty();
+		}
+
+		public Object next() {
+			FileWithRoot top = pop();
+			advanceQueue();
+			return top;
+		}
+
+
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+		
+		private void advanceQueue() {
+			while(!_stack.isEmpty() && !accept(peek())) {
+				FileWithRoot dir = pop();
+				if(!dir.file().isDirectory()) {
+					continue;
+				}
+				File[] children = dir.file().listFiles();
+				for (int childIdx = 0; childIdx < children.length; childIdx++) {
+					_stack.addFirst(new FileWithRoot(dir.root(), children[childIdx]));
+				}
+			}
+		}
+
+		private boolean accept(FileWithRoot file) {
+			return file.file().isFile() && file.file().getName().endsWith(_extension);
+		}
+
+		private void push(FileWithRoot root) {
+			_stack.addFirst(root);
+		}		
+
+		private FileWithRoot pop() {
+			return (FileWithRoot) _stack.removeFirst();
+		}
+
+		private FileWithRoot peek() {
+			return (FileWithRoot)_stack.getFirst();
+		}
+	}
+}
