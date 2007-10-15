10a74575929470ba26a81a3acbc1d587c279ff93
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-15 21:33:04 +0000

    cr: #COR-928 Test case to reproduce that databases are closed on ObjectNotStorableExceptions.

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/assorted/ObjectNotStorableExceptionTestCase.java
@@ -0,0 +1,38 @@
+/* Copyright (C) 2007  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.db4ounit.common.assorted;
+
+import com.db4o.ext.*;
+
+import db4ounit.*;
+import db4ounit.extensions.*;
+
+
+public class ObjectNotStorableExceptionTestCase extends AbstractDb4oTestCase{
+
+    public static void main(String[] args) {
+        new ObjectNotStorableExceptionTestCase().runSolo();
+    }
+    
+    public class Item {
+        public Object nakedObject;
+    }
+    
+    public void testObjectContainerAliveAfterObjectNotStorableException(){
+        final Item item = new Item();
+        item.nakedObject = new Object();
+        
+        Assert.expect(ObjectNotStorableException.class,new CodeBlock() {
+            public void run() throws Throwable {
+                store(item);
+            }
+        });
+        
+        item.nakedObject = null;
+        store(item);
+        
+        Item retrieved = (Item)retrieveOnlyInstance(Item.class);
+        Assert.areSame(item, retrieved);
+    }
+
+}
