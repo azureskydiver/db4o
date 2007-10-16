38dba472ac3c189108d4fb0a66a8b24ab129c3f5
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-16 11:22:13 +0000

    cr: #COR-923 closeOnTimeOuts move to the respective classes.

--- db4ounit.extensions/src/db4ounit/extensions/Db4oClientServerTestCase.java
+++ db4ounit.extensions/src/db4ounit/extensions/Db4oClientServerTestCase.java
@@ -1,10 +1,7 @@
 /* Copyright (C) 2007 db4objects Inc. http://www.db4o.com */
 package db4ounit.extensions;
 
-import com.db4o.cs.events.*;
-import com.db4o.events.*;
 import com.db4o.ext.*;
-import com.db4o.internal.cs.*;
 
 import db4ounit.extensions.fixtures.*;
 
@@ -17,15 +14,5 @@ public class Db4oClientServerTestCase extends AbstractDb4oTestCase implements Op
 	public ExtObjectContainer openNewClient() {
 		return clientServerFixture().openNewClient();
 	}
-	
-	protected void closeOnTimeouts(ClientObjectContainer client){
-	    ClientEventRegistryFactory.forClient(client).clientSocketReadTimeout().addListener(new EventListener4() {
-            public void onEvent(Event4 e, EventArgs args) {
-                CancellableEventArgs cancellableArgs = (CancellableEventArgs) args;
-                cancellableArgs.cancel();
-            }
-        });
-    }
-
 	
 }
