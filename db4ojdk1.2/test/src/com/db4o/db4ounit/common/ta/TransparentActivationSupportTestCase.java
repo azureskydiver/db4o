812f9ce92f4d3206d0c4a4802bf12453dfb8b7e8
richard_liang <richard_liang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-17 04:57:51 +0000

    rl: COR-927

---
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/common/ta/TransparentActivationSupportTestCase.java
@@ -0,0 +1,20 @@
+/* Copyright (C) 2007   db4objects Inc.   http://www.db4o.com */
+package com.db4o.db4ounit.common.ta;
+
+import com.db4o.config.*;
+import com.db4o.ta.*;
+
+import db4ounit.*;
+import db4ounit.extensions.*;
+import db4ounit.extensions.fixtures.*;
+
+public class TransparentActivationSupportTestCase extends AbstractDb4oTestCase implements OptOutCS {
+
+	protected void configure(Configuration config) {
+		config.add(new TransparentActivationSupport());
+	}
+	
+	public void testActivationDepth() {
+		Assert.areEqual(0, db().configure().activationDepth());
+	}
+}
