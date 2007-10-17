af74b322902947202dd3520100c5fce88b8d0cf2
richard_liang <richard_liang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-17 04:58:36 +0000

    rl: COR-927

---
+++ db4ojdk5/core/src/com/db4o/collections/ArrayList4.java
@@ -0,0 +1,234 @@
+/* Copyright (C) 2007  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.collections;
+
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+
+import com.db4o.activation.*;
+import com.db4o.ta.*;
+
+/**
+ * db4o ArrayList implementation, which is transparent activate awared.
+ * The behaviour is similar as java.util.ArrayList.
+ * 
+ * @see java.util.ArrayList
+ */
+
+public class ArrayList4<E> extends AbstractList4<E> implements Cloneable,
+		Serializable, RandomAccess, Activatable {
+
+	private static final long serialVersionUID = 7971683768827646182L;
+
+	private E[] elements;
+
+	private int capacity;
+
+	private int listSize;
+	
+	private transient Activator _activator;
+	
+	public void activate() {
+		if(_activator != null) {
+			_activator.activate();
+		}
+	}
+
+	public void bind(Activator activator) {
+		if(_activator != null || activator == null) {
+			throw new IllegalStateException();
+		}
+		_activator = activator;
+	}
+	
+	public ArrayList4() {
+		this(10);
+	}
+
+	@SuppressWarnings("unchecked")
+	public ArrayList4(Collection<? extends E> c) {
+		Object[] data = c.toArray();
+		capacity = data.length;
+		elements = (E[]) new Object[capacity];
+		listSize = data.length;
+		System.arraycopy(data, 0, elements, 0, data.length);
+	}
+
+	@SuppressWarnings("unchecked")
+	public ArrayList4(int initialCapacity) {
+		if (initialCapacity < 0) {
+			throw new IllegalArgumentException();
+		}
+		capacity = initialCapacity;
+		elements = (E[]) new Object[initialCapacity];
+	}
+
+	public void add(int index, E element) {
+		checkIndex(index, 0, size());
+		ensureCapacity(size() + 1);
+		System.arraycopy(elements, index, elements, index + 1, listSize
+					- index);
+			elements[index] = element;
+		increaseSize(1);
+		markModified();
+	}
+
+	public boolean addAll(Collection<? extends E> c) {
+		return addAll(size(), c);
+	}
+
+	public boolean addAll(int index, Collection<? extends E> c) {
+		checkIndex(index, 0, size());
+		int length = c.size();
+		if(length == 0) {
+			return false;
+		}
+		ensureCapacity(size() + length);
+		Object[] toBeAdded = c.toArray();
+		System.arraycopy(elements, index, elements, index+length, size() - index);
+		System.arraycopy(toBeAdded, 0, elements, index, length);
+		increaseSize(length);
+		markModified();
+		return true;
+	}
+
+	public void clear() { 
+		int size = size();
+		Arrays.fill(elements, 0, size, null);
+		setSize(0);
+		markModified();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Object clone() {
+		activate();
+		try {
+			ArrayList4 <E> clonedList = (ArrayList4<E>) super.clone();
+			clonedList.elements = elements.clone();
+			return clonedList;
+		} catch (CloneNotSupportedException e) {
+			throw new Error(e);
+		}
+	}
+
+	public void ensureCapacity(int minCapacity) {
+		activate();
+		if (minCapacity <= capacity) {
+			return;
+		}
+		resize(minCapacity);
+	}
+
+	public E get(int index) {
+		checkIndex(index, 0, size() - 1);
+		return elements[index];
+	}
+
+	public int indexOf(Object o) {
+		for (int index = 0; index < size(); ++index) {
+			E element = get(index);
+			if (o == null ? element == null : o.equals(element)) {
+				return index;
+			}
+		}
+		return -1;
+	}
+
+	public int lastIndexOf(Object o) {
+		for (int index = size() - 1; index >= 0; --index) {
+			E element = get(index);
+			if (o == null ? element == null : o.equals(element)) {
+				return index;
+			}
+		}
+		return -1;
+	}
+	
+	public E remove(int index) {
+		int size = size();
+		checkIndex(index, 0, size - 1);
+		E element = elements[index];
+		System.arraycopy(elements, index + 1, 
+				elements, index, size - index	- 1);
+		elements[size - 1] = null;
+		decreaseSize(1);
+		markModified();
+		return element;
+	}
+
+	protected void removeRange(int fromIndex, int toIndex) {
+		int size = size();
+		if ((fromIndex < 0 || fromIndex >= size || toIndex > size || toIndex < fromIndex)) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (fromIndex == toIndex) {
+			return;
+		}
+		int count = toIndex - fromIndex;
+		System.arraycopy(elements, toIndex, elements, fromIndex, size - toIndex);
+		Arrays.fill(elements, size - count, size, null);
+		decreaseSize(count);
+		markModified();
+	}
+	
+	public E set(int index, E element) {
+		checkIndex(index, 0, size() - 1);
+		E oldValue = elements[index];
+		elements[index] = element;
+		return oldValue;
+	}
+
+	public int size() {
+		activate();
+		return listSize;
+	}
+
+	public Object[] toArray() {
+		int size = size();
+		Object[] data = new Object[size];
+		System.arraycopy(elements, 0, data, 0, size);
+		return data;
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T> T[] toArray(T[] a) {
+		int size = size();
+		if(a.length < size) {
+			a = (T[]) Array.newInstance(a.getClass().getComponentType(), size);
+		}
+		System.arraycopy(elements, 0, a, 0, size);
+		return a;
+	}
+
+	public void trimToSize() {
+		resize(size());
+	}
+
+
+	@SuppressWarnings("unchecked")
+	private void resize(int minCapacity) {
+		markModified();
+		E[] temp = (E[]) new Object[minCapacity];
+		System.arraycopy(elements, 0, temp, 0, size());
+		elements = temp;
+		capacity = minCapacity;
+	}
+
+	void setSize(int count) {
+		listSize = count;
+	}
+	
+	void increaseSize(int count) {
+		listSize += count;
+	}
+	
+	void decreaseSize(int count) {
+		listSize -= count;
+	}
+	
+	void markModified() {
+		++modCount;
+	}
+
+}
---
+++ db4ojdk5/test/src/com/db4o/db4ounit/jre5/collections/ArrayList4TestCase.java
@@ -0,0 +1,213 @@
+/* Copyright (C) 2007  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.db4ounit.jre5.collections;
+
+import java.util.*;
+
+import com.db4o.collections.*;
+
+import db4ounit.*;
+
+public class ArrayList4TestCase implements TestLifeCycle {
+
+	public static void main(String[] args) {
+		new TestRunner(ArrayList4TestCase.class).run();
+	}
+	
+	public ArrayList4 <Integer> _list;
+
+	public void setUp() throws Exception {
+		_list = new ArrayList4<Integer>();
+		ArrayList4Asserter.createList(_list);
+	}
+
+	public void tearDown() throws Exception {
+		
+	}
+	
+	public void testConstructor() throws Exception {
+		ArrayList4<Integer> arrayList = new ArrayList4<Integer>();
+		fill(arrayList);
+		Assert.areEqual(ArrayList4Asserter.CAPACITY, arrayList.size());
+	}
+	
+	public void testConstructor_I_LegalArguments1() throws Exception {
+		ArrayList4<Integer> arrayList; 
+		arrayList = new ArrayList4<Integer>(ArrayList4Asserter.CAPACITY);
+		fill(arrayList);
+		Assert.areEqual(ArrayList4Asserter.CAPACITY, arrayList.size());
+	}
+	
+	public void testConstructor_I_LegalArguments2() throws Exception {
+		ArrayList4<Integer> arrayList; 
+		arrayList = new ArrayList4<Integer>(0);
+		fill(arrayList);
+		Assert.areEqual(ArrayList4Asserter.CAPACITY, arrayList.size());
+	}
+
+	public void testConstructor_I_IllegalArgumentException() throws Exception {
+		Assert.expect(IllegalArgumentException.class, new CodeBlock(){
+			public void run() throws Throwable {
+				new ArrayList4<Integer>(-1);
+			}
+		});
+	}
+	
+	public void testConstructor_LCollection_NullPointerException() throws Exception {
+		Assert.expect(NullPointerException.class, new CodeBlock(){
+			public void run() throws Throwable {
+				new ArrayList4<Integer>(null);
+			}
+		});
+	}
+	
+	public void testConstructor_LCollection() throws Exception {
+		ArrayList4<Integer> arrayList = new ArrayList4<Integer>(_list);
+		Assert.areEqual(_list.size(), arrayList.size());
+		Assert.isTrue(Arrays.equals(_list.toArray(), arrayList.toArray()));
+		
+	}
+
+	private void fill(ArrayList4<Integer> arrayList) {
+		for (int i = 0; i < ArrayList4Asserter.CAPACITY; i++) {
+			arrayList.add(new Integer(i));
+		}
+	}
+
+	public void testAdd() throws Exception {
+		ArrayList4Asserter.assertAdd(_list);
+	}
+
+	public void testAdd_LObject() throws Exception {
+		ArrayList4Asserter.assertAdd_LObject(_list);
+	}
+
+	public void testAddAll_LCollection() throws Exception {
+		ArrayList4Asserter.assertAddAll_LCollection(_list);
+	}
+
+	public void testClear() throws Exception {
+		ArrayList4Asserter.assertClear(_list);
+	}
+
+	public void testContains() throws Exception {
+		ArrayList4Asserter.assertContains(_list);
+	}
+
+	public void testContainsAll() throws Exception {
+		ArrayList4Asserter.assertContainsAll(_list);
+	}
+
+	public void testIndexOf() throws Exception {
+		ArrayList4Asserter.assertIndexOf(_list);
+	}
+
+	public void testIsEmpty() throws Exception {
+		ArrayList4Asserter.assertIsEmpty(_list);
+	}
+
+	public void testIterator() throws Exception {
+		ArrayList4Asserter.assertIterator(_list);
+	}
+
+	public void testLastIndexOf() throws Exception {
+		ArrayList4Asserter.assertLastIndexOf(_list);
+	}
+
+	public void testRemove_LObject() throws Exception {
+		ArrayList4Asserter.assertRemove_LObject(_list);
+	}
+
+	public void testRemoveAll() throws Exception {
+		ArrayList4Asserter.assertRemoveAll(_list);
+	}
+
+	public void testSet() throws Exception {
+		ArrayList4Asserter.assertSet(_list);
+	}
+
+	public void testSize() throws Exception {
+		ArrayList4Asserter.assertSize(_list);
+	}
+	
+	public void testToArray() throws Exception {
+		ArrayList4Asserter.assertToArray(_list);
+	}
+	
+	public void testToArray_LObject() throws Exception {
+		ArrayList4Asserter.assertToArray_LObject(_list);
+	}
+	
+	public void testToString() throws Exception {
+		ArrayList4Asserter.assertToString(_list);
+	}
+	
+	public void testTrimToSize_EnsureCapacity() throws Exception {
+		ArrayList4Asserter.assertTrimToSize_EnsureCapacity(_list);
+	}
+	
+	public void testTrimToSize_Remove() throws Exception {
+		ArrayList4Asserter.assertTrimToSize_Remove(_list);
+	}
+	
+	public void testTrimToSize_Iterator() throws Exception {
+		ArrayList4Asserter.assertTrimToSize_Iterator(_list);
+	}
+	
+	public void testEnsureCapacity_Iterator() throws Exception {
+		ArrayList4Asserter.assertEnsureCapacity_Iterator(_list);
+	}
+	
+	public void testClear_Iterator() throws Exception {
+		ArrayList4Asserter.assertClear_Iterator(_list);
+	}
+	
+	public void testClone() throws Exception {
+		ArrayList4Asserter.assertClone(_list);
+	}
+	
+	public void testEquals() throws Exception {
+		ArrayList4Asserter.assertEquals(_list);
+	}
+	
+	public void testIteratorNext_NoSuchElementException() throws Exception {
+		ArrayList4Asserter.assertIteratorNext_NoSuchElementException(_list);
+	}
+	
+	public void testIteratorNext_ConcurrentModificationException() throws Exception {
+		ArrayList4Asserter.assertIteratorNext_ConcurrentModificationException(_list);
+	}
+	
+	public void testIteratorNext() throws Exception {
+		ArrayList4Asserter.assertIteratorNext(_list);
+	}
+	
+	public void testIteratorRemove() throws Exception {
+		ArrayList4Asserter.assertIteratorRemove(_list);
+	}
+	
+	public void testRemove_IllegalStateException() throws Exception {
+		ArrayList4Asserter.assertRemove_IllegalStateException(_list);
+	}
+	
+	public void testIteratorRemove_ConcurrentModificationException() throws Exception {
+		ArrayList4Asserter.assertIteratorRemove_ConcurrentModificationException(_list);
+	}
+	
+	public void testSubList() throws Exception {
+		ArrayList4Asserter.assertSubList(_list);
+	}
+	
+	public void testSubList_ConcurrentModification() throws Exception {
+		ArrayList4Asserter.assertSubList_ConcurrentModification(_list);
+	}
+	
+	public void testSubList_Clear() throws Exception {
+		ArrayList4Asserter.assertSubList_Clear(_list);
+	}
+	
+	public void testSubList_Clear2() throws Exception {
+		ArrayList4Asserter.assertSubList_Clear2(_list);
+	}
+	
+}
