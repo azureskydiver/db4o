0eb21e0f2971923b8ee8365f1d2b8eea91e88dec
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-18 12:02:46 +0000

    az: #COR-929 put ta tests into different packages.

--- db4ojdk1.2/test/src/com/db4o/db4ounit/common/ta/config/TransparentActivationSupportTestCase.java
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/common/ta/config/TransparentActivationSupportTestCase.java
@@ -1,5 +1,7 @@
 /* Copyright (C) 2007   db4objects Inc.   http://www.db4o.com */
-package com.db4o.db4ounit.common.ta;
+package com.db4o.db4ounit.common.ta.config;
+
+import com.db4o.db4ounit.common.ta.*;
 
 import db4ounit.*;
 
--- db4ojdk1.2/test/src/com/db4o/db4ounit/common/ta/diagnostics/TransparentActivationDiagnosticsTestCase.java
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/common/ta/diagnostics/TransparentActivationDiagnosticsTestCase.java
@@ -1,8 +1,9 @@
 /* Copyright (C) 2007   db4objects Inc.   http://www.db4o.com */
-package com.db4o.db4ounit.common.ta;
+package com.db4o.db4ounit.common.ta.diagnostics;
 
 import com.db4o.activation.*;
 import com.db4o.config.*;
+import com.db4o.db4ounit.common.ta.*;
 import com.db4o.diagnostic.*;
 import com.db4o.internal.*;
 import com.db4o.ta.*;
