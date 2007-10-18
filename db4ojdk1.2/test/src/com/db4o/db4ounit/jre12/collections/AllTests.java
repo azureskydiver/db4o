33ddfab4e6ed019054a76ebc694feddc1f8fcaac
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-18 15:57:24 +0000

    rbo+acv: COR-822 - ported CollectionActivation from legacy test suite to db4ounit

--- db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/collections/AllTests.java
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/collections/AllTests.java
@@ -13,6 +13,7 @@ public class AllTests extends Db4oTestSuite {
 	protected Class[] testCases() {
 		return new Class[] {
 		    ArrayListCandidatesTestCase.class,
+		    CollectionActivationTestCase.class,
 			CollectionUuidTest.class,
 			EmbeddedClientObjectContainerJre12TestCase.class,
 			Iterator4JdkIteratorTestCase.class,
