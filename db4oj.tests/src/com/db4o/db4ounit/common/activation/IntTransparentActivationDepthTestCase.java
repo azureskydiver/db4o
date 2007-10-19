eb304690647153eb7c47ff80ae5fe18e98a8f105
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-19 07:50:30 +0000

    az: #COR-930 test ta activate() behaviour instead of testing ObjectReference#activate directly.

--- db4oj.tests/src/com/db4o/db4ounit/common/activation/IntTransparentActivationDepthTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/activation/IntTransparentActivationDepthTestCase.java
@@ -2,54 +2,35 @@
 
 package com.db4o.db4ounit.common.activation;
 
-import com.db4o.config.*;
-import com.db4o.internal.*;
+import com.db4o.db4ounit.common.ta.*;
 
 import db4ounit.*;
-import db4ounit.extensions.*;
 import db4ounit.extensions.fixtures.*;
 
 /**
  * @exclude
  */
-public class IntTransparentActivationDepthTestCase extends AbstractDb4oTestCase implements OptOutCS{
+public class IntTransparentActivationDepthTestCase extends TransparentActivationTestCaseBase implements OptOutCS{
 	public static void main(String[] args) {
 		new IntTransparentActivationDepthTestCase().runAll();
 	}
 
-	protected void configure(Configuration config) throws Exception {
-		config.activationDepth(0);
-		super.configure(config);
-	}
-
-	public static class Item {
-		public int value;
-
-		public LinkedItemList list;
-
-		public Item() {
-
-		}
-	}
-
 	protected void store() throws Exception {
-		Item item = new Item();
+		TAIntItem item = new TAIntItem();
 		item.value = 42;
-		item.list = LinkedItemList.newList(5);
+		item.list = LinkedList.newList(5);
 		store(item);
 	}
 
 	public void test() throws Exception {
-		Item item = (Item) retrieveOnlyInstance(Item.class);
+		TAIntItem item = (TAIntItem) retrieveOnlyInstance(TAIntItem.class);
 		asertNullItem(item);
-		ObjectReference itemRef = trans().referenceForObject(item);
-		itemRef.activate();
-		Assert.areEqual(42, item.value);
+		Assert.areEqual(42, item.getValue());
 		Assert.isNotNull(item.list);
 		Assert.isNull(item.list.next);
 	}
 
-	private void asertNullItem(Item item) {
+	private void asertNullItem(TAIntItem item) {
 		Assert.areEqual(0, item.value);
 		Assert.isNull(item.list);
 	}
