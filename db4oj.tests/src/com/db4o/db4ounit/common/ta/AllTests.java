4457a2fad3a97fe7322fd06b73d9404f81d616fe
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-19 07:57:51 +0000

    az: #COR-930 move to ta package.

--- db4oj.tests/src/com/db4o/db4ounit/common/ta/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/ta/AllTests.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2007 db4objects Inc. http://www.db4o.com */
 
-package com.db4o.db4ounit.common.activation;
+package com.db4o.db4ounit.common.ta;
 
 import db4ounit.extensions.Db4oTestSuite;
 
