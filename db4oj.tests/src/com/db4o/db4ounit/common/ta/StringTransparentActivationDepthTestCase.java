72531d2d9755207f9582c57392fce54923fe57a8
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-19 09:33:19 +0000

    az: #COR-930 String ta test.

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/ta/StringTransparentActivationDepthTestCase.java
@@ -0,0 +1,39 @@
+/* Copyright (C) 2007  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.db4ounit.common.ta;
+
+import db4ounit.*;
+import db4ounit.extensions.fixtures.*;
+
+/**
+ * @exclude
+ */
+public class StringTransparentActivationDepthTestCase extends TransparentActivationTestCaseBase implements OptOutCS{
+	public static void main(String[] args) {
+		new StringTransparentActivationDepthTestCase().runAll();
+	}
+
+	protected void store() throws Exception {
+		TAStringItem item = new TAStringItem();
+		item.value = "42";
+		item.obj = "hello";
+		item.list = LinkedList.newList(5);
+		store(item);
+	}
+
+	public void test() throws Exception {
+		TAStringItem item = (TAStringItem) retrieveOnlyInstance(TAStringItem.class);
+		asertNullItem(item);
+		Assert.areEqual("42", item.value());
+		Assert.areEqual("hello", item.object());
+		Assert.isNotNull(item.list());
+		Assert.isNull(item.list().next);
+	}
+
+	private void asertNullItem(TAStringItem item) {
+		Assert.isNull(item.value);
+		Assert.isNull(item.obj);
+		Assert.isNull(item.list);
+	}
+
+}
