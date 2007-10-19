d028274c504ccdac55bcde20201b136c1bbe55c0
richard_liang <richard_liang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-19 12:43:06 +0000

    rl: test label for defrag test

--- db4oj.tests/src/com/db4o/db4ounit/common/defragment/Db4oDefragSolo.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/defragment/Db4oDefragSolo.java
@@ -12,39 +12,44 @@ import com.db4o.foundation.*;
 import db4ounit.extensions.fixtures.*;
 
 public class Db4oDefragSolo extends Db4oSolo {
-		public Db4oDefragSolo(ConfigurationSource configSource) {
-			super(configSource);
-		}
+    public Db4oDefragSolo(ConfigurationSource configSource) {
+        super(configSource);
+    }
 
-		protected ObjectContainer createDatabase(Configuration config) {
-			File origFile=new File(getAbsolutePath());
-			if(origFile.exists()) {
-				try {
-					String backupFile = getAbsolutePath()+".defrag.backup";
-					ContextIDMapping mapping=
-						new TreeIDMapping();
-						//new BTreeIDMapping(getAbsolutePath()+".defrag.mapping",4096,1,1000);
-					DefragmentConfig defragConfig = new DefragmentConfig(getAbsolutePath(), backupFile, mapping);
-					defragConfig.forceBackupDelete(true);
-					// FIXME Cloning is ugly - wrap original in Decorator within DefragContext instead?
-					Configuration clonedConfig=(Configuration)((DeepClone)config).deepClone(null);
-					defragConfig.db4oConfig(clonedConfig);
-					Defragment.defrag(defragConfig, new DefragmentListener() {
-						public void notifyDefragmentInfo(DefragmentInfo info) {
-							System.err.println(info);
-						}
-					});
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			return super.createDatabase(config);
-		}
-		
-		public boolean accept(Class clazz) {
-			return super.accept(clazz)&&!OptOutDefragSolo.class.isAssignableFrom(clazz);
-		}
-		
-//		public void clean() {
-//		}
-	}
+    protected ObjectContainer createDatabase(Configuration config) {
+        File origFile = new File(getAbsolutePath());
+        if (origFile.exists()) {
+            try {
+                String backupFile = getAbsolutePath() + ".defrag.backup";
+                ContextIDMapping mapping = new TreeIDMapping();
+                // new
+                // BTreeIDMapping(getAbsolutePath()+".defrag.mapping",4096,1,1000);
+                DefragmentConfig defragConfig = new DefragmentConfig(
+                        getAbsolutePath(), backupFile, mapping);
+                defragConfig.forceBackupDelete(true);
+                // FIXME Cloning is ugly - wrap original in Decorator within
+                // DefragContext instead?
+                Configuration clonedConfig = (Configuration) ((DeepClone) config)
+                        .deepClone(null);
+                defragConfig.db4oConfig(clonedConfig);
+                Defragment.defrag(defragConfig, new DefragmentListener() {
+                    public void notifyDefragmentInfo(DefragmentInfo info) {
+                        System.err.println(info);
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return super.createDatabase(config);
+    }
+
+    public boolean accept(Class clazz) {
+        return super.accept(clazz)
+                && !OptOutDefragSolo.class.isAssignableFrom(clazz);
+    }
+
+    public String getLabel() {
+        return "Defrag-" + super.getLabel();
+    }
+}
