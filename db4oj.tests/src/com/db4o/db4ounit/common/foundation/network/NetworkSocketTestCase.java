cbd04c1b684811cae4e4b94b96dae3fc56f42b89
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-20 16:53:48 +0000

    cr: #COR-934 Fixed NetworkSocketTestCase to run successfully on my machine and included into the normal regression test run.

--- db4oj.tests/src/com/db4o/db4ounit/common/foundation/network/NetworkSocketTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/foundation/network/NetworkSocketTestCase.java
@@ -14,13 +14,13 @@ import db4ounit.*;
  */
 public class NetworkSocketTestCase implements TestLifeCycle {
 
-	private ServerSocket4 _server;
+	private ServerSocket4 _serverSocket;
 
 	private int _port;
 	
-	Socket4 client;
+	Socket4 _client;
 	
-	Socket4 serverSide;
+	Socket4 _server;
 
 	private PlainSocketFactory _plainSocketFactory = new PlainSocketFactory();
 
@@ -29,118 +29,167 @@ public class NetworkSocketTestCase implements TestLifeCycle {
 	}
 
 	public void setUp() throws Exception {
-		_server = new ServerSocket4(_plainSocketFactory, 0);
-		_port = _server.getLocalPort();
-		client = new NetworkSocket(_plainSocketFactory,
-				"localhost", _port);
-		serverSide = _server.accept();
+		_serverSocket = new ServerSocket4(_plainSocketFactory, 0);
+		_port = _serverSocket.getLocalPort();
+		_client = new NetworkSocket(_plainSocketFactory, "localhost", _port);
+		_server = _serverSocket.accept();
 	}
 
 	public void tearDown() throws Exception {
-		_server.close();
+		_serverSocket.close();
 	}
 
-	public void testRead_Close1() throws Exception {	
-		assertReadClose(client, new CodeBlock (){
+	public void testReadIntCloseClient() throws Exception {	
+		assertReadClose(_client, new CodeBlock (){
 			public void run() {
-				serverSide.read();
-			}			
-		});
-	}
-
-	public void testRead_Close2() throws Exception {
-		assertReadClose(serverSide, new CodeBlock (){
-			public void run() {
-				client.read();
+				_server.read();
 			}			
 		});
 	}
 
-	public void testReadBII_Close1() throws Exception {
-		assertReadClose(client, new CodeBlock (){
+	public void testReadIntCloseServer() throws Exception {
+		assertReadClose(_server, new CodeBlock (){
 			public void run() {
-				serverSide.read(new byte[10], 0, 10);
+				_client.read();
 			}			
 		});
 	}
 
-	public void testReadBII_Close2() throws Exception {
-		assertReadClose(serverSide, new CodeBlock (){
+	public void testReadByteArrayCloseClient() throws Exception {
+		assertReadClose(_client, new CodeBlock (){
 			public void run() {
-				client.read(new byte[10], 0, 10);
+				_server.read(new byte[10], 0, 10);
+			}			
+		});
+	}
+
+	public void testReadByteArrayCloseServer() throws Exception {
+		assertReadClose(_server, new CodeBlock (){
+			public void run() {
+				_client.read(new byte[10], 0, 10);
 			}			
 		});
 	}
 
 	
-	public void testWriteB_Close1() throws Exception {	
-		assertWriteClose(client, new CodeBlock (){
+	public void testWriteByteArrayCloseClient() throws Exception {	
+		assertWriteClose(_client, new CodeBlock (){
 			public void run() {
-				serverSide.write(new byte[10]);
-				serverSide.write(new byte[10]);
+			    _server.write(new byte[10]);
 			}			
 		});
 	}
 	
-	public void testWriteB_Close2() throws Exception {	
-		assertWriteClose(serverSide, new CodeBlock (){
+	public void testWriteByteArrayCloseServer() throws Exception {	
+		assertWriteClose(_server, new CodeBlock (){
 			public void run() {
-				client.write(new byte[10]);
-				client.write(new byte[10]);
+			    _client.write(new byte[10]);
 			}			
 		});
 	}
 	
-	public void testWriteBII_Close1() throws Exception {	
-		assertWriteClose(client, new CodeBlock (){
+	public void testWriteByteArrayPartCloseClient() throws Exception {	
+		assertWriteClose(_client, new CodeBlock (){
 			public void run() {
-				serverSide.write(new byte[10], 0, 10);
-				serverSide.write(new byte[10], 0, 10);
+			    _server.write(new byte[10], 0, 10);
 			}			
 		});
 	}
 	
-	public void testWriteBII_Close2() throws Exception {	
-		assertWriteClose(serverSide, new CodeBlock (){
+	public void testWriteByteArrayPartCloseServer() throws Exception {	
+		assertWriteClose(_server, new CodeBlock (){
 			public void run() {
-				client.write(new byte[10], 0, 10);
-				client.write(new byte[10], 0, 10);
+			    _client.write(new byte[10], 0, 10);
 			}			
 		});
 	}
 	
-	public void testWriteI_Close1() throws Exception {	
-		assertWriteClose(client, new CodeBlock (){
+	public void testWriteIntCloseClient() throws Exception {	
+		assertWriteClose(_client, new CodeBlock (){
 			public void run() {
-				serverSide.write(0xff);
-				serverSide.write(0xff);
+			    _server.write(0xff);
 			}			
 		});
 	}
 	
-	public void testWriteI_Close2() throws Exception {	
-		assertWriteClose(serverSide, new CodeBlock (){
+	public void testWriteIntCloseServer() throws Exception {	
+		assertWriteClose(_server, new CodeBlock (){
 			public void run() {
-				client.write(0xff);
-				client.write(0xff);
+			    _client.write(0xff);    
 			}			
 		});
 	}
 	
-	
-	private void assertReadClose(final Socket4 socketToBeClosed,final CodeBlock codeBlock) {
-		new Thread() {
-			public void run() {
-				Cool.sleepIgnoringInterruption(500);
-				socketToBeClosed.close();
-			}
-		}.start();
-		
-		Assert.expect(Db4oIOException.class, codeBlock);
-	}
-	
-	private void assertWriteClose(final Socket4 socketToBeClosed,final CodeBlock codeBlock) {
+	private void assertReadClose(final Socket4 socketToBeClosed,final CodeBlock codeBlock) throws InterruptedException {
+	    CatchAllThread thread = new CatchAllThread(codeBlock);
+	    thread.ensureStarted();
 		socketToBeClosed.close();
-		Assert.expect(Db4oIOException.class, codeBlock);
+		thread.join();
+		Assert.isInstanceOf(Db4oIOException.class, thread.caught());
 	}
+	
+	private void assertWriteClose(final Socket4 socketToBeClosed,final CodeBlock codeBlock){
+		socketToBeClosed.close();
+		Assert.expect(Db4oIOException.class, new CodeBlock() {
+            public void run() throws Throwable {
+                // This is a magic number: 
+                // On my machine all tests start to pass when I write at least 7 times. 
+                for (int i = 0; i < 7; i++) {
+                    codeBlock.run();
+                }
+            }
+        });
+	}
+	
+	
+	static class CatchAllThread {
+	    
+	    private final Thread _thread;
+	    
+	    boolean _isRunning;
+	    
+        final CodeBlock _codeBlock;
+        
+	    Throwable _throwable;
+	    
+	    public CatchAllThread(CodeBlock codeBlock){
+	        _thread = new Thread(new Runnable() {
+                public void run() {
+                    try{
+                        synchronized(this){
+                            _isRunning = true;
+                        }
+                        _codeBlock.run();
+                    } catch (Throwable t){
+                        _throwable = t;
+                    }
+                }
+            });
+	        _codeBlock = codeBlock;
+	    }
+	    
+	    public void join() throws InterruptedException {
+	        _thread.join();
+        }
+	    
+	    private boolean isRunning(){
+	        synchronized(this){
+	            return _isRunning;
+	        }
+	    }
+	    
+	    public void ensureStarted(){
+	        _thread.start();
+	        while(! isRunning()){
+	            Cool.sleepIgnoringInterruption(10);
+	        }
+	        Cool.sleepIgnoringInterruption(10);
+	    }
+	    
+	    public Throwable caught(){
+	        return _throwable;
+	    }
+	    
+	}
+	
 }
