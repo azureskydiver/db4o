dfa5e23e6e883e3fbf690a63d6210cf4a18cb58b
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-24 13:30:01 +0000

    rbo+cr: COR-822 - created AllTests test suite for common.ta

---
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/common/ta/AllTests.java
@@ -0,0 +1,16 @@
+package com.db4o.db4ounit.common.ta;
+
+import db4ounit.extensions.*;
+
+public class AllTests extends Db4oTestSuite {
+
+	protected Class[] testCases() {
+		return new Class[] {
+			com.db4o.db4ounit.common.ta.config.AllTests.class,
+			com.db4o.db4ounit.common.ta.diagnostics.AllTests.class,
+			com.db4o.db4ounit.common.ta.hierarchy.AllTests.class,
+			com.db4o.db4ounit.common.ta.nested.AllTests.class,
+		};
+	}
+
+}
