2c7353fb59ea710c9f6bb429157056276307d91f
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-10-27 18:48:17 +0000

    cr: #COR-822 Getting TAArrayListTestCase to pass and adding the test to regression.

--- db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/AllTestsJdk1_2.java
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/AllTestsJdk1_2.java
@@ -10,8 +10,10 @@ public class AllTestsJdk1_2 extends Db4oTestSuite {
 
 	protected Class[] testCases() {
 		return new Class[] {
+		    
 			// FIXME: solve the workspacePath issue and uncomment migration.AllCommonTests.class below
 //			com.db4o.db4ounit.common.migration.AllCommonTests.class,
+		    
 			com.db4o.db4ounit.common.defragment.LegacyDatabaseDefragTestCase.class,
 			com.db4o.db4ounit.common.ta.AllTests.class,
 			com.db4o.db4ounit.jre11.AllTests.class,
@@ -25,6 +27,7 @@ public class AllTestsJdk1_2 extends Db4oTestSuite {
 			com.db4o.db4ounit.jre12.collections.map.AllTests.class,
 			com.db4o.db4ounit.jre12.querying.AllTests.class,
 			com.db4o.db4ounit.jre12.regression.AllTests.class,
+			com.db4o.db4ounit.jre12.ta.collections.AllTests.class,
 		};
 	}
 }
