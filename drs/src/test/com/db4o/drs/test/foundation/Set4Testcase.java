1a47771b5fb05676dfe5b85deadc20f1c6cae9e6
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-11-01 16:36:37 +0000

    fixed Set4 (that I broke after a supposedly harmless refactoring :/)

--- drs/src/test/com/db4o/drs/test/foundation/Set4Testcase.java
+++ drs/src/test/com/db4o/drs/test/foundation/Set4Testcase.java
@@ -8,10 +8,27 @@ import db4ounit.TestCase;
 public class Set4Testcase implements TestCase {
 	
 	public void testSingleElementIteration() {
-		Set4 set = new Set4();
-		set.add("first");
+		Set4 set = newSet("first");
 		Assert.areEqual("first", Iterators.next(set.iterator()));
+	}
+
+	public void testContainsAll() {
+		Set4 set = newSet("42");
+		set.add("foo");
 		
+		Assert.isTrue(set.containsAll(newSet("42")));
+		Assert.isTrue(set.containsAll(newSet("foo")));
+		Assert.isTrue(set.containsAll(set));
+		
+		Set4 other = new Set4(set);
+		other.add("bar");
+		Assert.isFalse(set.containsAll(other));
+	}
+	
+	private Set4 newSet(final String firstElement) {
+		Set4 set = new Set4();
+		set.add(firstElement);
+		return set;
 	}
 
 }
