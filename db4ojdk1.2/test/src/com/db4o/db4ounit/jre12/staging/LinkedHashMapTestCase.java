68fbcd3e137ce756e029c3bb2daf3a7d4f1e03a3
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2007-11-05 11:22:59 +0000

    temporarily commented, LinkedHashMap not found for this project

--- db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/staging/LinkedHashMapTestCase.java
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/staging/LinkedHashMapTestCase.java
@@ -1,7 +1,5 @@
 package com.db4o.db4ounit.jre12.staging;
 
-import java.util.*;
-
 import db4ounit.*;
 import db4ounit.extensions.*;
 import db4ounit.extensions.fixtures.*;
@@ -12,19 +10,19 @@ public class LinkedHashMapTestCase extends AbstractDb4oTestCase implements OptOu
 	}
 
 	protected void store() throws Exception {
-		LinkedHashMap hashmap = new LinkedHashMap();
-		for (int i = 0; i < 42; ++i) {
-			hashmap.put(new Integer(i), "hello" + i);
-		}
-		store(hashmap);
+//		LinkedHashMap hashmap = new LinkedHashMap();
+//		for (int i = 0; i < 42; ++i) {
+//			hashmap.put(new Integer(i), "hello" + i);
+//		}
+//		store(hashmap);
 	}
 
 	public void test() throws Exception {
-		LinkedHashMap hashmap = (LinkedHashMap) retrieveOnlyInstance(LinkedHashMap.class);
-		long oldSize = db().systemInfo().totalSize();
-		store(hashmap);
-		db().commit();
-		long newSize = db().systemInfo().totalSize();
-		Assert.areEqual(oldSize, newSize);
+//		LinkedHashMap hashmap = (LinkedHashMap) retrieveOnlyInstance(LinkedHashMap.class);
+//		long oldSize = db().systemInfo().totalSize();
+//		store(hashmap);
+//		db().commit();
+//		long newSize = db().systemInfo().totalSize();
+//		Assert.areEqual(oldSize, newSize);
 	}
 }
