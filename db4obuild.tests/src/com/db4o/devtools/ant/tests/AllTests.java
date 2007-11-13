daeb4a03cd037e38f3667e7463fa0fab56ced617
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-11-13 18:07:12 +0000

    revised test cases

--- db4obuild.tests/src/com/db4o/devtools/ant/tests/AllTests.java
+++ db4obuild.tests/src/com/db4o/devtools/ant/tests/AllTests.java
@@ -3,14 +3,15 @@ package com.db4o.devtools.ant.tests;
 import db4ounit.extensions.Db4oTestSuite;
 
 public class AllTests extends Db4oTestSuite {
-
+	
 	@Override
 	protected Class[] testCases() {
-		return new Class[] { FolderDiffTestCase.class };
+		return new Class[] {
+			FolderDiffTestCase.class
+		};
 	}
 	
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		new AllTests().runAll();
 	}
 
