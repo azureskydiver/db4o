ce8b9891b9a1933b6fa27e72f728280196fc77ac
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2007-11-13 13:40:18 +0000

    Tests for db4obuild

---
+++ db4obuild.tests/src/com/db4o/devtools/ant/tests/AllTests.java
@@ -0,0 +1,17 @@
+package com.db4o.devtools.ant.tests;
+
+import db4ounit.extensions.Db4oTestSuite;
+
+public class AllTests extends Db4oTestSuite {
+
+	@Override
+	protected Class[] testCases() {
+		return new Class[] { FolderDiffTestCase.class };
+	}
+	
+	public static void main(String[] args)
+	{
+		new AllTests().runAll();
+	}
+
+}
