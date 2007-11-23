cc7c584d336bda837893cf284cfe263f7291d22e
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2007-11-23 22:50:41 +0000

    acv: COR-817

--- db4oj/core/src/com/db4o/Db4oVersion.java
+++ db4oj/core/src/com/db4o/Db4oVersion.java
@@ -6,9 +6,9 @@ package com.db4o;
 * @exclude
 */
 public class Db4oVersion {
-    public static final String NAME = "7.0.15.7763";
+    public static final String NAME = "7.0.18.8530";
     public static final int MAJOR = 7;
     public static final int MINOR = 0;
-    public static final int ITERATION = 15;
-    public static final int REVISION = 7763;
+    public static final int ITERATION = 18;
+    public static final int REVISION = 8530;
 }
