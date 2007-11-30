e1be8c5a7ff61610212161b1895cf489dbaadd41
richard_liang <richard_liang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-11-30 17:57:14 +0000

    rl: re-fix ArrayDictionary4 TA test case failure

--- db4ojdk5/core/src/com/db4o/collections/ArrayMap4.java
+++ db4ojdk5/core/src/com/db4o/collections/ArrayMap4.java
@@ -43,7 +43,7 @@ public class ArrayMap4<K, V> implements Map<K, V>, Serializable, Cloneable,
     private transient Activator _activator;
 
     public ArrayMap4() {
-        //this(16);
+        this(16);
     }
 
     public ArrayMap4(int initialCapacity) {
@@ -72,20 +72,6 @@ public class ArrayMap4<K, V> implements Map<K, V>, Serializable, Cloneable,
         }
         _activator = activator;
     }
-    
-    private Object[] getKeysArray() {
-        if (_keys == null) {
-            initializeBackingArray(16);
-        }
-        return _keys;
-    }
-    
-    private Object[] getValuesArray() {
-        if (_values == null) {
-            initializeBackingArray(16);
-        }
-        return _values;
-    }
 
 	/**
 	 * java.util.Map implementation but transparently 
@@ -99,8 +85,8 @@ public class ArrayMap4<K, V> implements Map<K, V>, Serializable, Cloneable,
         
         _startIndex = 0;
         _endIndex = 0;
-        Arrays.fill(getKeysArray(), null);
-        Arrays.fill(getValuesArray(), null);
+        Arrays.fill(_keys, null);
+        Arrays.fill(_values, null);
     }
 
 	/**
@@ -142,7 +128,7 @@ public class ArrayMap4<K, V> implements Map<K, V>, Serializable, Cloneable,
 	}
 
 	private int indexOfValue(V value) {
-		return indexOf(getValuesArray(), value);
+		return indexOf(_values, value);
 	}
 
 	/**
@@ -182,7 +168,7 @@ public class ArrayMap4<K, V> implements Map<K, V>, Serializable, Cloneable,
     }
 
 	private V valueAt(int index) {
-		return (V)getValuesArray()[index];
+		return (V)_values[index];
 	}
 
 	/**
@@ -218,7 +204,7 @@ public class ArrayMap4<K, V> implements Map<K, V>, Serializable, Cloneable,
     }
 
 	private K keyAt(int i) {
-		return (K)getKeysArray()[i];
+		return (K)_keys[i];
 	}
 
 	/**
@@ -245,7 +231,7 @@ public class ArrayMap4<K, V> implements Map<K, V>, Serializable, Cloneable,
      * @sharpen.ignore 
      */
 	private int indexOfKey(K key) {
-		return indexOf(getKeysArray(), key);
+		return indexOf(_keys, key);
 	}
 
 	private V replace(int index, V value) {
@@ -338,8 +324,8 @@ public class ArrayMap4<K, V> implements Map<K, V>, Serializable, Cloneable,
         activate();
         try {
             ArrayMap4<K, V> mapClone = (ArrayMap4<K, V>) super.clone();
-            mapClone._keys =  getKeysArray().clone();
-            mapClone._values = getValuesArray().clone();
+            mapClone._keys =  _keys.clone();
+            mapClone._values = _values.clone();
             return mapClone;
         } catch (CloneNotSupportedException e) {
             throw new Error(e);
@@ -428,10 +414,6 @@ public class ArrayMap4<K, V> implements Map<K, V>, Serializable, Cloneable,
     
     @SuppressWarnings("unchecked")
     private void ensureCapacity() {
-        if (_keys == null) {
-            initializeBackingArray(16);
-        }
-        
         if (_endIndex == _keys.length) {
             Object[] newKeys = new Object[_keys.length * 2];
             Object[] newValues = new Object[_values.length * 2];
