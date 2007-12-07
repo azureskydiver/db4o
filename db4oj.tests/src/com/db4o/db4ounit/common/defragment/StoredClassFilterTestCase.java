713109174a22c20756af685e731be6cd819b03dc
norberto_goussies <norberto_goussies@915b37e7-0715-0410-8bc3-b14b59444072>
2007-12-07 12:52:06 +0000

    rbo+ng : #COR-1025 removing slots of not accepted classes from ClassMetadaRepository when defragmenting

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/defragment/StoredClassFilterTestCase.java
@@ -0,0 +1,95 @@
+package com.db4o.db4ounit.common.defragment;
+
+import java.io.*;
+
+import com.db4o.*;
+import com.db4o.defragment.*;
+import com.db4o.ext.*;
+import com.db4o.foundation.io.*;
+import com.db4o.reflect.*;
+
+import db4ounit.*;
+
+
+public class StoredClassFilterTestCase implements TestCase {
+
+	private static final String DB4O_BACKUP = buildTempPath("defrag.db4o.backup");
+	private static final String DB4O_FILE = buildTempPath("defrag.db4o");
+
+	public static class SimpleClass {
+		public String _simpleField;
+		public SimpleClass(String simple){
+			_simpleField = simple;
+		}
+	}
+	
+	public static void main(String[] args) {
+		new TestRunner(StoredClassFilterTestCase.class).run();
+	}
+	
+	private static String buildTempPath(String fname) {
+		return com.db4o.db4ounit.util.IOServices.buildTempPath(fname);
+	}
+
+	public void test() throws Exception {
+		deleteAllFiles();
+		String fname = createDatabase();
+		defrag(fname);
+		assertStoredClasses(fname);
+	}
+
+	private void deleteAllFiles() {
+		File4.delete(DB4O_FILE);
+		File4.delete(DB4O_BACKUP);		
+	}
+
+	private void assertStoredClasses(String fname) {
+		ObjectContainer db = Db4o.openFile(fname);
+		try {
+			ReflectClass[] knownClasses = db.ext().knownClasses();
+			assertKnownClasses(knownClasses);
+		} finally {
+			db.close();
+		}
+	}
+
+	private void assertKnownClasses(ReflectClass[] knownClasses) {
+		for (int i = 0; i < knownClasses.length; i++) {
+			Assert.areNotEqual(fullyQualifiedName(SimpleClass.class), knownClasses[i].getName());
+		}
+	}
+
+	private String fullyQualifiedName(Class klass) {
+		return db4ounit.extensions.util.CrossPlatformServices.fullyQualifiedName(klass);
+	}
+
+	private void defrag(String fname) throws IOException {
+		DefragmentConfig config = new DefragmentConfig(fname);
+		config.storedClassFilter(ignoreClassFilter(SimpleClass.class));
+		Defragment.defrag(config);
+	}
+	
+	private StoredClassFilter ignoreClassFilter(final Class klass) {
+		return new StoredClassFilter(){
+			public boolean accept(StoredClass storedClass) {
+				return !storedClass.getName().equals(fullyQualifiedName(klass));
+			}
+		};
+	}
+
+	private String createDatabase() {
+		String fname = DB4O_FILE;
+		ObjectContainer db = Db4o.openFile(fname);
+		try {
+			db.set(new SimpleClass("verySimple"));
+			db.commit();
+		} finally {
+			db.close();
+		}
+		return fname;
+		
+
+	}
+	
+
+}
