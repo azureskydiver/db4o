2f2f1cf1540dfa3d380092c0fd72354a69b3a439
richard_liang <richard_liang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-12-09 04:07:26 +0000

    rl: fix COR-1007 (refactor order)

--- db4oj.tests/src/com/db4o/db4ounit/common/soda/ordered/STOrderingTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/soda/ordered/STOrderingTestCase.java
@@ -1,84 +1,76 @@
 package com.db4o.db4ounit.common.soda.ordered;
 
-import java.io.*;
-import com.db4o.*;
+import com.db4o.db4ounit.common.soda.util.*;
+import com.db4o.query.*;
 
-import com.db4o.db4ounit.common.soda.util.SodaBaseTestCase;
-import com.db4o.query.Query;
-import db4ounit.extensions.fixtures.OptOutCS;;
+import db4ounit.extensions.fixtures.*;
 
 /**
  * Tests for COR-1007
  */
 public class STOrderingTestCase extends SodaBaseTestCase implements OptOutCS {
-	public Object[] createData() {
-		return new Object[] {
-				/*new OrderTestSubject("Alexandr", 30, 5),   // 0
-				new OrderTestSubject("Cris", 30, 5),	   // 1
-				new OrderTestSubject("Boris", 30, 5), 	   // 2
-				new OrderTestSubject("Helen", 25, 5),	   // 3
-				new OrderTestSubject("Zeus", 25, 3),	   // 4
-				new OrderTestSubject("Alexsandra", 25, 3), // 5*/
-				new OrderTestSubject("Liza", 25, 4),	   // 6
-				//new OrderTestSubject("Bred", 25, 3),	   // 7
-				new OrderTestSubject("Liza", 25, 3),	   // 6
-				new OrderTestSubject("Gregory", 25, 4), }; // 8
-	}
-	
-	/**
-	 * Ignored for while due to COR-1007
-	 */
-	public void _testFirstAndSecondFieldsAreIrrelevant() {
-		Query q = newQuery();
-		q.constrain(OrderTestSubject.class);
-		q.descend("_seniority").orderAscending();
-		q.descend("_age").orderAscending();
-		q.descend("_name").orderAscending();
-		
-		expectOrdered(q, new int[] {1, 2, 0} );
-	}
-	
-	/**
-	 * Ignored for while due to COR-1007
-	 */
-	public void _testSecondAndThirdFieldsAreIrrelevant() {
-		Query q = newQuery();
-		q.constrain(OrderTestSubject.class);
-		q.descend("_age").orderAscending();
-		q.descend("_name").orderAscending();
-		q.descend("_seniority").orderAscending();
-		
-		expectOrdered(q, new int[] {2, 1, 0} );
-	}
-	
-	public void testOrderByNameAscending() {
-		Query q = newQuery();
-		q.constrain(OrderTestSubject.class);
-		q.descend("_name").orderAscending();
-		
-		//expectOrdered(q, new int[] {0, 5, 2, 7, 1, 8, 3, 6, 4} );
-		expectOrdered(q, new int[] {2, 1, 0} );
-	}
-	
-	public void testOrderByNameAndAgeAscending() {
-		Query q = newQuery();
-		q.constrain(OrderTestSubject.class);
-		q.descend("_seniority").constrain(new Integer(4)).equal(); // just to remove Liza 25 3 from the candidate list
-		
-		q.descend("_age").orderAscending();
-		q.descend("_name").orderAscending();
-		
-		//expectOrdered(q, new int[] {5, 7, 8, 3, 6, 4, 0, 2, 1} );
-		expectOrdered(q, new int[] {2, 0} );
-	}
-	
-	public void testAscendingOrderWithOutAge() {
-		Query q = newQuery();
-		q.constrain(OrderTestSubject.class);
-		q.descend("_seniority").orderAscending();
-		q.descend("_name").orderAscending();
-	
-		//expectOrdered(q, new int[] {5, 4, 7, 8, 6, 0, 2, 1, 3} );
-		expectOrdered(q, new int[] {1, 2, 0} );
-	}
+
+    public static void main(String[] args) {
+        new STOrderingTestCase().runSolo();
+    }
+
+    public Object[] createData() {
+        return new Object[] { new OrderTestSubject("Alexandr", 30, 5), // 0
+                new OrderTestSubject("Cris", 30, 5), // 1
+                new OrderTestSubject("Boris", 30, 5), // 2
+                new OrderTestSubject("Helen", 25, 5), // 3
+                new OrderTestSubject("Zeus", 25, 3), // 4
+                new OrderTestSubject("Alexsandra", 25, 3), // 5
+                new OrderTestSubject("Liza", 25, 4), // 6
+                new OrderTestSubject("Bred", 25, 3), // 7
+                new OrderTestSubject("Liza", 25, 3), // 8
+                new OrderTestSubject("Gregory", 25, 4), }; // 9
+    }
+
+    public void testFirstAndSecondFieldsAreIrrelevant() {
+        Query q = newQuery();
+        q.constrain(OrderTestSubject.class);
+        q.descend("_seniority").orderAscending();
+        q.descend("_age").orderAscending();
+        q.descend("_name").orderAscending();
+
+        expectOrdered(q, new int[] { 5, 7, 8, 4, 9, 6, 3, 0, 2, 1 });
+    }
+
+    public void testSecondAndThirdFieldsAreIrrelevant() {
+        Query q = newQuery();
+        q.constrain(OrderTestSubject.class);
+        q.descend("_age").orderAscending();
+        q.descend("_name").orderAscending();
+        q.descend("_seniority").orderAscending();
+
+        expectOrdered(q, new int[] { 5, 7, 9, 3, 8, 6, 4, 0, 2, 1 });
+    }
+
+    public void testOrderByNameAscending() {
+        Query q = newQuery();
+        q.constrain(OrderTestSubject.class);
+        q.descend("_name").orderAscending();
+
+        expectOrdered(q, new int[] { 0, 5, 2, 7, 1, 9, 3, 8, 6, 4 });
+    }
+
+    public void testOrderByNameAndAgeAscending() {
+        Query q = newQuery();
+        q.constrain(OrderTestSubject.class);
+
+        q.descend("_age").orderAscending();
+        q.descend("_name").orderAscending();
+
+        expectOrdered(q, new int[] { 5, 7, 9, 3, 6, 8, 4, 0, 2, 1 });
+    }
+
+    public void testAscendingOrderWithOutAge() {
+        Query q = newQuery();
+        q.constrain(OrderTestSubject.class);
+        q.descend("_seniority").orderAscending();
+        q.descend("_name").orderAscending();
+
+        expectOrdered(q, new int[] { 5, 7, 8, 4, 9, 6, 0, 2, 1, 3 });
+    }
 }
