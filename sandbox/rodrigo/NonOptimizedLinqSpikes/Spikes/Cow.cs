080836587770ac3cd6f326121491537fbc7e8ec9
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2007-12-17 13:05:23 +0000

    Non optimized linq benchmark

---
+++ sandbox/rodrigo/NonOptimizedLinqSpikes/Spikes/Cow.cs
@@ -0,0 +1,13 @@
+﻿namespace Spikes {
+    public class Cow {
+        string _code;
+
+        public Cow(string code) {
+            _code = code;
+        }
+
+        public string Code {
+            get { return _code; }
+        }
+    }
+}
---
+++ sandbox/rodrigo/NonOptimizedLinqSpikes/Spikes/CowEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Spikes {
+
+    public abstract class CowEvent {
+        Cow _cow;
+        DateTime _date;
+
+        public CowEvent(Cow cow, DateTime date) {
+            _cow = cow;
+            _date = date;
+        }
+
+        public Cow Cow {
+            get { return _cow; }
+        }
+
+        public DateTime Date {
+            get { return _date; }
+        }
+    }
+}
