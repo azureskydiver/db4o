439abb87bc3a3ed7756fd7c0556af7d3b209b4e5
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2007-12-25 21:46:32 +0000

    cr: #COR-1047 New interface ClientSideTask allows processing of messages by the client-side main thread. Messages are placed on the client side message loop and taken from there.

---
+++ db4oj/core/src/com/db4o/internal/cs/messages/ClientSideTask.java
@@ -0,0 +1,12 @@
+/* Copyright (C) 2007  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.internal.cs.messages;
+
+/**
+ * @exclude
+ */
+public interface ClientSideTask {
+	
+	public boolean runOnClient();
+
+}
