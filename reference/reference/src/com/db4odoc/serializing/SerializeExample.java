56b9929c9e4ddde7d6d0c194156299b6da17446a
tetyana_loskutova <tetyana_loskutova@915b37e7-0715-0410-8bc3-b14b59444072>
2007-12-26 16:51:18 +0000

    tl: Some code improvements

--- reference/reference/src/com/db4odoc/serializing/SerializeExample.java
+++ reference/reference/src/com/db4odoc/serializing/SerializeExample.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2004 - 2007 db4objects Inc. http://www.db4o.com */
 
-package com.db4odoc.serialize;
+package com.db4odoc.serializing;
 
 import java.io.File;
 import java.io.FileReader;
