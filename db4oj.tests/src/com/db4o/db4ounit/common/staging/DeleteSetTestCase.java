524d4e06dc3df753ee352a59c8e8ce4a2d1e03e6
andrew_zhang <andrew_zhang@915b37e7-0715-0410-8bc3-b14b59444072>
2007-12-27 08:07:29 +0000

    az: The object is gone after delete->set->commit.

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/staging/DeleteSetTestCase.java
@@ -0,0 +1,29 @@
+/* Copyright (C) 2007  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.db4ounit.common.staging;
+
+import db4ounit.extensions.*;
+
+public class DeleteSetTestCase extends AbstractDb4oTestCase {
+	
+	public static void main(String[] args) {
+		new DeleteSetTestCase().runAll();
+	}
+	
+	public static class Item {
+		
+	}
+	
+	protected void store() throws Exception {
+		store(new Item());
+	}
+	
+	public void test() throws Exception {
+		Object item = retrieveOnlyInstance(Item.class);
+		db().delete(item);
+		db().set(item);
+		db().commit();
+		assertOccurrences(Item.class, 1);
+	}
+
+}
