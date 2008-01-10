b64d73e8f38c4378c4dfe12964680450778459bf
norberto_goussies <norberto_goussies@915b37e7-0715-0410-8bc3-b14b59444072>
2008-01-10 12:44:14 +0000

    acv + ng :#COR-756 sending the id's of the objects in the TransportObjectContainer

---
+++ db4ojdk5/test/src/com/db4o/db4ounit/jre5/query/AllTests.java
@@ -0,0 +1,18 @@
+package com.db4o.db4ounit.jre5.query;
+
+import db4ounit.extensions.Db4oTestSuite;
+
+public class AllTests extends Db4oTestSuite {
+	public static void main(String[] args) {
+		new AllTests().runAll();
+	}
+	
+	@Override
+	protected Class[] testCases() {
+		return new Class[] {
+			COR756TestCase.class,
+		};
+	}
+
+
+}
