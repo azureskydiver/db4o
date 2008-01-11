78a1d0c2cf111d54cec17ec1bbfd6f57b3090373
czimmerli <czimmerli@915b37e7-0715-0410-8bc3-b14b59444072>
2008-01-11 16:12:12 +0000

    cz: COR-1077 - Moved IoCommands to new package com.db4o.bench.logging.replay.commands (accidentially also moved LogReplayer and ReplayExample, will be undone)

--- db4otools/src/com/db4o/bench/logging/replay/commands/LogReplayer.java
+++ db4otools/src/com/db4o/bench/logging/replay/commands/LogReplayer.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2004 - 2007 db4objects Inc. http://www.db4o.com */
 
-package com.db4o.bench.logging.replay;
+package com.db4o.bench.logging.replay.commands;
 
 import java.io.*;
 import java.util.*;
@@ -14,13 +14,13 @@ public class LogReplayer {
 	
 	private String _logFilePath;
 	private IoAdapter _io;
-	private Set _commands;
+	private Set _command;
 	private Map _counts;
 	
 	public LogReplayer(String logFilePath, IoAdapter io, Set commands) {
 		_logFilePath = logFilePath;
 		_io = io;
-		_commands = commands;
+		_command = commands;
 		_counts = new HashMap();
 		Iterator it = commands.iterator();
 		while (it.hasNext()) {
@@ -34,6 +34,10 @@ public class LogReplayer {
 	
 	public void setLog(String logFilePath) {
 		_logFilePath = logFilePath;
+	}
+	
+	public void replayLog() throws IOException {
+		playCommandList(readCommandList());
 	}
 	
 	public List4 readCommandList() throws IOException {
@@ -58,18 +62,9 @@ public class LogReplayer {
 		}
 	}
 	
-	public void replayLog() throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(_logFilePath));
-		String line = null;
-		while ( (line = reader.readLine()) != null ) {
-			replayLine(line);
-		}
-		reader.close();
-	}
-	
 	private IoCommand readLine(String line) {
 		if ( line.startsWith(LogConstants.WRITE_ENTRY) ) {
-			if (! _commands.contains(LogConstants.WRITE_ENTRY)) {
+			if (! _command.contains(LogConstants.WRITE_ENTRY)) {
 				return null;
 			}
 			int length = (int) parameter(LogConstants.WRITE_ENTRY,  line);
@@ -77,7 +72,7 @@ public class LogReplayer {
 			return new WriteCommand(length);
 		}
 		if ( line.startsWith(LogConstants.READ_ENTRY) ) {
-			if (! _commands.contains(LogConstants.READ_ENTRY)) {
+			if (! _command.contains(LogConstants.READ_ENTRY)) {
 				return null;
 			}
 			int length = (int) parameter(LogConstants.READ_ENTRY,  line);
@@ -85,14 +80,14 @@ public class LogReplayer {
 			return new ReadCommand(length);
 		}
 		if ( line.startsWith(LogConstants.SYNC_ENTRY) ) {
-			if (! _commands.contains(LogConstants.SYNC_ENTRY)) {
+			if (! _command.contains(LogConstants.SYNC_ENTRY)) {
 				return null;
 			}
 			incrementCount(LogConstants.SYNC_ENTRY);
 			return new SyncCommand();
 		}
 		if ( line.startsWith(LogConstants.SEEK_ENTRY) ) {
-			if (! _commands.contains(LogConstants.SEEK_ENTRY)) {
+			if (! _command.contains(LogConstants.SEEK_ENTRY)) {
 				return null;
 			}
 			int address = (int) parameter(LogConstants.READ_ENTRY,  line);
@@ -106,65 +101,65 @@ public class LogReplayer {
 	}
 
 
-	private void replayLine(String line) {
-		if ( line.startsWith(LogConstants.WRITE_ENTRY) ) {
-			if (_commands.contains(LogConstants.WRITE_ENTRY)) {
-				replayWrite(line);
-				incrementCount(LogConstants.WRITE_ENTRY);
-			}
-			return;
-		}
-		if ( line.startsWith(LogConstants.READ_ENTRY) ) {
-			if (_commands.contains(LogConstants.READ_ENTRY)) {
-				replayRead(line);
-				incrementCount(LogConstants.READ_ENTRY);
-			}
-			return;
-		}
-		if ( line.startsWith(LogConstants.SYNC_ENTRY) ) {
-			if (_commands.contains(LogConstants.SYNC_ENTRY)) {
-				replaySync();
-				incrementCount(LogConstants.SYNC_ENTRY);
-			}
-			return;
-		}
-		if ( line.startsWith(LogConstants.SEEK_ENTRY) ) {
-			if (_commands.contains(LogConstants.SEEK_ENTRY)) {
-				replaySeek(line);
-				incrementCount(LogConstants.SEEK_ENTRY);
-			}
-			return;
-		}
-		if(line.length() == 0){
-			return;
-		}
-		throw new IllegalArgumentException("Unknown command in log: " + line);
-	}
+//	private void replayLine(String line) {
+//		if ( line.startsWith(LogConstants.WRITE_ENTRY) ) {
+//			if (_command.contains(LogConstants.WRITE_ENTRY)) {
+//				replayWrite(line);
+//				incrementCount(LogConstants.WRITE_ENTRY);
+//			}
+//			return;
+//		}
+//		if ( line.startsWith(LogConstants.READ_ENTRY) ) {
+//			if (_command.contains(LogConstants.READ_ENTRY)) {
+//				replayRead(line);
+//				incrementCount(LogConstants.READ_ENTRY);
+//			}
+//			return;
+//		}
+//		if ( line.startsWith(LogConstants.SYNC_ENTRY) ) {
+//			if (_command.contains(LogConstants.SYNC_ENTRY)) {
+//				replaySync();
+//				incrementCount(LogConstants.SYNC_ENTRY);
+//			}
+//			return;
+//		}
+//		if ( line.startsWith(LogConstants.SEEK_ENTRY) ) {
+//			if (_command.contains(LogConstants.SEEK_ENTRY)) {
+//				replaySeek(line);
+//				incrementCount(LogConstants.SEEK_ENTRY);
+//			}
+//			return;
+//		}
+//		if(line.length() == 0){
+//			return;
+//		}
+//		throw new IllegalArgumentException("Unknown command in log: " + line);
+//	}
 
 
-	private void replaySync() {
-		_io.sync();
-	}
-	
-	private void replayRead(String line) {
-		byte[] buffer = prepareBuffer(LogConstants.READ_ENTRY, line);
-		_io.read(buffer, buffer.length);
-	}
+//	private void replaySync() {
+//		_io.sync();
+//	}
+//	
+//	private void replayRead(String line) {
+//		byte[] buffer = prepareBuffer(LogConstants.READ_ENTRY, line);
+//		_io.read(buffer, buffer.length);
+//	}
+//
+//	private void replayWrite(String line) {
+//		byte[] buffer = prepareBuffer(LogConstants.WRITE_ENTRY, line);		
+//		_io.write(buffer);
+//	}
+//	
+//	private void replaySeek(String line) {
+//		long pos = parameter(LogConstants.SEEK_ENTRY.length(), line);
+//		_io.seek(pos);
+//	}
 
-	private void replayWrite(String line) {
-		byte[] buffer = prepareBuffer(LogConstants.WRITE_ENTRY, line);		
-		_io.write(buffer);
-	}
-	
-	private void replaySeek(String line) {
-		long pos = parameter(LogConstants.SEEK_ENTRY.length(), line);
-		_io.seek(pos);
-	}
-
-	private byte[] prepareBuffer(String command, String line) {
-		int length = (int) parameter(command.length(),  line);
-		return new byte[length];
-	}
+//	private byte[] prepareBuffer(String command, String line) {
+//		int length = (int) parameter(command.length(),  line);
+//		return new byte[length];
+//	}
 	
 	private long parameter(String command, String line){
 		return parameter(command.length(),  line);
