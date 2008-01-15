16e651c5ec76953b3f2680b1a8b5e1400873774f
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2008-01-15 17:44:52 +0000

    acv: COR-756 - Fixed bug in unmarshall()

--- db4oj/core/src/com/db4o/internal/query/processor/QConEvaluation.java
+++ db4oj/core/src/com/db4o/internal/query/processor/QConEvaluation.java
@@ -40,7 +40,7 @@ public class QConEvaluation extends QCon {
 
     void marshall() {
         super.marshall();
-		if(Deploy.csharp || !Platform4.useNativeSerialization()){
+		if(!Platform4.useNativeSerialization()){
 			marshallUsingDb4oFormat();
 		}else{
     		try{
@@ -60,7 +60,7 @@ public class QConEvaluation extends QCon {
     void unmarshall(Transaction a_trans) {
         if (i_trans == null) {
             super.unmarshall(a_trans);
-            if(Deploy.csharp || Platform4.useNativeSerialization()){
+            if(Deploy.csharp){
             	i_evaluation = Serializer.unmarshall(container(), i_marshalledEvaluation, i_marshalledID);
             }else{
                 if(i_marshalledID > 0){
