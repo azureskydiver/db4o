cba55d13f1cbfb6065adcfb5f7003ff8d472e2ff
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-01-21 16:57:42 +0000

    cr + acv: #COR-199 Object handling is now working, but not together with old code that stores other first class objects. Checking in intermediate results, since we will experiment now.

--- db4oj.tests/src/com/db4o/db4ounit/common/assorted/PlainObjectTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/assorted/PlainObjectTestCase.java
@@ -1,5 +1,8 @@
 package com.db4o.db4ounit.common.assorted;
 
+import com.db4o.*;
+import com.db4o.query.*;
+
 import db4ounit.Assert;
 import db4ounit.extensions.AbstractDb4oTestCase;
 
@@ -12,19 +15,40 @@ public class PlainObjectTestCase extends AbstractDb4oTestCase{
     
     public static class Item{
         
+        public String _name;
+        
         public Object _plainObject;
+        
+        public Item(String name, Object plainObject) {
+            _name = name;
+            _plainObject = plainObject;
+        }
         
     }
     
     protected void store() throws Exception {
-        Item item = new Item();
-        item._plainObject = new Object();
+        Object plainObject = new Object();
+        Item item = new Item("one", plainObject);
         store(item);
+        retrieveItem("one");
+        Assert.isTrue(db().isStored(item._plainObject));
+        store(new Item("two", plainObject));
     }
     
     public void test(){
-        Item item = (Item) retrieveOnlyInstance(Item.class);
-        Assert.isNotNull(item._plainObject);
+        Item itemOne = retrieveItem("one");
+        Assert.isNotNull(itemOne._plainObject);
+        Assert.isTrue(db().isStored(itemOne._plainObject));
+        Item itemTwo = retrieveItem("two");
+        Assert.areSame(itemOne._plainObject, itemTwo._plainObject);
+    }
+
+    private Item retrieveItem(String name) {
+        Query query = newQuery(Item.class);
+        query.descend("_name").constrain(name);
+        ObjectSet objectSet = query.execute();
+        Assert.areEqual(1, objectSet.size());
+        return (Item) objectSet.next();
     }
 
 }
--- db4oj/core/src/com/db4o/internal/handlers/PlainObjectHandler.java
+++ db4oj/core/src/com/db4o/internal/handlers/PlainObjectHandler.java
@@ -6,8 +6,7 @@ import com.db4o.ext.Db4oIOException;
 import com.db4o.foundation.NotImplementedException;
 import com.db4o.foundation.PreparedComparison;
 import com.db4o.internal.*;
-import com.db4o.marshall.ReadContext;
-import com.db4o.marshall.WriteContext;
+import com.db4o.marshall.*;
 
 
 public class PlainObjectHandler implements TypeHandler4{
@@ -21,19 +20,36 @@ public class PlainObjectHandler implements TypeHandler4{
     }
 
     public Object read(ReadContext context) {
-        throw new NotImplementedException();
+        int id = context.readInt();
+        Transaction transaction = context.transaction();
+        Object obj = transaction.objectForIdFromCache(id);
+        if(obj != null){
+            return obj;
+        }
+        obj = new Object();
+        addReference(context, obj, id);
+        return obj;
     }
 
     public void write(WriteContext context, Object obj) {
         Transaction transaction = context.transaction();
         ObjectContainerBase container = transaction.container();
         int id = container.getID(transaction, obj);
-        if(id > 0){
-            return;
+        if(id <= 0){
+            id = container.newUserObject();
+            
+            // TODO: Free on rollback
+            
+            addReference(context, obj, id);
         }
-        id = container.newUserObject();
-        
-        throw new NotImplementedException();
+        context.writeInt(id);
+    }
+
+    private void addReference(Context context, Object obj, int id) {
+        Transaction transaction = context.transaction();
+        ObjectReference ref = new ObjectReference(id);
+        ref.setObjectWeak(transaction.container(), obj);
+        transaction.addNewReference(ref);
     }
 
     public PreparedComparison prepareComparison(Object obj) {
