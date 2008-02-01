f2f90a001a2a95078ec6b8499763dd61f822e74c
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2008-02-01 20:23:03 +0000

    COR-1126 - remove all joins if no or joins are present

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/foundation/NoDuplicatesQueueTestCase.java
@@ -0,0 +1,28 @@
+package com.db4o.db4ounit.common.foundation;
+
+import com.db4o.foundation.*;
+
+import db4ounit.*;
+
+public class NoDuplicatesQueueTestCase implements TestLifeCycle {
+
+	private Queue4 _queue;
+
+	public void test() {
+		_queue.add("A");
+		_queue.add("B");
+		_queue.add("B");
+		_queue.add("A");
+		Assert.areEqual("A", _queue.next());
+		Assert.areEqual("B", _queue.next());
+		Assert.isFalse(_queue.hasNext());
+	}
+	
+	public void setUp() throws Exception {
+		_queue = new NoDuplicatesQueue(new NonblockingQueue());
+	}
+
+	public void tearDown() throws Exception {
+		_queue = null;
+	}
+}
