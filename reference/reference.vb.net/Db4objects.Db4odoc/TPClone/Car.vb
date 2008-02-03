8f45c55f69b507d6435380726e61d99dec9bca33
tetyana_loskutova <tetyana_loskutova@915b37e7-0715-0410-8bc3-b14b59444072>
2008-02-03 12:38:56 +0000

    tl: #DOC-261 TP Clone example

---
+++ reference/reference.vb.net/Db4objects.Db4odoc/TPClone/Car.vb
@@ -0,0 +1,66 @@
+' Copyright (C) 2004 - 2007 db4objects Inc. http://www.db4o.com 
+
+Imports System
+Imports Db4objects.Db4o
+Imports Db4objects.Db4o.TA
+Imports Db4objects.Db4o.Activation
+
+Namespace Db4objects.Db4odoc.TPClone
+    Public Class Car
+        Implements IActivatable
+        Implements ICloneable
+        Private _model As String
+        Private _pilot As Pilot
+        'activator registered for this class
+
+        <Transient()> _
+        Public _activator As IActivator
+
+
+        Public Sub New(ByVal model As String, ByVal pilot As Pilot)
+            _model = model
+            _pilot = pilot
+        End Sub
+        ' end Car
+
+        'Bind the class to the specified object container, create the activator
+
+        Public Sub Bind(ByVal activator As IActivator) Implements IActivatable.Bind
+            If _activator Is activator Then
+                Return
+            End If
+            If activator IsNot Nothing AndAlso _activator IsNot Nothing Then
+                Throw New System.InvalidOperationException()
+            End If
+            _activator = activator
+        End Sub
+        ' end Bind
+
+        'Call the registered activator to activate the next level,
+        '         * the activator remembers the objects that were already 
+        '         * activated and won't activate them twice. 
+        '         
+
+        Public Sub Activate(ByVal purpose As ActivationPurpose) Implements IActivatable.Activate
+            If _activator Is Nothing Then
+                Return
+            End If
+            _activator.Activate(purpose)
+        End Sub
+        ' end Activate
+
+        Public Function Clone() As Object Implements ICloneable.Clone
+            Dim test As Car = DirectCast(MyBase.MemberwiseClone(), Car)
+            test._activator = Nothing
+            Return test
+        End Function
+        ' end Clone
+
+
+        Public Overloads Overrides Function ToString() As String
+            Activate(ActivationPurpose.Read)
+            Return String.Format("{0}[{1}]", _model, _pilot)
+        End Function
+        ' end ToString
+    End Class
+End Namespace
