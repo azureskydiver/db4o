8428ccdfb9839c27bc7d62fdfe8627adc65b9e55
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-02-06 13:18:01 +0000

    rbo+acv: ensure AssemblyVersion is correct for all assemblies

--- db4obuild/src/com/db4o/devtools/ant/AssemblyInfo.java
+++ db4obuild/src/com/db4o/devtools/ant/AssemblyInfo.java
@@ -3,31 +3,14 @@ package com.db4o.devtools.ant;
 import java.util.*;
 
 public class AssemblyInfo {
-    private static Calendar cal = Calendar.getInstance(Locale.US);
-
-    public static final String COPYRIGHT = "db4o 2005 - "
-            + cal.get(Calendar.YEAR);
+    
+	public static final String PRODUCT = "db4o - database for objects";
+    
+    public static final String COPYRIGHT = "db4o 2005 - " + currentYear();
 
     public static final String COMPANY = "db4objects Inc., San Mateo, CA, USA";
 
-    private final String title;
-
-    private final String product;
-
-    public AssemblyInfo(String title, String product) {
-        this.title = title;
-        this.product = product;
-    }
-
-    public static AssemblyInfo DB4O() {
-        return new AssemblyInfo("db4o", "db4o - database for objects");
-    }
-
-    public String title() {
-        return title;
-    }
-
-    public String product() {
-        return product;
-    }
+    private static int currentYear() {
+    	return Calendar.getInstance(Locale.US).get(Calendar.YEAR);
+	}
 }
