6edc178e20e0e3bd76f6e9186b4c93c68698d38f
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-02-12 18:50:41 +0000

    rbo+rl: COR-546 - fixed by capturing the deleted objects before commitImpl into FrozenObjectInfo instances; important note is that deleted objects won't be available in the network C/S scenario but only their UUID, internal ID and version.

--- db4oj/core/src/com/db4o/internal/cs/messages/MCommittedInfo.java
+++ db4oj/core/src/com/db4o/internal/cs/messages/MCommittedInfo.java
@@ -14,45 +14,96 @@ import com.db4o.internal.*;
 public class MCommittedInfo extends MsgD implements ClientSideMessage {
 
 	public MCommittedInfo encode(CallbackObjectInfoCollections callbackInfo) {
-		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		
-		encodeObjectInfoCollection(os, callbackInfo.added);
-		encodeObjectInfoCollection(os, callbackInfo.deleted);
-		encodeObjectInfoCollection(os, callbackInfo.updated);
-		
-		byte[] bytes = os.toByteArray();
+		byte[] bytes = encodeInfo(callbackInfo);
 		MCommittedInfo committedInfo = (MCommittedInfo) getWriterForLength(transaction(),
 			bytes.length);
 		committedInfo._payLoad.append(bytes);
 		return committedInfo;
 	}
 
-	private void encodeObjectInfoCollection(ByteArrayOutputStream os, ObjectInfoCollection collection){
+	private byte[] encodeInfo(CallbackObjectInfoCollections callbackInfo) {
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		
+		encodeObjectInfoCollection(os, callbackInfo.added, new InternalIDEncoder());
+		encodeObjectInfoCollection(os, callbackInfo.deleted, new FrozenObjectInfoEncoder());
+		encodeObjectInfoCollection(os, callbackInfo.updated, new InternalIDEncoder());
+		
+		return os.toByteArray();
+	}
+	
+	private final class FrozenObjectInfoEncoder implements ObjectInfoEncoder {
+		public void encode(ByteArrayOutputStream os, ObjectInfo info) {
+			try {
+				writeLong(os, info.getInternalID());
+				writeUUID(os, info.getUUID());
+				writeLong(os, info.getVersion());
+			} catch (IOException x) {
+				throw new Db4oException(x);
+			}
+		}
+
+		public ObjectInfo decode(ByteArrayInputStream is) {
+			try {
+				long id = readLong(is);
+				if (id == -1) {
+					return null;
+				}
+				
+				Db4oUUID uuid = readUUID(is);
+				long version = readLong(is);
+				return new FrozenObjectInfo(null, id, uuid, version);
+			} catch (IOException x) {
+				throw new Db4oException(x);
+			}
+		}
+	}
+
+	private final class InternalIDEncoder implements ObjectInfoEncoder {
+		public void encode(ByteArrayOutputStream os, ObjectInfo info) {
+			writeLong(os, info.getInternalID());
+		}
+
+		public ObjectInfo decode(ByteArrayInputStream is) {
+			long id = readLong(is);
+			if (id == -1) {
+				return null;
+			}
+			return new LazyObjectReference(transaction(), (int) id);
+		}
+	}
+
+	interface ObjectInfoEncoder {
+		void encode(ByteArrayOutputStream os, ObjectInfo info);
+		ObjectInfo decode(ByteArrayInputStream is);
+	}
+	
+	private void encodeObjectInfoCollection(ByteArrayOutputStream os,
+			ObjectInfoCollection collection, final ObjectInfoEncoder encoder) {
 		Iterator4 iter = collection.iterator();
 		while (iter.moveNext()) {
 			ObjectInfo obj = (ObjectInfo) iter.current();
-			writeLong(os, obj.getInternalID());
+			encoder.encode(os, obj);
 		}
 		writeLong(os, -1);
 	}
-
+	
 	public CallbackObjectInfoCollections decode() {
-		CallbackObjectInfoCollections callbackInfo = CallbackObjectInfoCollections.EMTPY;
 		ByteArrayInputStream is = new ByteArrayInputStream(_payLoad._buffer);
-		callbackInfo.added = decodeObjectInfoCollection(is);
-		callbackInfo.deleted = decodeObjectInfoCollection(is);
-		callbackInfo.updated = decodeObjectInfoCollection(is);
-		return callbackInfo;
+		
+		final ObjectInfoCollection added = decodeObjectInfoCollection(is, new InternalIDEncoder());
+		final ObjectInfoCollection deleted = decodeObjectInfoCollection(is, new FrozenObjectInfoEncoder());
+		final ObjectInfoCollection updated = decodeObjectInfoCollection(is, new InternalIDEncoder());
+		return new CallbackObjectInfoCollections(added, updated, deleted);
 	}
 
-	private ObjectInfoCollection decodeObjectInfoCollection(ByteArrayInputStream is){
+	private ObjectInfoCollection decodeObjectInfoCollection(ByteArrayInputStream is, ObjectInfoEncoder encoder){
 		final Collection4 collection = new Collection4();
 		while (true) {
-			long id = readLong(is);
-			if (id == -1) {
+			ObjectInfo info = encoder.decode(is);
+			if (null == info) {
 				break;
 			}
-			collection.add(new LazyObjectReference(transaction(), (int) id));
+			collection.add(info);
 		}
 		return new ObjectInfoCollectionImpl(collection);
 	}
@@ -84,4 +135,34 @@ public class MCommittedInfo extends MsgD implements ClientSideMessage {
 		return true;
 	}
 
+	private void writeUUID(ByteArrayOutputStream os, final Db4oUUID uuid)
+			throws IOException {
+		if (uuid == null) {
+			writeLong(os, -1);
+			return;
+		}
+		writeLong(os, uuid.getLongPart());
+		writeByteArray(os, uuid.getSignaturePart());
+	}
+	
+	private Db4oUUID readUUID(ByteArrayInputStream is)	throws IOException {
+		long longPart = readLong(is);
+		if (-1 == longPart) {
+			return null;
+		}
+		byte[] signature = readByteArray(is);
+		return new Db4oUUID(longPart, signature);
+	}
+
+	private byte[] readByteArray(ByteArrayInputStream is) throws IOException {
+		long length = readLong(is);
+		byte[] array = new byte[(int)length];
+		is.read(array);
+		return array;
+	}
+
+	protected void writeByteArray(ByteArrayOutputStream os, byte[] signaturePart) throws IOException {
+		writeLong(os, signaturePart.length);
+		os.write(signaturePart);
+	}
 }
