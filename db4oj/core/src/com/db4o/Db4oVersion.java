75ee27dd44f851a329178f7f568330945ee94c40
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-02-13 12:41:57 +0000

    cr: Testing of commit notifications are back now, sending directly from the server, without db4o-scm

--- db4oj/core/src/com/db4o/Db4oVersion.java
+++ db4oj/core/src/com/db4o/Db4oVersion.java
@@ -6,7 +6,7 @@ package com.db4o;
 * @exclude
 */
 public class Db4oVersion {
-    public static final String NAME = "6.4.29.44";
+    public static final String NAME = "6.4.29.45";
     public static final int MAJOR = 6;
     public static final int MINOR = 4;
     public static final int ITERATION = 29;
