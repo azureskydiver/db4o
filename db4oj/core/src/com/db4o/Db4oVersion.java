22d9d949e6511e4ec892599105d978997b89f676
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-02-13 11:41:33 +0000

    cr: Committing to get notification

--- db4oj/core/src/com/db4o/Db4oVersion.java
+++ db4oj/core/src/com/db4o/Db4oVersion.java
@@ -6,9 +6,9 @@ package com.db4o;
 * @exclude
 */
 public class Db4oVersion {
-    public static final String NAME = "7.1.28.9143";
-    public static final int MAJOR = 7;
-    public static final int MINOR = 1;
-    public static final int ITERATION = 28;
-    public static final int REVISION = 9139;
+    public static final String NAME = "6.4.29.42";
+    public static final int MAJOR = 6;
+    public static final int MINOR = 4;
+    public static final int ITERATION = 29;
+    public static final int REVISION = 42;
 }
