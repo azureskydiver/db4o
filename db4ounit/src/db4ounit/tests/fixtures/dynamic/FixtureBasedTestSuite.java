5821824c9ec7c74b72c2713333cf54118472d922
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-02-22 22:35:16 +0000

    UNIT-17 - code simplification

--- db4ounit/src/db4ounit/tests/fixtures/dynamic/FixtureBasedTestSuite.java
+++ db4ounit/src/db4ounit/tests/fixtures/dynamic/FixtureBasedTestSuite.java
@@ -16,29 +16,30 @@ public abstract class FixtureBasedTestSuite implements TestSuiteBuilder {
 
 	public Iterator4 build() {
 		final FixtureProvider[] providers = fixtureProviders();
-				
+		
 		final Iterable4 decorators = Iterators.map(Iterators.iterable(providers), new Function4() {
 			public Object apply(final Object arg) {
 				final FixtureProvider provider = (FixtureProvider)arg;
+				final ContextVariable variable = provider.variable();
 				return Iterators.map(provider, new Function4() {
 					public Object apply(final Object fixture) {
-						return new TestDecorator(){
-							public Test decorate(Test test) {
-								return new TestContext(test, provider.variable(), fixture);
-							}
-						};
+						return new FixtureDecorator(variable, fixture);
 					}
 				});
 			}
 		});
-		final Iterable4 product = Iterators.crossProduct(prepend(tests(), decorators));
-		return Iterators.map(product.iterator(), new Function4() {
+		final Iterable4 testsXdecorators = Iterators.crossProduct(new Iterable4[] {
+			tests(),
+			Iterators.crossProduct(decorators)
+		});
+		return Iterators.map(testsXdecorators, new Function4() {
 			public Object apply(Object arg) {
 				Iterator4 tuple = ((Iterable4)arg).iterator();
 				Test test = (Test)Iterators.next(tuple);
-				return decorate(test, tuple);
+				Iterable4 decorators = (Iterable4)Iterators.next(tuple);
+				return decorate(test, decorators.iterator());
 			}
-		});
+		}).iterator();
 	}
 
 	private Iterable4 tests() {
@@ -61,15 +62,7 @@ public abstract class FixtureBasedTestSuite implements TestSuiteBuilder {
 		}.build();
 		return Iterators.iterable(lazyTests);
 	}
-
-	private Iterable4[] prepend(Iterable4 tests, final Iterable4 decorators) {
-		Object[] iterables = Iterators.toArray(decorators.iterator());
-		final Iterable4[] source = new Iterable4[iterables.length+1];
-		source[0] = tests;
-		System.arraycopy(iterables, 0, source, 1, iterables.length);
-		return source;
-	}
-
+	
 	private Test decorate(Test test, Iterator4 decorators) {
 		while (decorators.moveNext()) {
 			test = ((TestDecorator)decorators.current()).decorate(test);
