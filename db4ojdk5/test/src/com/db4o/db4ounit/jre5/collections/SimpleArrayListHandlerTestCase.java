4c49ba4c47776fe925df6d555bb96129a5648563
norberto_goussies <norberto_goussies@915b37e7-0715-0410-8bc3-b14b59444072>
2008-03-28 11:42:41 +0000

    pr + ng: added tests

--- db4ojdk5/test/src/com/db4o/db4ounit/jre5/collections/SimpleArrayListHandlerTestCase.java
+++ db4ojdk5/test/src/com/db4o/db4ounit/jre5/collections/SimpleArrayListHandlerTestCase.java
@@ -4,12 +4,14 @@ package com.db4o.db4ounit.jre5.collections;
 
 import java.util.*;
 
+import com.db4o.*;
 import com.db4o.config.*;
 import com.db4o.ext.*;
 import com.db4o.foundation.*;
 import com.db4o.internal.*;
 import com.db4o.internal.marshall.*;
 import com.db4o.marshall.*;
+import com.db4o.query.*;
 import com.db4o.reflect.*;
 import com.db4o.reflect.generic.*;
 import com.db4o.typehandlers.*;
@@ -127,9 +129,54 @@ public class SimpleArrayListHandlerTestCase extends AbstractDb4oTestCase impleme
         store(item);
     }
     
-    public void test(){
+    public void _test(){
         Item item = (Item) retrieveOnlyInstance(Item.class);
-        Assert.areEqual(item.list.size(), 1);
-        Assert.areEqual("one", item.list.get(0));
+        assertListContent(item);
     }
+    
+    public void testQuery() throws Exception {
+    	Query q = newQuery(Item.class);
+    	q.descend("list").constrain("one");
+    	assertQueryResult(q);
+	}
+
+	private void assertListContent(Item item) {
+		Assert.areEqual(item.list.size(), 1);
+        Assert.areEqual("one", item.list.get(0));
+	}
+	
+	public void testContainsQuery() throws Exception {
+    	Query q = newQuery(Item.class);
+    	q.descend("list").constrain("e").endsWith(false);
+    	assertQueryResult(q);
+	}
+	
+	public void testFailingContainsQuery() throws Exception {
+    	Query q = newQuery(Item.class);
+    	q.descend("list").constrain("g").endsWith(false);
+    	assertEmptyQueryResult(q);
+	}
+	
+	public void testCompareItems() throws Exception {
+    	Query q = newQuery();
+    	Item item = new Item();
+    	item.list = new ArrayList();
+    	item.list.add("two");
+    	q.constrain(item);
+    	assertEmptyQueryResult(q);
+    }
+
+	private void assertEmptyQueryResult(Query q) {
+		ObjectSet set = q.execute();
+		Assert.isTrue(set.isEmpty());
+	}
+
+	private void assertQueryResult(Query q) {
+		ObjectSet set = q.execute();
+    	
+    	Assert.areEqual(1, set.size());
+    	Item item = (Item)set.next();
+        assertListContent(item);
+	}
+
 }
