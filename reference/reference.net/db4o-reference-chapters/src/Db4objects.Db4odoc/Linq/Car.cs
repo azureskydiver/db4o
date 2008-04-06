67a4ce6eb8060a7bc875974d08b6efa7707f0f08
tetyana_loskutova <tetyana_loskutova@915b37e7-0715-0410-8bc3-b14b59444072>
2008-04-06 13:35:48 +0000

    tl: Linq examples for reference doc

---
+++ reference/reference.net/db4o-reference-chapters/src/Db4objects.Db4odoc/Linq/Car.cs
@@ -0,0 +1,44 @@
+/* Copyright (C) 2004 - 2007 db4objects Inc. http://www.db4o.com */
+using System;
+
+namespace Db4objects.Db4odoc.Linq
+{
+    public class Car
+    {
+        string _model;
+        Pilot _pilot;
+        
+        public Car(string model, Pilot pilot)
+        {
+            _model = model;
+            _pilot = pilot;
+        }
+        
+        public Pilot Pilot
+        {
+            get
+            {
+                return _pilot;
+            }
+            
+            set
+            {
+                _pilot = value;
+            }
+        }
+        
+        public string Model
+        {
+            get
+            {
+                return _model;
+            }
+        }
+        
+        
+        override public string ToString()
+        {
+			return string.Format("{0}[{1}]", _model, _pilot);
+        }
+    }
+}
