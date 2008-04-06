974cdd96ab236c4de4a8225315905182f58a1346
tetyana_loskutova <tetyana_loskutova@915b37e7-0715-0410-8bc3-b14b59444072>
2008-04-06 14:08:17 +0000

    tl: Linq examples

---
+++ reference/reference.vb.net/Linq/Linq/Car.vb
@@ -0,0 +1,36 @@
+﻿' Copyright (C) 2004 - 2007 db4objects Inc. http://www.db4o.com 
+
+Imports System
+
+Namespace Db4objects.Db4odoc.Linq
+    Public Class Car
+        Private _model As String
+        Private _pilot As Pilot
+
+        Public Sub New(ByVal model As String, ByVal pilot As Pilot)
+            _model = model
+            _pilot = pilot
+        End Sub
+
+        Public Property Pilot() As Pilot
+            Get
+                Return _pilot
+            End Get
+
+            Set(ByVal value As Pilot)
+                _pilot = value
+            End Set
+        End Property
+
+        Public ReadOnly Property Model() As String
+            Get
+                Return _model
+            End Get
+        End Property
+
+
+        Public Overloads Overrides Function ToString() As String
+            Return String.Format("{0}[{1}]", _model, _pilot)
+        End Function
+    End Class
+End Namespace
---
+++ reference/reference.vb.net/Linq/Linq/LinqCollection/Pilot.vb
@@ -0,0 +1,43 @@
+﻿' Copyright (C) 2008 db4objects Inc. http://www.db4o.com 
+
+Namespace Db4objects.Db4odoc.LinqCollection
+    Class Pilot
+        Private _name As String
+        Private _points As Integer
+
+        Public Sub New(ByVal name As String, ByVal points As Integer)
+            _name = name
+            _points = points
+        End Sub
+
+        Public Property Name() As String
+            Get
+                Return _name
+            End Get
+            Set(ByVal value As String)
+                _name = value
+            End Set
+        End Property
+
+        Public ReadOnly Property Points() As Integer
+            Get
+                Return _points
+            End Get
+        End Property
+
+        Public Overloads Overrides Function Equals(ByVal obj As Object) As Boolean
+            If TypeOf obj Is Pilot Then
+                Return (DirectCast(obj, Pilot).Name.Equals(_name) AndAlso DirectCast(obj, Pilot).Points = _points)
+            End If
+            Return False
+        End Function
+
+        Public Overloads Overrides Function GetHashCode() As Integer
+            Return _name.GetHashCode() + _points
+        End Function
+
+        Public Overloads Overrides Function ToString() As String
+            Return String.Format("{0}/{1}", _name, _points)
+        End Function
+    End Class
+End Namespace
