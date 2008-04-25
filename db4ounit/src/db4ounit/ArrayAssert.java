6b2c258067674c22f7b8c037565211ad0286743e
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-04-25 10:50:53 +0000

    remove C# ambiguity between AreEqual<T>(T[], T[]) and AreEqual(Object[][], Object[][]) by introducing JaggedArrayAssert

--- db4ounit/src/db4ounit/ArrayAssert.java
+++ db4ounit/src/db4ounit/ArrayAssert.java
@@ -48,26 +48,6 @@ public class ArrayAssert {
 	    }
 	}
 	
-	public static void areEqual(int[][] expected, int[][] actual) {
-		if (expected == actual) return;
-		if (expected == null || actual == null) Assert.areSame(expected, actual);
-		Assert.areEqual(expected.length, actual.length);
-		Assert.areSame(expected.getClass(), actual.getClass());
-	    for (int i = 0; i < expected.length; i++) {
-	        areEqual(expected[i], actual[i]);
-	    }
-	}
-	
-	public static void areEqual(Object[][] expected, Object[][] actual) {
-		if (expected == actual) return;
-		if (expected == null || actual == null) Assert.areSame(expected, actual);
-		Assert.areEqual(expected.length, actual.length);
-		Assert.areSame(expected.getClass(), actual.getClass());
-	    for (int i = 0; i < expected.length; i++) {
-	        areEqual(expected[i], actual[i]);
-	    }
-	}
-
 	private static String indexMessage(int i) {
 		return "expected[" + i + "]";
 	}
