484b0924f2aac68e268f9052acc9751b33908e6f
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2008-05-05 19:35:38 +0000

    add return value to migration test suite to signal build failure

--- db4ojdk1.2/test/src/com/db4o/db4ounit/common/migration/AllCommonTests.java
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/common/migration/AllCommonTests.java
@@ -5,7 +5,7 @@ import db4ounit.extensions.*;
 public class AllCommonTests extends Db4oTestSuite {
     
     public static void main(String[] args) {
-        new AllCommonTests().runSolo();
+        System.exit(new AllCommonTests().runSolo());
     }
 	protected Class[] testCases() {
 		return new Class[]{
