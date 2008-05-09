b0ea1487d5d552314a555c20af03d28d4bb6acc7
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2008-05-09 11:53:48 +0000

    COR-1261 - ensure Hashtable and friends are not instantiated through serializable constructor (yields inconsistent state otherwise)

--- db4oj/core/src/com/db4o/internal/Platform4.java
+++ db4oj/core/src/com/db4o/internal/Platform4.java
@@ -281,6 +281,10 @@ public final class Platform4 {
         
         activationDepth(config, "P2HashMap", 2);
         activationDepth(config, "P1HashElement", 1);
+        
+        config.objectClass(UTIL + "Vector").callConstructor(true);
+        config.objectClass(UTIL + "Hashtable").callConstructor(true);
+        config.objectClass(UTIL + "LinkedList").callConstructor(true);
     }
 
     /**
