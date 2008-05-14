cce7868d4c725dad6e770af8bf36384eb86373a2
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-05-14 13:35:58 +0000

    cr+rbo: COR-1276 - custom reflector that bypasses reflection

---
+++ sandbox/profiling/JProbeReflectionProfiling/src/profiling/CustomItemReflector.java
@@ -0,0 +1,19 @@
+package profiling;
+
+import com.db4o.reflect.jdk.*;
+
+public class CustomItemReflector extends JdkReflector {
+
+	public CustomItemReflector() {
+		super(Item.class.getClassLoader());
+	}
+	
+	@Override
+	protected JdkClass createClass(Class clazz) {
+		if (clazz == Item.class) {
+			return new CustomItemClass(_parent, this);
+		}
+		return super.createClass(clazz);
+	}
+
+}
--- sandbox/profiling/JProbeReflectionProfiling/src/profiling/Main.java
+++ sandbox/profiling/JProbeReflectionProfiling/src/profiling/Main.java
@@ -1,13 +1,16 @@
 package profiling;
 
 import com.db4o.*;
+import com.db4o.config.*;
 import com.db4o.foundation.*;
 import com.db4o.foundation.io.*;
+import com.db4o.io.*;
 
 import db4ounit.*;
 
 public class Main {
 
+	private static final int OBJECT_COUNT = 1024*100;
 	private static final String FILENAME = "profiling.db4o";
 
 	interface ContainerBlock {
@@ -22,33 +25,54 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		for (int i=0; i<3; ++i) {
+			profile("default reflector", newConfiguration());
+			profile("custom reflector", customReflectorConfig());
+		}
+	}
+
+	private static Configuration newConfiguration() {
+		Configuration config = Db4o.newConfiguration();
+		config.io(new MemoryIoAdapter());
+		return config;
+	}
+
+	private static Configuration customReflectorConfig() {
+		Configuration config = newConfiguration();
+		config.reflectWith(new CustomItemReflector());
+		return config;
+	}
+
+	private static void profile(String label, Configuration config) {
 		File4.delete(FILENAME);
 		
 		final LongArrayByRef array = new LongArrayByRef();
-		withContainer("storing", new ContainerBlock() {
+		withContainer("[" + label + "] storing", config, new ContainerBlock() {
 			public void run(ObjectContainer container) {
 				array.value = store(container);
 			}
 		});
-		withContainer("retrieving", new ContainerBlock() {
+		withContainer("[" + label + "] retrieving", config, new ContainerBlock() {
 			public void run(ObjectContainer container) {
 				retrieveAll(container, array.value);
 			}
 		});
 	}
 
-	private static void withContainer(String label, ContainerBlock block) {
+	private static void withContainer(String label, Configuration config, ContainerBlock block) {
 		System.out.println("Entering '" + label + "' phase.");
 		StopWatch watch = new StopWatch();
-		final ObjectContainer container = Db4o.openFile(FILENAME);
+		final ObjectContainer container = Db4o.openFile(config, FILENAME);
 		try {
 			watch.start();
 			block.run(container);
 		} finally {
 			watch.stop();
 			container.close();
-			System.out.println("Leaving '" + label + "' phase.");
+			System.out.println("Leaving  '" + label + "' phase.");
 			System.out.println("Time taken: " + watch.elapsed() + "ms.");
+			System.out.println();
 		}
 	}
 
@@ -65,7 +89,7 @@ public class Main {
 	}
 
 	private static long[] store(ObjectContainer container) {
-		long[] ids = new long[1024];
+		long[] ids = new long[OBJECT_COUNT];
 		for (int i = 0; i < ids.length; i++) {
 			ids[i] = storeNewItem(container);
 		}
