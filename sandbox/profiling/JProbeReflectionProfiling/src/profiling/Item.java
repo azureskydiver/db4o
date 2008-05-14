f0fef193fb845ef012c8b359b2ec499095eb49cc
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-05-14 13:06:44 +0000

    cr+rbo: COR-1276 - preparing project for jprobe session

---
+++ sandbox/profiling/JProbeReflectionProfiling/src/profiling/Item.java
@@ -0,0 +1,13 @@
+package profiling;
+
+public class Item {
+
+	public int _intValue;
+	public String _stringValue;
+
+	public Item(int intValue, String stringValue) {
+		_intValue = intValue;
+		_stringValue = stringValue;
+	}
+
+}
