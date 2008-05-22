735ba1e15dc634e9bd127817eef15a7e6ec36f79
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-05-22 16:00:18 +0000

    cr: #COR-1283 Multidimensional arrays now pass on .NET with null bitmap also.

--- db4oj/core/src/com/db4o/internal/handlers/MultidimensionalArrayHandler3.java
+++ db4oj/core/src/com/db4o/internal/handlers/MultidimensionalArrayHandler3.java
@@ -2,13 +2,39 @@
 
 package com.db4o.internal.handlers;
 
+import com.db4o.*;
+import com.db4o.internal.*;
+import com.db4o.reflect.*;
+
 
 /**
  * @exclude
  */
 public class MultidimensionalArrayHandler3 extends MultidimensionalArrayHandler {
 
+    protected boolean isPrimitive(ReflectClass claxx) {
+        if(Deploy.csharp){
+            return false;
+        }
+        return claxx.isPrimitive();
+    }
+    
+    protected final boolean useJavaHandling() {
+        return ! Deploy.csharp;
+    }
+    
     protected boolean hasNullBitmap() {
         return false;
     }
+    
+    protected ReflectClass classReflector(Reflector reflector, ClassMetadata classMetadata, boolean isPrimitive){
+        if(Deploy.csharp && NullableArrayHandling.enabled()){
+            ReflectClass primitiveClaxx = Handlers4.primitiveClassReflector(classMetadata, reflector);
+            if(primitiveClaxx != null){
+                return primitiveClaxx;
+            }
+        }
+        return super.classReflector(reflector, classMetadata, isPrimitive);
+    }
+
 }
