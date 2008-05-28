6c0ba668978f6090b7a19ffa66f86ba4a2b27754
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-05-28 12:28:20 +0000

    disable auto building to have better control

--- decaf/decaf/src/decaf/builder/DecafApplication.java
+++ decaf/decaf/src/decaf/builder/DecafApplication.java
@@ -1,33 +1,50 @@
 package decaf.builder;
 
+import org.eclipse.core.resources.*;
 import org.eclipse.core.runtime.*;
 import org.eclipse.equinox.app.*;
 
 import sharpen.core.*;
+import sharpen.core.framework.*;
 
 public class DecafApplication implements IApplication {
 
 	public Object start(IApplicationContext context) throws Exception {
-		String[] args = argv(context);
-		String projectName = args[0];
+		final String[] args = argv(context);
+		final String projectName = args[0];
 		try {
-			JavaProject project = new JavaProject(projectName);
-			project.addNature(DecafNature.NATURE_ID);
+			disableAutoBuilding();
+			
+			final ConsoleProgressMonitor monitor = new ConsoleProgressMonitor();
+			
+			final JavaProject project = new JavaProject(projectName);
 			project.addSourceFolder("src");
-			project.joinBuild();
+			project.addNature(DecafNature.NATURE_ID);
+			project.buildProject(monitor);
+			
+			decafProjectFor(projectName).build(IncrementalProjectBuilder.INCREMENTAL_BUILD, monitor);
 		} catch (CoreException x) {
 			x.printStackTrace();
 		}
 		return IApplication.EXIT_OK;
 	}
 
+	private IProject decafProjectFor(final String projectName) {
+		return WorkspaceUtilities.getProject(projectName + ".decaf");
+	}
+
+	private void disableAutoBuilding() throws CoreException {
+		IWorkspace workspace = WorkspaceUtilities.getWorkspaceRoot().getWorkspace();
+		IWorkspaceDescription workspaceDescription = workspace.getDescription();
+		workspaceDescription.setAutoBuilding(false);
+		workspace.setDescription(workspaceDescription);
+	}
+
 	public void stop() {
 		// nothing to do
-
 	}
 	
 	private String[] argv(IApplicationContext context) {
 		return (String[])context.getArguments().get(IApplicationContext.APPLICATION_ARGS);
 	}
-
 }
