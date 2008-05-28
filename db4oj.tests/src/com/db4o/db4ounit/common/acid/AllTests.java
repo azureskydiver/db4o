7e9be449dcb044680e3c7f2b93d99ca1e53ce67d
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-05-28 13:42:03 +0000

    cr + pr: #COR-1286 Test case that demonstrates read committed isolation.

--- db4oj.tests/src/com/db4o/db4ounit/common/acid/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/acid/AllTests.java
@@ -10,6 +10,7 @@ public class AllTests extends Db4oTestSuite {
 	protected Class[] testCases() {
 		return new Class[] {
 			CrashSimulatingTestCase.class,
+			ReadCommittedIsolationTestCase.class,
 		};
 	}
 
