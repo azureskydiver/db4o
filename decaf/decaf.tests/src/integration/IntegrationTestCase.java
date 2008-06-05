3cf7868a68b567906877d535c55d16441d59d6d4
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-06-05 00:13:59 +0000

    INT-7 - integration test case mixing generic erasure with varargs methods; changed varargs transformation to use the ListRewrite API to minimize the changes that need to be applied to the original sources;

---
+++ decaf/decaf.tests/src/integration/IntegrationTestCase.java
@@ -0,0 +1,10 @@
+package integration;
+
+import decaf.tests.*;
+
+public class IntegrationTestCase extends DecafTestCaseBase {
+
+	public void testErasureInVarArgs() throws Exception {
+		runResourceTestCase("integration/ErasureInVarArgs");
+	}
+}
