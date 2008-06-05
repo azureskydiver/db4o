2e671737adf2a59682e3c45af9d33b2654f1fd15
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2008-06-05 12:25:36 +0000

    no commit message dispatcher for embedded server, no event args creation for events without listeners

--- db4oj/core/src/com/db4o/internal/events/EventPlatform.java
+++ db4oj/core/src/com/db4o/internal/events/EventPlatform.java
@@ -15,28 +15,46 @@ import com.db4o.query.*;
 public class EventPlatform {
 
 	public static void triggerQueryEvent(Transaction transaction, Event4Impl e, Query q) {
+		if(!e.hasListeners()) {
+			return;
+		}
 		e.trigger(new QueryEventArgs(transaction, q));
 	}
 
 	public static void triggerClassEvent(Event4Impl e, ClassMetadata clazz) {
+		if(!e.hasListeners()) {
+			return;
+		}
 		e.trigger(new ClassEventArgs(clazz));
 	}
 
 	public static boolean triggerCancellableObjectEventArgs(Transaction transaction, Event4Impl e, Object o) {
+		if(!e.hasListeners()) {
+			return true;
+		}
 		CancellableObjectEventArgs args = new CancellableObjectEventArgs(transaction, o);
 		e.trigger(args);
 		return !args.isCancelled();
 	}
 	
 	public static void triggerObjectEvent(Transaction transaction, Event4Impl e, Object o) {
+		if(!e.hasListeners()) {
+			return;
+		}
 		e.trigger(new ObjectEventArgs(transaction, o));
 	}
 
 	public static void triggerCommitEvent(Transaction transaction, Event4Impl e, CallbackObjectInfoCollections collections) {
+		if(!e.hasListeners()) {
+			return;
+		}
 		e.trigger(new CommitEventArgs(transaction, collections));
 	}
 	
 	public static void triggerObjectContainerEvent(ObjectContainer container, Event4Impl e) {
+		if(!e.hasListeners()) {
+			return;
+		}
 		e.trigger(new ObjectContainerEventArgs(container));
 	}
 	
