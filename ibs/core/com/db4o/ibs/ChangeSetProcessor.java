df367e79dfbaeae997a95e698c850d25e48a388b
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-06-19 21:59:37 +0000

    minor corrections to the docs

--- ibs/core/com/db4o/ibs/ChangeSetProcessor.java
+++ ibs/core/com/db4o/ibs/ChangeSetProcessor.java
@@ -3,6 +3,8 @@ package com.db4o.ibs;
 /**
  * A ChangeSetProcessor knows how to apply a {@link ChangeSet} produced by
  * its {@link ChangeSetBuilder} counterpart.
+ * 
+ * @see ChangeSetEngine
  */
 public interface ChangeSetProcessor {
 
