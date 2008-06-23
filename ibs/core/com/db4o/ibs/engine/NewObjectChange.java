1097d058fbad9e8bfa1e5a0953773b18c4233dc2
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-06-23 23:50:14 +0000

    INT-16 - new object

---
+++ ibs/core/com/db4o/ibs/engine/NewObjectChange.java
@@ -0,0 +1,11 @@
+package com.db4o.ibs.engine;
+
+import com.db4o.ext.*;
+
+public class NewObjectChange extends SlotBasedChange {
+
+	public NewObjectChange(ObjectInfo object) {
+		super(object);
+	}
+
+}
