bd4e29e394fc95349a409a6ce79bf90ba329e6f6
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2008-06-30 15:30:15 +0000

    copyright comments

--- db4oj/core/src/com/db4o/foundation/Sequence4.java
+++ db4oj/core/src/com/db4o/foundation/Sequence4.java
@@ -1,6 +1,14 @@
+/* Copyright (C) 2008   db4objects Inc.   http://www.db4o.com */
+
 package com.db4o.foundation;
 
+/**
+ * @exclude
+ */
 public interface Sequence4 {
+	
 	void add(Object element);
+	
 	boolean isEmpty();
+
 }
