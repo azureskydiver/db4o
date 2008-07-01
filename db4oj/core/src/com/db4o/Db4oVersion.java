84aeccc584250c8bb3869cb1ada87f9e82c39748
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-07-01 10:08:07 +0000

    cr: Hey, we are in 7.4 mode now.

--- db4oj/core/src/com/db4o/Db4oVersion.java
+++ db4oj/core/src/com/db4o/Db4oVersion.java
@@ -6,9 +6,9 @@ package com.db4o;
 * @exclude
 */
 public class Db4oVersion {
-    public static final String NAME = "7.3.46.10582";
+    public static final String NAME = "7.4.50.11064";
     public static final int MAJOR = 7;
-    public static final int MINOR = 3;
-    public static final int ITERATION = 46;
-    public static final int REVISION = 10582;
+    public static final int MINOR = 4;
+    public static final int ITERATION = 50;
+    public static final int REVISION = 11064;
 }
