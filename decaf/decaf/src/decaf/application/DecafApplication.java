800c0e610fcebe554dcab12ef6227c76141e9f18
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-07-07 16:00:45 +0000

    pr+rbo: INT-25 - fixed type mapping for simple types; Fixed qualified name and import handling (no need to use qualified names when the type is already imported).

--- decaf/decaf/src/decaf/application/DecafApplication.java
+++ decaf/decaf/src/decaf/application/DecafApplication.java
@@ -1,15 +1,16 @@
 package decaf.application;
 
-import org.eclipse.core.internal.resources.*;
-import org.eclipse.core.resources.*;
-import org.eclipse.core.runtime.*;
-import org.eclipse.equinox.app.*;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
 
-import decaf.builder.*;
-
-import sharpen.core.*;
-import sharpen.core.framework.*;
+import sharpen.core.JavaProject;
+import sharpen.core.framework.ConsoleProgressMonitor;
 import sharpen.core.resources.WorkspaceUtilities;
+import decaf.builder.DecafNature;
 
 public class DecafApplication implements IApplication {
 
