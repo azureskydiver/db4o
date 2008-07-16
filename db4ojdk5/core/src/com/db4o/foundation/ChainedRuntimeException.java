b489da0013a12183ee0fa4c2137b1981931974aa
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-07-16 17:17:22 +0000

    DCF-17 - ChainedRuntimeExceptionMixin needs to be public so it can be marshalled from server to client; ALL GREEN!

--- db4ojdk5/core/src/com/db4o/foundation/ChainedRuntimeException.java
+++ db4ojdk5/core/src/com/db4o/foundation/ChainedRuntimeException.java
@@ -29,54 +29,55 @@ public abstract class ChainedRuntimeException extends RuntimeException {
     public ChainedRuntimeException(String msg, Throwable cause) {
         super(msg, cause);
     }
-}
-
-/**
- * Provides jdk11 compatible exception chaining. decaf will mix this class
- * into {@link ChainedRuntimeException}.
- * 
- * @exclude
- */
-class ChainedRuntimeExceptionMixin {
-
-    private final ChainedRuntimeException _subject;
-    private final Throwable _cause;
-
-    public ChainedRuntimeExceptionMixin(ChainedRuntimeException mixee) {
-        _subject = mixee;
-        _cause = null;
-    }
     
-    public ChainedRuntimeExceptionMixin(ChainedRuntimeException mixee, String msg) {
-        _subject = mixee;
-        _cause = null;
-    }
-    
-    public ChainedRuntimeExceptionMixin(ChainedRuntimeException mixee, String msg, Throwable cause) {
-        _subject = mixee;
-        _cause = cause;
-    }
-
     /**
-    * @return The originating exception, if any
-    */
-    public final Throwable getCause() {
-        return _cause;
-    }
+     * Provides jdk11 compatible exception chaining. decaf will mix this class
+     * into {@link ChainedRuntimeException}.
+     * 
+     * @exclude
+     */
+    public static class ChainedRuntimeExceptionMixin {
 
-    public void printStackTrace() {
-        printStackTrace(System.err);
-    }
+        public ChainedRuntimeException _subject;
+        public Throwable _cause;
 
-    public void printStackTrace(PrintStream ps) {
-        printStackTrace(new PrintWriter(ps, true));
-    }
+        public ChainedRuntimeExceptionMixin(ChainedRuntimeException mixee) {
+            _subject = mixee;
+            _cause = null;
+        }
+        
+        public ChainedRuntimeExceptionMixin(ChainedRuntimeException mixee, String msg) {
+            _subject = mixee;
+            _cause = null;
+        }
+        
+        public ChainedRuntimeExceptionMixin(ChainedRuntimeException mixee, String msg, Throwable cause) {
+            _subject = mixee;
+            _cause = cause;
+        }
 
-    public void printStackTrace(PrintWriter pw) {
-        _subject.printStackTrace(pw);
-        if (_cause != null) {
-            pw.println("Nested cause:");
-            _cause.printStackTrace(pw);
+        /**
+        * @return The originating exception, if any
+        */
+        public final Throwable getCause() {
+            return _cause;
+        }
+
+        public void printStackTrace() {
+            printStackTrace(System.err);
+        }
+
+        public void printStackTrace(PrintStream ps) {
+            printStackTrace(new PrintWriter(ps, true));
+        }
+
+        public void printStackTrace(PrintWriter pw) {
+            _subject.printStackTrace(pw);
+            if (_cause != null) {
+                pw.println("Nested cause:");
+                _cause.printStackTrace(pw);
+            }
         }
     }
 }
+
