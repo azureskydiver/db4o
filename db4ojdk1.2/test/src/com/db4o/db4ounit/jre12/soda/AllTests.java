9f431a77bbe3d9848945b83a26b27298032489e1
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-07-30 11:43:30 +0000

    cr: #COR-1351 Vector QBE tests are turned off when new Typehandlers are turned on.

--- db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/soda/AllTests.java
+++ db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/soda/AllTests.java
@@ -5,12 +5,14 @@ package com.db4o.db4ounit.jre12.soda;
 import com.db4o.db4ounit.jre12.soda.collections.*;
 import com.db4o.db4ounit.jre12.soda.deepOR.*;
 import com.db4o.db4ounit.jre12.soda.experiments.*;
+import com.db4o.internal.*;
 
 import db4ounit.extensions.*;
 
 public class AllTests  extends Db4oTestSuite {
 	protected Class[] testCases() {
-		return new Class[]{
+		 
+		return merge(new Class[]{
 				HashtableModifiedUpdateDepthTestCase.class,
 				ObjectSetListAPITestCase.class,
 				STArrayListTTestCase.class,
@@ -28,18 +30,36 @@ public class AllTests  extends Db4oTestSuite {
 				STOwnCollectionTTestCase.class,
 				STTreeSetTTestCase.class,
 				STTreeSetUTestCase.class,
-				STVectorDTestCase.class,
-				STVectorEDTestCase.class,
 				STVectorTTestCase.class,
 				STVectorUTestCase.class,
 				STOrContainsTestCase.class,
 				STCurrentTestCase.class,
 				STIdentityEvaluationTestCase.class,
 				STNullOnPathTestCase.class,
-		};
+		}, vectorQbeTestCases());
+	}
+	
+	private Class[] merge(Class[] arr1, Class[] arr2){
+		Class[] res = new Class[arr1.length + arr2.length];
+		System.arraycopy(arr1, 0, res, 0, arr1.length);
+		System.arraycopy(arr2, 0, res, arr1.length, arr2.length);
+		return res;
 	}
 
 	public static void main(String[] args) {
 		new AllTests().runAll();
+	}
+	
+	private Class[] vectorQbeTestCases () {
+		return CollectionTypeHandlerRegistry.enabled() ? 
+				new Class[] {
+			//  QBE with vector is not expressible as SODA and it
+			//  will no longer work with new collection Typehandlers
+				} 
+		: 
+				new Class[] {
+						STVectorDTestCase.class,
+						STVectorEDTestCase.class,
+		};
 	}
 }
