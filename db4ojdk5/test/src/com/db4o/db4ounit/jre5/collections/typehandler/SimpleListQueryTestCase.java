aae359fd64c24f8b4c4f73964601bf120fd820cd
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-07-31 10:44:45 +0000

    cr: #COR-1351 Some more simple queries for lists.

---
+++ db4ojdk5/test/src/com/db4o/db4ounit/jre5/collections/typehandler/SimpleListQueryTestCase.java
@@ -0,0 +1,101 @@
+/* Copyright (C) 2008  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.db4ounit.jre5.collections.typehandler;
+
+import java.util.*;
+
+import com.db4o.*;
+import com.db4o.config.*;
+import com.db4o.internal.*;
+import com.db4o.query.*;
+import com.db4o.typehandlers.*;
+
+import db4ounit.*;
+import db4ounit.extensions.*;
+
+/**
+ * @exclude
+ */
+public class SimpleListQueryTestCase extends AbstractDb4oTestCase {
+	
+	public static class Item {
+		public List list;
+	}
+	
+	public static class FirstClassElement {
+		
+		public String name;
+		
+		public FirstClassElement(String name_){
+			name = name_;
+		}
+		
+		public boolean equals(Object obj) {
+			if(! (obj instanceof FirstClassElement)){
+				return false;
+			}
+			FirstClassElement other = (FirstClassElement) obj;
+			if(name == null){
+				return other.name == null;
+			}
+			return name.equals(other.name);
+		}
+		
+	}
+	
+	static final Object[] DATA = new Object[]{
+		"one",
+		"two",
+		new Integer(1),
+		new Integer(2),
+		new Integer(42),
+		new FirstClassElement("one"),
+		new FirstClassElement("fortytwo"),
+		
+	};
+	
+	
+	
+	protected void configure(Configuration config) throws Exception {
+		if(! NullableArrayHandling.enabled()){
+			config.registerTypeHandler(
+				new SingleClassTypeHandlerPredicate(ArrayList.class), 
+				new ListTypeHandler());
+		}
+		config.objectClass(Item.class).cascadeOnDelete(true);
+	}
+	
+	protected void store() throws Exception {
+		for (int i = 0; i < DATA.length; i++) {
+			storeItem(DATA[i]);
+		}
+	}
+	
+	private void storeItem(Object listElement){
+		Item item = new Item();
+		item.list = new ArrayList();
+		item.list.add(listElement);
+		store(item);
+	}
+	
+	public void testListConstrainQuery() {
+		for (int i = 0; i < DATA.length; i++) {
+			assertSingleElementQuery(DATA[i]);
+		}
+	}
+
+	private void assertSingleElementQuery(Object element) {
+		Query q = db().query();
+		q.constrain(Item.class);
+		q.descend("list").constrain(element);
+		assertSingleElementQueryResult(q, element);
+	}
+
+	private void assertSingleElementQueryResult(Query query, Object element) {
+		ObjectSet objectSet = query.execute();
+		Assert.areEqual(1, objectSet.size());
+		Item item = (Item) objectSet.next();
+		Assert.areEqual(element, item.list.get(0));
+	}
+
+}
