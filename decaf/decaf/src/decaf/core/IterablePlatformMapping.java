d77096ca72de2e4a276a1a381299b6b6cd4d21c9
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2008-08-04 15:09:09 +0000

    acv/pr: DCF-8 - iterator coercion for method invocations

---
+++ decaf/decaf/src/decaf/core/IterablePlatformMapping.java
@@ -0,0 +1,17 @@
+/* Copyright (C) 2008  db4objects Inc.   http://www.db4o.com */
+package decaf.core;
+
+import org.eclipse.jdt.core.dom.*;
+
+import decaf.builder.*;
+
+public interface IterablePlatformMapping {
+
+	String iteratorClassName();
+	String iteratorNextCheckName();
+	String iteratorNextElementName();
+
+	Expression coerceIterableExpression(Expression iterableExpr,
+			DecafASTNodeBuilder builder, DecafRewritingServices rewrite);
+
+}
--- decaf/decaf/src/decaf/core/TargetPlatform.java
+++ decaf/decaf/src/decaf/core/TargetPlatform.java
@@ -2,6 +2,12 @@
 
 package decaf.core;
 
+import java.util.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+import decaf.builder.*;
+
 
 
 public enum TargetPlatform {
@@ -21,17 +27,32 @@ public enum TargetPlatform {
 		public boolean isNone() {
 			return true;
 		}
+
+		@Override
+		public IterablePlatformMapping iterablePlatformMapping() {
+			return JDK12_ITERABLE_MAPPING;
+		}
 	},
 	JDK11 {
 		@Override
 		public DecafConfiguration defaultConfig() {
 			return DecafConfiguration.forJDK11();
 		}
+
+		@Override
+		public IterablePlatformMapping iterablePlatformMapping() {
+			return JDK11_ITERABLE_MAPPING;
+		}
 	},
 	JDK12 {
 		@Override
 		public DecafConfiguration defaultConfig() {
 			return DecafConfiguration.forJDK12();
+		}
+
+		@Override
+		public IterablePlatformMapping iterablePlatformMapping() {
+			return JDK12_ITERABLE_MAPPING;
 		}
 	};
 	
@@ -45,11 +66,54 @@ public enum TargetPlatform {
 
 	public abstract DecafConfiguration defaultConfig();
 
+	public abstract IterablePlatformMapping iterablePlatformMapping();
+	
 	public boolean isNone() {
 		return false;
 	}
 	
-	public boolean isJDK11() {
-		return isNone() || this.equals(JDK11);
+	private static class IterableJdk11Mapping implements IterablePlatformMapping {
+		
+		public String iteratorClassName() {
+			return "com.db4o.foundation.Iterator4";
+		}
+
+		public String iteratorNextCheckName() {
+			return "moveNext";
+		}
+
+		public String iteratorNextElementName() {
+			return "current";
+		}
+
+		public Expression coerceIterableExpression(Expression iterableExpr, DecafASTNodeBuilder builder, DecafRewritingServices rewrite) {
+			return iterableExpr;
+		}
+		
 	}
+
+	private static class IterableJdk12Mapping implements IterablePlatformMapping {
+
+		public String iteratorClassName() {
+			return Iterator.class.getName();
+		}
+
+		public String iteratorNextCheckName() {
+			return "hasNext";
+		}
+
+		public String iteratorNextElementName() {
+			return "next";
+		}
+		
+		public Expression coerceIterableExpression(Expression iterableExpr, DecafASTNodeBuilder builder, DecafRewritingServices rewrite) {
+			MethodInvocation coerceInvocation = builder.newMethodInvocation(builder.newQualifiedName("com", "db4o", "foundation", "IterableBaseFactory"), "coerce");
+			coerceInvocation.arguments().add(rewrite.safeMove(iterableExpr));
+			return coerceInvocation;
+		}
+	}
+
+	private static IterablePlatformMapping JDK11_ITERABLE_MAPPING = new IterableJdk11Mapping();
+	private static IterablePlatformMapping JDK12_ITERABLE_MAPPING = new IterableJdk12Mapping();
+
 }
