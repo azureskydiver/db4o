77d9ca1bcd9d7078925f98bdd1e4d2c3babae8b7
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2008-08-20 13:41:00 +0000

    acv + pr : COR-1363 - Test case to make sure events are properly raised

--- db4oj.tests/src/com/db4o/db4ounit/common/events/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/events/AllTests.java
@@ -18,6 +18,7 @@ public class AllTests extends Db4oTestSuite {
 			EventArgsTransactionTestCase.class,			
 			InstantiationEventsTestCase.class,
 			ObjectContainerEventsTestCase.class,
+			EventCountTestCase.class,
 		};
 	}
 
