3b7cd570ac9c18ee46a8cb56c5516538eae6a148
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-08-25 13:53:56 +0000

    cr: Added submissions to the performance competition with respective .jars that try to improve speed.

---
+++ db4opolepos/src/org/polepos/teams/db4o/HockenheimDb4o.java
@@ -0,0 +1,45 @@
+package org.polepos.teams.db4o;
+
+import org.polepos.circuits.hockenheim.*;
+
+import com.db4o.*;
+import com.db4o.query.*;
+
+public class HockenheimDb4o extends Db4oDriver implements HockenheimDriver {
+
+	public void query_ascending() {
+		Query query = db().query();
+		query.constrain(Item.class);
+		query.sortBy(new AscendingIdComparator());
+		query.execute();
+	}
+
+	public void query_descending() {
+		Query query = db().query();
+		query.constrain(Item.class);
+		query.sortBy(new DescendingIdComparator());
+		query.execute();
+	}
+
+	public void store() {
+		int count = setup().getObjectCount();
+		begin();
+		for (int i = 1; i <= count; i++) {
+			store(new Item(i));
+		}
+		commit();
+	}
+
+	public static class AscendingIdComparator implements QueryComparator<Item> {
+		public int compare(Item first, Item second) {
+			return first.id - second.id;
+		}
+	}
+
+	public static class DescendingIdComparator implements QueryComparator<Item> {
+		public int compare(Item first, Item second) {
+			return second.id - first.id;
+		}
+	}
+
+}
---
+++ db4opolepos/src/org/polepos/teams/db4o/HungaroringDb4o.java
@@ -0,0 +1,93 @@
+package org.polepos.teams.db4o;
+
+import org.polepos.circuits.hungaroring.*;
+import org.polepos.framework.*;
+
+import com.db4o.*;
+import com.db4o.config.*;
+import com.db4o.query.*;
+
+public class HungaroringDb4o extends Db4oDriver implements HungaroringDriver {
+
+	private static int maximumPayload;
+
+	public void takeSeatIn(Car car, TurnSetup setup)
+			throws CarMotorFailureException {
+		indexField(fieldNext());
+		indexField(fieldPayload());
+		super.takeSeatIn(car, setup);
+	}
+
+	private void indexField(String fieldName) {
+		ObjectClass objectClass = Db4o.configure().objectClass(HungaroringList.class);
+		objectClass.objectField(fieldName).indexed(true);
+	}
+
+	public void write() {
+		HungaroringList list = HungaroringList.generate(setup()
+				.getObjectCount());
+		maximumPayload = list.getPayload();
+		begin();
+		store(list);
+		commit();
+	}
+
+	public void queryWithTwoWorkers() throws Exception {
+		int workers = 2;
+		runQuery(workers);
+	}
+
+	private void runQuery(int threadCount) throws InterruptedException, Exception {
+		Worker[] workers = new Worker[threadCount];
+		for (int wi = 0; wi < threadCount; wi++) {
+			workers[wi] = new Worker();
+			workers[wi].start();
+		}
+		for (int wi = 0; wi < threadCount; wi++) {
+			workers[wi].join();
+		}
+		if (lastException != null)
+			throw lastException;
+	}
+	
+	public void queryWithFourWorkers() throws Exception {
+		int workers = 4;
+		runQuery(workers);
+	}	
+
+	private Exception lastException = null;
+
+	public class Worker extends Thread {
+
+		public void run() {
+			try {
+				int count = setup().getSelectCount();
+				for (int i = 1; i <= count; i++) {
+					Query q = newHungaroringListQuery();
+					Constraint c1 = q.descend(fieldPayload()).constrain(
+							new Integer(maximumPayload - 1)).greater();
+					// Constraint c2 = q.descend(fieldNext()).constrain(null);
+					// c1.and(c2);
+					doQuery(q);
+				}
+			} catch (Exception ex) {
+				lastException = ex;
+			}
+		}
+	}
+
+	private Query newHungaroringListQuery() {
+		Query q = db().query();
+		q.constrain(HungaroringList.class);
+		return q;
+	}
+
+	private String fieldNext() {
+		return HungaroringList.FIELD_NEXT;
+	}
+
+	private String fieldPayload() {
+		return HungaroringList.FIELD_PAYLOAD;
+	}
+
+}
