d83576c61389424eff109e003789c394a00a4ca6
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-08-28 13:01:09 +0000

    pr+rbo: COR-1368 - db4ojdk12 is no more

--- db4ojdk1.2/test/src/com/db4o/db4ounit/jre12/RunAllCommonMigrationTests.java
+++
@@ -1,9 +0,0 @@
-package com.db4o.db4ounit.jre12;
-
-import com.db4o.db4ounit.common.migration.*;
-
-public class RunAllCommonMigrationTests {
-    public static void main(String[] args) {
-        System.exit(new AllCommonTests().runSolo());
-    }
-}
--- db4ojdk1.2/test/src/com/db4o/test/ComparableAtom.java
+++
@@ -1,8 +0,0 @@
-/* Copyright (C) 2004 - 2005  db4objects Inc.  http://www.db4o.com */
-
-package com.db4o.test;
-
-
-public class ComparableAtom extends Atom implements Comparable{
-
-}
