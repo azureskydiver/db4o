74bb9da6bb5f436de16e81a82d8cbe6360272a54
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2008-08-29 16:08:17 +0000

    Build integration fixes: Db4oJarEnhancerTestCase needs excluding class loader to delegate interface class, ReadObjectNQTestCase needs everything public on jdk11

--- db4otaj/test/com/db4o/ta/instrumentation/test/AllTests.java
+++ db4otaj/test/com/db4o/ta/instrumentation/test/AllTests.java
@@ -8,7 +8,7 @@ import db4ounit.extensions.*;
 public class AllTests extends Db4oTestSuite {
 	
 	public static void main(String[] args) {
-		new AllTests().runSolo();
+		System.exit(new AllTests().runSolo());
 	}
 
 	protected Class[] testCases() {
