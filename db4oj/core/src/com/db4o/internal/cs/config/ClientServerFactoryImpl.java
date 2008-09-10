7d32f98379667467ea81ba3a2777b3452b3cd9f0
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-09-10 10:52:21 +0000

    cr: #COR-1384 Resolving dependancies that prevent CS from being split off into a separate project/library.

---
+++ db4oj/core/src/com/db4o/internal/cs/config/ClientServerFactoryImpl.java
@@ -0,0 +1,42 @@
+/* Copyright (C) 2008  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.internal.cs.config;
+
+import com.db4o.*;
+import com.db4o.config.*;
+import com.db4o.ext.*;
+import com.db4o.foundation.network.*;
+import com.db4o.internal.*;
+import com.db4o.internal.cs.*;
+
+/**
+ * @exclude
+ */
+public class ClientServerFactoryImpl implements ClientServerFactory{
+
+	public ObjectContainer openClient(Configuration config, String hostName,
+			int port, String user, String password,
+			NativeSocketFactory socketFactory) throws Db4oIOException,
+			OldFormatException, InvalidPasswordException {
+		if (user == null || password == null) {
+			throw new InvalidPasswordException();
+		}
+		NetworkSocket networkSocket = new NetworkSocket(socketFactory, hostName, port);
+		return new ClientObjectContainer(config, networkSocket, user, password, true);
+	}
+
+	public ObjectServer openServer(Configuration config,
+			String databaseFileName, int port, NativeSocketFactory socketFactory)
+			throws Db4oIOException, IncompatibleFileFormatException,
+			OldFormatException, DatabaseFileLockedException,
+			DatabaseReadOnlyException {
+		LocalObjectContainer container = (LocalObjectContainer)Db4o.openFile(config,databaseFileName);
+        if(container == null){
+            return null;
+        }
+        synchronized(container.lock()){
+            return new ObjectServerImpl(container, port, socketFactory);
+        }
+	}
+
+}
