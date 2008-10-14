6080d460e3c122b7790bb6be0707cde4cd559db3
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-10-14 12:36:06 +0000

    rbo + cr + acv: Fixing the dRS build.

--- drs/src/core/com/db4o/drs/db4o/Db4oProviderFactory.java
+++ drs/src/core/com/db4o/drs/db4o/Db4oProviderFactory.java
@@ -21,14 +21,14 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 package com.db4o.drs.db4o;
 
 import com.db4o.ObjectContainer;
-import com.db4o.internal.cs.ClientObjectContainer;
+import com.db4o.internal.*;
 
 /**
  * @exclude
  */
 public class Db4oProviderFactory {
 	public static Db4oReplicationProvider newInstance(ObjectContainer oc, String name) {
-		if (oc instanceof ClientObjectContainer)
+		if (isClient(oc))
 			return new ClientServerReplicationProvider(oc, name);
 		else
 			return new FileReplicationProvider(oc, name);
@@ -37,4 +37,10 @@ public class Db4oProviderFactory {
 	public static Db4oReplicationProvider newInstance(ObjectContainer oc) {
 		return newInstance(oc, null);
 	}
+	
+	private static boolean isClient(ObjectContainer oc){
+		return ((InternalObjectContainer)oc).isClient();
+	}
+	
+	
 }
