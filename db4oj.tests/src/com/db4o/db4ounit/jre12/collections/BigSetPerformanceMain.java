1536aee20bc2aa3413f45d4be53266a960d04972
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-10-16 14:05:12 +0000

    pr+rbo: run performance test with different element counts

--- db4oj.tests/src/com/db4o/db4ounit/jre12/collections/BigSetPerformanceMain.java
+++ db4oj.tests/src/com/db4o/db4ounit/jre12/collections/BigSetPerformanceMain.java
@@ -10,120 +10,130 @@ import com.db4o.foundation.io.*;
 import com.db4o.internal.*;
 
 import db4ounit.*;
+import db4ounit.fixtures.*;
 
 /**
  * @decaf.ignore
  * @sharpen.ignore
  */
-public class BigSetPerformanceMain implements TestLifeCycle{
+public class BigSetPerformanceMain extends FixtureBasedTestSuite {
 	
-	
-	private static final int COUNT = 1000;
-	
-	private static final int ADD_RUNS = 5;
-	
-	public static class Item {
-		
-		public int _value;
-		
-		public Item(int value){
-			_value = value;
-		}
-		
-	}
+	@Override
+    public FixtureProvider[] fixtureProviders() {
+		return new FixtureProvider[] {
+			new SubjectFixtureProvider(new Object[] { new Integer(10), new Integer(100), new Integer(1000), new Integer(10000) })
+		};
+    }
 
-		
-	private ObjectContainer _container;
-	
-	static final String FILENAME = Path4.getTempFileName();
-	
-	public void setUp() throws Exception {
-		Db4o.configure().bTreeNodeSize(1000);
-		_container = Db4o.openFile(FILENAME);
-		System.out.println("Element count: " + COUNT);
-		System.out.println("Add runs: " + ADD_RUNS);
-	}
-	
-	public void tearDown(){
-		_container.close();
-		File4.delete(FILENAME);
-	}
-	
-	public void testTimePlainList(){
-		List list = timePlainListCreation();
-		timePlainListSingleAdd(list);
-	}
-	
-	public void testTimeBigSet(){
-		Set set = timeBigSetCreation();
-		timeBigSetSingleAdd(set);
-	}
-	
-	private void timePlainListSingleAdd(List list) {
-		long start = System.currentTimeMillis();
-		for (int i = 0; i < ADD_RUNS; i++) {
-			list.add(new Item(i));
+	@Override
+    public Class[] testUnits() {
+		return new Class[] { BigSetPerformance.class };
+    }
+
+	public static class BigSetPerformance implements TestLifeCycle {
+
+		private static final int ADD_RUNS = 50;
+
+		public static class Item {
+
+			public int _value;
+
+			public Item(int value) {
+				_value = value;
+			}
+		}
+
+		private ObjectContainer _container;
+
+		static final String FILENAME = Path4.getTempFileName();
+
+		public void setUp() throws Exception {
+			Db4o.configure().bTreeNodeSize(1000);
+			_container = Db4o.openFile(FILENAME);
+			System.out.println("Element count: " + count());
+			System.out.println("Add runs: " + ADD_RUNS);
+		}
+
+		public void tearDown() {
+			_container.close();
+			File4.delete(FILENAME);
+		}
+
+		public void testTimePlainList() {
+			List list = timePlainListCreation();
+			timePlainListSingleAdd(list);
+		}
+
+		public void testTimeBigSet() {
+			Set set = timeBigSetCreation();
+			timeBigSetSingleAdd(set);
+		}
+
+		private void timePlainListSingleAdd(List list) {
+			long start = System.currentTimeMillis();
+			for (int i = 0; i < ADD_RUNS; i++) {
+				list.add(new Item(i));
+				_container.store(list);
+				_container.commit();
+			}
+			long stop = System.currentTimeMillis();
+			long duration = stop - start;
+			System.out.println("ArrayList single add: " + duration + "ms");
+
+		}
+
+		private List timePlainListCreation() {
+			long start = System.currentTimeMillis();
+			List list = new ArrayList();
+			for (int i = 0; i < count(); i++) {
+				list.add(new Item(i));
+			}
 			_container.store(list);
 			_container.commit();
+			long stop = System.currentTimeMillis();
+			long duration = stop - start;
+			System.out.println("ArrayList creation: " + duration + "ms");
+			return list;
 		}
-		long stop = System.currentTimeMillis();
-		long duration = stop - start;
-		System.out.println("ArrayList single add: " + duration + "ms" );
-		
-	}
 
-	private List timePlainListCreation() {
-		long start = System.currentTimeMillis();
-		List list = new ArrayList();
-		for (int i = 0; i < COUNT; i++) {
-			list.add(new Item(i));
-		}
-		_container.store(list);
-		_container.commit();
-		long stop = System.currentTimeMillis();
-		long duration = stop - start;
-		System.out.println("ArrayList creation: " + duration + "ms" );
-		return list;
-	}
-	
-	private Set timeBigSetCreation() {
-		long start = System.currentTimeMillis();
-		Set set = newBigSet();
-		for (int i = 0; i < COUNT; i++) {
-			set.add(new Item(i));
-		}
-		_container.store(set);
-		_container.commit();
-		long stop = System.currentTimeMillis();
-		long duration = stop - start;
-		System.out.println("Big Set creation: " + duration + "ms" );
-		return set;
-	}
-
-	private Set newBigSet() {
-	    return CollectionFactory.forObjectContainer(_container).newBigSet();
-    }
-	
-	private void timeBigSetSingleAdd(Set set) {
-		long start = System.currentTimeMillis();
-		for (int i = 0; i < ADD_RUNS; i++) {
-			set.add(new Item(i));
+		private Set timeBigSetCreation() {
+			long start = System.currentTimeMillis();
+			Set set = newBigSet();
+			for (int i = 0; i < count(); i++) {
+				set.add(new Item(i));
+			}
 			_container.store(set);
 			_container.commit();
+			long stop = System.currentTimeMillis();
+			long duration = stop - start;
+			System.out.println("Big Set creation: " + duration + "ms");
+			return set;
 		}
-		long stop = System.currentTimeMillis();
-		long duration = stop - start;
-		System.out.println("BigSet single add: " + duration + "ms" );
+
+		private int count() {
+			return ((Integer) SubjectFixtureProvider.value()).intValue();
+        }
+
+		private Set newBigSet() {
+			return CollectionFactory.forObjectContainer(_container).newBigSet();
+		}
+
+		private void timeBigSetSingleAdd(Set set) {
+			long start = System.currentTimeMillis();
+			for (int i = 0; i < ADD_RUNS; i++) {
+				set.add(new Item(i));
+				_container.store(set);
+				_container.commit();
+			}
+			long stop = System.currentTimeMillis();
+			long duration = stop - start;
+			System.out.println("BigSet single add: " + duration + "ms");
+		}
+
+		private Transaction trans() {
+			return ((ObjectContainerBase) _container).transaction();
+		}
 	}
 
-	private Transaction trans() {
-		return ((ObjectContainerBase)_container).transaction();
-	}
 	
-	
-
-
-	
-	
-
 }
