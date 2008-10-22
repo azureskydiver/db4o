2fa6530feb251193ddca263300a0bf084bf3305b
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-10-22 16:59:47 +0000

    cr: #COR-1439 Decaf.ignoring the CsSchemaUpdateTestCase

--- db4oj.tests/src/com/db4o/db4ounit/common/cs/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/cs/AllTests.java
@@ -2,6 +2,8 @@
 
 package com.db4o.db4ounit.common.cs;
 
+import com.db4o.db4ounit.common.util.*;
+
 import db4ounit.extensions.*;
 
 public class AllTests extends Db4oTestSuite {
@@ -9,30 +11,40 @@ public class AllTests extends Db4oTestSuite {
 	public static void main(String[] args) {
 		new AllTests().runAll();
     }
-
+	
 	protected Class[] testCases() {
+		Class[] commonCases = {
+		        CallConstructorsConfigTestCase.class,
+		        ClientDisconnectTestCase.class,
+	            ClientTimeOutTestCase.class,
+	            ClientTransactionHandleTestCase.class,
+	            ClientTransactionPoolTestCase.class,
+	            CloseServerBeforeClientTestCase.class,
+	            DeleteReaddTestCase.class,
+	            IsAliveTestCase.class,
+	            NoTestConstructorsQEStringCmpTestCase.class,
+	            ObjectServerTestCase.class,
+	            PrimitiveMessageTestCase.class,
+	            SendMessageToClientTestCase.class,
+	            ServerClosedTestCase.class,
+	            ServerPortUsedTestCase.class,
+	            ServerRevokeAccessTestCase.class,
+	            ServerTimeoutTestCase.class,
+	            ServerToClientTestCase.class,
+	            SetSemaphoreTestCase.class,
+	            SwitchingFilesFromClientTestCase.class,
+	            SwitchingFilesFromMultipleClientsTestCase.class,
+		};
+		return Db4oUnitTestUtil.mergeClasses(commonCases, nonDecafTestCases());
+	}
+
+	/**
+	 * @decaf.replaceFirst return new Class[0];
+	 */
+	private Class[] nonDecafTestCases() {
 		return new Class[] {
-	        CallConstructorsConfigTestCase.class,
-	        ClientDisconnectTestCase.class,
-            ClientTimeOutTestCase.class,
-            ClientTransactionHandleTestCase.class,
-            ClientTransactionPoolTestCase.class,
-            CloseServerBeforeClientTestCase.class,
-            CsSchemaUpdateTestCase.class,
-            DeleteReaddTestCase.class,
-            IsAliveTestCase.class,
-            NoTestConstructorsQEStringCmpTestCase.class,
-            ObjectServerTestCase.class,
-            PrimitiveMessageTestCase.class,
-            SendMessageToClientTestCase.class,
-            ServerClosedTestCase.class,
-            ServerPortUsedTestCase.class,
-            ServerRevokeAccessTestCase.class,
-            ServerTimeoutTestCase.class,
-            ServerToClientTestCase.class,
-            SetSemaphoreTestCase.class,
-            SwitchingFilesFromClientTestCase.class,
-            SwitchingFilesFromMultipleClientsTestCase.class,
+	            CsSchemaUpdateTestCase.class,
 		};
 	}
+	
 }
