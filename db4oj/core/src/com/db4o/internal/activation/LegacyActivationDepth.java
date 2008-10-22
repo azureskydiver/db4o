4f953a0cb04dd2aaab11fe46a79753ad633a0812
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-10-22 16:19:51 +0000

    cr + pr: #RIC-48 Looser handling for LegacyActivationDepth descend: Accept null ClassMetadata.

--- db4oj/core/src/com/db4o/internal/activation/LegacyActivationDepth.java
+++ db4oj/core/src/com/db4o/internal/activation/LegacyActivationDepth.java
@@ -22,7 +22,7 @@ public class LegacyActivationDepth extends ActivationDepthImpl {
 
 	public ActivationDepth descend(ClassMetadata metadata) {
 		if (null == metadata) {
-			throw new ArgumentNullException();
+			return new LegacyActivationDepth(_depth -1 , _mode);
 		}
 		return new LegacyActivationDepth(descendDepth(metadata), _mode);
 	}
