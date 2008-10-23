e128366c8bd49149c88ba9b4af7ffd7eb1e1a4cb
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2008-10-23 13:09:21 +0000

    Moved cascadeOnUpdate setting from #test() to #configure() to precede new call to #checkAllClassChanges() in ObjectServerImpl#<init>().

--- db4oj.tests/src/com/db4o/db4ounit/common/ext/RefreshTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/ext/RefreshTestCase.java
@@ -3,6 +3,7 @@
 package com.db4o.db4ounit.common.ext;
 
 import com.db4o.*;
+import com.db4o.config.*;
 import com.db4o.ext.*;
 import com.db4o.query.*;
 
@@ -12,26 +13,28 @@ import db4ounit.extensions.*;
 public class RefreshTestCase extends Db4oClientServerTestCase {
 
 	public static void main(String[] args) {
-		new RefreshTestCase().runClientServer();
+		new RefreshTestCase().runAll();
 	}
 
-	public String name;
+	public static class Item {
+		public String name;
+		public Item child;
 
-	public RefreshTestCase child;
-
-	public RefreshTestCase() {
-
+		public Item(String name, Item child) {
+			this.name = name;
+			this.child = child;
+		}
 	}
-
-	public RefreshTestCase(String name, RefreshTestCase child) {
-		this.name = name;
-		this.child = child;
+	
+	@Override
+	protected void configure(Configuration config) throws Exception {
+		config.objectClass(Item.class).cascadeOnUpdate(true);
 	}
 
 	protected void store() {
-		RefreshTestCase r3 = new RefreshTestCase("o3", null);
-		RefreshTestCase r2 = new RefreshTestCase("o2", r3);
-		RefreshTestCase r1 = new RefreshTestCase("o1", r2);
+		Item r3 = new Item("o3", null);
+		Item r2 = new Item("o2", r3);
+		Item r1 = new Item("o1", r2);
 		store(r1);
 	}
 
@@ -40,15 +43,8 @@ public class RefreshTestCase extends Db4oClientServerTestCase {
 		ExtObjectContainer oc1 = openNewClient();
 		ExtObjectContainer oc2 = openNewClient();
 		
-		// FIXME: Setting cascadeOnUpdate on an open ObjectContainer only works
-		//        by accident, if the class has not been used before.
-		//        Moving the configuration method here gets MTOC to pass, but
-		//        configuration methods should really tell the users direclty 
-		//        what works and what doesn't.
-		oc2.configure().objectClass(RefreshTestCase.class).cascadeOnUpdate(true);
-		
 		try {
-			RefreshTestCase r1 = getRoot(oc1);
+			Item r1 = getRoot(oc1);
 			r1.name = "cc";
 			oc1.refresh(r1, 0);
 			Assert.areEqual("cc", r1.name);
@@ -60,17 +56,14 @@ public class RefreshTestCase extends Db4oClientServerTestCase {
 			oc1.refresh(r1, 2);
 			Assert.areEqual("o2", r1.child.name);
 
-			RefreshTestCase r2 = getRoot(oc2);
+			Item r2 = getRoot(oc2);
 			r2.name = "o21";
 			r2.child.name = "o22";
 			r2.child.child.name = "o23";
 			oc2.store(r2);
 			oc2.commit();
 
-			// the next line is failing
 			oc1.refresh(r1, 3);
-			// but the following works
-			// r1 = getByName(oc1, "o21");
 			Assert.areEqual("o21", r1.name);
 			Assert.areEqual("o22", r1.child.name);
 			Assert.areEqual("o23", r1.child.child.name);
@@ -81,16 +74,16 @@ public class RefreshTestCase extends Db4oClientServerTestCase {
 		}
 	}
 
-	private RefreshTestCase getRoot(ObjectContainer oc) {
+	private Item getRoot(ObjectContainer oc) {
 		return getByName(oc, "o1");
 	}
 
-	private RefreshTestCase getByName(ObjectContainer oc, final String name) {
+	private Item getByName(ObjectContainer oc, final String name) {
 		Query q = oc.query();
-		q.constrain(RefreshTestCase.class);
+		q.constrain(Item.class);
 		q.descend("name").constrain(name);
 		ObjectSet objectSet = q.execute();
-		return (RefreshTestCase) objectSet.next();
+		return (Item) objectSet.next();
 	}
 
 }
