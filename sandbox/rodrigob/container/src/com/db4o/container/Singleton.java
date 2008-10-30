7a5352eccfe026547b48daf498e41136bbeab2a5
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-10-30 19:25:44 +0000

    using ASM to get instantiation overhead down to a minimum (only 3.8% now for 2.000.000 operations)

--- sandbox/rodrigob/container/src/com/db4o/container/Singleton.java
+++ sandbox/rodrigob/container/src/com/db4o/container/Singleton.java
@@ -2,6 +2,9 @@ package com.db4o.container;
 
 /**
  * Marker interface for singleton components.
+ * 
+ * A singleton component is instantiated only once
+ * by the container and shared among all clients.
  */
 public interface Singleton {
 
