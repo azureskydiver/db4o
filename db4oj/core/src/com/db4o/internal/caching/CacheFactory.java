571d344edb5be544d5e973dd74f8a24f840adfee
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-11-16 18:21:15 +0000

    COR-1457 - LRUCache for more straightforward comparison. IoAdapterWithCache yields the same performance as CachedIoAdapter when using the LRUCache.

--- db4oj/core/src/com/db4o/internal/caching/CacheFactory.java
+++ db4oj/core/src/com/db4o/internal/caching/CacheFactory.java
@@ -12,5 +12,9 @@ public class CacheFactory {
 	public static <K, V> Cache4<K, V> new2QCache(int size) {
 		return new LRU2QCache<K, V>(size);
 	}
+	
+	public static <K, V> Cache4<K, V> newLRUCache(int size) {
+		return new LRUCache<K, V>(size);
+	}
 
 }
