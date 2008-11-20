a4818ebf31b31ce160f02f1300a0a0557dd9e786
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-11-20 18:42:40 +0000

    cr + acv: #COR-1303 Modified version of SlotCachePerformanceTest

--- db4oj.tests/src/com/db4o/test/performance/SlotCachePerformanceTest.java
+++ db4oj.tests/src/com/db4o/test/performance/SlotCachePerformanceTest.java
@@ -1,10 +1,9 @@
 package com.db4o.test.performance;
 
-import java.io.*;
-
 import com.db4o.*;
 import com.db4o.config.*;
 import com.db4o.foundation.io.*;
+import com.db4o.internal.*;
 import com.db4o.io.*;
 import com.db4o.query.*;
 
@@ -17,11 +16,9 @@ public class SlotCachePerformanceTest {
 	
 	private static final int BENCHMARKS = 5;
 
-	private static final int STORED_ITEMS = 50000;
+	private static final int STORED_ITEMS = 30000;
 	
 	private static final int QUERY_ITERATIONS = 100;
-	
-	private static final int GET_ALL_ITERATIONS = 20000;
 	
 	private static final int QUERY_VALUES = 10;
 	
@@ -41,7 +38,6 @@ public class SlotCachePerformanceTest {
 		double totalRatio = 0;
 		for (int i=0; i<BENCHMARKS; ++i) {
 			
-			
 			final long t0 = test0.run();
 			final long t1 = test1.run();
 			
@@ -60,7 +56,7 @@ public class SlotCachePerformanceTest {
 	private static SlotCachePerformanceTest benchmarkWithSlotCache() {
 		return new SlotCachePerformanceTest("WithSlotCache", randomAccessFileAdapter(), 30);
 	}
-
+	
 	private static void report(SlotCachePerformanceTest test1, final double ratio) {
 	    System.out.println(test1._name + " is " + (ratio > 1 ? "slower by " : "faster by ") + ((int)(((ratio > 1 ? ratio : (1 - ratio)) * 100) % 100)) + "%");
     }
@@ -99,16 +95,17 @@ public class SlotCachePerformanceTest {
 
 	private long run() {
 		
-		openFile();
+		openNewFile();
 		if(STORED_ITEMS > 0){
 			for (int i = 0; i < STORED_ITEMS; i++) {
 				_container.store(new Item(i));
 			}
 		}
+		closeFile();
+		openFile();
 		
 		try {
 			final long t0 = System.nanoTime();
-			getAllItems();
 			queryItems();
 			final long t1 = System.nanoTime();
 			final long elapsed = t1-t0;
@@ -118,23 +115,6 @@ public class SlotCachePerformanceTest {
 		} finally {
 			dispose();
 		}
-    }
-
-	private void getAllItems() {
-		for (int i = 0; i < GET_ALL_ITERATIONS; i++) {
-			Query q = _container.query();
-			q.constrain("Item.class");
-			ObjectSet<Object> objectSet = q.execute();
-			objectSet.ext().getIDs();
-			while(objectSet.hasNext()){
-				final Item found = (Item)objectSet.next();
-				Assert.isNotNull(found);
-			}
-		}
-	}
-
-	private void commit() {
-	    _container.commit();
     }
 	
 	private void closeFile(){
@@ -146,10 +126,15 @@ public class SlotCachePerformanceTest {
 	    File4.delete(_filename);
     }
 
-	private void openFile() {
+	private void openNewFile() {
 	    _filename = Path4.getTempFileName();
+		openFile();
+    }
+	
+	private void openFile() {
 		_container = Db4oEmbedded.openFile(configuration(), _filename);
     }
+
 
 	private EmbeddedConfiguration configuration() {
 	    final EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
