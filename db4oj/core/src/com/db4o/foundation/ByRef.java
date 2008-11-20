c382b6a3b92f94e7fada96744e1c9224f46cee80
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-11-20 13:51:31 +0000

    pr+rbo: factory methods for type inference

--- db4oj/core/src/com/db4o/foundation/ByRef.java
+++ db4oj/core/src/com/db4o/foundation/ByRef.java
@@ -7,7 +7,15 @@ package com.db4o.foundation;
  * Useful as "out" or "by reference" function parameter.
  */
 public class ByRef<T> {
-    
-    public T value;
 
+	public static <T> ByRef<T> newInstance(T initialValue) {
+		final ByRef<T> instance = new ByRef<T>();
+		instance.value = initialValue;
+		return instance;
+	}
+	
+	public static <T> ByRef<T> newInstance() {
+		return new ByRef<T>();
+	}    
+    public T value;
 }
