bef37463e84695b3fc6893d8e9abba0dd4b840e8
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2008-12-03 10:09:12 +0000

    cr: The .NET build apparently doesn't like references to CS in the core code. Removed to get the build to pass.

--- db4oj/core/src/com/db4o/Db4oEmbedded.java
+++ db4oj/core/src/com/db4o/Db4oEmbedded.java
@@ -9,7 +9,7 @@ import com.db4o.internal.config.*;
 /**
  * Factory class to open db4o instances in embedded mode.
  * 
- * <br><br>See the {@link com.db4o.cs.Db4oClientServer} class in
+ * <br><br>See the <code>com.db4o.cs.Db4oClientServer</code> class in
  * db4o-[version]-cs-java[java-version].jar/ Db4objects.Db4o.CS.dll
  * for methods to open db4o servers and db4o clients.
  * @since 7.5
