4457bc62254bbc353c3d140e9b9bf096df9ee955
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-12-18 19:36:33 +0000

    OptOutSolo is implied

--- db4ounit.extensions/src/db4ounit/extensions/fixtures/OptOutAllButNetworkingCS.java
+++ db4ounit.extensions/src/db4ounit/extensions/fixtures/OptOutAllButNetworkingCS.java
@@ -4,5 +4,5 @@ package db4ounit.extensions.fixtures;
  * Marker interface to denote that implementing test cases should be excluded
  * from running with any fixture but a networking C/S one.
  */
-public interface OptOutAllButNetworkingCS extends OptOutFromTestFixture {
+public interface OptOutAllButNetworkingCS extends OptOutSolo {
 }
