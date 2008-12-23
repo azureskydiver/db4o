5de19c2bc45f9f9e805ad73122cfeb0757f449e1
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2008-12-23 12:43:34 +0000

    acv + rbo: OMN-120 - Minor refactoring

--- enterprise/omn/OmControlLibrary/LoginToSalesForce/ProxyLogin.cs
+++ enterprise/omn/OmControlLibrary/LoginToSalesForce/ProxyLogin.cs
@@ -1,16 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
 using OManager.BusinessLayer.QueryManager;
 using OManager.BusinessLayer.Login;
 using System.Net;
 using OMControlLibrary.Common;
 using OME.Logging.Common;
-using OME.Logging.Tracing;
 
 namespace OMControlLibrary.LoginToSalesForce
 {
@@ -26,26 +20,26 @@ namespace OMControlLibrary.LoginToSalesForce
 				ProxyAuthentication proxy = dbInt.RetrieveProxyInfo();
 				if (proxy != null)
 				{
-					this.textBoxUserID.Text = proxy.UserName;
-					this.textBoxPassword.Focus();
-					this.textBoxPort.Text = proxy.Port;
-					this.textBoxProxy.Text = proxy.ProxyAddress;
-					this.textBoxPassword.Text = Helper.DecryptPass(proxy.PassWord);
+					textBoxUserID.Text = proxy.UserName;
+					textBoxPassword.Focus();
+					textBoxPort.Text = proxy.Port;
+					textBoxProxy.Text = proxy.ProxyAddress;
+					textBoxPassword.Text = Helper.DecryptPass(proxy.PassWord);
 				}
 				else
 				{
 					string domain = Environment.UserDomainName;
 					string username = Environment.UserName;
-					this.textBoxUserID.Text = domain + CONST_BACKSLASH + username;
+					textBoxUserID.Text = domain + CONST_BACKSLASH + username;
 					if (((WebProxy)GlobalProxySelection.Select).Address != null)
 					{
 						int colonIndex = ((WebProxy)GlobalProxySelection.Select).Address.ToString().LastIndexOf(':');
 						string proxystr = ((WebProxy)GlobalProxySelection.Select).Address.ToString().Substring(0, colonIndex);
 						string port = ((WebProxy)GlobalProxySelection.Select).Address.ToString().Substring(colonIndex + 1, ((WebProxy)GlobalProxySelection.Select).Address.ToString().Length - colonIndex - 1);
 						port.TrimEnd('/');
-						this.textBoxPassword.Text = string.Empty;
-						this.textBoxProxy.Text = proxystr;
-						this.textBoxPort.Text = port.Substring(0, 4);
+						textBoxPassword.Text = string.Empty;
+						textBoxProxy.Text = proxystr;
+						textBoxPort.Text = port.Substring(0, 4);
 					}
 				}
 			}
