cda1ec2b109cc13b89140e7030cd01adf9b01209
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2009-01-01 12:56:58 +0000

    explicit reflector for memory storage test in OSGi mode

--- db4oj.tests/src/com/db4o/db4ounit/common/io/MemoryBinIsReusableTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/io/MemoryBinIsReusableTestCase.java
@@ -3,6 +3,7 @@ package com.db4o.db4ounit.common.io;
 import com.db4o.*;
 import com.db4o.config.*;
 import com.db4o.io.*;
+import com.db4o.reflect.jdk.*;
 
 import db4ounit.*;
 
@@ -21,8 +22,7 @@ public class MemoryBinIsReusableTestCase implements TestCase {
 	
 	public void test() {
 		MemoryStorage origStorage = new MemoryStorage();
-		EmbeddedConfiguration origConfig = Db4oEmbedded.newConfiguration();
-		origConfig.file().storage(origStorage);
+		EmbeddedConfiguration origConfig = config(origStorage);
 		ObjectContainer origDb = Db4oEmbedded.openFile(origConfig, BIN_URI);
 		origDb.store(new Item(ITEM_NAME));
 		origDb.close();
@@ -34,13 +34,18 @@ public class MemoryBinIsReusableTestCase implements TestCase {
 		MemoryBin newBin = new MemoryBin(data);
 		MemoryStorage newStorage = new MemoryStorage();
 		newStorage.bin(BIN_URI, newBin);
-		EmbeddedConfiguration newConfig = Db4oEmbedded.newConfiguration();
-		newConfig.file().storage(newStorage);
-		ObjectContainer newDb = Db4oEmbedded.openFile(newConfig, BIN_URI);
+		ObjectContainer newDb = Db4oEmbedded.openFile(config(newStorage), BIN_URI);
 		ObjectSet<Item> result = newDb.query(Item.class);
 		Assert.areEqual(1, result.size());
 		Assert.areEqual(ITEM_NAME, result.next()._name);
 		newDb.close();
+	}
+
+	private EmbeddedConfiguration config(MemoryStorage storage) {
+		EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
+		config.file().storage(storage);
+		config.common().reflectWith(new JdkReflector(Item.class.getClassLoader()));
+		return config;
 	}
 	
 }
