7226426016a6e0abed13e2a36a490686c0f849f3
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2009-01-05 15:23:12 +0000

    acv+pr: COR-1483 - PowerShell blog

---
+++ sandbox/rodrigo/blogs/PowerShell/PSTest/PSTest/AddDb4oObjectCommand.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Management.Automation;
+using Db4objects.Db4o;
+
+namespace CmdLets.Db4objects
+{
+    [Cmdlet(VerbsCommon.Add , "db4o-object")]
+    public class AddDb4oObjectCommand : Db4oObjectCommandBase
+    {
+        protected override void ProcessRecord()
+        {
+            using (var container = Db4oEmbedded.OpenFile(Configure(), DatabasePath))
+            {
+                if (null != Item)
+                {
+                    container.Store(Item);
+
+                    // Name+Country+Age / Name == "Adriano" || Country == "Brazil"
+                    // 
+                    // class Name_Country_Syntethic_1
+                    // {
+                    //      string Name;
+                    //      string Country; 
+                    //      int Age;
+                    // }
+                    //
+                    // from Name_Country_Syntethic_1 obj in db()
+                    // where obj.Name == "Adriano" || obj.Country == "Brazil"
+                    // select obj;
+                }
+            }
+        }
+
+        [Parameter(Position = 0)]
+        public PSObject Item
+        {
+            get; set;
+        }
+    }
+
+    public class Wrapper
+    {
+        private DateTime _date;
+        private int _value;
+
+        public Wrapper(DateTime date)
+        {
+            _date = date;
+            _value = new Random(date.Millisecond).Next();
+        }
+
+        public int Value
+        {
+            get { return _value;}
+            set { _value = value;}
+        }
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+        public override string ToString()
+        {
+            return "Wrapper: " + _date;
+        }
+    }
+}
---
+++ sandbox/rodrigo/blogs/PowerShell/PSTest/PSTest/SelectDb4oObjectCommand.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Management.Automation;
+using Db4objects.Db4o;
+
+namespace CmdLets.Db4objects
+{
+    [Cmdlet(VerbsCommon.Select, "db4o-object")]
+    public class SelectDb4oObjectCommand : Db4oObjectCommandBase
+    {
+        protected override void ProcessRecord()
+        {
+            using (var container = Db4oEmbedded.OpenFile(Configure(), DatabasePath))
+            {
+                DumpResults(container.Query().Execute());
+            }
+        }
+
+        private void DumpResults(IEnumerable results)
+        {
+			//foreach (var result in results)
+			//{
+			//    if (Match((PSObject) result))
+			//    { 
+			//        WriteObject(result); 
+			//    }
+			//}
+			IEnumerator enumerator = results.GetEnumerator();
+			if (enumerator.MoveNext())
+			{
+				WriteObject(enumerator.Current);
+			}
+        }
+
+        private bool Match(PSObject o)
+        {
+            foreach (var property in o.Properties)
+            {
+                //if (property.Value )
+            }
+            return false;
+        }
+        
+		//[Parameter(Mandatory = true)]
+		//public string Value
+		//{
+		//    set
+		//    {
+		//        _value = value;
+		//    }
+		//}
+
+        private string _value;
+    }
+}
