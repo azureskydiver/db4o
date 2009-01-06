aa67c11cc87b1059add2d4b8fbb57df7e6b3901b
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2009-01-06 12:05:43 +0000

    cr + rbo: #COR-1494 InternalObjectContainer#storeAll() writes all #store() calls in one batch. TP

---
+++ db4ounit.extensions/src/db4ounit/extensions/ObjectSetAssert.java
@@ -0,0 +1,53 @@
+package db4ounit.extensions;
+
+import com.db4o.ObjectSet;
+import com.db4o.foundation.Iterator4;
+import com.db4o.foundation.Iterators;
+
+import db4ounit.Iterator4Assert;
+
+public class ObjectSetAssert {
+
+	public static void sameContent(ObjectSet objectSet, Object... expectedItems) {
+		Iterator4Assert.sameContent(Iterators.iterate(expectedItems), iterate(objectSet));
+	}
+
+	public static Iterator4 iterate(ObjectSet objectSet) {
+		return new ObjectSetIterator4(objectSet);
+	}
+	
+	static class ObjectSetIterator4 implements Iterator4{
+	    
+	    private static final Object INVALID = new Object();
+	    
+	    private ObjectSet _objectSet;
+	    
+	    private Object _current;
+	    
+	    public ObjectSetIterator4(ObjectSet collection) {
+	        _objectSet = collection;
+	    }
+
+	    public Object current() {
+	        if(_current == INVALID){
+	            throw new IllegalStateException();
+	        }
+	        return _current;
+	    }
+
+	    public boolean moveNext() {
+	        if(_objectSet.hasNext()){
+	            _current = _objectSet.next();
+	            return true;
+	        }
+	        _current = INVALID;
+	        return false;
+	    }
+
+	    public void reset() {
+	        _objectSet.reset();
+	        _current = INVALID; 
+	    }
+	}
+
+}
