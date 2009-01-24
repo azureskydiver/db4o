733ecf609a30dd4c30765ba9e35094787a48c897
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-01-24 12:47:50 +0000

    reverting to 12492

---
+++ db4obuild/src/WixBuilder/WixBuilder/InclusionPattern.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WixBuilder
+{
+	public class InclusionPattern
+	{
+		private readonly string _pattern;
+
+		public InclusionPattern(string pattern)
+		{
+			_pattern = pattern.Replace(@"\", @"\\").Replace(".", "\\.").Replace("*", @".*");
+		}
+
+		public bool Matches(IFile file)
+		{
+			return Regex.IsMatch(file.Path, _pattern);
+		}
+	}
+}
