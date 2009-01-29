5b3de59d95954dd85ec05baa56d6465c3b19d298
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-01-29 17:31:54 +0000

    cr+rbo: generic Predicate4

--- db4oj/core/src/com/db4o/foundation/Predicate4.java
+++ db4oj/core/src/com/db4o/foundation/Predicate4.java
@@ -5,6 +5,6 @@ package com.db4o.foundation;
 /**
  * @exclude
  */
-public interface Predicate4 {
-	public boolean match(Object candidate);
+public interface Predicate4<T> {
+	public boolean match(T candidate);
 }
