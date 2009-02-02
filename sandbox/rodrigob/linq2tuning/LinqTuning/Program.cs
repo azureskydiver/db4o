2855b497fd03a68dc7243e5e2f79184e44fbbeb3
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-02-02 13:19:11 +0000

    cr+rbo: COR-1437 - Cache4CachingStrategy adaptor; most caches now using CacheFactory.NewL2QXCache; still need to measure performance impact - so far we haven't seen any

---
+++ sandbox/rodrigob/linq2tuning/LinqTuning/Program.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+using Db4objects.Db4o;
+using Db4objects.Db4o.IO;
+using Db4objects.Db4o.Linq;
+using Db4oUnit;
+using Db4oUnit.Util;
+using LinqTuning.Reference1;
+using LinqTuning.Reference2;
+
+namespace LinqTuning
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Trace.Listeners.Add(new TextWriterTraceListener(System.Console.Out));
+
+			for (int i = 0; i < 3; ++i)
+			{
+				ReportTime("Query over single property on single assembly",
+				           container => from Item1 item in container where item.Id != -1 select item);
+
+				ReportTime("Query over two properties in different assemblies",
+				           container => from Item2 item in container where item.Id != -1 && item.Name == "foo" select item);
+
+				ReportTime("Query over two properties twice in different assemblies",
+						   container => from Item2 item in container
+										where item.Id != -1
+											&& item.Name == "foo"
+											&& item.Id != 42
+											&& item.Name != "bar"
+										select item);
+			}
+
+		}
+
+		private static void ReportTime<T>(string label, Func<IObjectContainer, IEnumerable<T>> query)
+		{
+			System.Console.WriteLine("{0}: {1}", label, Time(query));
+		}
+
+		static TimeSpan Time<T>(Func<IObjectContainer, IEnumerable<T>> query)
+		{
+			using (IObjectContainer container = OpenInMemoryContainer())
+			{
+				var stopWatch = new StopWatch();
+				stopWatch.Start();
+
+				for (int i = 0; i < 10000; ++i)
+					Assert.AreEqual(0, query(container).Count());
+
+				stopWatch.Stop();
+				return stopWatch.Elapsed();
+			}
+		}
+
+		private static IObjectContainer OpenInMemoryContainer()
+		{
+			var configuration = Db4oEmbedded.NewConfiguration();
+			configuration.File.Storage = new MemoryStorage();
+			return Db4oEmbedded.OpenFile(configuration, "");
+		}
+	}
+}
