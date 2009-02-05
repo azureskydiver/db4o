5d7521f8b8b97e16079ed799909125a433a63fe0
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2009-02-05 17:57:37 +0000

    cr + acv: #COR-1529 ActivatableArrayList extends ArrayList and provides TA and TP capabilities.

---
+++ db4oj/core/src/com/db4o/collections/ActivatableArrayList.java
@@ -0,0 +1,171 @@
+/* Copyright (C) 2009  db4objects Inc.  http://www.db4o.com */
+
+package com.db4o.collections;
+
+import java.util.*;
+
+import com.db4o.activation.*;
+import com.db4o.internal.*;
+import com.db4o.ta.*;
+
+/**
+ * extends ArrayList with Transparent Activation and
+ * Transparent Persistence support
+ * @exclude
+ * @sharpen.ignore
+ */
+@decaf.Remove(decaf.Platform.JDK11)
+public class ActivatableArrayList <E> extends ArrayList<E> implements Activatable {
+	
+	private Activator _activator;
+
+	public void activate(ActivationPurpose purpose) {
+		if(_activator != null) {
+			_activator.activate(purpose);
+		}
+	}
+
+	public void bind(Activator activator) {
+    	if (_activator == activator) {
+    		return;
+    	}
+    	if (activator != null && _activator != null) {
+            throw new IllegalStateException();
+        }
+		_activator = activator;
+	}
+	
+	@Override
+	public boolean add(E e) {
+		activate(ActivationPurpose.WRITE);
+		return super.add(e);
+	};
+	
+	public void add(int index, E element) {
+		activate(ActivationPurpose.WRITE);
+		super.add(index, element);
+	};
+	
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		activate(ActivationPurpose.WRITE);
+		return super.addAll(c);
+	}
+	
+	@Override
+	public boolean addAll(int index, Collection<? extends E> c) {
+		activate(ActivationPurpose.WRITE);
+		return super.addAll(index, c);
+	}
+	
+	@Override
+	public void clear() {
+		activate(ActivationPurpose.WRITE);
+		super.clear();
+	}
+	
+	@Override
+	public boolean contains(Object o) {
+		activate(ActivationPurpose.READ);
+		return super.contains(o);
+	}
+	
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		activate(ActivationPurpose.READ);
+		return super.containsAll(c);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		activate(ActivationPurpose.READ);
+		return super.equals(o);
+	}
+	
+	@Override
+	public E get(int index) {
+		activate(ActivationPurpose.READ);
+		return super.get(index);
+	}
+	
+	@Override
+	public int hashCode() {
+		activate(ActivationPurpose.READ);
+		return super.hashCode();
+	}
+	
+	@Override
+	public int indexOf(Object o) {
+		activate(ActivationPurpose.READ);
+		return super.indexOf(o);
+	}
+	
+	@Override
+	public Iterator<E> iterator() {
+		activate(ActivationPurpose.READ);
+		return super.iterator();
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		activate(ActivationPurpose.READ);
+		return super.isEmpty();
+	}
+	
+	@Override
+	public int lastIndexOf(Object o) {
+		activate(ActivationPurpose.READ);
+		return super.lastIndexOf(o);
+	}
+	
+	@Override
+	public ListIterator<E> listIterator() {
+		activate(ActivationPurpose.READ);
+		return super.listIterator();
+	}
+	
+	@Override
+	public ListIterator<E> listIterator(int index) {
+		activate(ActivationPurpose.READ);
+		return super.listIterator(index);
+	}
+	
+	@Override
+	public E remove(int index) {
+		activate(ActivationPurpose.WRITE);		
+		return super.remove(index);
+	}
+	
+	@Override
+	public boolean remove(Object o) {
+		activate(ActivationPurpose.WRITE);
+		return super.remove(o);
+	}
+	
+	@Override
+	public E set(int index, E element) {
+		activate(ActivationPurpose.WRITE);
+		return super.set(index, element);
+	};
+	
+	@Override
+	public int size() {
+		activate(ActivationPurpose.READ);
+		return super.size();
+	}
+	
+	@Override
+	public Object[] toArray() {
+		activate(ActivationPurpose.READ);
+		return super.toArray();
+	}
+	
+	@Override
+	public <T extends Object> T[] toArray(T[] a) {
+		activate(ActivationPurpose.READ);
+		return super.toArray(a);
+	};
+	
+	
+
+}
