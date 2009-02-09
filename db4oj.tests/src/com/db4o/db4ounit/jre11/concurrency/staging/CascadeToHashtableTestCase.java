30b1e11b2ec06557041e2a8ba314149a6c8a1a72
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-02-09 17:11:16 +0000

    pr+rbo: reverting test cases

--- db4oj.tests/src/com/db4o/db4ounit/jre11/concurrency/staging/CascadeToHashtableTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/jre11/concurrency/staging/CascadeToHashtableTestCase.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2004   db4objects Inc.   http://www.db4o.com */
 
-package com.db4o.db4ounit.jre11.concurrency;
+package com.db4o.db4ounit.jre11.concurrency.staging;
 
 import java.util.*;
 
