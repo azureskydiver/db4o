5dd8a4f87f8a992c74a3d67204d8c103cc88e158
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2009-02-13 14:43:04 +0000

    cr + pr: BigInteger and BigDecimal tests only run successfully on JDK 1.3+

--- db4oj.tests/src/com/db4o/db4ounit/jre12/types/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/jre12/types/AllTests.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2004 - 2006 db4objects Inc. http://www.db4o.com */
 
-package com.db4o.db4ounit.jre11.types;
+package com.db4o.db4ounit.jre12.types;
 
 import db4ounit.extensions.*;
 
@@ -12,8 +12,8 @@ public class AllTests extends Db4oTestSuite {
 
 	protected Class[] testCases() {
 		return new Class[] {
-				com.db4o.db4ounit.jre11.types.arrays.AllTests.class,
-				com.db4o.db4ounit.jre11.types.collections.AllTests.class,
+				BigDecimalTestCase.class,
+				BigIntegerTestCase.class,
 		};
 	}
 }
