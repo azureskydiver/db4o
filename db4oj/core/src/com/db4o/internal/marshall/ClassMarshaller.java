4f101dc4f1cea54b6a9ecfcbb99d7557014bd748
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2009-02-14 11:55:47 +0000

    cr/pr: COR-1541 - check #hasClassIndex() when defragmenting class metadata

--- db4oj/core/src/com/db4o/internal/marshall/ClassMarshaller.java
+++ db4oj/core/src/com/db4o/internal/marshall/ClassMarshaller.java
@@ -131,7 +131,7 @@ public abstract class ClassMarshaller {
 		// ancestor ID
 		context.copyID();
 
-		context.writeInt(indexIDForWriting(classIndexID));
+		context.writeInt((classMetadata.hasClassIndex() ? indexIDForWriting(classIndexID) : 0));
 		
 		final int aspectCount = context.readInt();
 		
