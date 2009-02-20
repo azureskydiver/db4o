544df317c834525592c378bee0ba9fd1351d547f
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2009-02-20 12:32:25 +0000

    cr: Removing extra comma that fails the build

--- db4oj.tests/src/com/db4o/db4ounit/common/handlers/framework/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/handlers/framework/AllTests.java
@@ -7,7 +7,7 @@ public class AllTests extends Db4oTestSuite {
 	@Override
 	protected Class[] testCases() {
 		return new Class[] {
-			FirstClassHandlerSemanticsTestCase.class,
+			FirstClassHandlerSemanticsTestCase.class
 		};
 	}
 
