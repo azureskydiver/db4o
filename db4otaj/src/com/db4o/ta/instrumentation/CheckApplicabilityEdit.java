452e6da8bda953b9a7dae57c998b21544daafc05
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2009-02-25 14:01:07 +0000

    cr/pr: COR-1553 - don't exclude subclasses of supported collection classes from instrumentation

--- db4otaj/src/com/db4o/ta/instrumentation/CheckApplicabilityEdit.java
+++ db4otaj/src/com/db4o/ta/instrumentation/CheckApplicabilityEdit.java
@@ -2,6 +2,8 @@
 
 package com.db4o.ta.instrumentation;
 
+import java.util.*;
+
 import EDU.purdue.cs.bloat.editor.*;
 
 import com.db4o.instrumentation.core.*;
@@ -29,14 +31,26 @@ class CheckApplicabilityEdit implements BloatClassEdit {
 
 	private boolean isApplicableClass(Class clazz) {
 		Class curClazz = clazz;
-		// FIXME string reference to Enum is rather fragile
-		while (curClazz != Object.class && curClazz != null && !curClazz.getName().equals("java.lang.Enum")) {
+		while (curClazz != Object.class && curClazz != null && !isApplicablePlatformClass(curClazz)) {
 			if (BloatUtil.isPlatformClassName(curClazz.getName())) {
 				return false;
 			}
 			curClazz = curClazz.getSuperclass();
 		}
 		return true;
+	}
+
+	private boolean isApplicablePlatformClass(Class curClazz) {
+		return isEnum(curClazz) || isSupportedCollection(curClazz);
+	}
+
+	private boolean isSupportedCollection(Class curClazz) {
+		return curClazz == ArrayList.class;
+	}
+
+	private boolean isEnum(Class curClazz) {
+		// FIXME string reference to Enum is rather fragile
+		return curClazz.getName().equals("java.lang.Enum");
 	}
 
 	
