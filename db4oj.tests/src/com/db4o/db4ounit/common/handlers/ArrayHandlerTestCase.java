aaad736806533e8d5dddba065d961cbc255d4cf7
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-02-26 17:27:27 +0000

    acv+rbo: COR-1554 - cannot reproduce it

--- db4oj.tests/src/com/db4o/db4ounit/common/handlers/ArrayHandlerTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/handlers/ArrayHandlerTestCase.java
@@ -10,17 +10,50 @@ import com.db4o.typehandlers.*;
 import db4ounit.*;
 import db4ounit.extensions.*;
 
-
 public class ArrayHandlerTestCase extends AbstractDb4oTestCase {
     
-    public static void main(String[] args) {
-        new ArrayHandlerTestCase().runSolo();
+    public static class FloatArrayHolder {
+    	public float[] _floats;
+    	public float[][] _jaggedFloats;
+    	public Float[][] _jaggedFloatWrappers;
+    	
+    	public FloatArrayHolder(float... floats) {
+    		_floats = floats;
+    		_jaggedFloats = new float[][] { floats };
+    		_jaggedFloatWrappers = new Float[][] { wrap(floats) };
+    	}
+    	
+    	public static Float[] wrap(float[] floats) {
+    		final Float[] wrappers = new Float[floats.length];
+    		for (int i=0; i<floats.length; ++i) {
+	            wrappers[i] = floats[i];
+            }
+			return wrappers;
+        }
+
+		public float[] floats() {
+    		return _floats;
+    	}
+    	
+    	public float[] jaggedFloats() {
+    		return _jaggedFloats[0];
+    	}
+    	
+    	public Float[] jaggedWrappers() {
+    		return _jaggedFloatWrappers[0];
+    	}
     }
     
     public static class IntArrayHolder{
         public int[] _ints;
+        public int[][] _jaggedInts;
         public IntArrayHolder(int[] ints){
             _ints = ints;
+            _jaggedInts = new int[][] { _ints };
+        } 
+        
+        public int[] jaggedInts() {
+        	return _jaggedInts[0];
         }
     }
     
@@ -31,22 +64,39 @@ public class ArrayHandlerTestCase extends AbstractDb4oTestCase {
         }
     }
     
-    private ArrayHandler intArrayHandler(){
-        return arrayHandler(int.class, true);
-    }
-
-    private ArrayHandler stringArrayHandler(){
-        return arrayHandler(String.class, false);
+    public static void main(String[] args) {
+        new ArrayHandlerTestCase().runSolo();
     }
     
-    private ArrayHandler arrayHandler(Class clazz, boolean isPrimitive) {
-        ClassMetadata classMetadata = container().produceClassMetadata(reflector().forClass(clazz));
-        return new ArrayHandler(classMetadata.typeHandler(), isPrimitive);
+    public void testFloatArrayRoundtrip() throws Exception {
+    	final float[] expected = new float[] { Float.MIN_VALUE, Float.MIN_VALUE + 1, 0.0f, Float.MAX_VALUE - 1, Float.MAX_VALUE};
+		store(new FloatArrayHolder(expected));
+		reopen();
+		
+		final FloatArrayHolder stored = retrieveOnlyInstance(FloatArrayHolder.class);
+		ArrayAssert.areEqual(expected, stored.jaggedFloats());
+		ArrayAssert.areEqual(expected, stored.floats());
+		ArrayAssert.areEqual(FloatArrayHolder.wrap(expected), stored.jaggedWrappers());
     }
     
     public void testCanHold() {
     	Assert.isTrue(intArrayHandler().canHold(reflectClass(int.class)));
     	Assert.isFalse(intArrayHandler().canHold(reflectClass(long.class)));
+    }
+
+    public void testHandlerVersion(){
+        IntArrayHolder intArrayHolder = new IntArrayHolder(new int[0]);
+        store(intArrayHolder);
+        ReflectClass claxx = reflector().forObject(intArrayHolder);
+        ClassMetadata classMetadata = (ClassMetadata) container().typeHandlerForReflectClass(claxx);
+        FieldMetadata fieldMetadata = classMetadata.fieldMetadataForName("_ints");
+        TypeHandler4 arrayHandler = fieldMetadata.getHandler();
+        Assert.isInstanceOf(ArrayHandler.class, arrayHandler);
+        assertCorrectedHandlerVersion(arrayHandler, 0, ArrayHandler0.class);
+        assertCorrectedHandlerVersion(arrayHandler, 1, ArrayHandler1.class);
+        assertCorrectedHandlerVersion(arrayHandler, 2, ArrayHandler3.class);
+        assertCorrectedHandlerVersion(arrayHandler, 3, ArrayHandler3.class);
+        assertCorrectedHandlerVersion(arrayHandler, HandlerRegistry.HANDLER_VERSION, ArrayHandler.class);
     }
     
     public void testIntArrayReadWrite() {
@@ -57,7 +107,7 @@ public class ArrayHandlerTestCase extends AbstractDb4oTestCase {
         int[] actual = (int[]) intArrayHandler().read(readContext);
         ArrayAssert.areEqual(expected, actual);
     }
-
+    
     public void testIntArrayStoreObject() throws Exception{
         IntArrayHolder expectedItem = new IntArrayHolder(new int[] {1, 2, 3});
         db().store(expectedItem);
@@ -65,6 +115,7 @@ public class ArrayHandlerTestCase extends AbstractDb4oTestCase {
         IntArrayHolder readItem = (IntArrayHolder) retrieveOnlyInstance(IntArrayHolder.class);
         Assert.areNotSame(expectedItem, readItem);
         ArrayAssert.areEqual(expectedItem._ints, readItem._ints);
+        ArrayAssert.areEqual(expectedItem._ints, readItem.jaggedInts());
     }
     
     public void testStringArrayReadWrite() {
@@ -85,24 +136,22 @@ public class ArrayHandlerTestCase extends AbstractDb4oTestCase {
         ArrayAssert.areEqual(expectedItem._strings, readItem._strings);
     }
     
-    public void testHandlerVersion(){
-        IntArrayHolder intArrayHolder = new IntArrayHolder(new int[0]);
-        store(intArrayHolder);
-        ReflectClass claxx = reflector().forObject(intArrayHolder);
-        ClassMetadata classMetadata = (ClassMetadata) container().typeHandlerForReflectClass(claxx);
-        FieldMetadata fieldMetadata = classMetadata.fieldMetadataForName("_ints");
-        TypeHandler4 arrayHandler = fieldMetadata.getHandler();
-        Assert.isInstanceOf(ArrayHandler.class, arrayHandler);
-        assertCorrectedHandlerVersion(arrayHandler, 0, ArrayHandler0.class);
-        assertCorrectedHandlerVersion(arrayHandler, 1, ArrayHandler1.class);
-        assertCorrectedHandlerVersion(arrayHandler, 2, ArrayHandler3.class);
-        assertCorrectedHandlerVersion(arrayHandler, 3, ArrayHandler3.class);
-        assertCorrectedHandlerVersion(arrayHandler, HandlerRegistry.HANDLER_VERSION, ArrayHandler.class);
+    private ArrayHandler arrayHandler(Class clazz, boolean isPrimitive) {
+        ClassMetadata classMetadata = container().produceClassMetadata(reflector().forClass(clazz));
+        return new ArrayHandler(classMetadata.typeHandler(), isPrimitive);
     }
-    
+
     private void assertCorrectedHandlerVersion(TypeHandler4 arrayHandler, int version, Class handlerClass) {
         TypeHandler4 correctedHandlerVersion = container().handlers().correctHandlerVersion(arrayHandler, version);
         Assert.isInstanceOf(handlerClass, correctedHandlerVersion);
+    }
+    
+    private ArrayHandler intArrayHandler(){
+        return arrayHandler(int.class, true);
+    }
+    
+    private ArrayHandler stringArrayHandler(){
+        return arrayHandler(String.class, false);
     }
 
 }
