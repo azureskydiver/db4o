58435cc8a176d61d15cda96e82cbb5d2a847f5b5
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-03-03 16:02:05 +0000

    acv+rbo: COR-1514 - test case for TreeSet type handler

---
+++ db4oj/core/src/com/db4o/typehandlers/internal/TreeSetTypeHandler.java
@@ -0,0 +1,14 @@
+package com.db4o.typehandlers.internal;
+
+import com.db4o.marshall.*;
+import com.db4o.typehandlers.*;
+
+@decaf.Ignore(decaf.Platform.JDK11)
+public class TreeSetTypeHandler extends CollectionTypeHandler {
+	
+	@Override
+	public Object read(ReadContext context) {
+	    // TODO Auto-generated method stub
+	    return super.read(context);
+	}
+}
