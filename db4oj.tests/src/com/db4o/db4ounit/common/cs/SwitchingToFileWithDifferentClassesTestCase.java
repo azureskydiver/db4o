422400184403e3b906a268dc8e420a18730f1f57
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-03-05 17:58:15 +0000

    pr+rbo: COR-1514 - Simplifying ClassMetadataRepository: Configuration#refreshClasses is no more and ClientObjectContainer#readThis no longer calls ClassMetadataRepository#refreshClasses

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/cs/SwitchingToFileWithDifferentClassesTestCase.java
@@ -0,0 +1,63 @@
+package com.db4o.db4ounit.common.cs;
+
+import com.db4o.config.*;
+import com.db4o.internal.cs.*;
+
+import db4ounit.extensions.*;
+
+public class SwitchingToFileWithDifferentClassesTestCase extends StandaloneCSTestCaseBase {
+
+	public static class Data1 {
+		public int _id;
+
+		public Data1(int id) {
+			this._id = id;
+		}
+
+		@Override
+        public boolean equals(Object obj) {
+	        if (this == obj)
+		        return true;
+	        if (obj == null)
+		        return false;
+	        if (getClass() != obj.getClass())
+		        return false;
+	        Data1 other = (Data1) obj;
+	        if (_id != other._id)
+		        return false;
+	        return true;
+        }
+	}
+	
+	public static class Data2 extends Data1 {
+		public Data2(int id) {
+	        super(id);
+        }
+	}
+	
+	@Override
+	protected void configure(Configuration config) {	}
+
+	@Override
+	protected void runTest() throws Throwable {
+		
+		ClientObjectContainer clientA = openClient();
+		clientA.store(new Data1(1));
+		
+		ClientObjectContainer clientB = openClient();
+		clientB.store(new Data1(2));
+		clientB.commit();
+		
+		clientB.switchToFile(SwitchingFilesFromClientUtil.FILENAME_B);
+		
+		final Data2 data2 = new Data2(3);
+		clientA.store(data2);
+		clientA.commit();
+		
+		clientB.switchToMainFile();
+		
+		ObjectSetAssert.sameContent(clientB.query(Data2.class), data2);
+		
+	}
+
+}
