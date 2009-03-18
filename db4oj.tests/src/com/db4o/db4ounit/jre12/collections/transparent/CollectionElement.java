61f58643117606665dd32da180b241c06e7720e8
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2009-03-18 17:48:48 +0000

    cr/pr: COR-1572 - initial activatable map test case

---
+++ db4oj.tests/src/com/db4o/db4ounit/jre12/collections/transparent/CollectionElement.java
@@ -0,0 +1,11 @@
+/* Copyright (C) 2009  db4objects Inc.   http://www.db4o.com */
+
+package com.db4o.db4ounit.jre12.collections.transparent;
+
+/**
+ * @sharpen.remove
+ */
+@decaf.Remove(decaf.Platform.JDK11)
+public interface CollectionElement{
+	
+}
