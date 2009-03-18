653b5ff3c183d523a8f2e461fafc6d4ae16e5f71
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2009-03-18 00:48:39 +0000

    acv: INT-77: NullableTest and Application to allow a piece of code to be run against specific db4o version.

---
+++ sandbox/Adriano/Blogs/Db4oTestRunner/AbstractDb4oTesterBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Db4oTestRunner
+{
+	public abstract class AbstractDb4oTesterBase : MarshalByRefObject, ITestRunner
+	{
+		void ITestRunner.Run(ILogger logger)
+		{
+			_logger = logger;
+			Run();
+		}
+
+		protected abstract void Run();
+
+		protected string LogText()
+		{
+			return _log.ToString();
+		}
+
+		protected string TempFilePath()
+		{
+			if (_filePath == null)
+			{
+				_filePath = Path.GetTempFileName();
+			}
+
+			return _filePath;
+		}
+
+		protected ILogger _logger;
+		private string _filePath;
+		private readonly StringBuilder _log = new StringBuilder();
+	}
+}
---
+++ sandbox/Adriano/Blogs/Db4oTestRunner/RichTextBoxLogger.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Db4oTestRunner
+{
+	class RichTextBoxLogger : MarshalByRefObject, ILogger
+	{
+		public static ILogger For(RichTextBox target)
+		{
+			return new RichTextBoxLogger(target);
+		}
+
+		public void LogMessage(string message, params object[] args)
+		{
+			AppendText(message, args);
+		}
+
+		public void LogMessageFormated(Color foreground, Font font, string message, params object[] args)
+		{
+			AppendSelected(
+					text =>
+						{
+							text.SelectionColor = foreground;
+							text.SelectionFont = font;
+						},
+					message,
+					args);
+		}
+
+		public void LogError(string message, params object[] args)
+		{
+			AppendSelected(
+					text => text.SelectionColor = Color.Red,
+					message,
+					args);
+		}
+
+		public void LogException(Exception ex)
+		{
+			AppendSelected(
+				text =>
+					{
+						text.SelectionColor = Color.Red;
+						text.SelectionFont = new Font("Tahoma", 9, FontStyle.Bold);
+					}, 
+				ex.ToString());
+		}
+
+		private RichTextBoxLogger(RichTextBox target)
+		{
+			_target = target;
+		}
+
+		private void AppendSelected(Action<RichTextBox> action, string message, params object[] args)
+		{
+			try
+			{
+				int startIndex = _target.Text.Length;
+				AppendText(message, args);
+
+				_target.SelectionStart = startIndex;
+				_target.SelectionLength = _target.Text.Length - startIndex;
+
+				action(_target);
+			}
+			finally
+			{
+				_target.SelectionStart = _target.Text.Length-1;
+				_target.SelectionLength = 1;
+			}
+		}
+
+		private void AppendText(string message, object[] args)
+		{
+			_target.AppendText(string.Format(message, args) + "\r\n");
+		}
+
+		private readonly RichTextBox _target;
+	}
+}
