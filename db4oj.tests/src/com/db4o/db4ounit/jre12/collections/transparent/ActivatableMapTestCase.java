403217352321dd8b0c949bd3263558c3e410dd53
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2009-03-19 15:03:50 +0000

    cr/pr: COR-1572 - more test coverage for HashMap public API

--- db4oj.tests/src/com/db4o/db4ounit/jre12/collections/transparent/ActivatableMapTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/jre12/collections/transparent/ActivatableMapTestCase.java
@@ -4,6 +4,7 @@ package com.db4o.db4ounit.jre12.collections.transparent;
 
 import java.util.*;
 
+import com.db4o.activation.*;
 import com.db4o.collections.*;
 import com.db4o.config.*;
 import com.db4o.db4ounit.jre12.collections.*;
@@ -49,6 +50,10 @@ public class ActivatableMapTestCase extends AbstractDb4oTestCase {
 		assertEqualContent(createMap(), singleMap());
 	}
 
+	public void testMapIsNotActivated(){
+		Assert.isFalse(db().isActive(singleMap()));
+	}
+	
 	public void testActivatableElementsAreNotActivated() {
 		long[] ids = ActivatableCollectionTestUtil.allActivatableElementIds(db());
 		for (long id : ids) {
@@ -59,7 +64,118 @@ public class ActivatableMapTestCase extends AbstractDb4oTestCase {
 	public void testClear() throws Exception {
 		singleMap().clear();
 		reopen();
-		assertEqualContent(new HashMap<CollectionElement,CollectionElement>(), singleMap());
+		assertEmpty();
+	}
+
+	public void testClone() throws Exception{
+		ActivatableHashMap cloned = (ActivatableHashMap) ((HashMap)singleMap()).clone();
+		// assert that activator is null - should throw IllegalStateException if it isn't
+		cloned.bind(new Activator() {
+			public void activate(ActivationPurpose purpose) {
+			}
+		});
+		assertEqualContent(createMap(), cloned);
+	}
+	
+	public void testContainsKey() {
+		Map<CollectionElement, CollectionElement> actual = singleMap();
+		for (CollectionElement expectedKey : createMap().keySet()) {
+			Assert.isTrue(actual.containsKey(expectedKey));
+		}
+	}
+	
+	public void testContainsValue() {
+		Map<CollectionElement, CollectionElement> actual = singleMap();
+		for (CollectionElement expectedValue : createMap().values()) {
+			Assert.isTrue(actual.containsValue(expectedValue));
+		}
+	}
+	
+	public void testEntrySet() {
+		IteratorAssert.areEqual(createMap().entrySet().iterator(), singleMap().entrySet().iterator());
+	}
+	
+	public void testGet() {
+		assertEqualContent(createMap(), singleMap());
+	}
+	
+	public void testIsEmpty() {
+		Assert.isFalse(singleMap().isEmpty());
+	}
+	
+	public void testKeySet() {
+		IteratorAssert.areEqual(createMap().keySet().iterator(), singleMap().keySet().iterator());
+	}
+	
+	public void testPut() throws Exception {
+		Map<CollectionElement, CollectionElement> map = singleMap();
+		Element value = new Element("added value");
+		Element key = new Element("added key");
+		map.put(key, value);
+		reopen();
+		Assert.areEqual(value, singleMap().get(key));
+	}
+	
+	public void testPutAll() throws Exception {
+		Map<CollectionElement, CollectionElement> map = singleMap();
+		Map<CollectionElement, CollectionElement> added = new HashMap<CollectionElement, CollectionElement>();
+		added.put(new Element("added key 1"), new Element("added value 1"));
+		added.put(new Element("added key 2"), new Element("added value 2"));
+		map.putAll(added);
+		reopen();
+		Map<CollectionElement, CollectionElement> expected = createMap();
+		expected.putAll(added);
+		assertEqualContent(expected, singleMap());
+	}
+	
+	public void testRemove() throws Exception {
+		Map<CollectionElement, CollectionElement> map = singleMap();
+		for (CollectionElement key : createMap().keySet()) {
+			map.remove(key);
+		}
+		reopen();
+		assertEmpty();
+	}
+	
+	public void testSize() {
+		Assert.areEqual(createMap().size(), singleMap().size());
+	}
+	
+	public void testValues() {
+		IteratorAssert.areEqual(createMap().values().iterator(), singleMap().values().iterator());
+	}
+	
+	public void testEquals() {
+		Map<CollectionElement, CollectionElement> expected = createMap();
+		Map<CollectionElement, CollectionElement> map = singleMap();
+		Assert.isTrue(map.equals(expected));
+	}
+	
+	public void testHashCode() {
+		Map<CollectionElement, CollectionElement> expected = createMap();
+		Map<CollectionElement, CollectionElement> map = singleMap();
+		Assert.areEqual(expected.hashCode(), map.hashCode());
+	}
+
+	public void testKeySetIteratorRemove() throws Exception {
+		for (Iterator iter = singleMap().keySet().iterator(); iter.hasNext();) {
+			iter.next();
+			iter.remove();
+		}
+		reopen();
+		Assert.isTrue(singleMap().isEmpty());
+	}
+	
+	public void testRepeatedPut() throws Exception {
+		Element key1 = new Element("added key 1");
+		Element key2 = new Element("added key 2");
+		singleMap().put(key1, new Element("added value 1"));
+		db().purge();
+		singleMap().put(key2, new Element("added value 2"));
+		reopen();
+		Map<CollectionElement,CollectionElement> retrieved = singleMap();
+		Assert.isTrue(retrieved.containsKey(key1));
+		Assert.isTrue(retrieved.containsKey(key2));
 	}
 	
 	private void assertEqualContent(
@@ -70,11 +186,11 @@ public class ActivatableMapTestCase extends AbstractDb4oTestCase {
 			Assert.areEqual(expected.get(key), actual.get(key));
 		}
 	}
-	
-	public void testMapIsNotActivated(){
-		Assert.isFalse(db().isActive(singleMap()));
+
+	private void assertEmpty() {
+		assertEqualContent(new HashMap<CollectionElement,CollectionElement>(), singleMap());
 	}
-	
+
 	private Map<CollectionElement,CollectionElement> createMap() {
 		ActivatableHashMap<CollectionElement, CollectionElement> map = new ActivatableHashMap<CollectionElement, CollectionElement>();
 		map.put(new Element("plain/plain key"), new Element("plain/plain value"));
--- db4oj/core/src/com/db4o/collections/ActivatableHashMap.java
+++ db4oj/core/src/com/db4o/collections/ActivatableHashMap.java
@@ -15,7 +15,7 @@ import com.db4o.ta.*;
 @decaf.Remove(decaf.Platform.JDK11)
 public class ActivatableHashMap<K,V> extends HashMap<K,V> implements Activatable {
 
-	private Activator _activator;
+	private transient Activator _activator;
 
 	public ActivatableHashMap() {
 	}
@@ -55,14 +55,97 @@ public class ActivatableHashMap<K,V> extends HashMap<K,V> implements Activatable
 	}
 	
 	@Override
+	public Object clone() {
+		activate(ActivationPurpose.READ);
+		return super.clone();
+	}
+	
+	@Override
+	public boolean containsKey(Object key) {
+		activate(ActivationPurpose.READ);
+		return super.containsKey(key);
+	}
+	
+	@Override
+	public boolean containsValue(Object value) {
+		activate(ActivationPurpose.READ);
+		return super.containsValue(value);
+	}
+
+	@Override
+	public Set<java.util.Map.Entry<K, V>> entrySet() {
+		activate(ActivationPurpose.READ);
+		return super.entrySet();
+	}
+	
+	@Override
 	public V get(Object key) {
 		activate(ActivationPurpose.READ);
 		return super.get(key);
 	}
 
 	@Override
+	public boolean isEmpty() {
+		activate(ActivationPurpose.READ);
+		return super.isEmpty();
+	}
+	
+	/**
+	 * This method directly returns the set as provided by the super class.
+	 * It relies on all modfications going through the public interface of the HashMap
+	 * itself. If this is not the case, updates will get lost.
+	 */
+	@Override
 	public Set<K> keySet() {
 		activate(ActivationPurpose.READ);
 		return super.keySet();
 	}
+	
+	@Override
+	public V put(K key, V value) {
+		activate(ActivationPurpose.WRITE);
+		return super.put(key, value);
+	};
+	
+	@Override
+	public void putAll(Map<? extends K, ? extends V> m) {
+		activate(ActivationPurpose.WRITE);
+		super.putAll(m);
+	}
+	
+	@Override
+	public V remove(Object key) {
+		activate(ActivationPurpose.WRITE);
+		return super.remove(key);
+	}
+	
+	@Override
+	public int size() {
+		activate(ActivationPurpose.READ);
+		return super.size();
+	}
+	
+	/**
+	 * This method directly returns the collection as provided by the super class.
+	 * It relies on all modfications going through the public interface of the HashMap
+	 * itself. If this is not the case, updates will get lost.
+	 */
+	@Override
+	public Collection<V> values() {
+		activate(ActivationPurpose.READ);
+		return super.values();
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		activate(ActivationPurpose.READ);
+		return super.equals(o);
+	}
+	
+	@Override
+	public int hashCode() {
+		activate(ActivationPurpose.READ);
+		return super.hashCode();
+	}
+
 }
