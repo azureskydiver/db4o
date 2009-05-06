7746d72a5ba86393fff8326775744fd06b3ebc39
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-05-06 15:35:46 +0000

    pr/rbo: INT-88

--- db4oj.tests/src/com/db4o/db4ounit/common/fieldindex/NonIndexedFieldIndexItem.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/fieldindex/NonIndexedFieldIndexItem.java
@@ -1,4 +1,4 @@
-/* Copyright (C) 2006 db4objects Inc. http://www.db4o.com */
+/* Copyright (C) 2006 Versant Inc. http://www.db4o.com */
 
 package com.db4o.db4ounit.common.fieldindex;
 
--- db4oj/core/src/com/db4o/Db4oVersion.java
+++ db4oj/core/src/com/db4o/Db4oVersion.java
@@ -1,4 +1,4 @@
-/* Copyright (C) 2007   db4objects Inc.   http://www.db4o.com */
+/* Copyright (C) 2007   Versant Inc.   http://www.db4o.com */
 
 package com.db4o;
 
@@ -6,9 +6,9 @@ package com.db4o;
 * @exclude
 */
 public class Db4oVersion {
-    public static final String NAME = "7.9.86.12718";
+    public static final String NAME = "7.9.93.13058";
     public static final int MAJOR = 7;
     public static final int MINOR = 9;
-    public static final int ITERATION = 86;
-    public static final int REVISION = 12718;
+    public static final int ITERATION = 93;
+    public static final int REVISION = 13058;
 }
