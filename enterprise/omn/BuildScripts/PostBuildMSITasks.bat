13c161ea7382b32a9ebd40882ae804f2ffb9dccd
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-05-07 13:11:59 +0000

    cr/rbo: BLD-130 - error is happening during WiRunSQL.vbs - commenting out during investigation

--- enterprise/omn/BuildScripts/PostBuildMSITasks.bat
+++ enterprise/omn/BuildScripts/PostBuildMSITasks.bat
@@ -2,6 +2,6 @@
 @echo Processing: %1
 
 cscript.exe "%~dp0"\CustomAction_NoImpersonate.js "%1"
-cscript.exe "%~dp0"\WiRunSQL.vbs "%1" "INSERT INTO `Error` (`Error`, `Message`) VALUES (1001, 'Error [1]: [2]')"
+REM cscript.exe "%~dp0"\WiRunSQL.vbs "%1" "INSERT INTO `Error` (`Error`, `Message`) VALUES (1001, 'Error [1]: [2]')"
 cscript.exe "%~dp0"\RemoveBannerText.vbs "%1"
 cscript.exe "%~dp0"\BoldProductName.vbs "%1"
