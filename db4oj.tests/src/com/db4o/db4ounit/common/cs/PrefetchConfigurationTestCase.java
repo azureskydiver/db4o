863cd8400e461a22585ae742b4f6d225d4ec211a
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2009-05-11 14:47:44 +0000

    cr: PrefetchConfigurationTestCase shouldn't run in embedded CS

--- db4oj.tests/src/com/db4o/db4ounit/common/cs/PrefetchConfigurationTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/cs/PrefetchConfigurationTestCase.java
@@ -1,8 +1,9 @@
 package com.db4o.db4ounit.common.cs;
 
 import db4ounit.*;
+import db4ounit.extensions.fixtures.*;
 
-public class PrefetchConfigurationTestCase extends ClientServerTestCaseBase {
+public class PrefetchConfigurationTestCase extends ClientServerTestCaseBase implements OptOutAllButNetworkingCS{
 	
 	public void testDefaultPrefetchDepth() {
 		Assert.areEqual(0, client().config().prefetchDepth());
