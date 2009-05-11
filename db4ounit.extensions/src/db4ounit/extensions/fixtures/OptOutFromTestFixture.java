67320c5f3dd3b33e744287a50fc596c70a94e02b
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-05-11 13:54:50 +0000

    OptOut implies Db4oTestCase

--- db4ounit.extensions/src/db4ounit/extensions/fixtures/OptOutFromTestFixture.java
+++ db4ounit.extensions/src/db4ounit/extensions/fixtures/OptOutFromTestFixture.java
@@ -2,11 +2,13 @@
 
 package db4ounit.extensions.fixtures;
 
+import db4ounit.extensions.*;
+
 /**
  * 'Abstract' marker interface to denote that implementing test cases should be
  * excluded from running against specific fixtures. Concrete marker interfaces
  * for specific fixtures should extend OptOutFromTestFixture.
  */
-public interface OptOutFromTestFixture {
+public interface OptOutFromTestFixture extends Db4oTestCase {
 
 }
