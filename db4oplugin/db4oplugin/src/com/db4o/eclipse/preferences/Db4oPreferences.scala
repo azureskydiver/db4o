e82e5fc570e34d1b46508ecbd2cb2897293542e9
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2009-05-12 13:49:47 +0000

    acv/pr: COR-1629 - persist package filter preference list

--- db4oplugin/db4oplugin/src/com/db4o/eclipse/preferences/Db4oPreferences.scala
+++ db4oplugin/db4oplugin/src/com/db4o/eclipse/preferences/Db4oPreferences.scala
@@ -2,11 +2,16 @@ package com.db4o.eclipse.preferences
 
 import org.eclipse.core.resources._
 
+import scala.collection._
+
 object Db4oPreferences {
 
   val FILTER_REGEXP_PROPERTY_ID = "filter.regexp" 
+  val PACKAGE_LIST_PROPERTY_ID = "filter.packages" 
   val DEFAULT_REGEXP = ".*"
-
+  val PACKAGE_SEP = ","
+  val DEFAULT_PACKAGE_LIST = ""
+  
   def projectPreferences(project: IProject) =
     new ProjectScope(project).getNode(Db4oPluginActivator.PLUGIN_ID) match {
       case null => None
@@ -14,14 +19,35 @@ object Db4oPreferences {
     }
   
   def setFilterRegExp(project: IProject, filterRegExp: String) {
+    setPreference(project, FILTER_REGEXP_PROPERTY_ID, filterRegExp)
+  }
+
+  def getFilterRegExp(project: IProject) =
+    getPreference(project, FILTER_REGEXP_PROPERTY_ID, DEFAULT_REGEXP)
+  
+  def setPackageList(project: IProject, packages: Set[String]) {
+    setPreference(project, PACKAGE_LIST_PROPERTY_ID, packages.mkString(PACKAGE_SEP))
+  }
+
+  def getPackageList(project: IProject) = {
+    var packages = immutable.ListSet.empty[String]
+    val packageStr = getPreference(project, PACKAGE_LIST_PROPERTY_ID, DEFAULT_PACKAGE_LIST)
+    if(!packageStr.isEmpty) {
+      packageStr.split(PACKAGE_SEP).foreach(packages += _)
+    }
+    packages
+  }
+  
+  def getPreference(project: IProject, key: String, defaultValue: String) = {
+    projectPreferences(project).map(_.get(key, defaultValue))
+        .getOrElse(defaultValue)
+  }  
+
+  private def setPreference(project: IProject, key: String, value: String) {
     projectPreferences(project).map((node) => {
-      node.put(FILTER_REGEXP_PROPERTY_ID, filterRegExp);
+      node.put(key, value);
       node.flush();
     })
   }
 
-  def getFilterRegExp(project: IProject) = {
-    projectPreferences(project).map(_.get(FILTER_REGEXP_PROPERTY_ID, DEFAULT_REGEXP))
-        .getOrElse(DEFAULT_REGEXP)
-  }  
 }
