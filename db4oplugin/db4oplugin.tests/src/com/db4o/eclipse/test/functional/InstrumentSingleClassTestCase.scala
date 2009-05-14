10b97b537820ecaaae8f059f74496fb0208745eb
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2009-05-14 14:59:29 +0000

    cr/pr: COR-1642 - test cases for package/regexp combinations, preferences and property page

--- db4oplugin/db4oplugin.tests/src/com/db4o/eclipse/test/functional/InstrumentSingleClassTestCase.scala
+++ db4oplugin/db4oplugin.tests/src/com/db4o/eclipse/test/functional/InstrumentSingleClassTestCase.scala
@@ -6,6 +6,10 @@ import com.db4o.eclipse.test.util._
 
 import org.junit._
 
+import AndOrEnum._
+
+import scala.collection.immutable._
+
 class InstrumentSingleClassTestCase extends Db4oPluginTestCaseTrait {
 
   @Before
@@ -20,17 +24,52 @@ class InstrumentSingleClassTestCase extends Db4oPluginTestCaseTrait {
   }
 
   @Test
-  def matchingClassIsInstrumented {
-    Db4oPreferences.setFilterRegExp(project.getProject, java.util.regex.Pattern.compile("foo\\.Foo"))
-    assertInstrumentSingleClass(true)
+  def packageAndRegex {
+    assertInstrumented("^foo\\..*", "foo", AndOrEnum.And, true)
   }
 
   @Test
-  def nonMatchingClassIsNotInstrumented {
-    Db4oPreferences.setFilterRegExp(project.getProject, java.util.regex.Pattern.compile("fooX\\.Foo"))
-    assertInstrumentSingleClass(false)
+  def packageOrRegex {
+    assertInstrumented("^foo\\..*", "foo", AndOrEnum.Or, true)
   }
 
+  @Test
+  def packageAndNotRegex {
+    assertInstrumented("^fooX\\..*", "foo", AndOrEnum.And, false)
+  }
+
+  @Test
+  def packageOrNotRegex {
+    assertInstrumented("^fooX\\..*", "foo", AndOrEnum.Or, true)
+  }
+
+  @Test
+  def notPackageAndRegex {
+    assertInstrumented("^foo\\..*", "fooX", AndOrEnum.And, false)
+  }
+
+  @Test
+  def notPackageOrRegex {
+    assertInstrumented("^foo\\..*", "fooX", AndOrEnum.Or, true)
+  }
+
+  @Test
+  def notPackageAndNotRegex {
+    assertInstrumented("^fooX\\..*", "fooX", AndOrEnum.And, false)
+  }
+
+  @Test
+  def notPackageOrNotRegex {
+    assertInstrumented("^fooX\\..*", "fooX", AndOrEnum.Or, false)
+  }
+
+  def assertInstrumented(regEx: String, packageName: String, combinator: AndOrEnum, expected: Boolean) {
+    Db4oPreferences.setFilterRegExp(project.getProject, java.util.regex.Pattern.compile(regEx))
+    Db4oPreferences.setPackageList(project.getProject, ListSet(packageName))
+    Db4oPreferences.setFilterCombinator(project.getProject, combinator)
+    assertInstrumentSingleClass(expected)
+  }
+  
   def assertInstrumentSingleClass(expectInstrumentation: Boolean) {
     project.createCompilationUnit(
       "foo",
