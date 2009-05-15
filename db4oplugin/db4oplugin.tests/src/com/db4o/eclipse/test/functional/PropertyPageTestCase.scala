de36fb44d53dd7f7d1a6f573e82a8e4da5ddea5e
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2009-05-15 02:33:29 +0000

    simple test for combinator preference setting

--- db4oplugin/db4oplugin.tests/src/com/db4o/eclipse/test/functional/PropertyPageTestCase.scala
+++ db4oplugin/db4oplugin.tests/src/com/db4o/eclipse/test/functional/PropertyPageTestCase.scala
@@ -1,6 +1,7 @@
 package com.db4o.eclipse.test.functional
 
 import com.db4o.eclipse.ui._
+import com.db4o.eclipse.preferences._
 
 import org.eclipse.swt._
 import org.eclipse.swt.widgets._
@@ -12,11 +13,15 @@ import Assert._
 class PropertyPageTestCase extends Db4oPluginTestCaseTrait {
 
   var shell: Shell = null
+  var page: Db4oInstrumentationPropertyPage = null
   
   @Before
   override def setUp {
     super.setUp
     shell = new Shell
+    page = new Db4oInstrumentationPropertyPage()
+    page.setElement(project.getJavaProject)
+    page.createControl(new Composite(shell, SWT.NULL))
   }
   
   @After
@@ -26,20 +31,40 @@ class PropertyPageTestCase extends Db4oPluginTestCaseTrait {
   }
   
   @Test
-  def testPropertyPage {
-    val page = new Db4oInstrumentationPropertyPage()
-    page.setElement(project.getJavaProject)
-    val parent = new Composite(shell, SWT.NULL)
-    assertFalse(findWidget(page.getControl, Db4oInstrumentationPropertyPage.REGEXP_TEXT_ID).isDefined)
-    page.createControl(parent)
+  def testFilterRegExpText {
     assertTrue(findWidget(page.getControl, Db4oInstrumentationPropertyPage.REGEXP_TEXT_ID).isDefined)
     val regExpText = findWidget(page.getControl, Db4oInstrumentationPropertyPage.REGEXP_TEXT_ID).get.asInstanceOf[Text]
+    assertEquals(Db4oPreferences.DEFAULT_REGEXP, regExpText.getText)
     regExpText.setText("\\")
     assertFalse(page.performOk)
     regExpText.setText("foo")
     assertTrue(page.performOk)
+    assertEquals(Db4oPreferences.getFilterRegExp(project.getProject).toString, "foo")
   }
-  
+
+  @Test
+  def testFilterCombinator {
+    assertTrue(findWidget(page.getControl, Db4oInstrumentationPropertyPage.COMBINATOR_AND_BUTTON_ID).isDefined)
+    assertTrue(findWidget(page.getControl, Db4oInstrumentationPropertyPage.COMBINATOR_OR_BUTTON_ID).isDefined)
+    val andButton = findWidget(page.getControl, Db4oInstrumentationPropertyPage.COMBINATOR_AND_BUTTON_ID).get.asInstanceOf[Button]
+    val orButton = findWidget(page.getControl, Db4oInstrumentationPropertyPage.COMBINATOR_OR_BUTTON_ID).get.asInstanceOf[Button]
+    val defaultIsAnd = Db4oPreferences.DEFAULT_COMBINATOR == AndOrEnum.And
+    val buttons = 
+      if(defaultIsAnd) {
+        (andButton, orButton)
+      }
+      else {
+        (orButton, andButton)
+      }
+    // TODO setSelection() doesn't trigger any events :/
+    assertTrue(buttons._1.getSelection)
+    assertFalse(buttons._2.getSelection)
+    buttons._2.setSelection(true)
+    buttons._1.setSelection(false)
+    assertTrue(page.performOk)
+    assertEquals(Db4oPreferences.getFilterCombinator(project.getProject), if(defaultIsAnd) AndOrEnum.And else AndOrEnum.Or)
+  }
+
   private def findWidget(root: Widget, id: String): Option[Widget] = {
     if(root == null) {
       return None
