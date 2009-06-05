3163bfbe0a513d1e610fc5a3718afcf83a2a449b
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2009-06-05 00:03:30 +0000

    OMJ-144: first simple tests

---
+++ enterprise/omj/com.db4o.ome.test/src/com/db4o/omplus/datalayer/test/IntegrationTestCase.java
@@ -0,0 +1,29 @@
+/* Copyright (C) 2009  Versant Inc.   http://www.db4o.com */
+package com.db4o.omplus.datalayer.test;
+
+import java.util.*;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import com.db4o.omplus.*;
+import com.db4o.omplus.datalayer.*;
+
+public class IntegrationTestCase {
+
+	private static final String KEY = "key";
+
+	@Test
+	public void testAppDataStore() {
+		OMEDataStore dataStore = Activator.getDefault().getOMEDataStore();
+		IDbInterface db = Activator.getDefault().getDatabaseInterface();
+		db.setDbPath("a");
+		dataStore.setContextLocalEntry(KEY, new ArrayList<String>());
+		assertNotNull(dataStore.getContextLocalEntry(KEY));
+		db.setDbPath("b");
+		assertNull(dataStore.getContextLocalEntry(KEY));
+		db.setDbPath("a");
+		assertNotNull(dataStore.getContextLocalEntry(KEY));
+	}
+	
+}
