aae06ffb4ac29af6f0c585bc75c7c106c94f0040
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2009-06-12 12:44:20 +0000

    cr + acv: #COR-1682 Fixed DatabaseFileLockedException to be thrown when file is locked by another process.

---
+++ db4oj.tests/src/com/db4o/db4ounit/common/assorted/DatabaseFileLockedTestCase.java
@@ -0,0 +1,47 @@
+/* Copyright (C) 2009  Versant Corp.  http://www.db4o.com */
+
+package com.db4o.db4ounit.common.assorted;
+
+import java.io.*;
+
+import com.db4o.*;
+import com.db4o.config.*;
+import com.db4o.db4ounit.common.api.*;
+import com.db4o.ext.*;
+import com.db4o.internal.*;
+
+import db4ounit.*;
+
+@decaf.Remove
+public class DatabaseFileLockedTestCase extends TestWithTempFile{
+	
+	public void testLockedFile() throws IOException{
+		RandomAccessFile raf = new RandomAccessFile(tempFile(), "rw");
+		Object channel = Reflection4.invoke(raf, "getChannel");
+		try {
+			Reflection4.invoke(channel, "tryLock");
+		}catch(ReflectException rex){
+			Assert.fail("File shouldn't be locked already.");
+			rex.printStackTrace();
+		}
+
+		Assert.expect(DatabaseFileLockedException.class, new CodeBlock() {
+			public void run() throws Throwable {
+				EmbeddedObjectContainer objectContainer = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), tempFile());
+			}
+		});
+		raf.close();
+	}
+	
+	public void test(){
+		EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
+		EmbeddedObjectContainer objectContainer1 = Db4oEmbedded.openFile(config, tempFile());
+		Assert.expect(DatabaseFileLockedException.class, new CodeBlock() {
+			public void run() throws Throwable {
+				EmbeddedObjectContainer objectContainer2 = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), tempFile());
+			}
+		});
+		objectContainer1.close();
+	}
+
+}
