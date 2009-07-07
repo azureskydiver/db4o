01b3805975932d463d9a78fef39f6558fce5a347
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-07-07 12:21:05 +0000

    extract useful base class for string based value type handlers

--- db4oj/core/src/com/db4o/internal/handlers/StringBufferHandler.java
+++ db4oj/core/src/com/db4o/internal/handlers/StringBufferHandler.java
@@ -2,71 +2,18 @@
 
 package com.db4o.internal.handlers;
 
-import com.db4o.ext.*;
-import com.db4o.foundation.*;
-import com.db4o.internal.*;
-import com.db4o.internal.delete.*;
-import com.db4o.marshall.*;
-import com.db4o.reflect.*;
-import com.db4o.typehandlers.*;
 
-public final class StringBufferHandler implements ValueTypeHandler, BuiltinTypeHandler,
-        VariableLengthTypeHandler, QueryableTypeHandler {
+public final class StringBufferHandler extends StringBasedValueTypeHandlerBase<StringBuffer> {
 
-    private ReflectClass _classReflector;
-    
-    public void defragment(DefragmentContext context) {
-        stringHandler(context).defragment(context);
-    }
-
-    public void delete(DeleteContext context) throws Db4oIOException {
-        stringHandler(context).delete(context);
-    }
-
-    public Object read(ReadContext context) {
-        Object read = stringHandler(context).read(context);
-        if (null == read) {
-            return null;
-        }
-        return new StringBuffer((String) read);
-    }
-
-    public void write(WriteContext context, Object obj) {
-        stringHandler(context).write(context, obj.toString());
-    }
-
-    private StringHandler stringHandler(Context context) {
-        return handlers(context)._stringHandler;
-    }
-
-    private HandlerRegistry handlers(Context context) {
-        return ((InternalObjectContainer) context.objectContainer()).handlers();
-    }
-
-    public PreparedComparison prepareComparison(Context context, Object obj) {
-        return stringHandler(context).prepareComparison(context, obj);
-    }
-
-    /*
-     * @see com.db4o.internal.BuiltinTypeHandler#classReflector(com.db4o.reflect.Reflector)
-     */
-    public ReflectClass classReflector() {
-        return _classReflector;
-    }
-
-	public void registerReflector(Reflector reflector) {
-        _classReflector = reflector.forClass(StringBuffer.class);
+	public StringBufferHandler() {
+		super(StringBuffer.class);
 	}
 	
-	public boolean isSimple() {
-		return true;
-	}
-	
-	public boolean descendsIntoMembers() {
-		return false;
+	protected StringBuffer convertString(String str) {
+		return new StringBuffer(str);
 	}
 
-	public boolean canHold(ReflectClass type) {
-		return type.equals(classReflector());
-    }
+	protected String convertObject(StringBuffer obj) {
+		return obj.toString();
+	}
 }
