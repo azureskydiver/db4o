171c144a85c03fdfcb6a6c8042948693562be81b
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-07-07 12:21:05 +0000

    extract useful base class for string based value type handlers

---
+++ db4oj/core/src/com/db4o/internal/handlers/StringBasedValueTypeHandlerBase.java
@@ -0,0 +1,82 @@
+/* Copyright (C) 2008  Versant Inc.  http://www.db4o.com */
+
+package com.db4o.internal.handlers;
+
+import com.db4o.ext.*;
+import com.db4o.foundation.*;
+import com.db4o.internal.*;
+import com.db4o.internal.delete.*;
+import com.db4o.marshall.*;
+import com.db4o.reflect.*;
+import com.db4o.typehandlers.*;
+
+public abstract class StringBasedValueTypeHandlerBase<T> implements ValueTypeHandler, BuiltinTypeHandler,
+        VariableLengthTypeHandler, QueryableTypeHandler {
+
+	public final Class<T> _clazz;
+    private ReflectClass _classReflector;
+    
+    public StringBasedValueTypeHandlerBase(Class<T> clazz) {
+    	_clazz = clazz;
+    }
+    
+    public void defragment(DefragmentContext context) {
+        stringHandler(context).defragment(context);
+    }
+
+    public void delete(DeleteContext context) throws Db4oIOException {
+        stringHandler(context).delete(context);
+    }
+
+    public Object read(ReadContext context) {
+        Object read = stringHandler(context).read(context);
+        if (null == read) {
+            return null;
+        }
+        String str = (String) read;
+		return convertString(str);
+    }
+
+	protected abstract T convertString(String str);
+
+    public void write(WriteContext context, Object obj) {
+        stringHandler(context).write(context, convertObject((T)obj));
+    }
+
+	protected abstract String convertObject(T obj);
+
+    private StringHandler stringHandler(Context context) {
+        return handlers(context)._stringHandler;
+    }
+
+    private HandlerRegistry handlers(Context context) {
+        return ((InternalObjectContainer) context.objectContainer()).handlers();
+    }
+
+    public PreparedComparison prepareComparison(Context context, Object obj) {
+        return stringHandler(context).prepareComparison(context, obj);
+    }
+
+    /*
+     * @see com.db4o.internal.BuiltinTypeHandler#classReflector(com.db4o.reflect.Reflector)
+     */
+    public ReflectClass classReflector() {
+        return _classReflector;
+    }
+
+	public void registerReflector(Reflector reflector) {
+        _classReflector = reflector.forClass(StringBuffer.class);
+	}
+	
+	public boolean isSimple() {
+		return true;
+	}
+	
+	public boolean descendsIntoMembers() {
+		return false;
+	}
+
+	public boolean canHold(ReflectClass type) {
+		return type.equals(classReflector());
+    }
+}
