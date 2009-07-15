c53189afe925a89a5191ffc443fbd26caa56dab3
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2009-07-15 17:17:50 +0000

    acv: COR-1648 - Exclude Client/Server test from Silverlight

--- db4oj.tests/src/com/db4o/db4ounit/common/exceptions/propagation/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/exceptions/propagation/AllTests.java
@@ -1,17 +1,27 @@
+/* Copyright (C) 2009 Versant Inc.   http://www.db4o.com */
 package com.db4o.db4ounit.common.exceptions.propagation;
 
 import db4ounit.extensions.*;
 
-public class AllTests extends Db4oTestSuite {
+public class AllTests extends ComposibleTestSuite {
 	
 	public static void main(String[] args) {
 		new AllTests().runAll();
 	}
 
 	protected Class[] testCases() {
+		return composeTests(
+				new Class[] {
+						ExceptionDuringTopLevelCallTestSuite.class, 
+				});
+	}
+	
+	/**
+	 * @sharpen.if !SILVERLIGHT
+	 */
+	protected Class[] composeWith() {
 		return new Class[] {
-				ExceptionDuringTopLevelCallTestSuite.class,
-				com.db4o.db4ounit.common.exceptions.propagation.cs.AllTests.class,
+				com.db4o.db4ounit.common.exceptions.propagation.cs.AllTests.class,				
 		};
 	}
 
