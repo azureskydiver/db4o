615313461a1313a76bde5fdc3f98c5149c225891
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-08-20 12:22:25 +0000

    cr+rbo: common.set => common.store

--- db4oj.tests/src/com/db4o/db4ounit/common/store/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/store/AllTests.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2004 - 20067 Versant Inc. http://www.db4o.com */
 
-package com.db4o.db4ounit.common.set;
+package com.db4o.db4ounit.common.store;
 
 import db4ounit.extensions.*;
 
--- db4oj.tests/src/com/db4o/db4ounit/common/store/DeepSetTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/store/DeepSetTestCase.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2007   Versant Inc.   http://www.db4o.com */
 
-package com.db4o.db4ounit.common.set;
+package com.db4o.db4ounit.common.store;
 
 import com.db4o.ext.*;
 
