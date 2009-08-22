88821b545d52439cb1bfe9408ff4e116e66612d3
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2009-08-22 15:04:17 +0000

    acv+pr: 	COR-1723: Emit ClassHasNoFields if "aspects" field is null or its count is zero

--- db4oj.tests/src/com/db4o/db4ounit/common/diagnostics/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/diagnostics/AllTests.java
@@ -12,6 +12,8 @@ public class AllTests extends Db4oTestSuite {
 
 	protected Class[] testCases() {
 		return new Class[] {
+				ClassHasNoFieldsTestCase.class,
+				DescendIntoTranslatorTestCase.class,
 				DiagnosticsTestCase.class,
 				IndexFieldDiagnosticTestCase.class,
 		};
