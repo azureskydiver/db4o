c83d943de528441927c7009ebb47d3add4be6281
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2009-09-22 14:57:10 +0000

    acv+rbo:DCF-35 - Compiler settings in compliance with target platform

--- decaf/decaf/src/decaf/Resources.java
+++ decaf/decaf/src/decaf/Resources.java
@@ -4,8 +4,10 @@ import java.io.*;
 
 public class Resources {
 
+	public static final String DECAF_ANNOTATIONS_JAR = "decaf-annotations.jar";
+
 	public static String decafAnnotationsJar() throws IOException {
-		return Activator.getResource("lib/decaf-annotations.jar");
+		return Activator.getResource("lib/" + DECAF_ANNOTATIONS_JAR);
 	}
 
 }
