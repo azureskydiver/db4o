280a5c57d8020c29a445b6d1e551231c2b6d2fde
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2009-10-01 13:56:50 +0000

    acv/pr: BLD-139 - extend jar selection strategy to accept a list of indices into the db4o jar file list

--- db4obuild/src/com/db4o/devtools/ant/GarbageCollectJarFolderTask.java
+++ db4obuild/src/com/db4o/devtools/ant/GarbageCollectJarFolderTask.java
@@ -8,11 +8,15 @@ import org.apache.tools.ant.*;
 
 public class GarbageCollectJarFolderTask extends Task {
 
-	private static final int MAX_FILES = 2;
 	private File _jarFolder;
+	private int _maxFileCount;
 	
 	public void setJarFolder(File jarFolder) {
 		_jarFolder = jarFolder;
+	}
+	
+	public void setMaxFiles(int maxFileCount) {
+		_maxFileCount = maxFileCount;
 	}
 	
 	public void execute() throws BuildException {
@@ -20,13 +24,13 @@ public class GarbageCollectJarFolderTask extends Task {
 			throw new BuildException("jarFolder - not an existing directory: " + _jarFolder);
 		}
 		File[] files = _jarFolder.listFiles();
-		if(files.length >= MAX_FILES) {
+		if(files.length >= _maxFileCount) {
 			Arrays.sort(files, new Comparator<File>() {
 				public int compare(File f1, File f2) {
 					return new Long(f1.lastModified()).compareTo(f2.lastModified());
 				}
 			});
-			for(int fileIdx = 0; fileIdx < files.length - MAX_FILES; fileIdx++) {
+			for(int fileIdx = 0; fileIdx < files.length - _maxFileCount; fileIdx++) {
 				System.err.println("DELETE " + files[fileIdx]);
 				files[fileIdx].delete();
 			}
