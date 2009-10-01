b7125aa15c8c7de462ea06eb160f56179105f1fd
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2009-10-01 12:29:44 +0000

    pr+rbo: COR-1761 - java.util.Comparator => com.db4o.foundation.Comparison4 for jdk11

--- decaf/decaf/src/decaf/config/DecafConfiguration.java
+++ decaf/decaf/src/decaf/config/DecafConfiguration.java
@@ -42,6 +42,7 @@ public class DecafConfiguration {
 		mapping.put(List.class.getName(), "com.db4o.foundation.Sequence4");
 		mapping.put(ArrayList.class.getName(), "com.db4o.foundation.Collection4");
 		mapping.put(Collections.class.getName(), "com.db4o.foundation.Collections4");
+		mapping.put(Comparator.class.getName(), "com.db4o.foundation.Comparison4");
 		mapping.put(Iterator.class.getName() , "com.db4o.foundation.Iterator4");
 		mapping.put(Iterable.class.getName() , "com.db4o.foundation.Iterable4");
 		mapping.put(Set.class.getName(), "com.db4o.foundation.Set4");
