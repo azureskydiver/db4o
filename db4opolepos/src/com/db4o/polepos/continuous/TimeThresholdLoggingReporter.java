dde7f765db6bff281090b1ca8a01c1dc8c3980b4
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2009-10-08 11:46:08 +0000

    INT-109: temporarily add time threshold logging

---
+++ db4opolepos/src/com/db4o/polepos/continuous/TimeThresholdLoggingReporter.java
@@ -0,0 +1,41 @@
+/* Copyright (C) 2009  Versant Inc.   http://www.db4o.com */
+package com.db4o.polepos.continuous;
+
+import org.polepos.framework.*;
+import org.polepos.reporters.*;
+
+public class TimeThresholdLoggingReporter implements Reporter {
+
+	private static final long MIN_TIME = 10000;
+	private static final long MAX_TIME = 30000;
+	private final String _currentTeamName;
+	
+	public TimeThresholdLoggingReporter(String currentTeamName) {
+		_currentTeamName = currentTeamName;
+	}
+	
+	public void report(Team team, Car car, TurnSetup[] setups, TurnResult[] results) {
+		if(!_currentTeamName.equals(team.name())) {
+			return;
+		}
+		for(Result result : results[results.length - 1]) {
+			long time = result.getTime();
+			if(time < MIN_TIME || time > MAX_TIME) {
+				System.err.println(result.getCircuit().name() + "/" + result.getLap().name() + ": " +time + " ms");
+			}
+		}
+	}
+
+	public void sendToCircuit(Circuit circuit) {
+	}
+
+	public void startSeason() {
+	}
+
+	public void endSeason() {
+	}
+
+	public void noDriver(Team team, Circuit circuit) {
+	}
+
+}
