e97914e20ea5ec89849193afdeead54a2756f296
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2009-10-19 21:02:20 +0000

    clean up mbeans factory, registration support and re-enable client connections test case

--- db4oj.tests/src/com/db4o/db4ounit/optional/monitoring/cs/ClientConnectionsTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/optional/monitoring/cs/ClientConnectionsTestCase.java
@@ -2,6 +2,8 @@
 
 package com.db4o.db4ounit.optional.monitoring.cs;
 
+import javax.management.*;
+
 import com.db4o.cs.*;
 import com.db4o.cs.config.*;
 import com.db4o.cs.internal.*;
@@ -22,7 +24,7 @@ public class ClientConnectionsTestCase extends TestWithTempFile implements OptOu
 	private static final String USER = "db4o";
 	private static final String PASSWORD = "db4o";
 
-	public void _testConnectedClients() {
+	public void testConnectedClients() {
 		for(int i=0; i < 5; i++) {
 			Assert.areEqual(0, connectedClientCount(), "No client yet.");
 			ExtObjectContainer client1 = openNewSession();
@@ -54,7 +56,7 @@ public class ClientConnectionsTestCase extends TestWithTempFile implements OptOu
 	}
 
 	private long connectedClientCount() {
-		MBeanProxy bean = new MBeanProxy(Db4oMBeans.mBeanNameFor(ClientConnectionsMBean.class, Db4oMBeans.mBeanIDForPath(tempFile())));
+		MBeanProxy bean = new MBeanProxy(Db4oMBeans.mBeanNameFor(ClientConnectionsMBean.class, Db4oMBeans.mBeanIDForContainer(_server.objectContainer())));
 		return bean.<Integer>getAttribute("ConnectedClientCount").longValue();
 	}
 
@@ -63,14 +65,34 @@ public class ClientConnectionsTestCase extends TestWithTempFile implements OptOu
 		super.setUp();
 		
 		ServerConfiguration serverConfiguration = Db4oClientServer.newServerConfiguration();
-		serverConfiguration.common().add(new NetworkingMonitoringSupport());
 		
 		_server = (ObjectServerImpl) Db4oClientServer.openServer(serverConfiguration, tempFile(), Db4oClientServer.ARBITRARY_PORT);
 		_server.grantAccess(USER, PASSWORD);
 		
+		registerBean();
+		
 		// We depend on the order of client connection/disconnection event firing.
 		// We want the listener in the test to be notified before the one in the bean.
 		_listener = registerCloseEventNotification();
+	}
+
+	// TODO: Where does this setup code go? COR-1779
+	private void registerBean() throws JMException {
+		final ClientConnections bean = Db4oMBeans.newClientConnectionsMBean(_server);
+		bean.register();
+		((ObjectServerEvents)_server).closed().addListener(new EventListener4<ServerClosedEventArgs>() {
+			public void onEvent(Event4<ServerClosedEventArgs> e, ServerClosedEventArgs args) {
+				bean.unregister();
+			}
+		});
+		
+		((ObjectServerEvents)_server).clientConnected().addListener(new EventListener4<ClientConnectionEventArgs>() { public void onEvent(Event4<ClientConnectionEventArgs> e, ClientConnectionEventArgs args) {
+			bean.notifyClientConnected();
+		}});
+		
+		((ObjectServerEvents)_server).clientDisconnected().addListener(new EventListener4<StringEventArgs>() { public void onEvent(Event4<StringEventArgs> e, StringEventArgs args) {
+			bean.notifyClientDisconnected();
+		}});
 	}
 
 	private EventListener4<StringEventArgs> registerCloseEventNotification() {
