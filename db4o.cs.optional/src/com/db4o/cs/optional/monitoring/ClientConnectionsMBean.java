553c563d45caf2bb36b43da41037294e3ffb58da
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2009-11-26 23:41:07 +0000

    acv+cr: COR-1815 - Introduced project db4o.cs.optional

---
+++ db4o.cs.optional/src/com/db4o/cs/optional/monitoring/ClientConnectionsMBean.java
@@ -0,0 +1,14 @@
+/* Copyright (C) 2009 Versant Inc. http://www.db4o.com */
+
+package com.db4o.cs.optional.monitoring;
+
+/**
+ * @exclude
+ * JMX MBean for tracking client connections.
+ * 
+ * @since 7.12
+ */
+@decaf.Ignore
+public interface ClientConnectionsMBean {
+	int getConnectedClientCount();
+}
---
+++ db4o.cs.optional/src/com/db4o/cs/optional/monitoring/MonitoredClientSocket4.java
@@ -0,0 +1,31 @@
+/* Copyright (C) 2009 Versant Inc. http://www.db4o.com */
+
+package com.db4o.cs.optional.monitoring;
+
+import static com.db4o.foundation.Environments.my;
+
+import com.db4o.*;
+import com.db4o.foundation.network.*;
+
+/**
+ * @exclude
+ */
+@decaf.Ignore
+public class MonitoredClientSocket4 extends MonitoredSocket4Base {
+
+	protected MonitoredClientSocket4(Socket4 socket) {
+		super(socket);		
+	}
+	
+	@Override
+	protected Networking bean() {
+		if (null == _bean)
+		{
+			_bean = Db4oClientServerMBeans.newClientNetworkingStatsMBean(my(ObjectContainer.class)); 
+		}
+		
+		return _bean; 
+	}	
+	
+	private Networking _bean;
+}
