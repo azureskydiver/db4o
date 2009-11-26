530858caaaf46f0cafed7987235b5944f83283cf
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2009-11-26 23:41:07 +0000

    acv+cr: COR-1815 - Introduced project db4o.cs.optional

---
+++ db4o.cs.optional/src/com/db4o/cs/optional/monitoring/NetworkingMonitoringSupport.java
@@ -0,0 +1,25 @@
+/* Copyright (C) 2009  Versant Corp.  http://www.db4o.com */
+
+package com.db4o.cs.optional.monitoring;
+
+import com.db4o.config.*;
+import com.db4o.cs.config.*;
+import com.db4o.cs.internal.config.*;
+import com.db4o.foundation.network.*;
+import com.db4o.internal.*;
+
+/**
+ * publishes statistics about networking activities to JMX.
+ */
+@decaf.Ignore
+public class NetworkingMonitoringSupport implements ConfigurationItem {
+
+	public void apply(InternalObjectContainer container) {
+	}
+
+	public void prepare(Configuration configuration) {
+		NetworkingConfiguration networkConfig = Db4oClientServerLegacyConfigurationBridge.asNetworkingConfiguration(configuration);
+		Socket4Factory currentSocketFactory = networkConfig.socketFactory();
+		networkConfig.socketFactory(new MonitoredSocket4Factory(currentSocketFactory));
+	}
+}
