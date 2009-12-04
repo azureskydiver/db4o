107ddc0c4e59708490ef1da87c9401ff0410ab10
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2009-12-04 12:37:09 +0000

    acv/pr: remove "optional" from monitoring package for consistency

---
+++ db4o.cs.optional/src/com/db4o/cs/monitoring/MonitoredSocket4Base.java
@@ -0,0 +1,32 @@
+/* Copyright (C) 2009 Versant Inc. http://www.db4o.com */
+
+package com.db4o.cs.monitoring;
+
+import java.io.*;
+
+import com.db4o.cs.foundation.*;
+
+/**
+ * @exclude
+ */
+@decaf.Ignore
+abstract class MonitoredSocket4Base extends Socket4Decorator {
+	public MonitoredSocket4Base(Socket4 socket) {
+		super(socket);
+	}
+
+	public void write(byte[] bytes, int offset, int count) throws IOException {
+		super.write(bytes, offset, count);
+		bean().notifyWrite(count);
+	}
+	
+	@Override
+	public int read(byte[] buffer, int offset, int count) throws IOException {
+		int bytesReceived = super.read(buffer, offset, count);
+		bean().notifyRead(bytesReceived);
+		
+		return bytesReceived;
+	}
+	
+	protected abstract Networking bean();
+}
---
+++ db4o.cs.optional/src/com/db4o/cs/monitoring/NetworkingMBean.java
@@ -0,0 +1,22 @@
+/* Copyright (C) 2009 Versant Inc. http://www.db4o.com */
+
+package com.db4o.cs.monitoring;
+
+import com.db4o.monitoring.*;
+
+/**
+ * @exclude
+ * JMX MBean for networking IO statistics.
+ * 
+ * @see MonitoredSocket4Factory
+ */
+@decaf.Ignore
+public interface NetworkingMBean extends ResettableMBean {
+	
+	double getBytesSentPerSecond();
+	
+	double getBytesReceivedPerSecond();
+	
+	double getMessagesSentPerSecond();
+
+}
