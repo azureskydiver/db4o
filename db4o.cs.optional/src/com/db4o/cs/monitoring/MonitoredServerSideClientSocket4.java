5e495d4ba259db7e71b0b3af989517f8b8c23994
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2009-12-04 12:37:09 +0000

    acv/pr: remove "optional" from monitoring package for consistency

---
+++ db4o.cs.optional/src/com/db4o/cs/monitoring/MonitoredServerSideClientSocket4.java
@@ -0,0 +1,25 @@
+/* Copyright (C) 2009 Versant Inc. http://www.db4o.com */
+
+package com.db4o.cs.monitoring;
+
+import com.db4o.cs.foundation.*;
+
+/**
+ * @exclude
+ */
+@decaf.Ignore
+public class MonitoredServerSideClientSocket4 extends MonitoredSocket4Base {
+
+	public MonitoredServerSideClientSocket4(Socket4 socket, Networking bean) {
+		super(socket);
+		
+		_bean = bean;
+	}
+
+	@Override
+	protected Networking bean() {
+		return _bean;
+	}
+
+	private Networking _bean;
+}
