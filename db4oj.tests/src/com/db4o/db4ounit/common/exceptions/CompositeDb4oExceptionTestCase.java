c06ec3b3979ab4842676901776349b6db5caa59a
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2009-12-16 18:24:03 +0000

    no need to print the stack trace

--- db4oj.tests/src/com/db4o/db4ounit/common/exceptions/CompositeDb4oExceptionTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/exceptions/CompositeDb4oExceptionTestCase.java
@@ -24,7 +24,6 @@ public class CompositeDb4oExceptionTestCase implements TestCase{
 			StringAssert.contains("throwCompositeException", stackTrace);
 			StringAssert.contains("method1", stackTrace);
 			StringAssert.contains("method2", stackTrace);
-			ex.printStackTrace();
 		}
 	}
 
