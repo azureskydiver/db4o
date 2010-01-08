f54390d5dd682fec5bf4ccd59858f9d3427e4d93
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2010-01-08 13:25:29 +0000

    acv/pr: COR-1856 - build/run ant instrumentation test in cc build

--- db4otools/test/com/db4o/enhance/test/Db4oEnhancerAntTaskTestCaseLauncher.java
+++ db4otools/test/com/db4o/enhance/test/Db4oEnhancerAntTaskTestCaseLauncher.java
@@ -10,8 +10,19 @@ public class Db4oEnhancerAntTaskTestCaseLauncher implements TestCase {
 
 	private static final String BUILD_XML = "testscript/Db4oEnhancerAntTaskBuild.xml";
 
+	private static String userClassPath = null;
+	private static String sourceClassPath = null;
+	private static String sourcePath = null;
+	private static boolean forceProperties = false;
+	
 	public static void main(String[] args) {
-		new ConsoleTestRunner(Db4oEnhancerAntTaskTestCaseLauncher.class).run();
+		if(args != null && args.length > 2) {
+			userClassPath = args[0];
+			sourceClassPath = args[1];
+			sourcePath = args[2];
+			forceProperties = true;
+		}
+		System.exit(new ConsoleTestRunner(Db4oEnhancerAntTaskTestCaseLauncher.class).run());
 	}
 	
 	public void test() throws Exception {
@@ -21,13 +32,18 @@ public class Db4oEnhancerAntTaskTestCaseLauncher implements TestCase {
 		DefaultLogger consoleLogger = new DefaultLogger();
 		consoleLogger.setErrorPrintStream(System.err);
 		consoleLogger.setOutputPrintStream(System.out);
-		consoleLogger.setMessageOutputLevel(Project.MSG_VERBOSE);
+		consoleLogger.setMessageOutputLevel(Project.MSG_WARN);
 		p.addBuildListener(consoleLogger);
 
 		try {
 			p.fireBuildStarted();
 			p.init();
 			p.setUserProperty("ant.file", buildFile.getAbsolutePath());
+			if(forceProperties) {
+				p.setNewProperty("user.classpath", userClassPath);
+				p.setNewProperty("source.classpath", sourceClassPath);
+				p.setNewProperty("source.path", sourcePath);
+			}
 			ProjectHelper helper = ProjectHelper.getProjectHelper();
 			p.addReference("ant.projectHelper", helper);
 			helper.parse(p, buildFile);
