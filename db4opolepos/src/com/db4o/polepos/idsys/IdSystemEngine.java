c349eadaa40b52d0aec87037b0bf62016601c53a
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2010-03-18 18:55:29 +0000

    cr: more profiling code to test BTreeIdSystem performance

--- db4opolepos/src/com/db4o/polepos/idsys/IdSystemEngine.java
+++ db4opolepos/src/com/db4o/polepos/idsys/IdSystemEngine.java
@@ -17,19 +17,30 @@ public class IdSystemEngine {
 	public IdSystemEngine(Storage storage, String path) {
 		_storage = storage;
 		_path = path;
+		new File(path).mkdirs();
 	}
 
+	public IdSystemEngine(String path) {
+		this(null, path);
+	}
+	
 	public LocalObjectContainer open(IdSystemConfigurator configurator) {
 		return (LocalObjectContainer) Db4oEmbedded.openFile(config(configurator), _path);
 	}
 
 	public void clear() throws IOException {
+		if(_storage == null){
+			new File(_path).delete();
+			return;
+		}
 		_storage.delete(_path);
 	}
 	
 	private EmbeddedConfiguration config(IdSystemConfigurator configurator) {
 		EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
-		config.file().storage(_storage);
+		if(_storage != null){
+			config.file().storage(_storage);
+		}
 		configurator.configure(config.idSystem());
 		return config;
 	}
