91f095e7735dde64417fe2554f42cc6c9bdabfa8
roman_stoffel <roman_stoffel@915b37e7-0715-0410-8bc3-b14b59444072>
2010-05-03 16:17:33 +0000

    rs: missing files of previous commit: DOC-492 Explain TP/TA & collection in more detail

---
+++ reference/CodeExamples/dotNet/CSharpExamples/Code/ta/collectionEnhanceIssues/EnhancementLimitations.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+
+namespace Db4oDoc.Code.TA.CollectionEnhanceIssues
+{
+    // #example: Can be enhanced by the db4o-tools
+    public class CanBeEnhanced
+    {
+        private IList<string> _names = new List<string>();
+
+        public bool ContainsName(string item)
+        {
+            return _names.Contains(item);
+        }
+
+        public void AddName(string item)
+        {
+            _names.Add(item);
+        }
+    }
+    // #end example
+    namespace AfterEnhancement
+    {
+        // #example: Is enhanced to
+        public class CanBeEnhanced
+        {
+            private IList<string> _names = new List<string>();
+
+            public bool ContainsName(string item)
+            {
+                return _names.Contains(item);
+            }
+
+            public void AddName(string item)
+            {
+                _names.Add(item);
+            }
+        }
+        // #end example
+    
+    }
+
+
+    // #example: Cannot be enhanced by the db4o-tools
+    public class CannotBeEnhanced
+    {
+        // cannot be enhanced, because it uses the concrete type
+        private List<string> _names = new List<string>();
+
+        public bool ContainsName(string item)
+        {
+            return _names.Contains(item);
+        }
+
+        public void AddName(string item)
+        {
+            _names.Add(item);
+        }
+    }
+    // #end example
+}
