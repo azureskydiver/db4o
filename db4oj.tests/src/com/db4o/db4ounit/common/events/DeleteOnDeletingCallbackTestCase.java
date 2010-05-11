8b5ca0ea4f7199c4cb8e9e754cc80cf1e85e3a24
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2010-05-11 12:27:51 +0000

    cr: Added synchronized/lock functionality to DeleteOnDeletingCallbackTestCase so it doesn't produce spurious failures.

--- db4oj.tests/src/com/db4o/db4ounit/common/events/DeleteOnDeletingCallbackTestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/events/DeleteOnDeletingCallbackTestCase.java
@@ -2,10 +2,13 @@ package com.db4o.db4ounit.common.events;
 
 import com.db4o.*;
 import com.db4o.config.*;
+import com.db4o.cs.internal.*;
+import com.db4o.events.*;
 import com.db4o.foundation.*;
 
 import db4ounit.*;
 import db4ounit.extensions.*;
+import db4ounit.extensions.fixtures.*;
 
 public class DeleteOnDeletingCallbackTestCase extends AbstractDb4oTestCase {
 	
@@ -31,11 +34,38 @@ public class DeleteOnDeletingCallbackTestCase extends AbstractDb4oTestCase {
 	
 	public void test() throws Exception {
 		
+		final BooleanByRef disconnected = new BooleanByRef();
+		final Lock4 lock = new Lock4();
+		if(isNetworking()){
+			Db4oClientServerFixture clientServerFixture = (Db4oClientServerFixture) fixture();
+			ObjectServerEvents objectServerEvents = (ObjectServerEvents) clientServerFixture.server();
+			objectServerEvents.clientDisconnected().addListener(new EventListener4<StringEventArgs>() { public void onEvent(Event4<StringEventArgs> e, StringEventArgs args) {
+				lock.run(new Closure4() { public Object run() {
+					disconnected.value = true;
+					lock.awake();
+					return null;
+				}});
+				
+			}});
+		}
+		
 		final RootItem root = retrieveOnlyInstance(RootItem.class);
 		root.child = new Item();
 		db().store(root);
 		db().delete(root);
 		reopen();
+		
+		if(isNetworking()){
+			lock.run(new Closure4() {
+				public Object run() {
+					if(! disconnected.value){
+						lock.snooze(1000000);
+					}
+					return null;
+				}
+			});
+		}
+		
 		assertClassIndexIsEmpty();
 	}
 
