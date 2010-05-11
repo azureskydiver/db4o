858cc0c90c23108e2f395dcfd468b1cc855ebe60
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2010-05-11 13:53:30 +0000

    cr: DeleteOnDeletingCallbackTestCase no longer runs with Silverlight because it has CS knowledge.

--- db4oj.tests/src/com/db4o/db4ounit/common/events/AllTests.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/events/AllTests.java
@@ -3,30 +3,40 @@ package com.db4o.db4ounit.common.events;
 
 import db4ounit.extensions.*;
 
-public class AllTests extends Db4oTestSuite {
+public class AllTests extends ComposibleTestSuite {
 	
 	public static void main(String[] args) {
 		new AllTests().runAll();
 	}
 
 	protected Class[] testCases() {
+		return composeTests(
+				new Class[] {			
+						ActivationEventsTestCase.class,
+						ClassRegistrationEventsTestCase.class,
+						CreationEventsTestCase.class,
+						DeleteEventOnClientTestCase.class,
+						DeletionEventExceptionTestCase.class,
+						DeletionEventsTestCase.class,
+						EventArgsTransactionTestCase.class,			
+						EventCountTestCase.class,
+						ExceptionPropagationInEventsTestSuite.class,
+						InstantiationEventsTestCase.class,
+						ObjectContainerEventsTestCase.class,
+						ObjectContainerOpenEventTestCase.class,
+						QueryEventsTestCase.class,
+						UpdateInCallbackThrowsTestCase.class,
+				});
+	}
+	
+	/**
+	 * @sharpen.if !SILVERLIGHT
+	 */
+	@Override
+	protected Class[] composeWith() {
 		return new Class[] {
-			ActivationEventsTestCase.class,
-			ClassRegistrationEventsTestCase.class,
-			CreationEventsTestCase.class,
-			DeleteEventOnClientTestCase.class,
-			DeleteOnDeletingCallbackTestCase.class,
-			DeletionEventExceptionTestCase.class,
-			DeletionEventsTestCase.class,
-			EventArgsTransactionTestCase.class,			
-			EventCountTestCase.class,
-			ExceptionPropagationInEventsTestSuite.class,
-			InstantiationEventsTestCase.class,
-			ObjectContainerEventsTestCase.class,
-			ObjectContainerOpenEventTestCase.class,
-			QueryEventsTestCase.class,
-			UpdateInCallbackThrowsTestCase.class,
-		};
+				DeleteOnDeletingCallbackTestCase.class,
+			};
 	}
 
 }
