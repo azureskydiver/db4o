196fddc1887e0973106e7bbac1de99f9d867f8d2
roman_stoffel <roman_stoffel@915b37e7-0715-0410-8bc3-b14b59444072>
2010-06-14 21:41:20 +0000

    rs: DOC-513 Unique Constain missing parts      DOC-505 Illustrate how to use db4o in a web-scenario      other small fixes

---
+++ reference/CodeExamples/dotNet/VisualBasicExamples/Code/container/sessions/Db4oSessions.vb
@@ -0,0 +1,55 @@
+Imports Db4objects.Db4o
+Imports Db4objects.Db4o.CS
+
+Namespace Db4oDoc.Code.Container.Sessions
+    Public Class Db4oSessions
+        Private Const DatabaseFileName As String = "database.db4o"
+
+
+        Public Sub Sessions()
+            ' #example: Session object container
+            Dim rootContainer As IEmbeddedObjectContainer = Db4oEmbedded.OpenFile(DatabaseFileName)
+
+            ' open the db4o-session. For example at the beginning for a web-request
+            Using session As IObjectContainer = rootContainer.OpenSession()
+                ' do the operations on the session-container
+                session.Store(New Person("Joe"))
+            End Using
+            ' #end example
+
+            rootContainer.Dispose()
+        End Sub
+
+        Public Sub EmbeddedClient()
+            ' #example: Embedded client
+            Dim server As IObjectServer = Db4oClientServer.OpenServer(DatabaseFileName, 0)
+
+            ' open the db4o-embedded client. For example at the beginning for a web-request
+            Using container As IObjectContainer = server.OpenClient()
+                ' do the operations on the session-container
+                container.Store(New Person("Joe"))
+            End Using
+            ' #end example
+
+            server.Dispose()
+        End Sub
+
+
+        Private Class Person
+            Private m_name As String
+
+            Public Sub New(ByVal name As String)
+                Me.m_name = name
+            End Sub
+
+            Public Property Name() As String
+                Get
+                    Return m_name
+                End Get
+                Set(ByVal value As String)
+                    m_name = value
+                End Set
+            End Property
+        End Class
+    End Class
+End Namespace
