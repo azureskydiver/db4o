c0bd9b934ce75404f81ced2023df03817ef58eab
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2010-07-06 15:35:09 +0000

    acv/pr: UNIT-32 - moved up transmogrify implementation to test suite base

--- db4ounit/src/db4ounit/ClassLevelFixtureTestSuite.java
+++ db4ounit/src/db4ounit/ClassLevelFixtureTestSuite.java
@@ -4,6 +4,9 @@ import com.db4o.foundation.*;
 
 public class ClassLevelFixtureTestSuite extends OpaqueTestSuiteBase {
 
+	public static final String TEARDOWN_METHOD_NAME = "classTearDown";
+	public static final String SETUP_METHOD_NAME = "classSetUp";
+	
 	private final Class<?> _clazz;
 	
 	public ClassLevelFixtureTestSuite(Class<?> clazz, Closure4<Iterator4<Test>> tests) {
@@ -13,30 +16,19 @@ public class ClassLevelFixtureTestSuite extends OpaqueTestSuiteBase {
 
 	@Override
 	protected void suiteSetUp() throws Exception {
-		_clazz.getMethod("classSetUp", null).invoke(null, null);
+		_clazz.getMethod(SETUP_METHOD_NAME, null).invoke(null, null);
 	}
 
 	@Override
 	protected void suiteTearDown() throws Exception {
-		_clazz.getMethod("classTearDown", null).invoke(null, null);
+		_clazz.getMethod(TEARDOWN_METHOD_NAME, null).invoke(null, null);
 	}
 
 	public String label() {
 		return _clazz.getName();
 	}
 	
-	public Test transmogrify(final Function4<Test, Test> fun) {
-		return new ClassLevelFixtureTestSuite(_clazz, 
-			new Closure4<Iterator4<Test>>() {
-				public Iterator4<Test> run() {
-					return Iterators.map(tests().run(), new Function4<Test, Test>() {
-						public Test apply(Test test) {
-							return fun.apply(test);
-						}
-					});
-				}
-			});
+	protected OpaqueTestSuiteBase transmogrified(Closure4<Iterator4<Test>> tests) {
+		return new ClassLevelFixtureTestSuite(_clazz, tests);
 	}
-
-
 }
