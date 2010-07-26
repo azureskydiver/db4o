dffb7b7cc85d4b0a83ed5464e1ffb9e974f37197
roman_stoffel <roman_stoffel@915b37e7-0715-0410-8bc3-b14b59444072>
2010-07-26 13:34:18 +0000

    rs: DOC-530: Split the Class-Configration in mutliple topics to increate the overview rs: DOC-470 Silverlight documentation rs: DOC-398 Document new exception handling in 7.11 release

--- reference/CodeExamples/silverlight/silverlight/Code/silverlight/configuration/io/IOExamples.cs
+++ reference/CodeExamples/silverlight/silverlight/Code/silverlight/configuration/io/IOExamples.cs
@@ -2,18 +2,18 @@ using Db4objects.Db4o;
 using Db4objects.Db4o.Config;
 using Db4objects.Db4o.IO;
 
-namespace silverlight.Code.configuration.io
+namespace Db4oDoc.Silverlight.Configuration.IO
 {
     public class IOExamples
     {
-        public void useIsolatedStorage()
+        public void UseIsolatedStorage()
         {
             // #example: use the isolated storage on silverlight
             IEmbeddedConfiguration configuration = Db4oEmbedded.NewConfiguration();
             configuration.File.Storage = new IsolatedStorageStorage();
-            // #end example
 
             IObjectContainer container = Db4oEmbedded.OpenFile(configuration, "database.db4o");
+            // #end example
         }
     }
 }
