1a53bf6fac0a14e2de6301d8ece0f668f37659ec
roman_stoffel <roman_stoffel@915b37e7-0715-0410-8bc3-b14b59444072>
2010-08-23 09:41:32 +0000

    rs: HiLo-example draft which doesn't work for now rs: - DOC-533 Explain .OpenSession() in more detail. Especially in Client-Server-Mode     - added session-container documetation in general rs: DOC-29 Using of connection pooling in CS

--- reference/CodeExamples/dotNet/CSharpExamples/Boot.cs
+++ reference/CodeExamples/dotNet/CSharpExamples/Boot.cs
@@ -1,7 +1,5 @@
 
 using System;
-using Db4oDoc.Code.Query.Soda;
-using Db4oDoc.Code.Strategies.StoringStatic;
 
 namespace Db4oDoc
 {
@@ -9,7 +7,6 @@ namespace Db4oDoc
     {
         public static void Main(string[] args)
         {
-            SodaQueryExamples.Main(args);
             Console.Read();
         }
     }
