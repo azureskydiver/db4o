ad23b01a8f64ba8a9d3cc67f9b2065b5d45e5116
fabioroger <fabioroger@915b37e7-0715-0410-8bc3-b14b59444072>
2010-08-30 21:34:36 +0000

    frm+acv: refactoring tcp communication network client out of the factory

---
+++ drs/src/core/com/db4o/drs/versant/ipc/tcp/TcpClient.java
@@ -0,0 +1,117 @@
+package com.db4o.drs.versant.ipc.tcp;
+
+import java.io.*;
+import java.net.*;
+
+import com.db4o.drs.versant.ipc.*;
+import com.db4o.drs.versant.ipc.ObjectLifecycleMonitorNetwork.ClientChannelControl;
+import com.db4o.rmi.*;
+
+public class TcpClient implements ClientChannelControl {
+
+	volatile boolean running = true;
+	
+	private Thread thread;
+
+	private Socket socket;
+
+	private Distributor<ObjectLifecycleMonitor> remotePeer;
+
+	public ObjectLifecycleMonitor sync() {
+		ensureInited();
+		return remotePeer.sync();
+	}
+
+	public ObjectLifecycleMonitor async() {
+		ensureInited();
+		return remotePeer.async();
+	}
+	
+	private void ensureInited() {
+		if (remotePeer == null) {
+			init();
+		}
+	}
+
+	public void stop() {
+		running = false;
+		if (socket == null) {
+			return;
+		}
+		try {
+			socket.close();
+		} catch (IOException e) {
+		}
+	}
+	
+	public void join() throws InterruptedException {
+		if (thread == null) {
+			return;
+		}
+		thread.join();
+	}
+	
+	private synchronized void init() {
+		
+		if (remotePeer != null) {
+			return;
+		}
+
+		try {
+			socket = connect();
+
+			if (socket == null) {
+			}
+
+			final DataOutputStream out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+			final DataInputStream in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+
+			remotePeer = new Distributor<ObjectLifecycleMonitor>(new ByteArrayConsumer() {
+
+				public void consume(byte[] buffer, int offset, int length) throws IOException {
+					out.writeInt(length);
+					out.write(buffer, offset, length);
+					out.flush();
+				}
+			}, ObjectLifecycleMonitor.class);
+			
+			thread = new Thread("TcpClient receiver thread") {
+				@Override
+				public void run() {
+					try {
+						while(true) {
+							TcpCommunicationNetwork.feed(in, remotePeer);
+						}
+					} catch (IOException e) {
+//						e.printStackTrace();
+					}
+				}
+			};
+			thread.setDaemon(true);
+			thread.start();
+			
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+	
+	private Socket connect() throws IOException {
+		Socket s = null;
+		while (running) {
+			try {
+				s = new Socket(TcpCommunicationNetwork.HOST, TcpCommunicationNetwork.PORT);
+				break;
+			} catch (ConnectException e) {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e1) {
+					throw new RuntimeException(e1);
+				}
+			} catch (UnknownHostException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return s;
+	}
+}
