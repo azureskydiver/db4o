b0b6d0de5fae4866acf254b59162bca4a2d2a2fc
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2010-09-03 15:08:36 +0000

    acv/pr: DRS-155 - implement db id deletion at jvi level

--- drs/src/test/com/db4o/drs/test/versant/VodJviTestCase.java
+++ drs/src/test/com/db4o/drs/test/versant/VodJviTestCase.java
@@ -29,9 +29,23 @@ public class VodJviTestCase extends VodDatabaseTestCaseBase implements TestLifeC
 	
 	public void testNewDbId(){
 		// using the same name twice will fail
-		Assert.isGreater(0, _jvi.newDbId("VodJviTestCase.test" + System.currentTimeMillis() ));
+		String name = VodJvi.safeDatabaseName("VodJviTestCase.test" + System.currentTimeMillis());
+		short id = _jvi.newDbId(name);
+		Assert.isGreater(0, id);
+		Assert.areEqual(id, _jvi.dbIdFor(name));
 	}
 
+	public void testDeleteDbId() {
+		final String name = VodJvi.safeDatabaseName("VodJviTestCase.test" + System.currentTimeMillis());
+		_jvi.newDbId(name);
+		_jvi.deleteDbId(name);
+		Assert.expect(IllegalArgumentException.class, new CodeBlock() {
+			public void run() throws Throwable {
+				_jvi.dbIdFor(name);
+			}
+		});
+	}
+	
 	public void setUp() throws Exception {
 		_jvi = new VodJvi(_vod);
 	}
