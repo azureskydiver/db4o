e34388850ea484e1d328a391bcec815ac12ca4e3
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2010-10-02 17:16:58 +0000

    cr: default recursion depth of 5 and default reflector

--- db4oj/core/src/com/db4o/qlin/Prototypes.java
+++ db4oj/core/src/com/db4o/qlin/Prototypes.java
@@ -45,15 +45,16 @@ public class Prototypes {
 	
 	private final int _recursionDepth;
 	
-	public Prototypes() {
-		this(null, 0, false);
-	}	
-	
 	public Prototypes(Reflector reflector, int recursionDepth, boolean ignoreTransient){
 		_reflector = reflector;
 		_recursionDepth = recursionDepth;
 		_ignoreTransient = ignoreTransient;
 	}
+	
+	public Prototypes() {
+		this(defaultReflector(), 5, false);
+	}	
+	
 	
 	/**
 	 * returns a prototype object for a specific class.
