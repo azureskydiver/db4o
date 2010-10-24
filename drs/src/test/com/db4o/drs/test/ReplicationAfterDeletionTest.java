c2a6418ae89335a7b9aa916a64fa0bd683d3770a
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2010-10-24 13:39:31 +0000

    cr: DRS-150 ReplicationAfterDeletion passes now

--- drs/src/test/com/db4o/drs/test/ReplicationAfterDeletionTest.java
+++ drs/src/test/com/db4o/drs/test/ReplicationAfterDeletionTest.java
@@ -40,7 +40,7 @@ public class ReplicationAfterDeletionTest extends DrsTestCase {
 		SPCChild child = new SPCChild("c1");
 		SPCParent parent = new SPCParent(child, "p1");
 		a().provider().storeNew(parent);
-		a().provider().commit();
+		a().provider().commitAndWaitFor(parent);
 
 		replicateAll(a().provider(), b().provider());
 	}
