a7752806f3070211625c730bb397d21b8dc8e2d1
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2010-10-28 10:34:13 +0000

    cr: setting verbose to false in the default setting since the logging hasn't been helpful

--- drs/src/core/com/db4o/drs/inside/DrsDebug.java
+++ drs/src/core/com/db4o/drs/inside/DrsDebug.java
@@ -8,7 +8,7 @@ public class DrsDebug {
 	
 	public static final boolean runEventListenerEmbedded = !production;
 	
-	public static final boolean verbose = !production;
+	public static final boolean verbose = false;
 
 	
 	public static long timeout(long designed) {
