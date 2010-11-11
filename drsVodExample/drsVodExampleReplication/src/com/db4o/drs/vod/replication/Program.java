519f64d452921ccd5f1ce7c7e26c4f218aaf97e5
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2010-11-11 18:14:49 +0000

    cr + frm: Project cleanup. Implemented logging database content.

--- drsVodExample/drsVodExampleReplication/src/com/db4o/drs/vod/replication/Program.java
+++ drsVodExample/drsVodExampleReplication/src/com/db4o/drs/vod/replication/Program.java
@@ -1,5 +1,11 @@
-package com.db4o.drs.vod.replication;
 /* Copyright (C) 2004 - 2010  Versant Inc.  http://www.db4o.com */
+package com.db4o.drs.vod.replication;
+
+import java.util.*;
+
+import javax.jdo.*;
+import com.db4o.drs.vod.example.*;
+import com.db4o.drs.vod.example.data.*;
 
 public class Program {
 
@@ -7,13 +13,21 @@ public class Program {
 		new Program().run();
 	}
 
+	private PersistenceManager _pm;
+
 	private void run() {
+		_pm = JdoConnector.getPersistenceManager();
+		_pm.currentTransaction().begin();
+		printVodDatabaseContent();
 		
+		_pm.currentTransaction().commit();
+		_pm.close();
 	}
+		
 
 	private void printVodDatabaseContent(){
-		
-		
+		Collection<Contract> contracts = (Collection<Contract>) _pm.newQuery(Contract.class).execute();
+		System.out.println(contracts.toString());
 		
 		
 	}
