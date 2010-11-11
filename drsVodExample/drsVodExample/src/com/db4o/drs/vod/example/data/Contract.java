727c326641de72475f10ceefe044cd11bb11ba13
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2010-11-11 16:36:55 +0000

    cr + pr: #DRS-190 initial checkin of sample projects

---
+++ drsVodExample/drsVodExample/src/com/db4o/drs/vod/example/data/Contract.java
@@ -0,0 +1,23 @@
+/* Copyright (C) 2004 - 2010  Versant Inc.  http://www.db4o.com */
+
+package com.db4o.drs.vod.example.data;
+
+public class Contract {
+	
+	private String _name;
+	
+	private Customer _customer;
+	
+	private int _amount;
+	
+	public Contract(){
+		
+	}
+	
+	public Contract(String name, Customer customer, int amount) {
+		_name = name;
+		_customer = customer;
+		_amount = amount;
+	}
+
+}
---
+++ drsVodExample/drsVodExampleReplication/src/com/db4o/drs/vod/replication/Program.java
@@ -0,0 +1,20 @@
+package com.db4o.drs.vod.replication;
+/* Copyright (C) 2004 - 2010  Versant Inc.  http://www.db4o.com */
+
+public class Program {
+
+	public static void main(String[] args) {
+		new Program().run();
+	}
+
+	private void run() {
+		
+	}
+
+	private void printVodDatabaseContent(){
+		
+		
+		
+		
+	}
+}
