643502e98db14b1c4bb72b257763cee2cf51597a
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2010-11-12 14:05:40 +0000

    deleting drsVodExample, will recreate

--- sandbox/drsVodExample/scripts/startEventProcessor.bat
+++
@@ -1,2 +0,0 @@
-call setEnvironment.bat
-start java -cp %LOCAL_CLASSPATH% com.db4o.drs.versant.eventprocessor.EventProcessorApplication -client localhost -clientport %CLIENT_PORT% -database %DATABASE_NAME% -eventProcessorHost localhost -eventProcessorPort %EVENTPROCESSOR_PORT% -logfile %LOG_FILE% -server localhost -serverport %SERVER_PORT% -verbose false
