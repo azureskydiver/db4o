0fa94360b49a15c2381db2bb74e68fddda78d00e
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2010-11-16 13:21:44 +0000

    cr + acv: #DRS-190 Adjusted paths in setEnvironment, modified README.txt

--- sandbox/drsVodExample/scripts/setEnvironment.bat
+++ sandbox/drsVodExample/scripts/setEnvironment.bat
@@ -1,16 +1,22 @@
 
+SET EXAMPLE_HOME=%CD%\..
 
 SET JAVA=java
-REM In case you need a different VM 
 REM SET JAVA="C:\Program Files (x86)\Java\jdk1.6.0_17\bin\java.exe"
 
-SET EXAMPLE_HOME=D:\Workspaces\trunk\drsVodExample
+SET DB4O_VERSION=8.0.173.15095
+
+SET CLI=%EXAMPLE_HOME%\lib\commons-cli-1.2.jar
+SET DB4O_JAR=%EXAMPLE_HOME%\lib\db4o-%DB4O_VERSION%-all-java5.jar
+SET DRS_PATH=%EXAMPLE_HOME%\lib\dRS-%DB4O_VERSION%-core.jar
+
+REM delete the following before releasing
+SET PATCH_JAR=%EXAMPLE_HOME%\lib\vod8.drs.patch.jar
+
+REM for debugging:
+REM SET DRS_PATH=D:/workspaces/trunk/drs/bin
 
 SET DATABASE_NAME=dRSVodExample
-
-SET SERVER_PORT=4000
-SET CLIENT_PORT=4100
-SET EVENTPROCESSOR_PORT=4200
 
 SET VERSANT_BIN=%VERSANT_ROOT%\bin
 SET VERSANT_LIB=%VERSANT_ROOT%\lib
@@ -23,17 +29,9 @@ SET ASM=%VERSANT_LIB%\asm-all-3.1.jar
 SET VED_CONFIGFILE=config.ved.win
 SET LOG_FILE="drsLogFile.log"
 
-SET DB4O_VERSION=8.0.173.15095
-
-SET CLI=%EXAMPLE_HOME%\lib\commons-cli-1.2.jar
-SET DB4O_JAR=%EXAMPLE_HOME%\lib\db4o-%DB4O_VERSION%-all-java5.jar
-SET DRS_PATH=%EXAMPLE_HOME%\lib\dRS-%DB4O_VERSION%-core.jar
-
-REM for debugging:
-REM SET DRS_PATH=D:/workspaces/trunk/drs/bin
-
-REM delete the following before releasing
-SET PATCH_JAR=%EXAMPLE_HOME%\lib\vod8.drs.patch.jar
+SET SERVER_PORT=4000
+SET CLIENT_PORT=4100
+SET EVENTPROCESSOR_PORT=4200
 
 SET LOCAL_CLASSPATH=%PATCH_JAR%;%VOD_JDO%;%JVI%;%DRS_PATH%;%CLI%;%JDO_JAR%;%ASM%;%DB4O_JAR%;
 
