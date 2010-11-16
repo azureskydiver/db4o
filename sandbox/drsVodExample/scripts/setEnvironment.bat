af2a61e93b03c7666a97a6e07162db471439d9d0
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2010-11-16 12:31:46 +0000

    cr: removed dependance to PIZZA, it's not needed.

--- sandbox/drsVodExample/scripts/setEnvironment.bat
+++ sandbox/drsVodExample/scripts/setEnvironment.bat
@@ -1,6 +1,8 @@
 
-REM SET JAVA="C:\Program Files (x86)\Java\jdk1.6.0_17\bin\java.exe"
+
 SET JAVA=java
+REM In case you need a different VM 
+REM SET JAVA="C:\Program Files (x86)\Java\jdk1.6.0_17\bin\java.exe"
 
 SET EXAMPLE_HOME=D:\Workspaces\trunk\drsVodExample
 
@@ -16,7 +18,6 @@ SET VERSANT_LIB=%VERSANT_ROOT%\lib
 SET JVI=%VERSANT_LIB%\jvi80.jar
 SET VOD_JDO=%VERSANT_LIB%\vodjdo.jar
 SET JDO_JAR=%VERSANT_LIB%\jdo2-api-2.1.jar
-SET PIZZA=%VERSANT_LIB%\pizza.jar
 SET ASM=%VERSANT_LIB%\asm-all-3.1.jar
 
 SET VED_CONFIGFILE=config.ved.win
@@ -27,11 +28,12 @@ SET DB4O_VERSION=8.0.173.15095
 SET CLI=%EXAMPLE_HOME%\lib\commons-cli-1.2.jar
 SET DB4O_JAR=%EXAMPLE_HOME%\lib\db4o-%DB4O_VERSION%-all-java5.jar
 SET DRS_PATH=%EXAMPLE_HOME%\lib\dRS-%DB4O_VERSION%-core.jar
+
 REM for debugging:
 REM SET DRS_PATH=D:/workspaces/trunk/drs/bin
 
 REM delete the following before releasing
 SET PATCH_JAR=%EXAMPLE_HOME%\lib\vod8.drs.patch.jar
 
-SET LOCAL_CLASSPATH=%PATCH_JAR%;%VOD_JDO%;%JVI%;%DRS_PATH%;%CLI%;%PIZZA%;%JDO_JAR%;%ASM%;%DB4O_JAR%;
+SET LOCAL_CLASSPATH=%PATCH_JAR%;%VOD_JDO%;%JVI%;%DRS_PATH%;%CLI%;%JDO_JAR%;%ASM%;%DB4O_JAR%;
 
