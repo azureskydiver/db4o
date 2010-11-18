d7fd2de8ca4c976fe2a901e65159a7aeca3d51e0
fabioroger <fabioroger@915b37e7-0715-0410-8bc3-b14b59444072>
2010-11-18 13:16:04 +0000

    frm+cr: DRS-200 - added a bytecode transformer to be able to enhance classes from jar files on the fly.

--- drs/src/core/com/db4o/drs/versant/enhancer/EnhancerDebug.java
+++ drs/src/core/com/db4o/drs/versant/enhancer/EnhancerDebug.java
@@ -1,5 +1,8 @@
 package com.db4o.drs.versant.enhancer;
 
+/**
+ * @sharpen.ignore
+ */
 public class EnhancerDebug {
 
 	public static final boolean verbose = true;
--- drs/src/core/com/db4o/drs/versant/enhancer/EnhancerMain.java
+++ drs/src/core/com/db4o/drs/versant/enhancer/EnhancerMain.java
@@ -6,18 +6,24 @@ import java.io.*;
 
 import com.versant.core.jdo.tools.enhancer.*;
 
+/**
+ * @sharpen.ignore
+ */
 public class EnhancerMain {
 	
 	public static void main(String[] args) throws Exception {
 		
-		if(args == null || args.length != 1){
-			throw new RuntimeException("Expected one argument: PropertiesFilePath");
+		if(args == null || args.length < 1 || args.length > 2){
+			throw new RuntimeException("Expected one or two argument: PropertiesFilePath [outputDirectory]");
 		}
 		String propertiesFilePath = args[0];
 		
 		
 		
 		Enhancer enhancer = new Enhancer();
+		if (args.length == 2) {
+			enhancer.setOutputDir(new File(args[1]));
+		}
 		enhancer.setPropertiesFile(new File(propertiesFilePath));
 		enhancer.enhance();
 	}
