26f6d9e060ad0930c1a827b89b7292589aef8ac9
fabioroger <fabioroger@915b37e7-0715-0410-8bc3-b14b59444072>
2010-11-18 13:15:27 +0000

    frm+cr: added directory crawler facility in Path4, to visit each file.

--- db4oj.tests/src/com/db4o/db4ounit/common/foundation/Path4TestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/foundation/Path4TestCase.java
@@ -2,6 +2,10 @@
 
 package com.db4o.db4ounit.common.foundation;
 
+import java.io.*;
+import java.util.*;
+
+import com.db4o.foundation.*;
 import com.db4o.foundation.io.*;
 
 import db4ounit.*;
@@ -17,6 +21,52 @@ public class Path4TestCase implements TestCase{
 	public void testGetTempFileName(){
 		String tempFileName = Path4.getTempFileName();
 		Assert.isTrue(File4.exists(tempFileName));
+		File4.delete(tempFileName);
+	}
+	
+	/**
+	 * @sharpen.if !SILVERLIGHT
+	 */
+	public void testForEachFile() throws FileNotFoundException {
+		
+		String tempFileName = Path4.getTempFileName();
+		File root = new File(tempFileName);
+		root.delete();
+		root.mkdirs();
+		createDirAndFile(createDirAndFile(createDirAndFile(root)));
+		
+		final Set<String> canonicalFilePaths = new HashSet<String>();
+		
+		Path4.forEachFile(root.getAbsolutePath(), new Procedure4<Pair<String,File>>() {
+			public void apply(Pair<String, File> value) {
+				if (value.second.isFile()) {
+					canonicalFilePaths.add(value.first);
+				}
+			}
+		});
+		Assert.isTrue(canonicalFilePaths.remove("dir/file.txt"));
+		Assert.isTrue(canonicalFilePaths.remove("dir/dir/file.txt"));
+		Assert.isTrue(canonicalFilePaths.remove("dir/dir/dir/file.txt"));
+		Assert.isTrue(canonicalFilePaths.isEmpty());
+		
+		Path4.forEachFile(root.getAbsolutePath(), new Procedure4<Pair<String,File>>() {
+			public void apply(Pair<String, File> value) {
+				value.second.delete();
+			}
+		});
+		
+		root.delete();
+	}
+
+	private File createDirAndFile(File root) throws FileNotFoundException {
+		File dir = new File(root, "dir");
+		dir.mkdir();
+		File f = new File(dir, "file.txt");
+		PrintWriter out = new PrintWriter(f);
+		out.println("some text");
+		out.flush();
+		out.close();
+		return dir;
 	}
 	
 }
