ba4f54f5e0f1a086df4b93bb2d1b935f9db5232b
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2010-11-18 16:19:30 +0000

    cr  #DRS-200 Removing #forEachFile from Path4 since it's very specific and won't sharpen.

--- db4oj.tests/src/com/db4o/db4ounit/common/foundation/Path4TestCase.java
+++ db4oj.tests/src/com/db4o/db4ounit/common/foundation/Path4TestCase.java
@@ -2,10 +2,6 @@
 
 package com.db4o.db4ounit.common.foundation;
 
-import java.io.*;
-import java.util.*;
-
-import com.db4o.foundation.*;
 import com.db4o.foundation.io.*;
 
 import db4ounit.*;
@@ -22,52 +18,6 @@ public class Path4TestCase implements TestCase{
 		String tempFileName = Path4.getTempFileName();
 		Assert.isTrue(File4.exists(tempFileName));
 		File4.delete(tempFileName);
-	}
-	
-	/**
-	 * @sharpen.if !SILVERLIGHT
-	 */
-	public void testForEachFile() throws FileNotFoundException {
-		
-		String tempFileName = Path4.getTempFileName();
-		File root = new File(tempFileName);
-		root.delete();
-		root.mkdirs();
-		createDirAndFile(createDirAndFile(createDirAndFile(root)));
-		
-		final Set<String> canonicalFilePaths = new HashSet<String>();
-		
-		Path4.forEachFile(root.getAbsolutePath(), new Procedure4<Pair<String,File>>() {
-			public void apply(Pair<String, File> value) {
-				if (value.second.isFile()) {
-					canonicalFilePaths.add(value.first);
-				}
-			}
-		});
-		Assert.isTrue(canonicalFilePaths.remove("dir/file.txt"));
-		Assert.isTrue(canonicalFilePaths.remove("dir/dir/file.txt"));
-		Assert.isTrue(canonicalFilePaths.remove("dir/dir/dir/file.txt"));
-		Assert.isTrue(canonicalFilePaths.isEmpty());
-		
-		Path4.forEachFile(root.getAbsolutePath(), new Procedure4<Pair<String,File>>() {
-			public void apply(Pair<String, File> value) {
-				value.second.delete();
-			}
-		});
-		
-		root.delete();
-	}
-
-	private File createDirAndFile(File root) throws FileNotFoundException {
-		File dir = new File(root, "dir");
-		dir.mkdir();
-		File f = new File(dir, "file.txt");
-		try {
-			f.createNewFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return dir;
 	}
 	
 }
