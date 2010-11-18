eb78d5cd576dd68f9b3db35a0e63b89ccee4859b
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2010-11-18 14:48:13 +0000

    cr: making the dRS build pass for .NET, part 2

--- drs/src/test/com/db4o/drs/test/Db4oDrsTestSuiteBuilder.java
+++ drs/src/test/com/db4o/drs/test/Db4oDrsTestSuiteBuilder.java
@@ -11,30 +11,26 @@ public class Db4oDrsTestSuiteBuilder implements TestSuiteBuilder {
 	}
 	
 	public Iterator4 iterator() {
-		
 		return Iterators.concat(
+			Iterators.concat(
+				new DrsTestSuiteBuilder(
+					new Db4oDrsFixture("db4o-a"),
+					new Db4oDrsFixture("db4o-b"),
+					Db4oDrsTestSuite.class).iterator(),
+				new DrsTestSuiteBuilder(
+					new Db4oClientServerDrsFixture("db4o-cs-a", 0xdb40), 
+					new Db4oClientServerDrsFixture("db4o-cs-b", 4455),
+					Db4oDrsTestSuite.class).iterator()),
+			Iterators.concat(
+				new DrsTestSuiteBuilder(
+					new Db4oDrsFixture("db4o-a"),
+					new Db4oClientServerDrsFixture("db4o-cs-b", 4455),
+					Db4oDrsTestSuite.class).iterator(),
 			
-			new DrsTestSuiteBuilder(
-				new Db4oDrsFixture("db4o-a"),
-				new Db4oDrsFixture("db4o-b"),
-				Db4oDrsTestSuite.class),
-				
-			new DrsTestSuiteBuilder(
-				new Db4oClientServerDrsFixture("db4o-cs-a", 0xdb40), 
-				new Db4oClientServerDrsFixture("db4o-cs-b", 4455),
-				Db4oDrsTestSuite.class),
-				
-			new DrsTestSuiteBuilder(
-				new Db4oDrsFixture("db4o-a"),
-				new Db4oClientServerDrsFixture("db4o-cs-b", 4455),
-				Db4oDrsTestSuite.class),
-	
-			new DrsTestSuiteBuilder(
-				new Db4oClientServerDrsFixture("db4o-cs-a", 4455), 
-				new Db4oDrsFixture("db4o-b"), 
-				Db4oDrsTestSuite.class)
-			
-		).iterator();
+				new DrsTestSuiteBuilder(
+					new Db4oClientServerDrsFixture("db4o-cs-a", 4455), 
+					new Db4oDrsFixture("db4o-b"), 
+					Db4oDrsTestSuite.class).iterator()));
 	}
 
 }
