9ea19659f6272ae6779dd27591a54a7f8f81790d
patrick_roemer <patrick_roemer@915b37e7-0715-0410-8bc3-b14b59444072>
2011-02-02 19:38:51 +0000

    OMJ-181 - using storedfield api for indexing rather than config, skipped #isIndexable() check

--- enterprise/omj/com.db4o.ome/src/com/db4o/omplus/datalayer/propertyViewer/ConfigureIndex.java
+++ enterprise/omj/com.db4o.ome/src/com/db4o/omplus/datalayer/propertyViewer/ConfigureIndex.java
@@ -9,61 +9,28 @@ import com.db4o.reflect.*;
 
 public class ConfigureIndex {
 	
-	IDbInterface db;
+	DatabaseModel dbModel;
 	
-	public ConfigureIndex(IDbInterface db) {
-		this.db = db;
-	}
-	
-	public static boolean isLocal(){
-		return (!Activator.getDefault().dbModel().db().isClient());
-	}
-	
-	// FIXME should use ConnectionParams
-	private void reconnect(){
-		String path = Activator.getDefault().dbModel().db().getDbPath();
-		Activator.getDefault().dbModel().disconnect();
-		ObjectContainer oc = Db4o.openFile(path);
-		Activator.getDefault().dbModel().connect(oc, path);
-	}
-	
-	private boolean isIndexable(StoredField storedField, ObjectContainer db) {
-		
-		ReflectClass storedType = null;
-		try {
-			storedType = storedField.getStoredType();
-		}catch(Exception e){
-			
-		}
-		if (storedType != null) { // primitive arrays return null
-			if (storedType.isPrimitive() ) {
-				return true;
-			}
-		}
-		return false;
+	public ConfigureIndex(DatabaseModel dbModel) {
+		this.dbModel = dbModel;
 	}
 	
 	public void createIndex(ClassProperties clsProperties){
-		boolean reconnect = false;
-		ReflectClass clazz = db.reflectHelper().getReflectClazz(clsProperties.getClassname());
-		StoredClass storedClz = db.getStoredClass(clazz.getName());
+		ReflectClass clazz = dbModel.db().reflectHelper().getReflectClazz(clsProperties.getClassname());
+		StoredClass storedClz = dbModel.db().getStoredClass(clazz.getName());
 		if(storedClz != null) {
 			for(FieldProperties field : clsProperties.getFields()) {
 				StoredField sField = storedClz.storedField(field.getFieldName(), storedClz);
-				if(isIndexable(sField, db.getDB())) {
-					index(clazz, field.getFieldName(), field.isIndexed());
-					if(!reconnect)
-						reconnect = true;
+				if(sField.hasIndex() != field.isIndexed()) {
+					if(field.isIndexed()) {
+						sField.createIndex();
+					}
+					else {
+						sField.dropIndex();
+					}
 				}
 			}
-			if(reconnect)
-				reconnect();
 		}
 	}
 	
-	@SuppressWarnings("deprecation")
-	private void index(ReflectClass clazz, String fieldName, boolean isIndexed){
-		Db4o.configure().objectClass(clazz).objectField(fieldName).indexed(isIndexed);
-	}
-
 }
