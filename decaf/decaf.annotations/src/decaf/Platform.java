71b9d6b9321fb1667f7d23feedfd8dc50fbbd209
fabioroger <fabioroger@915b37e7-0715-0410-8bc3-b14b59444072>
2011-03-04 14:55:05 +0000

    frm+acv: DCF-49 - added platform compatibility checking for decaf

--- decaf/decaf.annotations/src/decaf/Platform.java
+++ decaf/decaf.annotations/src/decaf/Platform.java
@@ -1,13 +1,66 @@
 package decaf;
 
-public enum Platform {
+interface NegatablePlatform {
+	void fillCompatibility(boolean[] compatibility, boolean compatible);
+}
 
+public enum Platform implements NegatablePlatform {
+
+	JMX,
+	
 	JDK11,
-	JDK12,
-	JDK15,
-	JDK16,
-	ANDROID,
-	SHARPEN,
-	ALL
+	JDK12(JDK11),
+	JDK15(JDK12, JMX),
+	JDK16(JDK15),
+	
+	ANDROID(JDK15, JMX.not()),
+	SHARPEN(JDK15),
+	
+	ALL;
+	
+	private final NegatablePlatform[] parents;
+	private boolean[] compatibility = null;
+
+	Platform(NegatablePlatform... parent) {
+		this.parents = parent;
+	}
+
+	public boolean compatibleWith(Platform other) {
+		return compatibility()[other.ordinal()];
+	}
+
+	public boolean[] compatibility() {
+		if (compatibility != null) {
+			return compatibility;
+		}
+		compatibility = new boolean[Platform.values().length];
+
+		fillCompatibility(compatibility, true);
+
+		return compatibility;
+	}
+	
+	public void fillCompatibility(boolean[] compatibility, boolean compatible) {
+		compatibility[ordinal()] = compatible;
+		for (NegatablePlatform parent : parents) {
+			parent.fillCompatibility(compatibility, compatible);
+		}
+	}
+
+	NegatablePlatform not() {
+		return new Negate(this);
+	}
+
+	public static class Negate implements NegatablePlatform {
+		private final Platform platform;
+
+		Negate(Platform platform) {
+			this.platform = platform;
+		}
+
+		public void fillCompatibility(boolean[] compatibility, boolean value) {
+			platform.fillCompatibility(compatibility, false);
+		}
+	}
 
 }
