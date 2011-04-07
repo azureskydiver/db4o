ef0ef49c48f269768e017719205df15b5a0e2ec9
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2011-04-07 19:13:34 +0000

    db4o.net update

--- db4o.net/Db4objects.Db4o.CS.Optional/Properties/AssemblyInfo.cs
+++ db4o.net/Db4objects.Db4o.CS.Optional/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Security;
 
 [assembly: ComVisible (false)]
 
-[assembly: AssemblyVersion("8.1.193.14430")]
+[assembly: AssemblyVersion("8.1.194.14430")]
 
 #if !CF && !SILVERLIGHT
 [assembly: AllowPartiallyTrustedCallers]
