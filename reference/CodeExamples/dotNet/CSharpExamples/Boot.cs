f35954662a1fe4a8a83e46bc769dec52a7fb81ff
roman_stoffel <roman_stoffel@915b37e7-0715-0410-8bc3-b14b59444072>
2011-07-08 15:00:50 +0000

    rs: DOC-652 Document Traverse-Index API

--- reference/CodeExamples/dotNet/CSharpExamples/Boot.cs
+++ reference/CodeExamples/dotNet/CSharpExamples/Boot.cs
@@ -1,13 +1,15 @@
 
 using System;
 using Db4oDoc.Code.Concurrency.Transactions;
+using Db4oDoc.Code.indexing.traverse;
+
 namespace Db4oDoc
 {
     public class Boot
     {
         public static void Main(string[] args)
         {
-            TransactionOperations.Main(args);
+            TraverseIndexExample.Main(args);
 
             Console.Read();
         }
