fc1e5f9e072f36a6383c046593fe8f018c30ccff
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2011-10-14 11:52:59 +0000

    cr + pr: Check for isClosed() in ObjectContainerBase#close() was wrong.

--- db4oj/core/src/com/db4o/internal/ObjectContainerBase.java
+++ db4oj/core/src/com/db4o/internal/ObjectContainerBase.java
@@ -337,9 +337,6 @@ public abstract class ObjectContainerBase  implements TransientClass, Internal4,
 
     final public boolean close() {
 		synchronized (_lock) {
-			if(isClosed()){
-				return false;
-			}
 			callbacks().closeOnStarted(this);
 			if(DTrace.enabled){
 				DTrace.CLOSE_CALLED.log(this.toString());
