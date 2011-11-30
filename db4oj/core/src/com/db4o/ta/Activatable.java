5d42ebb84e5e2df439bdc0e9ade5488370bd2a6f
roman_stoffel <roman_stoffel@915b37e7-0715-0410-8bc3-b14b59444072>
2011-11-30 09:23:09 +0000

    rs: Javadoc updates

--- db4oj/core/src/com/db4o/ta/Activatable.java
+++ db4oj/core/src/com/db4o/ta/Activatable.java
@@ -4,119 +4,147 @@ package com.db4o.ta;
 import com.db4o.activation.*;
 
 /**
- * Activatable must be implemented by classes in order to support Transparent
- * Activation.<br>
+ * The Activatable interface must be implemented by classes in order to support transparent
+ * activation/persistence.<br>
  * <br>
  * 
  * The Activatable interface may be added to persistent classes by hand or by
  * using the db4o enhancer. For further information on the enhancer see the
- * chapter "Enhancement" in the db4o tutorial.<br>
+ * chapter "Enhancement" in the db4o reference documentation.<br>
  * <br>
  * 
- * The basic idea for Transparent Activation is as follows:<br>
- * Objects have an activation depth of 0, i.e. by default they are not activated
+ * The basic idea for Transparent Activation is:<br>
+ * Objects have an activation depth of 0, so they are not activated
  * at all. Whenever a method is called on such an object, the first thing to do
- * before actually executing the method body is to activate the object to level
- * 1, i.e. populating its direct members.<br>
+ * before actually executing the method body is to activate the object to
+ * populating its direct members.<br>
  * <br>
  * 
  * To illustrate this approach, we will use the following simple class.<br>
- * <br>
- * <code>
- * public class Item {<br>
- * &#160;&#160;&#160;private Item _next;<br><br>
+ * <pre class="prettyprint"><code>
+ * public class Item {
+ *      private Item next;
  *   
- * &#160;&#160;&#160;public Item(Item next) {<br>
- * &#160;&#160;&#160;&#160;&#160;&#160;_next = next;<br>
- * &#160;&#160;&#160;}<br><br>
+ *      public Item(Item next) {
+ *          this.next = next;
+ *      }
  *   
- * &#160;&#160;&#160;public Item next() {<br>
- * &#160;&#160;&#160;&#160;&#160;&#160;return _next;<br>
- * &#160;&#160;&#160;}<br>
- * }<br><br></code>
+ *      public Item next() {
+ *          return next;
+ *      }
+ *      public void setNext(Item itemToBeSet) {
+ *          this.next = itemToBeSet;
+ *      }
+ *      // ...
+ * }</code></pre>
  * 
  * The basic sequence of actions to get the above scheme to work is the
  * following:<br>
  * <br>
  * 
- * - Whenever an object is instantiated from db4o, the database registers an
+ * Whenever an object is instantiated from db4o, the database registers an
  * activator for this object. To enable this, the object has to implement the
  * Activatable interface and provide the according bind(Activator) method. The
  * default implementation of the bind method will simply store the given
  * activator reference for later use.<br>
- * <br>
- * <code>
- * public class Item implements Activatable {<br>
- * &#160;&#160;&#160;transient Activator _activator;<br><br>
+ * <pre class="prettyprint"><code>
+ * public class Item implements Activatable {
+ *     private Item next;
+ *     private transient Activator activator;
  * 
- * &#160;&#160;&#160;public void bind(Activator activator) {<br>
- * &#160;&#160;&#160;&#160;&#160;&#160;if (null != _activator) {<br>
- * &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;throw new IllegalStateException();<br>
- * &#160;&#160;&#160;&#160;&#160;&#160;}<br>
- * &#160;&#160;&#160;&#160;&#160;&#160;_activator = activator;<br>
- * &#160;&#160;&#160;}<br><br>
- *   
- * &#160;&#160;&#160;// ...<br>
- * }<br><br></code>
+ *     public void bind(Activator activator) {
+ *         if (this.activator == activator) {
+ *              return;
+ *         }
+ *         if (activator != null && null != this.activator) {
+ *              throw new IllegalStateException("Object can only be bound to one activator");
+ *         }
+ *         this.activator = activator;
+ *     }
+ *     public void activate(ActivationPurpose activationPurpose) {
+ *         if(null!=activator){
+ *              activator.activate(activationPurpose);
+ *         }
+ *     }
+ *     // ...
+ * }</code></pre>
  * 
- * - The first action in every method body of an activatable object should be a
- * call to the corresponding Activator's activate() method. (Note that this is
- * not enforced by any interface, it is rather a convention, and other
- * implementations are possible.)<br>
- * <br>
- * <code>
- * public class Item implements Activatable {<br>
- * &#160;&#160;&#160;public void activate() {<br>
- * &#160;&#160;&#160;&#160;&#160;&#160;if (_activator == null) return;<br>
- * &#160;&#160;&#160;&#160;&#160;&#160;_activator.activate();<br>
- * &#160;&#160;&#160;}<br><br>
- * 
- * &#160;&#160;&#160;public Item next() {<br>
- * &#160;&#160;&#160;&#160;&#160;&#160;activate();<br>
- * &#160;&#160;&#160;&#160;&#160;&#160;return _next;<br>
- * &#160;&#160;&#160;}<br>
- * }<br><br></code>
- * 
- * - The activate() method will check whether the object is already activated.
+ * The first action in every method body of an activatable object should be a
+ * call to the corresponding Activator's activate() method with the given purpose.
+ * If the method is reading the purpose should be {@link ActivationPurpose#READ},
+ * for writing {@link ActivationPurpose#WRITE}. <br>
+ * <pre class="prettyprint"><code>
+ * public class Item implements Activatable {
+ *     private Item next;
+ *     private transient Activator activator;
+ *
+ *     public void bind(Activator activator) {
+ *         if (this.activator == activator) {
+ *              return;
+ *         }
+ *         if (activator != null && null != this.activator) {
+ *              throw new IllegalStateException("Object can only be bound to one activator");
+ *         }
+ *         this.activator = activator;
+ *     }
+ *     public void activate(ActivationPurpose activationPurpose) {
+ *         if(null!=activator){
+ *              activator.activate(activationPurpose);
+ *         }
+ *     }
+ *     public Item next() {
+ *         activate(ActivationPurpose.READ);
+ *         return next;
+ *     }
+ *     public void setNext(Item itemToBeSet) {
+ *         activate(ActivationPurpose.Write);
+ *         this.next = itemToBeSet;
+ *     }
+ *     // ...
+ * }</code></pre>
+ *
+ * You always need to call activate() before any data access in the object. Otherwise transparent activation / persistence will not work.
+ * Since this process is error prone we recommend to use the enhancer tools shipped with db4o.
+ *
+ * The activate() method will check whether the object is already activated.
  * If this is not the case, it will request the container to activate the object
  * to level 1 and set the activated flag accordingly.<br>
  * <br>
- * 
+ *
  * To instruct db4o to actually use these hooks (i.e. to register the database
- * when instantiating an object), TransparentActivationSupport has to be
+ * when instantiating an object), {@link TransparentActivationSupport} or {@link TransparentPersistenceSupport}  has to be
  * registered with the db4o configuration.<br>
  * <br>
- * <code>
- * Configuration config = ...<br>
- * config.add(new TransparentActivationSupport());<br><br>
- * </code>
+ * <pre class="prettyprint"><code>
+ * EmbeddedConfiguration config = ... // your configuration
+ * configuration.common().add(new TransparentActivationSupport());
+ * </code></pre>
  * 
- * Java: If you implement this interface manually and intend to pass this class through
+ * If you implement this interface manually and intend to pass this class through
  * the db4o bytecode instrumentation process, make sure you also implement the
  * {@link ActivatableInstrumented} marker interface.
  */
 public interface Activatable {
 
 	/**
-	 * called by db4o upon instantiation. <br>
-	 * <br>
+	 * Called by db4o after the object instantiation.
+     * This method is called to bind the object to the current activator<br/>
+	 * <br/>
 	 * The recommended implementation of this method is to store the passed
 	 * {@link Activator} in a transient field of the object.
 	 * 
-	 * @param activator
-	 *            the Activator
+	 * @param activator the Activator instance to bind
 	 */
 	void bind(Activator activator);
 
 	/**
-	 * should be called by every reading field access of an object. <br>
-	 * <br>
+	 * Should be called by every reading field access of an object. <br/>
+	 * <br/>
 	 * The recommended implementation of this method is to call
 	 * {@link Activator#activate(ActivationPurpose)} on the {@link Activator}
 	 * that was previously passed to {@link #bind(Activator)}.
 	 * 
-	 * @param purpose
-	 *            TODO
+	 * @param purpose Whereever this object is accessed to read or write. See {@link ActivationPurpose}
 	 */
 	void activate(ActivationPurpose purpose);
 }
