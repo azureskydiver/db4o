07407ae10827da89ff83a3e7a480744e86e47a65
carl_rosenberger <carl_rosenberger@915b37e7-0715-0410-8bc3-b14b59444072>
2012-02-07 17:22:27 +0000

    cr: Updated drsVodExample to the latest version. Fixed issue with port. 4088 seems to be hardwired in VodDatabase.

--- sandbox/drsVodExample/scripts/linux/createDatabase.sh
+++ sandbox/drsVodExample/scripts/linux/createDatabase.sh
@@ -3,3 +3,4 @@ source $(dirname "$0")/setEnvironment.sh
 $VERSANT_BIN/makedb $DATABASE_NAME
 $VERSANT_BIN/createdb $DATABASE_NAME
 $VERSANT_BIN/startdb $DATABASE_NAME
+$VERSANT_BIN/dbuser -add -n drs -passwd drs $DATABASE_NAME
