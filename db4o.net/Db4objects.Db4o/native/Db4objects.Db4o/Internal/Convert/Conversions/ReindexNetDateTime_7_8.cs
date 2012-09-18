447e3614bde4e06b36208faa84a1a717058e1692
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2012-09-18 17:28:32 +0000

    acv+pr: COR-2294 - refactored common code to its own class file.

--- db4o.net/Db4objects.Db4o/native/Db4objects.Db4o/Internal/Convert/Conversions/ReindexNetDateTime_7_8.cs
+++ db4o.net/Db4objects.Db4o/native/Db4objects.Db4o/Internal/Convert/Conversions/ReindexNetDateTime_7_8.cs
@@ -1,9 +1,6 @@
 ﻿/* Copyright (C) 2004 - 2009   Versant Inc.   http://www.db4o.com */
 using System;
-using Db4objects.Db4o.Ext;
-using Db4objects.Db4o.Foundation;
-using Db4objects.Db4o.Reflect;
-using Db4objects.Db4o.Reflect.Net;
+using Db4objects.Db4o.native.Db4objects.Db4o.Internal.Convert.Conversions;
 
 namespace Db4objects.Db4o.Internal.Convert.Conversions
 {
@@ -16,41 +13,13 @@ namespace Db4objects.Db4o.Internal.Convert.Conversions
 
 		private static void ReindexDateTimeFields(ConversionStage stage)
 		{
-			DateTimeFieldReindexer reindexer = new DateTimeFieldReindexer();
+			var reindexer = new FieldReindexer<DateTime>();
 			
 			ClassMetadataIterator i = stage.File().ClassCollection().Iterator();
 			while (i.MoveNext())
 			{
 				ClassMetadata classmetadata = i.CurrentClass();
 				classmetadata.TraverseDeclaredFields(reindexer);
-			}
-		}
-		
-		private class DateTimeFieldReindexer : IProcedure4
-		{
-			public void Apply(object field)
-			{
-				if (!((FieldMetadata)field).HasIndex())
-				{
-					return;
-				}
-				ReindexDateTimeField(((FieldMetadata)field));
-			}
-
-			private static void ReindexDateTimeField(IStoredField field)
-			{
-				IReflectClass claxx = field.GetStoredType();
-				if (claxx == null)
-				{
-					return;
-				}
-
-				Type t = NetReflector.ToNative(claxx);
-				if (t == typeof(DateTime) || t == typeof(DateTime?))
-				{
-					field.DropIndex();
-					field.CreateIndex();
-				}
 			}
 		}
     }
