ebab067cee7374491f0c3ef87fc2a867856d78b7
adriano_verona <adriano_verona@915b37e7-0715-0410-8bc3-b14b59444072>
2012-09-18 17:16:12 +0000

    minor refactorings

--- db4o.net/Db4objects.Db4o/native/Db4objects.Db4o/Internal/Convert/Conversions/DropGuidClassAndFieldIndexes_7_12.cs
+++ db4o.net/Db4objects.Db4o/native/Db4objects.Db4o/Internal/Convert/Conversions/DropGuidClassAndFieldIndexes_7_12.cs
@@ -8,7 +8,7 @@ namespace Db4objects.Db4o.Internal.Convert.Conversions
 {
 	public partial class DropGuidClassAndFieldIndexes_7_12 : DropClassIndexesConversion
 	{
-		private FieldReindexer<Guid> reindexer = new FieldReindexer<Guid>();
+		private readonly FieldReindexer<Guid> reindexer = new FieldReindexer<Guid>();
 
 		protected override bool Accept(ClassMetadata classmetadata)
 		{
